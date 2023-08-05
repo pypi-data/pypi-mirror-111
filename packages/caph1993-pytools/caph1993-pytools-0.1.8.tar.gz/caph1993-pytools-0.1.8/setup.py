a5531d9371c57d1b376eb2848e6a5f2214367879
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:10 +0000

    Add package caph1993-pytools/0.1.8/caph1993-pytools-0.1.8.tar.gz

---
+++ packages/caph1993-pytools/caph1993-pytools-0.1.8.tar.gz/caph1993-pytools-0.1.8/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+'''
+To upload:
+python3 setup.py check
+python3 setup.py sdist
+twine upload dist/*
+'''
+
+setup(
+    name='caph1993-pytools',
+    version='0.1.8',
+    description='Python toolbox of Carlos Pinzón',
+    url='https://github.com/caph1993/caph1993-pytools',
+    author='Carlos Pinzón',
+    author_email='caph1993@gmail.com',
+    license='MIT',
+    packages=[
+        'cp93storage',
+        'cp93functools',
+        'cp93audio',
+    ],
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
