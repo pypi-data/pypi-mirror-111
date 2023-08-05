1776b29c4d892292d3e3484c215ac041308a40a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:13 +0000

    Add package leon-test-package/0.2/leon_test_package-0.2.tar.gz

---
+++ packages/leon-test-package/leon_test_package-0.2.tar.gz/leon_test_package-0.2/leon_test_package/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created by Leon at 6/28/2021
+
+"""
+__author__ = "Leon"
+__version__ = "0.0.2"
+__email__ = "yang.li@zillnk.com"
+
+def joke():
+    return (u'This is a test package from leon'
+    )
