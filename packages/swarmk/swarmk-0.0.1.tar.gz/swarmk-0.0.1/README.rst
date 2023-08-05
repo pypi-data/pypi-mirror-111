713e4734a2035db270f2c87ed10d5ac081008e48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:35 +0000

    Add package swarmk/0.0.1/swarmk-0.0.1.tar.gz

---
+++ packages/swarmk/swarmk-0.0.1.tar.gz/swarmk-0.0.1/README.rst
@@ -0,0 +1,16 @@
+# The original code for the library is here
+(dandynaufaldi/particle-swarm-optimized-clustering)[https://github.com/dandynaufaldi/particle-swarm-optimized-clustering=
+
+#use
+```python
+import swarmk as sw
+```
+
+or
+
+```python
+from swarmk.pso import ParticleSwarmOptimizedClustering
+from swarmk.particle import quantization_error, calc_sse
+from swarmk.utils import normalize
+from swarmk.kmeans import KMeans
+```
---
+++ packages/swarmk/swarmk-0.0.1.tar.gz/swarmk-0.0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup ,find_packages
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+    
+setup(
+    name='swarmk',
+    version='0.0.1',
+    description='a pip install swarmk',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    packages=['swarmk'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    author='mlib_4_you',
+    author_email='Atlantes0land@gmail.com',
+    install_requires = [
+        'numpy',
+        'pandas',
+    ],
+    keywords=['swarm' ,'swarmk' ,'swarm lib' ,'pos' ,'swarm clustering', 'particle-swarm-optimized-clustering']
+)
