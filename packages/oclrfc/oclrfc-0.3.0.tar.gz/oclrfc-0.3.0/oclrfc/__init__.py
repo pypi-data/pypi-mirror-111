e2241010ec2a6cd15c2bd70343691419020601d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:45 +0000

    Add package oclrfc/0.3.0/oclrfc-0.3.0.tar.gz

---
+++ packages/oclrfc/oclrfc-0.3.0.tar.gz/oclrfc-0.3.0/oclrfc/__init__.py
@@ -0,0 +1,4 @@
+from ._converter import RFC_to_OCL
+from ._classifier import OCLRandomForestClassifier
+from ._label_classifier import OCLRandomForestLabelClassifier
+from ._utils import generate_feature_stack
