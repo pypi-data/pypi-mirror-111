894e82e577ace7d37201650194b3aef2c076bab4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:58 +0000

    Add package oidcat/0.5.1/oidcat-0.5.1.tar.gz

---
+++ packages/oidcat/oidcat-0.5.1.tar.gz/oidcat-0.5.1/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+USERNAME = 'beasteers'
+NAME = 'oidcat'
+
+import os
+import imp
+version = imp.load_source(
+    '{}.__version__'.format(NAME),
+    os.path.join(os.path.dirname(__file__), NAME, '__version__.py'))
+version = version.__version__
+
+setuptools.setup(
+    name=NAME,
+    version=version,
+    description='easy oidc client & server',
+    long_description=open('README.md').read().strip(),
+    long_description_content_type='text/markdown',
+    author='Bea Steers',
+    author_email='bea.steers@gmail.com',
+    # url='https://github.com/{}/{}'.format(USERNAME, NAME),
+    packages=setuptools.find_packages(),
+    # entry_points={'console_scripts': ['{name}={name}:main'.format(name=NAME)]},
+    install_requires=['requests'],
+    extras_require={'server': ['flask', 'flask_oidc', 'sqlitedict']},
+    license='MIT License',
+    keywords='')
