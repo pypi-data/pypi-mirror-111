15010cc1a5f5f8b3b90ca9721fefe2587e58f16f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:26 +0000

    Add package abtrap/0.1.0/abtrap-0.1.0-py3-none-any.whl

---
+++ packages/abtrap/abtrap-0.1.0-py3-none-any.whl/abtrap/__init__.py
@@ -0,0 +1,13 @@
+"""
+Python package `abtrap`: building invisibility shields to protect network devices.
+"""
+
+import pandas as pd
+import numpy as np
+
+
+def do_nothing():
+    """
+    This function does nothing.
+    """
+    pass
