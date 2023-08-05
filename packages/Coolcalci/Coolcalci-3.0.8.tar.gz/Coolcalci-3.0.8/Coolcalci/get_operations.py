b94ae76fdedd5135a30f3f180da1f9dd7da1efd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:12 +0000

    Add package Coolcalci/3.0.8/Coolcalci-3.0.8.tar.gz

---
+++ packages/Coolcalci/Coolcalci-3.0.8.tar.gz/Coolcalci-3.0.8/Coolcalci/get_operations.py
@@ -0,0 +1,24 @@
+def add(num1,num2):
+    total = str(num1 + num2)
+    return total
+def sub(num1,num2):
+    total = str(num1 - num2)
+    return total
+def mul(num1,num2):
+    total = str(num1 * num2)
+    return total
+def div(num1,num2):
+    total = str(num1 / num2)
+    return total
+def add_print(num1,num2):
+    total = str(num1 + num2)
+    print(total)
+def sub_print(num1,num2):
+    total = str(num1 - num2)
+    print(total)
+def mul_print(num1,num2):
+    total = str(num1 * num2)
+    print(total)
+def div_print(num1,num2):
+    total = str(num1 / num2)
+    print(total)
