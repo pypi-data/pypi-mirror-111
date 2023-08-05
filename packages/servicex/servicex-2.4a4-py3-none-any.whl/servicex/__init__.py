cb24801ea464438117c6df14357c4ea851fbb4db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:08 +0000

    Add package servicex/2.4a4/servicex-2.4a4-py3-none-any.whl

---
+++ packages/servicex/servicex-2.4a4-py3-none-any.whl/servicex/__init__.py
@@ -0,0 +1,15 @@
+from .servicex import ServiceXDataset, StreamInfoUrl  # NOQA
+from .utils import (  # NOQA
+    ServiceXException,
+    ServiceXUnknownRequestID,
+    ServiceXFailedFileTransform,
+    ServiceXFatalTransformException,
+    StatusUpdateCallback,
+    StatusUpdateFactory,
+    ServiceXUnknownDataRequestID,
+    clean_linq,
+    DatasetType,
+)
+from .servicex_adaptor import ServiceXAdaptor  # NOQA
+from .minio_adaptor import MinioAdaptor  # NOQA
+from .cache import Cache, ignore_cache  # NOQA
