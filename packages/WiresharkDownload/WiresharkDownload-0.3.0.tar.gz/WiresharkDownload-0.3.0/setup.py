f735d8211fa49ed5b70926d3d85a5e87aafd210b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:14:21 +0000

    Add package WiresharkDownload/0.3.0/WiresharkDownload-0.3.0.tar.gz

---
+++ packages/WiresharkDownload/WiresharkDownload-0.3.0.tar.gz/WiresharkDownload-0.3.0/setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup
+setup(
+name='WiresharkDownload',
+version='0.3.0',
+description='I LIKE THIS',
+author='pleasefeedme',
+author_email='myfakeemail@gmail.com',
+license='Python. Free to use no attribution required.',
+packages=[],
+install_requires=[])
