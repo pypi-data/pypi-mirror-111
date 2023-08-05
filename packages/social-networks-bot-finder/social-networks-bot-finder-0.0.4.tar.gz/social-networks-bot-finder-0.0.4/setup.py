116ec87ffd34809060f3f0a30028fd609c216656
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:40 +0000

    Add package social-networks-bot-finder/0.0.4/social-networks-bot-finder-0.0.4.tar.gz

---
+++ packages/social-networks-bot-finder/social-networks-bot-finder-0.0.4.tar.gz/social-networks-bot-finder-0.0.4/setup.py
@@ -0,0 +1,45 @@
+import setuptools
+
+# https://www.freecodecamp.org/news/build-your-first-python-package/
+
+VERSION = "0.0.4"
+DESCRIPTION = "CLI tool to help detecting if a twitter account is a bot"
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setuptools.setup(
+    name="social-networks-bot-finder",
+    description=DESCRIPTION,
+    version=VERSION,
+    author="Ambanum",
+    url="https://github.com/ambanum/social-networks-bot-finder",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    packages=["botfinder"],
+    setup_requires=["setuptools_scm"],
+    use_scm_version=False,
+    include_package_data=True,
+    install_requires=[
+        "click==8.0.1",
+        "joblib==1.0.1",
+        "pandas==1.2.5",
+        "shap==0.39.0",
+    ],
+    dependency_links=[
+        "git+https://github.com/JustAnotherArchivist/snscrape.git@5e6bc4ec50fe7e705e04e03f00f75c5d6068ab9b"
+    ],
+    python_requires="~=3.8",
+    extras_require={
+        "test": ["coverage"],
+    },
+    entry_points={
+        "console_scripts": [
+            "botfinder=botfinder.cli:main",
+        ],
+    },
+)
