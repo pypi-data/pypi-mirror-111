d94db4219d73e25dc732b25aca5418d1487d4a80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:15 +0000

    Add package ghstats/2.0.0/ghstats-2.0.0.tar.gz

---
+++ packages/ghstats/ghstats-2.0.0.tar.gz/ghstats-2.0.0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+from os import path
+
+with open(path.join(path.abspath(path.dirname(__file__)), 'README.rst')) as f:
+    readme = f.read()
+
+setup(
+    name='ghstats',
+    version='2.0.0',
+    packages=['ghstats'],
+    description='GitHub Release download count and other statistics.',
+    long_description=readme,
+    author='Alexander Gorishnyak',
+    author_email='kefir500@gmail.com',
+    license='MIT',
+    url='https://github.com/kefir500/ghstats',
+    keywords='github release download count stats statistics',
+    python_requires='>=3',
+    entry_points={
+        'console_scripts': [
+            'ghstats = ghstats.ghstats:main_cli'
+        ]
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Utilities'
+    ]
+)
