df0e3e6f5f198f785e7d03bacc23009d8f86b62c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:52 +0000

    Add package matrixmodule/0.0.1/matrixmodule-0.0.1.tar.gz

---
+++ packages/matrixmodule/matrixmodule-0.0.1.tar.gz/matrixmodule-0.0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = 'A package for matrix operations.'
+LONG_DESCRIPTION = 'A package for basic linear algebra operations.'
+
+setup(
+    name="matrixmodule",
+    version=VERSION,
+    author="Elliott Walker",
+    author_email="<22walkerelliott@gmail.com>",
+    description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[],
+        keywords=['python', 'first package'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
