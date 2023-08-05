3cb6b42ee5b40ac0f96465753cbd342a1f056b03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:00 +0000

    Add package spamdetection/0.0.1/spamdetection-0.0.1.tar.gz

---
+++ packages/spamdetection/spamdetection-0.0.1.tar.gz/spamdetection-0.0.1/spamdetection/params.py
@@ -0,0 +1,19 @@
+"""
+Declares parameters used by other modules contained in the package.
+"""
+
+import os.path
+    
+# Path to the model file
+MODEL_FILENAME = "sms_model_v1.pkl"
+MODEL_DIRECTORY = "Model"
+MODEL_PATH = os.path.join(MODEL_DIRECTORY, MODEL_FILENAME)
+    
+# With this threshold, there are no false positives in the validation set
+PERMISSIVE_THRESHOLD = .61
+    
+# With this threshold, there are no false negatives in the validation set
+AGGRESSIVE_THRESHOLD = .07
+
+# URL for the web app
+WEB_APP_URL = 'https://nuveo-teste-ia.herokuapp.com/'
