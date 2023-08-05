89fbd5094225431dd52ea8cc4a14bb6a89a416cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:39 +0000

    Add package graphanime/0.1.0/graphanime-0.1.0.tar.gz

---
+++ packages/graphanime/graphanime-0.1.0.tar.gz/graphanime-0.1.0/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+setup(
+
+    name='graphanime',
+    
+    version="0.1.0",
+
+    packages=find_packages(),
+
+    author= "ALTAVILLA Théo / DUPLAN Xavier / EL RAWAS Gaëthan / ZOUAOUI Sofiane",
+
+    author_email = "sofiane.zouaoui@insa-cvl.fr",
+
+    description="create execution graph in GIF/PDF with LaTex",
+
+    long_description=open('./README.md').read(),
+    long_description_content_type="text/markdown",
+
+    install_requires=["pdf2image","apng"],
+
+    include_package_data=True,
+
+    url='https://github.com/Sosso8305/LaTeX-to-GIF-Python',
+
+    classifiers=[
+        "Programming Language :: Python",
+        "Natural Language :: French",
+    ],
+
+    license="MIT Lisence"
+)
