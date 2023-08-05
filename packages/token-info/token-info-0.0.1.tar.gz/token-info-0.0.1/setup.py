a8ad51891f37e74ba735b59065688478bb9a3957
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:47 +0000

    Add package token-info/0.0.1/token-info-0.0.1.tar.gz

---
+++ packages/token-info/token-info-0.0.1.tar.gz/token-info-0.0.1/setup.py
@@ -0,0 +1,24 @@
+  
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='token-info',
+  version='0.0.1',
+  description='tokeninfo is a Python Package created for simply scraping discord account data with the use of an account token.',
+  long_description=open('README.md').read(),
+  url='https://github.com/7uk/tokeninfo-package',  
+  author='7uk',
+  author_email='doopycheats@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='discord', 
+  packages=find_packages(),
+)
