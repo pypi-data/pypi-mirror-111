4b16ea9d3b4ea8941de0a6438adfafe59fbf5ead
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:16:20 +0000

    Add package susposter/1.17.7/susposter-1.17.7.tar.gz

---
+++ packages/susposter/susposter-1.17.7.tar.gz/susposter-1.17.7/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['susposter']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'susposter',
+    'version': '1.17.7',
+    'description': 'so sussy',
+    'long_description': '# susposter\nsussy imposter\n',
+    'author': 'Cryptex',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/Cryptex-github',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
