11326d0d8008fd8ac0ca26dcbf3f0944b5713735
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:28 +0000

    Add package gaussandbinom-probability/0.2/gaussandbinom_probability-0.2.tar.gz

---
+++ packages/gaussandbinom-probability/gaussandbinom_probability-0.2.tar.gz/gaussandbinom_probability-0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='gaussandbinom_probability',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      long_description='Gaussian and Binomial distributions',
+      packages=['gaussandbinom_probability'],
+      author='Akif Acar',
+      author_email='mehmetakifacar@gmail.com',
+      zip_safe=False)
