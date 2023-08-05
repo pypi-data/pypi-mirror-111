2b424143ec59c8bab28ef95d646a23dc0b516f08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:12 +0000

    Add package pipython3/0.1.3/pipython3-0.1.3.tar.gz

---
+++ packages/pipython3/pipython3-0.1.3.tar.gz/pipython3-0.1.3/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="pipython3",
+    version="0.1.3",
+    url = "https://bitbucket.org/iotabio/pipython/src/master/",
+    author = "IotaBiosciences",
+    author_email = "kaelan@iota.bio",
+    description="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True
+)
