f6ecff92ca026dbd255ede1cddc47fd8fa3648d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:44 +0000

    Add package quo/2021.2/quo-2021.2.tar.gz

---
+++ packages/quo/quo-2021.2.tar.gz/quo-2021.2/src/quo/decorators/autopswd.py
@@ -0,0 +1,25 @@
+def autopswd(*param_decls, **kwargs):
+    """Add a ``--password`` option which prompts for a password, hiding
+    input and asking to enter the value again for confirmation.
+
+    :param param_decls: One or more option names. Defaults to the single
+        value ``"--password"``.
+    :param kwargs: Extra arguments are passed to :func:`app`.
+    
+    Example::
+
+    @quo.app('--password', prompt=True, autoconfirm=True,
+              hide_input=True)
+     def changeadmin(password):
+     pass
+    """
+
+    if not param_decls:
+        param_decls = ("--password",)
+
+    kwargs.setdefault("prompt", True)
+    kwargs.setdefault("autoconfirm", True)
+    kwargs.setdefault("hide_input", True)
+    return app(*param_decls, **kwargs)
+
+
