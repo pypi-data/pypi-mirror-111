1bb2950fef29501150bd07d0e3d8889866652e4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:36 +0000

    Add package pytrustplatform/0.12.3.21/pytrustplatform-0.12.3.21-py3-none-any.whl

---
+++ packages/pytrustplatform/pytrustplatform-0.12.3.21-py3-none-any.whl/pytrustplatform/version.py
@@ -0,0 +1,4 @@
+""" This file was generated when pytrustplatform was built """
+VERSION = '0.12.3.21'
+COMMIT_ID = '9ef03c2c1682dc14d378fd36e1b35e8bfd9decf9'
+BUILD_DATE = '2021-06-28 10:26:03 +0000'
