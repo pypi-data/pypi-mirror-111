a7287b9b69d06980a243be656832df7acda88ba6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:44 +0000

    Add package oknlp/0.0.2/oknlp-0.0.2-cp37-cp37m-manylinux_2_12_x86_64.manylinux2010_x86_64.whl

---
+++ packages/oknlp/oknlp-0.0.2-cp37-cp37m-manylinux_2_12_x86_64.manylinux2010_x86_64.whl/oknlp/algorithm/typing/__init__.py
@@ -0,0 +1,26 @@
+from typing import List
+from .BaseTyping import BaseTyping
+from .bert_typing import BertTyping
+
+
+def get_by_name(name: str = "", **kwargs) -> BaseTyping:
+    """根据条件获取一个Typing类的实例，无法根据条件获取时返回BertTyping()
+
+    :param str name: Typing类使用到的方法
+
+        * "bert"->返回以Bert模型实现的算法
+
+        * 默认返回以Bert模型实现的算法
+
+    :returns: 一个Typing类的实例
+    """
+    name = name.lower()
+    if name == "bert":
+        return BertTyping(**kwargs)
+    return BertTyping(**kwargs)
+
+
+def get_all(**kwargs) -> List[BaseTyping]:
+    """获取所有Typing类的实例
+    """
+    return [BertTyping(**kwargs)]
