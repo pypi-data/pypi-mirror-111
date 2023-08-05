34f0aa6b2036265454b6e2dcce20007bbddfe2c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:27 +0000

    Add package mlvizlib/0.0.4/mlvizlib-0.0.4-py2.py3-none-any.whl

---
+++ packages/mlvizlib/mlvizlib-0.0.4-py2.py3-none-any.whl/mlvizlib/classification/__init__.py
@@ -0,0 +1,15 @@
+"""
+The :mod:`mlvizlib.classification` module implements a variety of classification visualizations.
+"""
+
+#  from mlvizlib import classification
+#  from mlvizlib.classification import _confusion_matrix  # required
+from mlvizlib.classification.viz_confusion_matrix import confusion_matrix  # used to make this function importable
+#  from ._confusion_matrix import confusion_matrix_kalastaja  # used to make this function importable
+
+
+from ..utils import _input_validation
+
+__all__ = [
+    "confusion_matrix"
+    ]
