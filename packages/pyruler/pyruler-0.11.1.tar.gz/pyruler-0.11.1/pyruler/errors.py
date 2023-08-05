8c59094cd7b90b0b8485740863faf8cb7cab7b77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:23 +0000

    Add package pyruler/0.11.1/pyruler-0.11.1.tar.gz

---
+++ packages/pyruler/pyruler-0.11.1.tar.gz/pyruler-0.11.1/pyruler/errors.py
@@ -0,0 +1,21 @@
+"""Custom ruler errors."""
+
+
+class RuleError(Exception):
+    """Base Rule error."""
+
+
+class RulerError(Exception):
+    """Base Ruler error."""
+
+
+class RuleSetError(Exception):
+    """Base RuleSet error."""
+
+
+class RulerConfigError(Exception):
+    """Base error for Ruler configuration."""
+
+
+class RuleSetConfigError(Exception):
+    """Base error for RuleSet configuration."""
