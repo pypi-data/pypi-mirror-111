7d8ef3926cd675710e0f7ef52d9e886140c0791e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:16 +0000

    Add package rosetta-dispatcher/0.0.18/rosetta-dispatcher-0.0.18.tar.gz

---
+++ packages/rosetta-dispatcher/rosetta-dispatcher-0.0.18.tar.gz/rosetta-dispatcher-0.0.18/rosetta_dispatcher/model/dispatch_types.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+
+class DispatchRequestType(str, Enum):
+    JSON = 'JSON'
+
+
+class DispatchResponseStatus(str, Enum):
+    OK = 'OK'
+    TIMEOUT = 'TIMEOUT'
+    BACKEND_ERROR = 'BACKEND_ERROR'
