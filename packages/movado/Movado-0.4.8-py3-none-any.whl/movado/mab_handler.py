ce8cb51437d0c83284f2118b90ed16b851fe3ea9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:16:12 +0000

    Add package movado/0.4.8/Movado-0.4.8-py3-none-any.whl

---
+++ packages/movado/Movado-0.4.8-py3-none-any.whl/movado/mab_handler.py
@@ -0,0 +1,98 @@
+from typing import List
+from pathlib import Path
+from abc import ABC, abstractmethod
+import numpy as np
+
+
+class MabHandler(ABC):
+    def __init__(
+        self,
+        debug: bool = False,
+        debug_path: str = "mab",
+        skip_debug_initialization: bool = False,
+        controller_params=None,
+    ):
+        self._mab = None
+        self._last_predict_probability: float = -1
+        self._debug = debug
+        self._sample_prefix = ""
+        self.__costs: List[float] = []
+        self.__controller_params = controller_params
+        self.__mab_debug = ""
+        if debug and not skip_debug_initialization:
+            self.initialize_debug(debug_path)
+
+    def initialize_debug(self, debug_path):
+        Path("movado_debug").mkdir(exist_ok=True)
+        self.__mab_debug = "movado_debug/" + debug_path + ".csv"
+        Path(self.__mab_debug).open("w").close()
+        if self.__controller_params:
+            Path(self.__mab_debug).open("a").write(
+                "Model_Parameters, Mean Cost, Action, Context, Probability\n"
+            )
+        else:
+            Path(self.__mab_debug).open("a").write(
+                "Mean Cost, Action, Context, Probability\n"
+            )
+
+    @abstractmethod
+    def predict(self, context: List[float]) -> float:
+        pass
+
+    def learn(
+        self,
+        action: float,
+        cost: float,
+        context: List[float],
+        forced_predict_probability: int = None,
+    ) -> None:
+        sample: str = (
+            self._sample_prefix
+            + str(action)
+            + ":"
+            + str(cost)
+            + ":"
+            + (
+                str(self._last_predict_probability)
+                if not forced_predict_probability
+                else str(forced_predict_probability)
+            )
+            + " | "
+        )
+        for feature in context:
+            sample += str(feature) + " "
+        self._mab.learn(sample)
+        if self._debug:
+            self.__costs.append(cost)
+            optional_params = (
+                (str(self.__controller_params) + ", ")
+                if self.__controller_params
+                else ""
+            )
+            Path(self.__mab_debug).open("a").write(
+                optional_params
+                + str(np.mean(self.__costs))
+                + ", "
+                + str(action)
+                + ", "
+                + str(context)
+                + ", "
+                + (
+                    str(self._last_predict_probability)
+                    if not forced_predict_probability
+                    else str(self._last_predict_probability)
+                )
+                + "\n"
+            )
+
+    def get_mean_cost(self) -> float:
+        if self.__costs:
+            return float(np.mean(self.__costs))
+        else:
+            return 0
+
+    def get_last_predict_probability(self) -> float:
+        return self._last_predict_probability
+
+    def set_last_predict_probability(self, last_predict_probability: float) -> None:
+        self._last_predict_probability = last_predict_probability
---
+++ packages/movado/Movado-0.4.8-py3-none-any.whl/movado/movado.py
@@ -0,0 +1,105 @@
+from collections import OrderedDict
+from typing import List, Callable, Optional
+
+from movado.controller import Controller
+from movado.estimator import Estimator
+from functools import wraps
+from movado.distance_controller import DistanceController
+from movado.hoeffding_adaptive_tree_model import HoeffdingAdaptiveTreeModel
+from movado.chained_estimator import ChainedEstimator
+import movado.movado_static
+
+
+# This imports are unused but populate the global symbol table for the globals() call
+# noinspection PyUnresolvedReferences
+from movado.mab_controller import MabController  # pylint: disable=unused-import
+
+# noinspection PyUnresolvedReferences
+from movado.meta_controller import MetaController  # pylint: disable=unused-import
+
+# noinspection PyUnresolvedReferences
+from movado.kernel_regression_model import (
+    KernelRegressionModel,
+)  # pylint: disable=unused-import
+
+
+def approximate(
+    **kwargs,
+) -> Callable[
+    [Callable[[List[float]], List[float]]], Callable[[List[float]], List[float]]
+]:
+    def approximate_decorator(
+        func: Callable[[List[float]], List[float]]
+    ) -> Callable[[List[float]], List[float]]:
+        controller: Controller
+        estimator: Estimator
+        is_first_call: bool = True
+
+        @wraps(func)
+        def wrapper(*wrapper_args) -> List[float]:
+            # args must either contain the point to be analyzed
+            # or it must contain self (for class methods) and then the point
+            nonlocal is_first_call
+            nonlocal controller
+            nonlocal estimator
+            if len(wrapper_args) < 1 or len(wrapper_args) > 2:
+                raise Exception(
+                    "The decorated function must have a single input which is a list of numbers, "
+                    + "self may also be present for non-static class methods, in such case self is "
+                    + "expected to be the first argument followed by the point and nothing"
+                )
+            if len(wrapper_args) == 1:
+                point = wrapper_args[0]
+            else:
+                point = wrapper_args[1]
+            if is_first_call:
+                is_first_call = False
+                selected_model = globals().get(str(kwargs.get("estimator")) + "Model")
+                model = (
+                    HoeffdingAdaptiveTreeModel if not selected_model else selected_model
+                )
+                # TODO add multi-output non-chained estimators
+                outputs: int = kwargs.get("outputs")
+                if not outputs:
+                    raise Exception(
+                        "Please specify outputs as a kwarg as the number of targets of the fitness function"
+                    )
+                estimator = ChainedEstimator(
+                    model(
+                        **{
+                            k[k.find("_") + 1 :]: v
+                            for k, v in kwargs.items()
+                            if k[: k.find("_")] == "estimator"
+                        }
+                    ),
+                    outputs,
+                )
+                selected_controller = globals().get(
+                    str(kwargs.get("controller")) + "Controller"
+                )
+                controller = (
+                    DistanceController
+                    if not selected_controller
+                    else selected_controller
+                )
+
+                controller = controller(
+                    func,
+                    estimator,
+                    self_exact=None if len(wrapper_args) == 1 else wrapper_args[0],
+                    **{
+                        k[k.find("_") + 1 :]: v
+                        for k, v in kwargs.items()
+                        if k[: k.find("_")] == "controller"
+                    },
+                )
+
+            return list(
+                controller.compute_objective(point)
+            )  # we expect the first and only argument of the function to be the input point
+
+        if kwargs.get("disabled"):
+            return func
+        return wrapper
+
+    return approximate_decorator
