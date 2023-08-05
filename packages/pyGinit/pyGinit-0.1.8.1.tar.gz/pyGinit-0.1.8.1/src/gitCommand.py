32e34e77fc34e311cc64f88402a9bb11fda99355
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:19 +0000

    Add package pyGinit/0.1.8.1/pyGinit-0.1.8.1.tar.gz

---
+++ packages/pyGinit/pyGinit-0.1.8.1.tar.gz/pyGinit-0.1.8.1/src/gitCommand.py
@@ -0,0 +1,23 @@
+from configparser import ConfigParser
+from pathlib import Path
+from os import path, system, devnull
+from subprocess import Popen, call, STDOUT, PIPE
+import git
+
+config_obj = ConfigParser()
+parser = config_obj.read(path.join(Path.home(), ".pyGinitconfig.ini"))
+
+
+def execute_git(username, password, repo_name):
+    """
+    initialize git repository and push to remote
+    """
+    url = "https://{username}:{password}@github.com/{username}/{repo_name}.git".format(
+        username=username, repo_name=repo_name.replace(" ", "-"), password=password
+    )
+    repo = git.Repo.init()
+    repo.git.add("--all")
+    repo.index.commit("initial commit")
+    repo.git.push(url, "HEAD:master")
+    repo.create_remote("origin", url)
+    print("remote name : origin \nurl :" + url)
