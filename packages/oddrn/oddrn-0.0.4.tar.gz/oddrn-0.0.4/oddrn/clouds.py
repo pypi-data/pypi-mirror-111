4c018ae0e2396627f17f1331d85897c7872cf143
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:43 +0000

    Add package oddrn/0.0.4/oddrn-0.0.4.tar.gz

---
+++ packages/oddrn/oddrn-0.0.4.tar.gz/oddrn-0.0.4/oddrn/clouds.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class AWS:
+    cloud: str
+    region: str
+    account: str
+
+    def get_oddrn(self):
+        return f"aws/regions/{self.region}/accounts/{self.account}"
+
+
+cloud_map = {
+    "aws": AWS
+}
