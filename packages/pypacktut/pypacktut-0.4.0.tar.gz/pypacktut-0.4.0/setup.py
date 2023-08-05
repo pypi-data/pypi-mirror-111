cf5b0ad3ddf134871f572ec4de10f10854036fb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:57 +0000

    Add package pypacktut/0.4.0/pypacktut-0.4.0.tar.gz

---
+++ packages/pypacktut/pypacktut-0.4.0.tar.gz/pypacktut-0.4.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.4.0'
+DESCRIPTION = 'Python package tutorial'
+LONG_DESCRIPTION = 'Package package tutorial with a slightly longer description'
+find_packages_in = ['pypacktut/*', ]
+other_reqs = ['numpy>=1.14.5']
+
+setup(
+    name='pypacktut',
+    version=VERSION,
+    author="Nitin Singh",
+    author_email="singh.nitin512@gmail.com",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(include=find_packages_in),
+    install_requires=other_reqs,
+    keywords=['python', 'package tutorial'],
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
