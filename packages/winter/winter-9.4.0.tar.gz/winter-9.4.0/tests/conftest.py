833afe7561a144590036c3bc200a09ffb2513b19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:39 +0000

    Add package winter/9.4.0/winter-9.4.0.tar.gz

---
+++ packages/winter/winter-9.4.0.tar.gz/winter-9.4.0/tests/conftest.py
@@ -0,0 +1,53 @@
+from pathlib import Path
+
+import django
+from injector import CallableProvider
+from injector import Injector
+from injector import Module
+from injector import singleton
+from sqlalchemy import create_engine
+from sqlalchemy.engine import Engine
+
+from winter.core import set_injector
+from .entities import Guest
+
+
+def pytest_configure():
+    from django.conf import settings
+    app_dir = Path(__file__)
+    settings.configure(
+        ROOT_URLCONF='tests.urls',
+        REST_FRAMEWORK={
+            'DEFAULT_RENDERER_CLASSES': ('winter_django.renderers.JSONRenderer',),
+            'UNAUTHENTICATED_USER': Guest,
+        },
+        TEMPLATES=[
+            {
+                'BACKEND': 'django.template.backends.django.DjangoTemplates',
+                'DIRS': [app_dir.parent.name + '/templates'],
+                'APP_DIRS': True,
+                'OPTIONS': {
+                    'debug': True,  # We want template errors to raise
+                },
+            },
+        ],
+        INSTALLED_APPS=(
+            # Hack for making module discovery working
+            'django.contrib.admin',
+            'django.contrib.contenttypes',
+            # End hack
+            'tests',
+        ),
+    )
+    injector = Injector([Configuration])
+    set_injector(injector)
+    django.setup()
+
+
+class Configuration(Module):
+    def configure(self, binder):
+        binder.bind(Engine, to=CallableProvider(make_engine), scope=singleton)
+
+
+def make_engine():
+    return create_engine('sqlite://')
---
+++ packages/winter/winter-9.4.0.tar.gz/winter-9.4.0/tests/entities/users.py
@@ -0,0 +1,26 @@
+import uuid
+
+
+class User:
+    @property
+    def is_authenticated(self):
+        return False
+
+    @property
+    def is_anonymous(self):  # pragma: no cover
+        return False
+
+
+class Guest(User):
+    @property
+    def is_anonymous(self):  # pragma: no cover
+        return True
+
+
+class AuthorizedUser(User):
+    def __init__(self, pk=None):
+        self.pk = pk if pk is not None else uuid.uuid4()
+
+    @property
+    def is_authenticated(self):
+        return True
