fc6805be4bab8567d745c6a4919593cf49455c5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:42 +0000

    Add package transformers-keras/0.3.0/transformers_keras-0.3.0.tar.gz

---
+++ packages/transformers-keras/transformers_keras-0.3.0.tar.gz/transformers_keras-0.3.0/transformers_keras/__init__.py
@@ -0,0 +1,21 @@
+import logging
+
+import tensorflow as tf
+
+from .callbacks import SavedModelExporter, TransformerLearningRate
+from .layers import (DecoderLayer, EncoderLayer, MultiHeadAttention,
+                     PointWiseFeedForwardNetwork, ScaledDotProductAttention)
+from .losses import MaskedSparseCategoricalCrossentropy
+from .metrics import MaskedSparseCategoricalAccuracy
+from .modeling_albert import (Albert, AlbertEmbedding, AlbertEncoder,
+                              AlbertEncoderGroup, AlbertEncoderLayer)
+from .modeling_bert import (Bert, BertEmbedding, BertEncoder, BertEncoderLayer,
+                            BertIntermediate)
+from .modeling_utils import complete_inputs
+from .tokenizers.bert_tokenizer import BertTokenizer
+from .tokenizers.tokenizer import BasicTokenizer, WordpieceTokenizer
+
+__name__ = 'transformers_keras'
+__version__ = '0.3.0'
+
+logging.basicConfig(format="%(asctime)s %(levelname)7s %(filename)15s %(lineno)4d] %(message)s", level=logging.INFO)
