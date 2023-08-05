d491352a0521356ade4bae27f9a4ad0b93de21ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:20 +0000

    Add package cellfinder-napari/0.0.17/cellfinder-napari-0.0.17.tar.gz

---
+++ packages/cellfinder-napari/cellfinder-napari-0.0.17.tar.gz/cellfinder-napari-0.0.17/setup.py
@@ -0,0 +1,63 @@
+from setuptools import setup, find_packages
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+requirements = [
+    "napari",
+    "napari-plugin-engine >= 0.1.4",
+    "napari-ndtiffs",
+    "brainglobe-napari-io",
+    "cellfinder-core>=0.2.4",
+]
+
+setup(
+    name="cellfinder-napari",
+    version="0.0.17",
+    author="Adam Tyson",
+    author_email="code@adamltyson.com",
+    license="BSD-3-Clause",
+    description="Efficient cell detection in large images",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    include_package_data=True,
+    python_requires=">=3.7",
+    install_requires=requirements,
+    extras_require={
+        "dev": [
+            "black",
+            "pytest-cov",
+            "pytest",
+            "gitpython",
+            "coverage>=5.0.3",
+            "bump2version",
+            "pre-commit",
+            "flake8",
+            "check-manifest",
+        ]
+    },
+    url="https://brainglobe.info/cellfinder",
+    project_urls={
+        "Source Code": "https://github.com/brainglobe/brainreg-segment",
+        "Bug Tracker": "https://github.com/brainglobe/brainreg-segment/issues",
+        "Documentation": "https://docs.brainglobe.info/brainreg-segment",
+        "User Support": "https://forum.image.sc/tag/brainglobe",
+    },
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Framework :: napari",
+        "Topic :: Scientific/Engineering :: Image Recognition",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={"napari.plugin": ["cellfinder = cellfinder_napari.plugins"]},
+)
