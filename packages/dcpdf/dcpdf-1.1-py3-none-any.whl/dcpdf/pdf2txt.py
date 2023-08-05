715dafd23ce624fdb6c09120c56e1ae33ce0e434
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:34 +0000

    Add package dcpdf/1.1/dcpdf-1.1-py3-none-any.whl

---
+++ packages/dcpdf/dcpdf-1.1-py3-none-any.whl/dcpdf/pdf2txt.py
@@ -0,0 +1,15 @@
+""" simple description
+this is a placeholder for python packages
+"""
+
+
+def convert(path):
+    """ Convert th given pdf to text
+
+    Parameters:
+    path(str): the path to pdf file
+
+    Returns:
+    str: the content of the pdf file as text
+    """
+    print("pdf2Text")
