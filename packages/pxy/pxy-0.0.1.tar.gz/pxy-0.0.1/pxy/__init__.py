d23313eacd0ca664cc3f4a65431d7a7afc866af7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:24 +0000

    Add package pxy/0.0.1/pxy-0.0.1.tar.gz

---
+++ packages/pxy/pxy-0.0.1.tar.gz/pxy-0.0.1/pxy/__init__.py
@@ -0,0 +1,10 @@
+__title__ = 'pxy'
+
+__description__ = 'tools of python'
+__url__ = 'https://github.com/foyoux/pxy'
+__version__ = '0.0.1'
+__author__ = 'foyou'
+__author_email__ = 'yimi.0822@qq.com'
+__license__ = 'GPL-3.0'
+__copyright__ = f'Copyright 2021 {__author__}'
+__ide__ = 'PyCharm - https://www.jetbrains.com/pycharm/'
