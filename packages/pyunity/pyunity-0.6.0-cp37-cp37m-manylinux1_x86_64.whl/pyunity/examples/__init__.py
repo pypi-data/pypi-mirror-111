81a7db12b6112679414432918986f9de0c2bd011
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:54 +0000

    Add package pyunity/0.6.0/pyunity-0.6.0-cp37-cp37m-manylinux1_x86_64.whl

---
+++ packages/pyunity/pyunity-0.6.0-cp37-cp37m-manylinux1_x86_64.whl/pyunity/examples/__init__.py
@@ -0,0 +1,34 @@
+from pyunity.scenes.sceneManager import KeyboardInterruptKill
+from . import (
+    example1, example2, example3, example4, example5, example6, example7, example8, example9
+)
+from ..scenes import SceneManager
+from .. import Logger
+import sys
+
+example_list = [
+    example1,
+    example2,
+    example3,
+    example4,
+    example5,
+    example6,
+    example7,
+    example8,
+    example9,
+]
+
+SceneManager.KeyboardInterruptKill = True
+
+def show():
+    if len(sys.argv) == 1:
+        num = 0
+    else:
+        num = int(sys.argv[1])
+    if not num:
+        for index, example in enumerate(example_list):
+            Logger.Log("\nExample", str(index + 1))
+            example.main()
+            SceneManager.RemoveScene(SceneManager.GetSceneByName("Scene"))
+    else:
+        example_list[num - 1].main()
