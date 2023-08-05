61ecae54d74a42a36a8c49c08512e486e4644395
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:28 +0000

    Add package pyportfolio/1.0.0/pyportfolio-1.0.0.tar.gz

---
+++ packages/pyportfolio/pyportfolio-1.0.0.tar.gz/pyportfolio-1.0.0/pyportfolio/__init__.py
@@ -0,0 +1,7 @@
+from .models import Scheme
+from .portfolio import Portfolio
+from .scheme_factory import create_scheme_from_casparser
+
+__version__ = "1.0.0"
+__author__ = "Kaushik S Kalmady / kaushiksk"
+__license__ = "MIT"
