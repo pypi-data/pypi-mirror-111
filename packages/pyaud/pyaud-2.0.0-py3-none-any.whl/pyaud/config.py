20b76acb74a4fdd33cb51ac6ca18e464b9cd8af4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:04 +0000

    Add package pyaud/2.0.0/pyaud-2.0.0-py3-none-any.whl

---
+++ packages/pyaud/pyaud-2.0.0-py3-none-any.whl/pyaud/config.py
@@ -0,0 +1,316 @@
+"""
+pyaud.config
+============
+
+Config module for ini parsing.
+"""
+import copy
+import logging.config as logging_config
+import os
+import shutil
+from collections.abc import MutableMapping
+from configparser import ConfigParser as _ConfigParser
+from pathlib import Path
+from typing import Any, Dict, Iterator, List, Optional, TextIO, Union
+
+import appdirs
+import toml.decoder as toml_decoder
+import toml.encoder as toml_encoder
+
+from .environ import NAME
+
+RCFILE = f".{NAME}rc"
+TOMLFILE = f"{NAME}.toml"
+PYPROJECT = "pyproject.toml"
+CONFIGDIR = Path(appdirs.user_config_dir(NAME))
+DEBUG = "DEBUG"
+INFO = "INFO"
+WARNING = "WARNING"
+ERROR = "ERROR"
+CRITICAL = "CRITICAL"
+LEVELS = [DEBUG, INFO, WARNING, ERROR, CRITICAL]
+DEFAULT_CONFIG: Dict[str, Any] = dict(
+    clean={"exclude": ["*.egg*", ".mypy_cache", ".env", "instance"]},
+    logging={
+        "version": 1,
+        "disable_existing_loggers": True,
+        "formatters": {
+            "standard": {
+                "format": "%(asctime)s %(levelname)s %(name)s %(message)s"
+            }
+        },
+        "handlers": {
+            "default": {
+                "class": "logging.handlers.TimedRotatingFileHandler",
+                "formatter": "standard",
+                "when": "d",
+                "backupCount": 60,
+                "filename": str(
+                    Path(appdirs.user_log_dir(NAME)) / f"{NAME}.log"
+                ),
+            }
+        },
+        "root": {"level": "INFO", "handlers": ["default"], "propagate": False},
+    },
+    indexing={"exclude": ["whitelist.py", "conf.py", "setup.py"]},
+    audit={
+        "modules": [
+            "format",
+            "format-docs",
+            "format-str",
+            "imports",
+            "typecheck",
+            "unused",
+            "lint",
+            "coverage",
+            "readme",
+            "docs",
+        ]
+    },
+)
+
+
+class ConfigParser(_ConfigParser):  # pylint: disable=too-many-ancestors
+    """ConfigParser inherited class with some tweaks."""
+
+    def __init__(self) -> None:
+        super().__init__(default_section="")
+        self.configfile = CONFIGDIR / f"{NAME}.ini"
+        self._resolve()
+
+    def _read_proxy(self) -> None:
+        self.read(self.configfile)
+        for section in self.sections():
+            for key in self[section]:
+                self[section][key] = os.path.expandvars(self[section][key])
+
+    def _resolve(self) -> None:
+        if self.configfile.is_file():
+            self._read_proxy()
+
+    def getlist(self, section: str, key: str) -> List[str]:
+        """Return a comma separated ini list as a Python list.
+
+        :param section: Section containing the key-value pair.
+        :param key:     The key who's comma separated list will be
+                        parsed.
+        :return:        Python list parsed from command separated
+                        values.
+        """
+        retval = []
+        if section in self:
+            retval.extend(
+                [e.strip() for e in self[section][key].split(",") if e != ""]
+            )
+
+        return retval
+
+
+class _MutableMapping(MutableMapping):  # pylint: disable=too-many-ancestors
+    """Inherit to replicate subclassing of ``dict`` objects."""
+
+    def __init__(self) -> None:
+        self._dict: Dict[str, Any] = dict()
+
+    def __repr__(self) -> str:
+        return f"<{self.__class__.__name__} {self._dict}>"
+
+    def __len__(self) -> int:
+        return self._dict.__len__()
+
+    def __delitem__(self, key: Any) -> None:
+        self._dict.__delitem__(key)
+
+    def __setitem__(self, index: Any, value: Any) -> None:
+        self._dict = self._nested_update(self._dict, {index: value})
+
+    def __getitem__(self, index: Any) -> Any:
+        return self._dict.__getitem__(index)
+
+    def __iter__(self) -> Iterator:
+        return iter(self._dict)
+
+    def _nested_update(
+        self, obj: Dict[str, Any], update: Dict[str, Any]
+    ) -> Dict[str, Any]:
+        # add to __setitem__ to ensure that no entire dict keys with
+        # missing nested keys overwrite all other values
+        # run recursively to cover all nested objects if value is a dict
+        # if value is a str pass through ``Path.expanduser()`` to
+        # translate paths prefixed with ``~/`` for ``/home/<user>``
+        # if value is all else assign it to obj key
+        # return obj for recursive assigning of nested dicts
+        for key, value in update.items():
+            if isinstance(value, dict):
+                value = self._nested_update(obj.get(key, {}), value)
+
+            elif isinstance(value, str):
+                value = str(Path(value).expanduser())
+
+            obj[key] = value
+
+        return obj
+
+
+class _TomlArrayEncoder(toml_encoder.TomlEncoder):
+    """Pass to ``toml.encoder`` functions (dump and dumps).
+
+    Set rule for string encoding so arrays are collapsed if they exceed
+    line limit.
+    """
+
+    def dump_list(self, v: Any) -> str:
+        """Rule for dumping arrays.
+
+        :param v:   Array from toml file.
+        :return:    toml encoded to str.
+        """
+        start, stop = (4 * " ", "\n") if len(str(v)) > 79 else ("", "")
+        retval = f"[{stop}"
+        for item in v:
+            retval += f"{start}{self.dump_value(item)}, {stop}"
+
+        return f"{retval[:-2] if retval.endswith(', ') else retval}]"
+
+
+class _Toml(_MutableMapping):  # pylint: disable=too-many-ancestors
+    """Base class for all ``toml`` object interaction."""
+
+    _encoder = _TomlArrayEncoder()
+
+    def _format_dump(self, obj: Dict[str, Any]) -> Dict[str, Any]:
+        for key, value in obj.items():
+            if isinstance(value, dict):
+                value = self._format_dump(value)
+
+            if isinstance(value, str):
+                value = value.replace(str(Path.home()), "~")
+
+            obj[key] = value
+
+        return obj
+
+    def dump(self, fout: TextIO, obj: Optional[MutableMapping] = None) -> str:
+        """Native ``dump`` method to include encoder.
+
+        :param fout:    TextIO file stream.
+        :param obj:     Mutable mapping dict-like object.
+        :return:        str object in toml encoded form.
+        """
+        return toml_encoder.dump(
+            self._format_dump(dict(self) if obj is None else dict(obj)),
+            fout,
+            encoder=self._encoder,
+        )
+
+    def dumps(self, obj: Optional[MutableMapping] = None) -> str:
+        """Native ``dump(from)s(tr)`` method to include encoder.
+
+        :param obj: Mutable mapping dict-like object.
+        :return:    str object in toml encoded form.
+        """
+        return toml_encoder.dumps(
+            self._format_dump(dict(self) if obj is None else dict(obj)),
+            encoder=self._encoder,
+        )
+
+    def load(self, fin: TextIO, *args: Any) -> None:
+        """Native ``load (from file)`` method.
+
+        :param fin: File stream.
+        """
+        obj = toml_decoder.load(fin)
+        for arg in args:
+            obj = obj.get(arg, obj)
+
+        self.update(obj)
+
+
+def configure_global() -> None:
+    """Setup object with default config settings.
+
+    Create config file with default config settings if one does not
+    already exist. Load base config file which may or may not still have
+    the default settings configured.
+    """
+    configfile = CONFIGDIR / TOMLFILE
+    backupfile = CONFIGDIR / f".{TOMLFILE}.bak"
+    default_config = copy.deepcopy(DEFAULT_CONFIG)
+    config = ConfigParser()
+    if configfile.is_file():
+        while True:
+            with open(configfile) as fin:
+                try:
+                    toml.load(fin)
+                    shutil.copyfile(configfile, backupfile)
+                    break
+
+                except toml_decoder.TomlDecodeError:
+                    if backupfile.is_file():
+                        os.rename(backupfile, configfile)
+                    else:
+                        break
+
+    for key in default_config:
+        if key not in toml:
+            toml[key] = default_config[key]
+
+    exclude = config.getlist("CLEAN", "exclude")
+    if exclude:
+        toml["clean"]["exclude"] = exclude
+
+    CONFIGDIR.mkdir(exist_ok=True, parents=True)
+    with open(configfile, "w") as fout:
+        toml.dump(fout)
+
+
+def load_config(opt: Optional[Union[str, os.PathLike]] = None):
+    """Load configs in order, each one overriding the previous.
+
+    :param opt: Optional extra path which will override all others.
+    """
+    files = [
+        CONFIGDIR / TOMLFILE,
+        Path.home() / RCFILE,
+        Path.cwd() / RCFILE,
+        Path.cwd() / PYPROJECT,
+    ]
+    if opt is not None:
+        files.append(Path(opt))
+
+    for file in files:
+        if file.is_file():
+            with open(file) as fin:
+                toml.load(fin, "tool", NAME)
+
+
+def generate_rcfile():
+    """Print default config file in ``Toml`` format."""
+    print(toml.dumps(DEFAULT_CONFIG), end="")
+
+
+def configure_logging(verbose: int = 0) -> None:
+    """Set loglevel via commandline.
+
+    Override environment variable if loglevel has already been set.
+
+    :param verbose: Level to raise log verbosity by.
+    """
+    config = toml["logging"]
+
+    # create logging dir and it's parents if they do not exist already
+    Path(config["handlers"]["default"]["filename"]).expanduser().parent.mkdir(
+        exist_ok=True, parents=True
+    )
+
+    # tweak loglevel if commandline argument is provided
+    config["root"]["level"] = LEVELS[
+        max(0, LEVELS.index(config["root"]["level"]) - verbose)
+    ]
+
+    # load values to ``logging``
+    logging_config.dictConfig(toml["logging"])
+
+
+toml = _Toml()
+configure_global()
---
+++ packages/pyaud/pyaud-2.0.0-py3-none-any.whl/pyaud/modules.py
@@ -0,0 +1,494 @@
+"""
+pyaud.modules
+=============
+"""
+import os
+import shutil
+import tempfile
+from pathlib import Path
+from subprocess import CalledProcessError
+
+from .config import generate_rcfile, toml
+from .environ import (
+    DOCS,
+    DOCS_CONF,
+    NAME,
+    PIPFILE_LOCK,
+    README,
+    TESTS,
+    TempEnvVar,
+    find_package,
+)
+from .utils import (
+    LineSwitch,
+    PyAuditError,
+    Subprocess,
+    check_command,
+    colors,
+    deploy_docs,
+    get_branch,
+    git,
+    tree,
+    write_command,
+)
+
+
+def make_clean(**kwargs: bool) -> None:
+    """Remove all unversioned package files recursively.
+
+    :param kwargs:  Additional keyword arguments for ``git clean``.
+    """
+    exclude = toml["clean"]["exclude"]
+    return git.clean(  # type: ignore
+        "-fdx", *[f"--exclude={e}" for e in exclude], **kwargs
+    )
+
+
+def make_coverage(**kwargs: bool) -> None:
+    """Run package unit-tests with ``pytest`` and ``coverage``.
+
+    :param kwargs:  Pass keyword arguments to ``pytest`` and ``call``.
+    """
+    coverage = Subprocess("coverage")
+    returncode = make_tests(*[f"--cov={e}" for e in tree.reduce()], **kwargs)
+    if not returncode:
+        with TempEnvVar(kwargs, suppress=True):
+            coverage.call("xml", **kwargs)
+    else:
+        print("No coverage to report")
+
+
+def make_deploy(**kwargs: bool) -> None:
+    """Deploy package documentation and test coverage.
+
+    :param kwargs: Keyword arguments for ``deploy_module``.
+    """
+
+    deploy_modules = [make_deploy_cov, make_deploy_docs]
+    for deploy_module in deploy_modules:
+        colors.cyan.bold.print(
+            "\n{}".format(
+                deploy_module.__name__.replace("make_", f"{NAME} ").replace(
+                    "_", "-"
+                )
+            )
+        )
+        deploy_module(**kwargs)
+
+
+def make_deploy_cov(**kwargs: bool) -> None:
+    """Upload coverage data to ``Codecov``.
+
+    If no file exists otherwise announce that no file has been created
+    yet. If no ``CODECOV_TOKEN`` environment variable has been exported
+    or defined in ``.env`` announce that no authorization token has been
+    created yet.
+
+    :param kwargs: Pass keyword arguments to ``call``.
+    """
+    codecov = Subprocess("codecov")
+    coverage_xml = Path.cwd() / os.environ["PYAUD_COVERAGE_XML"]
+    if coverage_xml.is_file():
+        if os.environ["CODECOV_TOKEN"] != "":
+            codecov.call("--file", Path.cwd() / coverage_xml, **kwargs)
+        else:
+            print("CODECOV_TOKEN not set")
+    else:
+        print("No coverage report found")
+
+
+def make_deploy_docs(**kwargs: bool) -> None:
+    """Deploy package documentation to ``gh-pages``.
+
+    Check that the branch is being pushed as master (or other branch
+    for tests). If the correct branch is the one in use deploy.
+    ``gh-pages`` to the orphaned branch - otherwise do nothing and
+    announce.
+
+    :param kwargs: Pass keyword arguments to ``make_docs``.
+    """
+    if get_branch() == "master":
+        git_credentials = ["PYAUD_GH_NAME", "PYAUD_GH_EMAIL", "PYAUD_GH_TOKEN"]
+        null_vals = [k for k in git_credentials if os.environ[k] == ""]
+        if not null_vals:
+            if not Path(Path.cwd() / os.environ["BUILDDIR"] / "html").is_dir():
+                make_docs(**kwargs)
+
+            deploy_docs()
+        else:
+            print("The following is not set:")
+            for null_val in null_vals:
+                print(f"- {null_val}")
+
+            print()
+            print("Pushing skipped")
+    else:
+        colors.green.print("Documentation not for master")
+        print("Pushing skipped")
+
+
+def make_docs(**kwargs: bool) -> None:
+    """Compile package documentation with ``Sphinx``.
+
+    This is so the hyperlink isn't exactly the same as the package
+    documentation. Build the ``Sphinx`` html documentation. Return the
+    README's title to what it originally was.
+
+    :param kwargs:  Pass keyword arguments to ``call``.
+    :return:        Exit status.
+    """
+    make_toc(**kwargs)
+
+    readme_rst = "README"
+    underline = len(readme_rst) * "="
+    build_dir = Path.cwd() / os.environ["BUILDDIR"]
+    if build_dir.is_dir():
+        shutil.rmtree(build_dir)
+
+    sphinx_build = Subprocess("sphinx-build")
+    if Path(Path.cwd() / DOCS).is_dir():
+        with LineSwitch(Path.cwd() / README, {0: readme_rst, 1: underline}):
+            command = ["-M", "html", Path.cwd() / DOCS, build_dir, "-W"]
+            sphinx_build.call(*command, **kwargs)
+            colors.green.bold.print("Build successful")
+    else:
+        print("No docs found")
+
+
+def make_files(**kwargs: bool) -> None:
+    """Audit project data files.
+
+    Make ``docs/<APPNAME>.rst``, ``whitelist.py``, and
+    ``requirements.txt`` if none already exist, update them if they do
+    and changes are needed or pass if nothing needs to be done.
+
+    :param kwargs:  Pass keyword arguments to ``func``.
+    :return:        Exit status.
+    """
+    for func in (make_requirements, make_toc, make_whitelist):
+        func(**kwargs)
+
+
+@check_command
+def make_format(**kwargs: bool) -> int:
+    """Audit code against ``Black``.
+
+    :param kwargs:  Pass keyword arguments to ``call``.
+    :key fix:       Do not raise error - fix problem instead.
+    :return:        Exit status.
+    """
+    black = Subprocess("black", loglevel="debug")
+    args = tree.reduce()
+    try:
+        return black.call("--check", *args, **kwargs)
+
+    except CalledProcessError as err:
+        black.call(*args, **kwargs)
+        if kwargs.get("fix", False):
+            return black.call(*args, **kwargs)
+
+        raise PyAuditError(f"{black} {tuple([str(p) for p in args])}") from err
+
+
+@check_command
+def make_lint(**kwargs: bool) -> int:
+    """Lint code with ``pylint``.
+
+    :param kwargs:  Pass keyword arguments to ``call``.
+    :return:        Exit status.
+    """
+    with TempEnvVar(os.environ, PYCHARM_HOSTED="True"):
+        args = tree.reduce()
+        pylint = Subprocess("pylint")
+        return pylint.call("--output-format=colorized", *args, **kwargs)
+
+
+@write_command("PYAUD_REQUIREMENTS", required="PYAUD_PIPFILE_LOCK")
+def make_requirements(**kwargs: bool) -> None:
+    """Audit requirements.txt with Pipfile.lock.
+
+    :param kwargs:  Pass keyword arguments to ``call``.
+    :key fix:       Do not raise error - fix problem instead.
+    """
+    # get the stdout for both production and development packages
+    pipfile_lock_path = Path.cwd() / PIPFILE_LOCK
+    requirements_path = Path.cwd() / os.environ["PYAUD_REQUIREMENTS"]
+
+    # get the stdout for both production and development packages
+    p2req = Subprocess("pipfile2req", capture=True)
+    p2req.call(pipfile_lock_path, **kwargs)
+    p2req.call(pipfile_lock_path, "--dev", **kwargs)
+
+    # write to file and then use sed to remove the additional
+    # information following the semi-colon
+    stdout = list(set("\n".join(p2req.stdout()).splitlines()))
+    stdout.sort()
+    with open(requirements_path, "w") as fout:
+        for content in stdout:
+            fout.write(f"{content.split(';')[0]}\n")
+
+
+def make_tests(*args: str, **kwargs: bool) -> int:
+    """Run the package unit-tests with ``pytest``.
+
+    :param args:    Additional positional arguments for ``pytest``.
+    :param kwargs:  Pass keyword arguments to ``call``.
+    :return:        Exit status.
+    """
+    tests = Path.cwd() / TESTS
+    patterns = ("test_*.py", "*_test.py")
+    pytest = Subprocess("pytest")
+    rglob = [
+        f
+        for f in tree
+        for p in patterns
+        if f.match(p) and str(tests) in str(f)
+    ]
+    if rglob:
+        return pytest.call(*args, **kwargs)
+
+    print("No tests found")
+    return 1
+
+
+@write_command("PYAUD_TOC", required="PYAUD_DOCS")
+def make_toc(**kwargs: bool) -> None:
+    """Audit docs/<NAME>.rst toc-file.
+
+    :param kwargs:  Pass keyword arguments to ``call``.
+    :key fix:       Do not raise error - fix problem instead.
+    :return:        Exit status.
+    """
+    toc_attrs = [
+        ".. automodule::",
+        "   :members:",
+        "   :undoc-members:",
+        "   :show-inheritance:",
+    ]
+    package = find_package()
+    docspath = Path.cwd() / DOCS
+    tocpath = docspath / f"{package}.rst"
+    if Path(Path.cwd() / DOCS_CONF).is_file():
+        apidoc = Subprocess("sphinx-apidoc")
+        apidoc.call(
+            "-o", docspath, Path.cwd() / package, "-f", devnull=True, **kwargs
+        )
+
+        with open(tocpath) as fin:
+            contents = fin.read().splitlines()
+
+        with open(tocpath, "w") as fout:
+            fout.write(f"{package}\n{len(package) * '='}\n\n")
+            for content in contents:
+                if any(a in content for a in toc_attrs):
+                    fout.write(f"{content}\n")
+
+        modules = (docspath / f"{package}.src.rst", docspath / "modules.rst")
+        for module in modules:
+            if module.is_file():
+                os.remove(module)
+
+
+@check_command
+def make_typecheck(**kwargs: bool) -> int:
+    """Typecheck code with ``mypy``.
+
+    Check that there are no errors between the files and their
+    stub-files.
+
+    :param kwargs:  Pass keyword arguments to ``call``.
+    :return:        Exit status.
+    """
+    mypy = Subprocess("mypy")
+    return mypy.call("--ignore-missing-imports", *tree.reduce(), **kwargs)
+
+
+@check_command
+def make_unused(**kwargs: bool) -> int:
+    """Audit unused code with ``vulture``.
+
+    Create whitelist first with --fix.
+
+    :param kwargs:  Pass keyword arguments to ``call``.
+    :key fix:       Do not raise error - fix problem instead.
+    :return:        Exit status.
+    """
+    whitelist = Path.cwd() / os.environ["PYAUD_WHITELIST"]
+    args = tree.reduce()
+    vulture = Subprocess("vulture")
+    while True:
+        try:
+            if whitelist.is_file():
+                args.append(whitelist)
+
+            return vulture.call(*args, **kwargs)
+
+        except CalledProcessError as err:
+            if kwargs.get("fix", False):
+                make_whitelist(**kwargs)
+                return 0
+
+            raise PyAuditError(
+                f"{vulture} {tuple([str(i) for i in args])}"
+            ) from err
+
+
+@write_command("PYAUD_WHITELIST")
+def make_whitelist(**kwargs: bool) -> None:
+    """Check whitelist.py file with ``vulture``.
+
+    This will consider all unused code an exception so resolve code that
+    is not to be excluded from the ``vulture`` search first.
+
+    :param kwargs:  Pass keyword arguments to ``call``.
+    :key fix:       Do not raise error - fix problem instead.
+    :return:        Exit status.
+    """
+    vulture = Subprocess("vulture", capture=True)
+
+    # append whitelist exceptions for each individual module
+    for item in tree.reduce():
+        if item.exists():
+            with TempEnvVar(kwargs, suppress=True):
+                vulture.call(item, "--make-whitelist", **kwargs)
+
+    # clear contents of instantiated ``TextIO' object to write a new
+    # file and not append
+    stdout = [i for i in "\n".join(vulture.stdout()).splitlines() if i != ""]
+    stdout.sort()
+    with open(Path.cwd() / os.environ["PYAUD_WHITELIST"], "w") as fout:
+        for line in stdout:
+            fout.write(f"{line.replace(str(Path.cwd()) + os.sep, '')}\n")
+
+
+@check_command
+def make_imports(**kwargs: bool) -> int:
+    """Audit imports with ``isort``.
+
+    ``Black`` and ``isort`` clash in some areas when it comes to
+    ``Black`` sorting imports. To avoid  running into false positives
+    when running both in conjunction (as ``Black`` is uncompromising)
+    run ``Black`` straight after. To effectively test this, for lack of
+    stdin functionality, use ``tempfile.NamedTemporaryFunction`` to
+    first evaluate contents from original file, then after ``isort``,
+    then after ``Black``. If nothing has changed, even if ``isort``
+    has changed a file, then the imports are sorted enough for
+    ``Black``'s standard. If there is a change raise ``PyAuditError`` if
+    ``-f/--fix`` or ``-s/--suppress`` was not passed to the commandline.
+    If ``-f/--fix`` was passed then replace the original file with the
+    temp file's contents.
+
+    :param kwargs:  Pass keyword arguments to ``call``.
+    :key fix:       Do not raise error - fix problem instead.
+    :return:        Exit status.
+    """
+    isort = Subprocess("isort", devnull=True)
+    black = Subprocess("black", loglevel="debug", devnull=True)
+    for item in tree:
+        if item.is_file():
+
+            # collect original file's contents
+            with open(item) as fin:
+                content = fin.read()
+
+            # write original file's contents to temporary file
+            tmp = tempfile.NamedTemporaryFile(delete=False)
+            with open(tmp.name, "w") as fout:
+                fout.write(content)
+
+            # run both ``isort`` and ``black`` on the temporary file,
+            # leaving the original file untouched
+            isort.call(tmp.name, **kwargs)
+            black.call(tmp.name, "--line-length", "79", **kwargs)
+
+            # collect the results from the temporary file
+            with open(tmp.name) as fin:
+                result = fin.read()
+
+            os.remove(tmp.name)
+            if result != content:
+                if kwargs.get("fix"):
+                    print(f"Fixed {item.relative_to(Path.cwd())}")
+
+                    # replace original file's contents with the temp
+                    # file post ``isort`` and ``Black``
+                    with open(item, "w") as fout:
+                        fout.write(result)
+
+                else:
+                    raise PyAuditError(
+                        "{} {}".format(
+                            make_imports.__name__,
+                            tuple([str(p) for p in tree.reduce()]),
+                        )
+                    )
+
+    return 0
+
+
+def make_readme(**kwargs: bool) -> None:
+    """Parse, test, and assert RST code-blocks.
+
+    :key suppress:  Suppress error and continue running even with a
+                    non-zero exit status.
+    :return:        Subprocess exit status.
+    """
+    with TempEnvVar(os.environ, PYCHARM_HOSTED="True"):
+        readmtester = Subprocess("readmetester")
+        if Path(Path.cwd() / README).is_file():
+            readmtester.call(Path.cwd() / README, **kwargs)
+        else:
+            print("No README.rst found in project root")
+
+
+@check_command
+def make_format_str(**kwargs: bool) -> int:
+    """Format f-strings with ``flynt``.
+
+    :param kwargs:  Pass keyword arguments to ``call``.
+    :key fix:       Do not raise error - fix problem instead.
+    :return:        Exit status.
+    """
+    flynt = Subprocess("flynt")
+    args = ("--line-length", "72", "--transform-concats", *tree.reduce())
+    try:
+        return flynt.call(
+            "--dry-run", "--fail-on-change", *args, devnull=True, **kwargs
+        )
+
+    except CalledProcessError as err:
+        if kwargs.get("fix", False):
+            return flynt.call(*args, **kwargs)
+
+        raise PyAuditError(f"{flynt} {tuple([str(p) for p in args])}") from err
+
+
+@check_command
+def make_format_docs(**kwargs: bool) -> int:
+    """Format docstrings with ``docformatter``.
+
+    :param kwargs:  Pass keyword arguments to ``call``.
+    :key fix:       Do not raise error - fix problem instead.
+    :return:        Exit status.
+    """
+    docformatter = Subprocess("docformatter")
+    args = ("--recursive", "--wrap-summaries", "72", *tree.reduce())
+    try:
+        return docformatter.call("--check", *args, **kwargs)
+
+    except CalledProcessError as err:
+        args = ("--in-place", *args)
+        if kwargs.get("fix", False):
+            return docformatter.call(*args, **kwargs)
+
+        raise PyAuditError(
+            f"{docformatter} {tuple([str(p) for p in args])}"
+        ) from err
+
+
+def make_generate_rcfile(**__: bool) -> None:
+    """Print rcfile to stdout.
+
+    Print rcfile to stdout so it may be piped to chosen filepath.
+    """
+    generate_rcfile()
