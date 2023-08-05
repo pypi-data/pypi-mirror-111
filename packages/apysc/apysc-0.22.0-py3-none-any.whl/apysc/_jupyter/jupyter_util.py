30fb30465cde541616a41f9772fdc2a66af14f3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:16:20 +0000

    Add package apysc/0.22.0/apysc-0.22.0-py3-none-any.whl

---
+++ packages/apysc/apysc-0.22.0-py3-none-any.whl/apysc/_jupyter/jupyter_util.py
@@ -0,0 +1,43 @@
+"""
+The module that is implemented each Jupyter interface and definition.
+"""
+
+import os
+
+
+def display_on_jupyter(
+        html_file_name: str,
+        dest_dir_path: str = './',
+        minify: bool = True) -> None:
+    """
+    Save the overall HTML and display it on the Jupyter.
+
+    Parameters
+    ----------
+    html_file_name : str, default 'index.html'
+        The output HTML file name.
+    dest_dir_path : str
+        Destination directory path to save HTML.
+    minify : bool, default True
+        Boolean value whether minify a HTML or not.
+        False setting is useful when debugging.
+
+    Notes
+    -----
+    This interface requires the Jupyter library (e.g., `notebook` package).
+    """
+    from IPython.display import HTML
+    from IPython.display import display
+
+    from apysc import save_overall_html
+    from apysc._file import file_util
+    save_overall_html(
+        dest_dir_path=dest_dir_path,
+        html_file_name=html_file_name,
+        minify=minify,
+        skip_js_lib_exporting=True,
+        embed_js_libs=True,
+        verbose=0)
+    file_path: str = os.path.join(dest_dir_path, html_file_name)
+    html_str: str = file_util.read_txt(file_path=file_path)
+    display(HTML(html_str))
