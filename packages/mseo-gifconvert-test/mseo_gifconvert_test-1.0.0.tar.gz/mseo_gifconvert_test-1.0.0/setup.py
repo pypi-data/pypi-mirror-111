6e77f441870c6d5e058f16f8b6724e1c18b9ffde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:24 +0000

    Add package mseo-gifconvert-test/1.0.0/mseo_gifconvert_test-1.0.0.tar.gz

---
+++ packages/mseo-gifconvert-test/mseo_gifconvert_test-1.0.0.tar.gz/mseo_gifconvert_test-1.0.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name             = 'mseo_gifconvert_test',
+    version          = '1.0.0',
+    description      = 'Test package __mseo',
+    author           = 'mseo',
+    author_email     = 'mseo@mvista.com',
+    url              = '',
+    download_url     = '',
+    install_requires = ['pillow'],
+	include_package_data=True,
+	packages=find_packages(),
+    keywords         = ['GIFCONVERTER', 'gifconverter'],
+    python_requires  = '>=3',
+    zip_safe=False,
+    classifiers      = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ]
+) 
