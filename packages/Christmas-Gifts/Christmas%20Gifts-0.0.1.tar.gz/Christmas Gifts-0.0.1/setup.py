ca39908e6919a792e90a71cb88ecc606deec0a34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:05 +0000

    Add package Christmas-Gifts/0.0.1/Christmas%20Gifts-0.0.1.tar.gz

---
+++ packages/Christmas-Gifts/Christmas%20Gifts-0.0.1.tar.gz/Christmas Gifts-0.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+
+name = "Christmas Gifts",
+
+version = "0.0.1",
+
+packages = ["Sack"],
+
+author = "Shanthosh aka Santa",
+
+author_email = "santasandy1702@gmail.com",
+
+description = "A PACKAGE FOR EVERYTHING IN PYTHON",
+
+long_description_content_type = "text/markdown",
+
+long_description = "This package is now initially built with a 3 dimensional database to store your coordinates for any type of virtual or real 3 dimensional project in python",
+
+)
