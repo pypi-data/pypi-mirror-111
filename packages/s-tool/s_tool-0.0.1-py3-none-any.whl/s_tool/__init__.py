699e315967f0364ec440fc0cf97f7dbc65fa0205
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:57 +0000

    Add package s-tool/0.0.1/s_tool-0.0.1-py3-none-any.whl

---
+++ packages/s-tool/s_tool-0.0.1-py3-none-any.whl/s_tool/__init__.py
@@ -0,0 +1,12 @@
+"""Package s-tool.
+
+A selenium wrapper to perform various tasks.
+
+MIT License
+
+Copyright (c) 2021 Python World
+"""
+
+from importlib.metadata import version
+
+__version__ = version(__package__)
