ec8c8e507785b8053285d8dcfc8d504513b6e771
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:05 +0000

    Add package xlsxwriter-celldsl/0.4.0/xlsxwriter-celldsl-0.4.0.tar.gz

---
+++ packages/xlsxwriter-celldsl/xlsxwriter-celldsl-0.4.0.tar.gz/xlsxwriter-celldsl-0.4.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+with open("./README.md", encoding='utf-8') as in_:
+    setup(
+        name='xlsxwriter-celldsl',
+        version='0.4.0',
+        packages=['xlsxwriter_celldsl'],
+        url='https://github.com/DeltaEpsilon7787/xlsxwriter-celldsl',
+        license='MIT',
+        author='DeltaEpsilon7787',
+        author_email='deltaepsilon7787@gmail.com',
+        description='A library to write scripts for generating Excel files using XlsxWriter in a more structured '
+                    'manner by avoiding dealing with absolute coordinates.',
+        long_description=in_.read(),
+        long_description_content_type="text/markdown",
+        python_requires='>=3.6',
+        install_requires=[
+            "attrs",
+            "xlsxwriter",
+        ],
+        extras_require={
+            'testing': ['pytest', 'pytest-mock']
+        },
+    )
