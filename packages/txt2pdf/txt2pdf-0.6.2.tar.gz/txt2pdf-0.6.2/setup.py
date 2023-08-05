c1ec407e7713da3189910b1ae591970c9d9ac03c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:47 +0000

    Add package txt2pdf/0.6.2/txt2pdf-0.6.2.tar.gz

---
+++ packages/txt2pdf/txt2pdf-0.6.2.tar.gz/txt2pdf-0.6.2/setup.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+txt2pdf - setup file
+"""
+
+import txt2pdf
+
+from setuptools import setup, find_packages
+
+
+def parse_requirements(requirements, ignore=("setuptools",)):
+    """
+    Read dependencies from requirements file (with version numbers if any)
+
+    Notes:
+        - this implementation does not support requirements files with extra
+          requirements
+        - this implementation has been taken from TailorDev/Watson's setup file
+    """
+    with open(requirements) as f:
+        packages = set()
+        for line in f:
+            line = line.strip()
+            if line.startswith(("#", "-r", "--")):
+                continue
+            if "#egg=" in line:
+                line = line.split("#egg=")[1]
+            pkg = line.strip()
+            if pkg not in ignore:
+                packages.add(pkg)
+        return list(packages)
+
+
+setup(
+    name="txt2pdf",
+    version=txt2pdf.__version__,
+    packages=find_packages(),
+    scripts=[
+        "scripts/txt2pdf",
+    ],
+    install_requires=parse_requirements("requirements.txt"),
+    setup_requires=[
+        "pytest-runner",
+    ],
+    tests_require=parse_requirements("requirements-dev.txt"),
+    author="Julien Maupetit & c4ffein",
+    author_email="c4ffein@gmail.com",
+    description="txt2pdf, a Markdown to PDF conversion tool",
+    license="MIT",
+    keywords="markdown converter css pdf",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Customer Service",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Other Audience",
+        "License :: OSI Approved :: MIT License",
+        "Environment :: Console",
+        "Operating System :: MacOS",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Office/Business",
+        "Topic :: Utilities",
+    ],
+)
