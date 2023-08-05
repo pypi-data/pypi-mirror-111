fc117edd61df6621e3a4bf3fb13c69af1c9325e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:20 +0000

    Add package backends/1.3.2/backends-1.3.2.tar.gz

---
+++ packages/backends/backends-1.3.2.tar.gz/backends-1.3.2/lab/tensorflow/shaping.py
@@ -0,0 +1,103 @@
+import tensorflow as tf
+from plum import Union
+
+from . import dispatch, B, Numeric
+from ..types import Int, TFNumeric, NPNumeric
+
+__all__ = []
+
+
+@dispatch
+def length(a: Numeric):
+    return tf.size(a)
+
+
+@dispatch
+def expand_dims(a: Numeric, axis=0):
+    return tf.expand_dims(a, axis=axis)
+
+
+@dispatch
+def squeeze(a: Numeric):
+    return tf.squeeze(a)
+
+
+@dispatch
+def broadcast_to(a: Numeric, *shape: Int):
+    return tf.broadcast_to(a, shape)
+
+
+@dispatch
+def diag(a: Numeric):
+    if B.rank(a) == 1:
+        return tf.linalg.diag(a)
+    elif B.rank(a) == 2:
+        return tf.linalg.diag_part(a)
+    else:
+        raise ValueError("Input must have rank 1 or 2.")
+
+
+@dispatch
+def diag_extract(a: Numeric):
+    return tf.linalg.diag_part(a)
+
+
+@dispatch
+def diag_construct(a: TFNumeric):
+    return tf.linalg.diag(a)
+
+
+@dispatch
+def stack(*elements: Numeric, axis=0):
+    return tf.stack(elements, axis=axis)
+
+
+@dispatch
+def unstack(a: Numeric, axis=0):
+    return tf.unstack(a, axis=axis)
+
+
+@dispatch
+def reshape(a: Numeric, *shape: Int):
+    return tf.reshape(a, shape=shape)
+
+
+@dispatch
+def concat(*elements: Numeric, axis=0):
+    return tf.concat(elements, axis=axis)
+
+
+@dispatch
+def tile(a: Numeric, *repeats: Int):
+    return tf.tile(a, repeats)
+
+
+@dispatch
+def take(a: TFNumeric, indices_or_mask, axis=0):
+    if B.rank(indices_or_mask) != 1:
+        raise ValueError("Indices or mask must be rank 1.")
+    is_mask, indices_or_mask = _is_mask_and_convert(indices_or_mask)
+    if is_mask:
+        return tf.boolean_mask(a, indices_or_mask, axis=axis)
+    else:
+        return tf.gather(a, indices_or_mask, axis=axis)
+
+
+@dispatch
+def _is_mask_and_convert(indices_or_mask: TFNumeric):
+    return indices_or_mask.dtype == bool, indices_or_mask
+
+
+@dispatch
+def _is_mask_and_convert(indices_or_mask: NPNumeric):
+    return indices_or_mask.dtype == bool, tf.constant(indices_or_mask)
+
+
+@dispatch
+def _is_mask_and_convert(indices_or_mask: Union[tuple, list]):
+    if len(indices_or_mask) == 0:
+        # Treat an empty tuple or list as a list of no indices. The data type does not
+        # matter, except that it must be integer.
+        return False, tf.constant([], dtype=tf.int32)
+    else:
+        return B.dtype(indices_or_mask[0]) == bool, indices_or_mask
