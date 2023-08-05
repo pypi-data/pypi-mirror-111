62c15d334611d24ed9e9336e15fb43c251bb3b5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:27 +0000

    Add package mlvizlib/0.0.4/mlvizlib-0.0.4.tar.gz

---
+++ packages/mlvizlib/mlvizlib-0.0.4.tar.gz/mlvizlib-0.0.4/mlvizlib/classification/__init__.py
@@ -0,0 +1,16 @@
+"""
+The :mod:`mlvizlib.classification` module implements a variety of classification visualizations.
+"""
+
+#  from mlvizlib import classification
+#  from mlvizlib.classification import _confusion_matrix  # required
+#  from mlvizlib.classification._confusion_matrix import confusion_matrix  # used to make this function importable
+#  from ._confusion_matrix import confusion_matrix_kalastaja  # used to make this function importable
+
+
+#__all__ = [
+#    "confusion_matrix"
+#    ]
+
+# import the modules (.py) into the subpackages
+from . import viz_confusion_matrix
