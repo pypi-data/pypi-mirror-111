4ca316c95592ba1f9676d78a1a8927698a726dd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:11 +0000

    Add package SymmetryGecko/0.0.8/SymmetryGecko-0.0.8.tar.gz

---
+++ packages/SymmetryGecko/SymmetryGecko-0.0.8.tar.gz/SymmetryGecko-0.0.8/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+setup(
+    name= 'SymmetryGecko', # for pip install
+    packages= ['SymmetryGecko'],
+    include_package_data=True,
+    version= '0.0.8',
+    license= 'MIT',
+    description= 'CoinGecko API for Symmetry Finance',
+    author= 'Mr. Bluesky',
+    author_email= 'solana.bluesky@gmail.com',
+    url = 'https://github.com/symmetry-protocol/coingeckoAPI.git',
+    download_url = 'https://github.com/symmetry-protocol/coingeckoAPI.git',
+    intsall_requires= [
+        'requests'
+    ],
+    classifiers= [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+)
