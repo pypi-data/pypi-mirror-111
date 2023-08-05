87c84370f7200b73e4e43e0b882e77bb95c58a56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:22 +0000

    Add package EISeg/0.1.3/EISeg-0.1.3-py3-none-any.whl

---
+++ packages/EISeg/EISeg-0.1.3-py3-none-any.whl/eiseg/__init__.py
@@ -0,0 +1,15 @@
+import sys
+import os.path as osp
+
+pjpath = osp.dirname(osp.realpath(__file__))
+sys.path.append(pjpath)
+
+
+from run import main
+from models import models
+
+
+__APPNAME__ = "EISeg"
+
+if __name__ == "__main__":
+    main()
