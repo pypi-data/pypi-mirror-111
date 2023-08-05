895e7df7e353dd28363be6c8bfc5fdf33a87853d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:37 +0000

    Add package SymmetryGecko/0.0.13/SymmetryGecko-0.0.13.tar.gz

---
+++ packages/SymmetryGecko/SymmetryGecko-0.0.13.tar.gz/SymmetryGecko-0.0.13/SymmetryGecko/src/log.py
@@ -0,0 +1,18 @@
+import datetime
+from . import config
+
+def prettify_list(data):
+    """ returns list as pretty sting """
+    result = "[\n"
+    for d in data:
+        result += "\t" + str(d) + ",\n"
+    result += "]"
+    return result
+
+def log(message, filename, add_prefix=True):
+    """" add_prefix = True adds timestamp as prefix """
+    filepath = config.FOLDERPATH_LOG() + datetime.datetime.now().strftime("%B %d, %Y") + "-" + filename
+    if not add_prefix: filepath = config.FOLDERPATH_LOG() + filename
+
+    with open(filepath, "w+") as f:
+        f.write( str(message))
