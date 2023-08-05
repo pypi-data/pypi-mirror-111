bf9baceae92f295d3c7048755b69cfdc44e83305
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:09 +0000

    Add package distributions-chitts/0.1/distributions_chitts-0.1.tar.gz

---
+++ packages/distributions-chitts/distributions_chitts-0.1.tar.gz/distributions_chitts-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_chitts',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_chitts'],
+      zip_safe=False)
