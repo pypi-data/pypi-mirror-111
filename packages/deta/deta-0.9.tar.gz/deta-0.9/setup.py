1454b8aabab1ec65da48e33590b83ec80c5fac2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:14 +0000

    Add package deta/0.9/deta-0.9.tar.gz

---
+++ packages/deta/deta-0.9.tar.gz/deta-0.9/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name="deta",
+    version="0.9",
+    description="Python SDK for Deta Base & Deta Drive.",
+    url="http://github.com/deta/deta-python",
+    author="Deta",
+    author_email="hello@deta.sh",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=["deta"],
+)
