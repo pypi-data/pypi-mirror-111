ec52a3574218e42ea55d66db93eb6a08e32ac321
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:09 +0000

    Add package osin/0.1.4/osin-0.1.4.tar.gz

---
+++ packages/osin/osin-0.1.4.tar.gz/osin-0.1.4/osin/ui/toggle_list/frontend/build/precache-manifest.94163efd3d84188f0d50a87c96c8ad8d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c5ad4e46d1500b30ef0da002a3548ee",
+    "url": "./index.html"
+  },
+  {
+    "revision": "10c7e069e13c026bd96c",
+    "url": "./static/js/2.edf6e967.chunk.js"
+  },
+  {
+    "revision": "fb9b6627bef6830e6daba977eaebd560",
+    "url": "./static/js/2.edf6e967.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0913a538b5793d29ddf9",
+    "url": "./static/js/main.f63112e9.chunk.js"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  }
+]);
