30eedd9e1bb997569bf5b6df01fa065f45799c00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:26 +0000

    Add package SymmetryGecko/0.0.3/SymmetryGecko-0.0.3.tar.gz

---
+++ packages/SymmetryGecko/SymmetryGecko-0.0.3.tar.gz/SymmetryGecko-0.0.3/SymmetryGecko/__init__.py
@@ -0,0 +1,12 @@
+""" call one of the api_calls functions that are imported """
+
+import os
+from . import config
+config.FOLDERPATH_INIT = os.path.dirname( os.path.abspath(__file__) )
+
+# ----------------------------------------------------------------------
+from .src2.api_calls import get_market_data, get_complete_data, get_historical_data
+# ----------------------------------------------------------------------
+
+if __name__ == "__main__":
+    pass
