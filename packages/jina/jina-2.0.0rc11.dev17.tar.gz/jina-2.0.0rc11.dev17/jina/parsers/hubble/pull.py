074e5d7b684a7f68dc230100a48dda328f215738
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:12 +0000

    Add package jina/2.0.0rc11.dev17/jina-2.0.0rc11.dev17.tar.gz

---
+++ packages/jina/jina-2.0.0rc11.dev17.tar.gz/jina-2.0.0rc11.dev17/jina/parsers/hubble/pull.py
@@ -0,0 +1,20 @@
+"""Argparser module for hub push"""
+from ..helper import add_arg_group
+
+
+def mixin_hub_pull_parser(parser):
+    """Add the arguments for hub pull to the parser
+    :param parser: the parser configure
+    """
+    gp = add_arg_group(parser, title='Pull')
+    gp.add_argument(
+        'uri',
+        type=str,
+        help='The URI of the executor to download (e.g., jinahub(+docker)://dummy_executor)',
+    )
+    gp.add_argument(
+        '--install-deps',
+        action='store_true',
+        default=False,
+        help='If set, install the dependencies automatically',
+    )
