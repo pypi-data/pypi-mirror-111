b9fa30d174b28a1b9ca2b5697fb45a316f096269
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:29 +0000

    Add package pywebflowerlibpackage/1.1.3/pywebflowerlibpackage-1.1.3-py3-none-any.whl

---
+++ packages/pywebflowerlibpackage/pywebflowerlibpackage-1.1.3-py3-none-any.whl/pywebflowerlibpackage/constants/web_bots_keywords.py
@@ -0,0 +1,14 @@
+WEB_ROBOTS_KEYWORDS = [
+  "bingbot",
+  "msnbot",
+  "bingpreview",
+  "aolbuild",
+  "duckduckbot",
+  "adsbot-google",
+  "googlebot",
+  "mediapartners-google",
+  "teoma",
+  "slurp",
+  "yandex.com/bots",
+  "facebookexternalhit"
+]
