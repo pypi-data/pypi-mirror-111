4725a5f6e0e0c81dd5ac72129e4e9d4e7e432763
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:50 +0000

    Add package readme-py/0.0.3/readme_py-0.0.3-py3-none-any.whl

---
+++ packages/readme-py/readme_py-0.0.3-py3-none-any.whl/readme_py/structure/structure.py
@@ -0,0 +1,48 @@
+from ..elements import Element,Link,Header,Br
+from typing import List, Any
+
+class Section:
+    title: str = ""
+    inner: List[Element] = []
+    header_size: int = 2
+    have_header: bool = True
+
+    def to_markdown(self):
+        from ..generator import md
+        markdown_text: str = ""
+        markdown_text += (md(Header(self.header_size, self.title)) + md(Br())*2) if self.have_header else ""
+
+        for el in self.inner:
+            markdown_text += md(el) + md(Br())*2
+
+        return markdown_text
+
+
+class Readme:
+    title: str = ""
+    description: str = ""
+    badges: List[Any] = []
+    sections: List[Section] = []
+    have_header: bool = True
+
+    def __init__(self, title: str = "", description: str = "", sections: List[Section] = [], badges: List[Any] = []) -> None:
+        self.title = title
+        self.description = description
+        self.badges = badges
+        self.sections = sections
+
+    def to_markdown(self):
+        from ..generator import md
+        markdown_text: str = ""
+        markdown_text += (md(Header(1, self.title)) + md(Br())*2) if self.have_header else ""
+
+        for section in self.sections:
+            markdown_text += md(section)
+
+        return markdown_text
+
+
+class LicenseSection(Section):
+    def __init__(self, license_type: str, license_link: str) -> None:
+        self.title = "License"
+        self.inner.append(Link(license_type, license_link))
