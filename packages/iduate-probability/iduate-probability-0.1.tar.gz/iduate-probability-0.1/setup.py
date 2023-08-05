bf76ffa755bef312289a2b77991a463e001d6d84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:27:58 +0000

    Add package iduate-probability/0.1/iduate-probability-0.1.tar.gz

---
+++ packages/iduate-probability/iduate-probability-0.1.tar.gz/iduate-probability-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='iduate-probability',
+      version='0.1',
+      description='probability distributions - Gaussian, Binomial',
+      packages=['distributions'],
+      zip_safe=False)
