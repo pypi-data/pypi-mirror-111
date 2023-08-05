08c134a1a2443b795eae251e0a21e085e1c6aa10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:57 +0000

    Add package pyndb/2.653/pyndb-2.653.tar.gz

---
+++ packages/pyndb/pyndb-2.653.tar.gz/pyndb-2.653/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+with open("VERSION", "r") as vfile:
+    version = vfile.read()
+
+setuptools.setup(name="pyndb",
+      version=version,
+      packages=["pyndb"],
+      author="jvadair",
+      author_email="dev@jvadair.com",
+      description="A way of saving data to a file with syntactic convenience",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/jvadair/pyndb",
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+      ],
+)
