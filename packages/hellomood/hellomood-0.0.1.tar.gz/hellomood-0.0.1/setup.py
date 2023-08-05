80f9dca9accf7ad10fe52f057d189b18fd615f01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:10 +0000

    Add package hellomood/0.0.1/hellomood-0.0.1.tar.gz

---
+++ packages/hellomood/hellomood-0.0.1.tar.gz/hellomood-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+setup(
+    name="hellomood",
+    version="0.0.1",
+    packages=['hellomood',],
+    url="https://github.com/dianas11xx/MoodipyTest",
+    author="Noteworthy",
+    license = "GPL 3",
+    package_data = {"hellomood":['*.jpeg', '*.png', '*.txt', '*.py']},
+    description="use sentiment analysis to create a playlist that matches someone's mood and also predict songs to rise in popularity.",
+    install_requires=['nltk==3.6.2', 'screeninfo==0.6.7', 'spotipy==2.18.0', 'requests>=2.25.0', 'PyQt5>=5.12','urllib3>=1.26.0', 'PyQt5-sip>=4.19.19'],
+    entry_points={"console_scripts": ["hellomood=hellomood.main:main"]},
+    include_package_data=True
+)
