08c1d276af8c7daa6755d3c1a2ce543a767c3ad3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:15:54 +0000

    Add package fastapi-framework/0.9.4.1/fastapi_framework-0.9.4.1-py3-none-any.whl

---
+++ packages/fastapi-framework/fastapi_framework-0.9.4.1-py3-none-any.whl/fastapi_framework/redis.py
@@ -0,0 +1,29 @@
+from aioredis import Redis, create_redis_pool, create_redis
+from dotenv import load_dotenv
+from os import getenv
+
+load_dotenv()
+
+REDIS_HOST = getenv("REDIS_HOST", "localhost")
+REDIS_PORT = getenv("REDIS_PORT")
+
+
+class RedisDependency:
+    """FastAPI Dependency for Redis Connections"""
+
+    redis: Redis = None
+
+    def __call__(self):
+        return self.redis
+
+    async def init(self):
+        """Initialises the Redis Dependency"""
+        self.redis: Redis = await create_redis_pool(f"redis://{REDIS_HOST}:{REDIS_PORT}")
+
+
+redis_dependency: RedisDependency = RedisDependency()
+
+
+async def get_redis() -> Redis:
+    """Returns a NEW Redis connection"""
+    return await create_redis(f"redis://{REDIS_HOST}:{REDIS_PORT}")
