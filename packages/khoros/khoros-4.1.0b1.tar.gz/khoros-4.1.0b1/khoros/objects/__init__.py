39b4623c93d691237ccbb3d657c5043f367d62c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:08 +0000

    Add package khoros/4.1.0b1/khoros-4.1.0b1.tar.gz

---
+++ packages/khoros/khoros-4.1.0b1.tar.gz/khoros-4.1.0b1/khoros/objects/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+:Module:            khoros.objects
+:Synopsis:          This module contains sub-modules that are specific to the various Community API objects.
+:Usage:             ``from khoros import objects``
+:Example:           ``objects.archives.archive(khoros_obj, '123', suggested_url, return_status=True)``
+:Created By:        Jeff Shurtliff
+:Last Modified:     Jeff Shurtliff
+:Modified Date:     25 Mar 2021
+"""
+
+__all__ = ['albums', 'archives', 'messages', 'roles', 'settings', 'subscriptions', 'users']
+
+# Import all submodules by default
+from . import albums
+from . import archives
+from . import messages
+from . import roles
+from . import settings
+from . import subscriptions
+from . import users
