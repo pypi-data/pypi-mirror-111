349eed8c154f336cbaec3bd4a3e08e34147b5560
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:16:14 +0000

    Add package susposter/1.17.7/susposter-1.17.7-py3-none-any.whl

---
+++ packages/susposter/susposter-1.17.7-py3-none-any.whl/susposter/__init__.py
@@ -0,0 +1,15 @@
+from io import BytesIO
+
+__version__ = "1.17.7"
+
+def sussy():
+  print("so sussy")
+  return "so sussy"
+
+def sus():
+  print("https://github.com/Cryptex-github/susposter/raw/main/susposter/susposter.png")
+  return "https://github.com/Cryptex-github/susposter/raw/main/susposter/susposter.png"
+
+def amongus():
+  print("https://github.com/Cryptex-github/susposter/raw/main/susposter/sussy.png")
+  return "https://github.com/Cryptex-github/susposter/raw/main/susposter/sussy.png"
