99f8086da1f7df24d1c8b21e7a99c24480a855df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:44 +0000

    Add package tff-utils/0.0.4/tff_utils-0.0.4-py3-none-any.whl

---
+++ packages/tff-utils/tff_utils-0.0.4-py3-none-any.whl/tff_utils/configuration.py
@@ -0,0 +1,13 @@
+import configargparse
+
+
+def parse() -> configargparse.Namespace:
+    p = configargparse.ArgParser()
+    p.add('-c', '--config', required=False, is_config_file=True, help='config file path')
+    p.add('--host', action='append', default=[], help='remote host address, simulate training if not set')
+    return p.parse_args()
+
+
+if __name__ == '__main__':
+    options = parse()
+    print(options)
