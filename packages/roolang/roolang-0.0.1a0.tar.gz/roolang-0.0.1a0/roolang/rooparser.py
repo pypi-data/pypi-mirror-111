dd4ad56fbfb92e8e1e996ee4bceeb427007f7d11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:44 +0000

    Add package roolang/0.0.1a0/roolang-0.0.1a0.tar.gz

---
+++ packages/roolang/roolang-0.0.1a0.tar.gz/roolang-0.0.1a0/roolang/rooparser.py
@@ -0,0 +1,28 @@
+from roolexer import generator
+
+from typing import List
+from rply import ParserGenerator, Token
+
+
+parser = ParserGenerator([l.name for l in generator.rules])
+
+
+class rooBulli(SyntaxError):
+    pass
+
+@parser.production("main : expr")
+def main(p: List[str]) -> str:
+    return p[0]
+
+@parser.production('string : STRING')
+def string(p: List[str]) -> str:
+    return p[0].getstr().strip("'").strip('"')
+
+@parser.production("expr : rooPog string")
+def rooPog(p: List[str]) -> None:
+    ...
+    
+
+@parser.error
+def rooBulli_error_handler(token):
+    raise rooBulli(f"rooBulli: Unexpected {token.gettokentype()} in {token.getsourcepos()}\nvalue : '{token.getstr()}'")
