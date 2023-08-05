08b715c0cd591a2fc7eca90b56cf4115a8ad48c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:23 +0000

    Add package pyriemann/0.2.7/pyriemann-0.2.7.tar.gz

---
+++ packages/pyriemann/pyriemann-0.2.7.tar.gz/pyriemann-0.2.7/pyriemann/utils/covariance.py
@@ -0,0 +1,349 @@
+import warnings
+
+import numpy as np
+
+from sklearn.covariance import oas, ledoit_wolf, fast_mcd, empirical_covariance
+
+# Mapping different estimator on the sklearn toolbox
+
+
+def _lwf(X):
+    """Wrapper for sklearn ledoit wolf covariance estimator"""
+    C, _ = ledoit_wolf(X.T)
+    return C
+
+
+def _oas(X):
+    """Wrapper for sklearn oas covariance estimator"""
+    C, _ = oas(X.T)
+    return C
+
+
+def _scm(X):
+    """Wrapper for sklearn sample covariance estimator"""
+    return empirical_covariance(X.T)
+
+
+def _mcd(X):
+    """Wrapper for sklearn mcd covariance estimator"""
+    _, C, _, _ = fast_mcd(X.T)
+    return C
+
+
+def _check_est(est):
+    """Check if a given estimator is valid"""
+
+    # Check estimator exist and return the correct function
+    estimators = {
+        'cov': np.cov,
+        'scm': _scm,
+        'lwf': _lwf,
+        'oas': _oas,
+        'mcd': _mcd,
+        'corr': np.corrcoef
+    }
+
+    if callable(est):
+        # All good (cross your fingers)
+        pass
+    elif est in estimators.keys():
+        # Map the corresponding estimator
+        est = estimators[est]
+    else:
+        # raise an error
+        raise ValueError(
+            """%s is not an valid estimator ! Valid estimators are : %s or a
+             callable function""" % (est, (' , ').join(estimators.keys())))
+    return est
+
+
+def covariances(X, estimator='cov'):
+    """Estimation of covariance matrix.
+
+    Parameters
+    ----------
+    X : ndarray, shape (n_trials, n_channels, n_samples)
+        ndarray of trials.
+
+    estimator : {'cov', 'scm', 'lwf', 'oas', 'mcd', 'corr'} (default: 'scm')
+        covariance matrix estimator:
+
+        * 'cov' for numpy based covariance matrix, https://numpy.org/doc/stable/reference/generated/numpy.cov.html
+        * 'scm' for sample covariance matrix, https://scikit-learn.org/stable/modules/generated/sklearn.covariance.empirical_covariance.html
+        * 'lwf' for shrunk Ledoit-Wolf covariance matrix, https://scikit-learn.org/stable/modules/generated/sklearn.covariance.ledoit_wolf.html
+        * 'oas' for oracle approximating shrunk covariance matrix, https://scikit-learn.org/stable/modules/generated/sklearn.covariance.OAS.html
+        * 'mcd' for minimum covariance determinant matrix, https://scikit-learn.org/stable/modules/generated/sklearn.covariance.MinCovDet.html
+        * 'corr' for correlation coefficient matrix, https://numpy.org/doc/stable/reference/generated/numpy.corrcoef.html
+
+    Returns
+    -------
+    covmats : ndarray, shape (n_trials, n_channels, n_channels)
+        ndarray of covariance matrices.
+
+    References
+    ----------
+    .. [1] https://scikit-learn.org/stable/modules/covariance.html
+    """  # noqa
+    est = _check_est(estimator)
+    Nt, Ne, Ns = X.shape
+    covmats = np.zeros((Nt, Ne, Ne))
+    for i in range(Nt):
+        covmats[i, :, :] = est(X[i, :, :])
+    return covmats
+
+
+def covariances_EP(X, P, estimator='cov'):
+    """Special form covariance matrix."""
+    est = _check_est(estimator)
+    Nt, Ne, Ns = X.shape
+    Np, Ns = P.shape
+    covmats = np.zeros((Nt, Ne + Np, Ne + Np))
+    for i in range(Nt):
+        covmats[i, :, :] = est(np.concatenate((P, X[i, :, :]), axis=0))
+    return covmats
+
+
+def eegtocov(sig, window=128, overlapp=0.5, padding=True, estimator='cov'):
+    """Convert EEG signal to covariance using sliding window"""
+    est = _check_est(estimator)
+    X = []
+    if padding:
+        padd = np.zeros((int(window / 2), sig.shape[1]))
+        sig = np.concatenate((padd, sig, padd), axis=0)
+
+    Ns, Ne = sig.shape
+    jump = int(window * overlapp)
+    ix = 0
+    while (ix + window < Ns):
+        X.append(est(sig[ix:ix + window, :].T))
+        ix = ix + jump
+
+    return np.array(X)
+
+
+###############################################################################
+
+
+def cross_spectrum(X, window=128, overlap=0.75, fmin=None, fmax=None, fs=None):
+    """Compute the complex cross-spectral matrices of a real signal X.
+
+    Parameters
+    ----------
+    X : ndarray, shape (n_channels, n_samples)
+        Multi-channel time-series.
+    window : int (default 128)
+        The length of the FFT window used for spectral estimation.
+    overlap : float (default 0.75)
+        The percentage of overlap between window.
+    fmin : float | None, (default None)
+        The minimal frequency to be returned.
+    fmax : float | None, (default None)
+        The maximal frequency to be returned.
+    fs : float | None, (default None)
+        The sampling frequency of the signal.
+
+    Returns
+    -------
+    S : ndarray, shape (n_channels, n_channels, n_freqs)
+        Cross-spectral matrices, for each frequency bin.
+    freqs : ndarray, shape (n_freqs,)
+        The frequencies associated to cross-spectra.
+
+    References
+    ----------
+    .. [1] https://en.wikipedia.org/wiki/Cross-spectrum
+    """
+    Ne, Ns = X.shape
+    number_freqs = int(window / 2)
+
+    step = int((1.0 - overlap) * window)
+    step = max(1, step)
+
+    number_windows = int((Ns - window) / step + 1)
+    # pre-allocation of memory
+    fdata = np.zeros((number_windows, Ne, number_freqs), dtype=complex)
+    win = np.hanning(window)
+
+    # Loop on all frequencies
+    for window_ix in range(int(number_windows)):
+
+        # time markers to select the data
+        # marker of the beginning of the time window
+        t1 = int(window_ix * step)
+        # marker of the end of the time window
+        t2 = int(t1 + window)
+        # select current window and apodize it
+        cdata = X[:, t1:t2] * win
+
+        # FFT calculation
+        fdata[window_ix, :, :] = np.fft.fft(
+            cdata, n=window, axis=1)[:, 0:number_freqs]
+
+    # adjust frequency range to specified range (in case it is a parameter)
+    if fs is not None:
+        if fmin is None:
+            fmin = 0
+        if fmax is None:
+            fmax = fs / 2
+        if fmax <= fmin:
+            raise ValueError('Parameter fmax must be superior to fmin')
+        if 2.0 * fmax > fs:  # check Nyquist-Shannon
+            raise ValueError('Parameter fmax must be inferior to fs/2')
+        f = np.arange(0, 1, 1.0 / number_freqs) * (fs / 2.0)
+        Fix = (f >= fmin) & (f <= fmax)
+        fdata = fdata[:, :, Fix]
+        freqs = f[Fix]
+    else:
+        if fmin is not None:
+            warnings.warn('Parameter fmin not used because fs is None')
+        if fmax is not None:
+            warnings.warn('Parameter fmax not used because fs is None')
+        freqs = None
+
+    Nf = fdata.shape[2]
+    S = np.zeros((Ne, Ne, Nf), dtype=complex)
+    for i in range(Nf):
+        S[:, :, i] = np.dot(fdata[:, :, i].conj().T, fdata[:, :, i])
+    S /= number_windows * np.linalg.norm(win)**2
+
+    return S, freqs
+
+
+def cospectrum(X, window=128, overlap=0.75, fmin=None, fmax=None, fs=None):
+    """Compute co-spectral matrices, the real part of cross-spectra.
+
+    Parameters
+    ----------
+    X : ndarray, shape (n_channels, n_samples)
+        Multi-channel time-series.
+    window : int (default 128)
+        The length of the FFT window used for spectral estimation.
+    overlap : float (default 0.75)
+        The percentage of overlap between window.
+    fmin : float | None, (default None)
+        The minimal frequency to be returned.
+    fmax : float | None, (default None)
+        The maximal frequency to be returned.
+    fs : float | None, (default None)
+        The sampling frequency of the signal.
+
+    Returns
+    -------
+    S : ndarray, shape (n_channels, n_channels, n_freqs)
+        Co-spectral matrices, for each frequency bin.
+    freqs : ndarray, shape (n_freqs,)
+        The frequencies associated to cospectra.
+    """
+    S, freqs = cross_spectrum(
+        X=X,
+        window=window,
+        overlap=overlap,
+        fmin=fmin,
+        fmax=fmax,
+        fs=fs)
+
+    return S.real, freqs
+
+
+def coherence(X, window=128, overlap=0.75, fmin=None, fmax=None, fs=None):
+    """Compute coherence.
+
+    Parameters
+    ----------
+    X : ndarray, shape (n_channels, n_samples)
+        Multi-channel time-series.
+    window : int (default 128)
+        The length of the FFT window used for spectral estimation.
+    overlap : float (default 0.75)
+        The percentage of overlap between window.
+    fmin : float | None, (default None)
+        The minimal frequency to be returned.
+    fmax : float | None, (default None)
+        The maximal frequency to be returned.
+    fs : float | None, (default None)
+        The sampling frequency of the signal.
+
+    Returns
+    -------
+    C : ndarray, shape (n_channels, n_channels, n_freqs)
+        Coherence matrices, for each frequency bin.
+    """
+    S, _ = cross_spectrum(X, window, overlap, fmin, fmax, fs)
+    S2 = np.abs(S)**2  # squared cross-spectral modulus
+    C = np.zeros_like(S2)
+    for f in range(S2.shape[-1]):
+        psd = np.sqrt(np.diag(S2[..., f]))
+        C[..., f] = S2[..., f] / np.outer(psd, psd)
+    return C
+
+
+###############################################################################
+
+
+def normalize(X, norm):
+    """Normalize a set of square matrices, using trace or determinant.
+
+    Parameters
+    ----------
+    X : ndarray, shape (..., n, n)
+        The set of square matrices, at least 2D ndarray. Matrices must be
+        invertible for determinant-normalization.
+
+    norm : {"trace", "determinant"}
+        The type of normalization.
+
+    Returns
+    -------
+    Xn : ndarray, shape (..., n, n)
+        The set of normalized matrices, same dimensions as X.
+    """
+    if X.ndim < 2:
+        raise ValueError('Input must have at least 2 dimensions')
+    if X.shape[-2] != X.shape[-1]:
+        raise ValueError('Matrices must be square')
+
+    if norm == "trace":
+        denom = np.trace(X, axis1=-2, axis2=-1)
+    elif norm == "determinant":
+        denom = np.abs(np.linalg.det(X)) ** (1 / X.shape[-1])
+    else:
+        raise ValueError("'%s' is not a supported normalization" % norm)
+
+    while denom.ndim != X.ndim:
+        denom = denom[..., np.newaxis]
+    Xn = X / denom
+    return Xn
+
+
+def get_nondiag_weight(X):
+    """Compute non-diagonality weights of a set of square matrices, following
+    Eq(B.1) in [1]_.
+
+    Parameters
+    ----------
+    X : ndarray, shape (..., n, n)
+        The set of square matrices, at least 2D ndarray.
+
+    Returns
+    -------
+    weights : ndarray, shape (...,)
+        The non-diagonality weights for matrices.
+
+    References
+    ----------
+    .. [1] M. Congedo, C. Gouy-Pailler, C. Jutten, "On the blind source
+        separation of human electroencephalogram by approximate joint
+        diagonalization of second order statistics", Clin Neurophysiol, 2008
+    """
+    if X.ndim < 2:
+        raise ValueError('Input must have at least 2 dimensions')
+    if X.shape[-2] != X.shape[-1]:
+        raise ValueError('Matrices must be square')
+
+    X2 = X**2
+    # sum of squared diagonal elements
+    denom = np.trace(X2, axis1=-2, axis2=-1)
+    # sum of squared off-diagonal elements
+    num = np.sum(X2, axis=(-2, -1)) - denom
+    weights = (1.0 / (X.shape[-1] - 1)) * (num / denom)
+    return weights
---
+++ packages/pyriemann/pyriemann-0.2.7.tar.gz/pyriemann-0.2.7/pyriemann/utils/tangentspace.py
@@ -0,0 +1,72 @@
+import numpy as np
+
+from .base import sqrtm, invsqrtm, logm, expm
+from .mean import mean_covariance
+###############################################################
+# Tangent Space
+###############################################################
+
+
+def tangent_space(covmats, Cref):
+    """Project a set of covariance matrices in the tangent space. according to
+    the reference point Cref
+
+    :param covmats: np.ndarray
+        Covariance matrices set, Ntrials X Nchannels X Nchannels
+    :param Cref: np.ndarray
+        The reference covariance matrix
+    :returns: np.ndarray
+        the Tangent space , a matrix of Ntrials X (Nchannels*(Nchannels+1)/2)
+
+    """
+    Nt, Ne, Ne = covmats.shape
+    Cm12 = invsqrtm(Cref)
+    idx = np.triu_indices_from(Cref)
+    Nf = int(Ne * (Ne + 1) / 2)
+    T = np.empty((Nt, Nf))
+    coeffs = (np.sqrt(2) * np.triu(np.ones((Ne, Ne)), 1) +
+              np.eye(Ne))[idx]
+    for index in range(Nt):
+        tmp = np.dot(np.dot(Cm12, covmats[index, :, :]), Cm12)
+        tmp = logm(tmp)
+        T[index, :] = np.multiply(coeffs, tmp[idx])
+    return T
+
+
+def untangent_space(T, Cref):
+    """Project a set of Tangent space vectors back to the manifold.
+
+    :param T: np.ndarray
+        The Tangent space, a matrix of
+        n_trials X (n_channels * (n_channels + 1)/2)
+    :param Cref: np.ndarray
+        The reference covariance matrix
+
+    :returns: np.ndarray
+        A set of Covariance matrix, n_trials X n_channels X n_channels
+    """
+    Nt, Nd = T.shape
+    Ne = int((np.sqrt(1 + 8 * Nd) - 1) / 2)
+    C12 = sqrtm(Cref)
+
+    idx = np.triu_indices_from(Cref)
+    covmats = np.empty((Nt, Ne, Ne))
+    covmats[:, idx[0], idx[1]] = T
+    for i in range(Nt):
+        triuc = np.triu(covmats[i], 1) / np.sqrt(2)
+        covmats[i] = (np.diag(np.diag(covmats[i])) + triuc + triuc.T)
+        covmats[i] = expm(covmats[i])
+        covmats[i] = np.dot(np.dot(C12, covmats[i]), C12)
+
+    return covmats
+
+
+def transport(Covs, Cref, metric='riemann'):
+    """Parallel transport of two set of covariance matrix.
+
+    """
+    C = mean_covariance(Covs, metric=metric)
+    iC = invsqrtm(C)
+    E = sqrtm(np.dot(np.dot(iC, Cref), iC))
+    out = np.array([np.dot(np.dot(E, c), E.T) for c in Covs])
+    return out
