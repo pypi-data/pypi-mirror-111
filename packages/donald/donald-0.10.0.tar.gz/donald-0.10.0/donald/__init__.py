2e5687b7a2f15114993499d5d4f2718a020e81b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:09 +0000

    Add package donald/0.10.0/donald-0.10.0.tar.gz

---
+++ packages/donald/donald-0.10.0.tar.gz/donald-0.10.0/donald/__init__.py
@@ -0,0 +1,25 @@
+"""Donald tasks' manager for Asyncio."""
+import logging
+import asyncio
+
+
+AIOFALSE = asyncio.Future()
+AIOFALSE.set_result(False)
+AIOTRUE = asyncio.Future()
+AIOTRUE.set_result(True)
+
+
+logger = logging.getLogger(__name__)
+handle = logging.StreamHandler()
+handle.setFormatter(logging.Formatter(
+    '[%(asctime)-15s] [%(process)d] [%(levelname)s] %(message)s', '%Y-%m-%d %H:%M:%S'))
+logger.addHandler(handle)
+
+
+from .core import Donald, CronTab as crontab, run_donald # noqa
+
+
+__version__ = "0.10.0"
+__project__ = "Donald"
+__author__ = "Kirill Klenov <horneds@gmail.com>"
+__license__ = "BSD"
