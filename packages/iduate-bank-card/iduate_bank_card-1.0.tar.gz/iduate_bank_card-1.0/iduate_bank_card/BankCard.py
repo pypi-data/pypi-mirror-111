005a9ce0d4359b44119baa129042382d8e93222d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:42 +0000

    Add package iduate-bank-card/1.0/iduate_bank_card-1.0.tar.gz

---
+++ packages/iduate-bank-card/iduate_bank_card-1.0.tar.gz/iduate_bank_card-1.0/iduate_bank_card/BankCard.py
@@ -0,0 +1,60 @@
+class BankCard():
+    """ The purpose of BankCard class is for calculating checksum from card number and 
+    if it is divisible by 10 which would be a valid card number by luhn's algorithm.
+    
+    Attributes:
+        number (int) bank card number
+        checksum (int) representing the checksum of the bank card number
+        isValid (boolean) representing whether the bank card number is valid or not
+            
+    """
+    def __init__(self, num):
+        
+        self.number = num
+        self.checksum = self.luhn_checksum()
+        self.isValid = self.luhn_isValid()
+    
+    def luhn_checksum(self):
+    
+        """Method to calculate the checksum of the credit card number.
+        
+        Args: 
+            None
+        Returns: 
+            int: checksum of the bank card number
+    
+        """
+        
+        cc_num = []
+        results = []
+        num_str = str(self.number)
+
+        for i in range(len(num_str)):
+    
+            cc_num.append(num_str[i])
+            cc_num_r = cc_num[::-1]
+    
+        for i in range(len(cc_num_r)):
+    
+            if (i + 1) % 2 == 0:
+                result = int(cc_num_r[i]) * 2
+                results.append(sum(int(i) for i in str(result)))
+            else:
+                results.append(int(cc_num_r[i]))
+
+        checksum = sum(results)
+        
+        return checksum
+    
+    def luhn_isValid(self):
+    
+        """Method to calculate whether card number is valid by Luhn's algorithm.
+        
+        Args: 
+            None
+        Returns: 
+            boolean: True/False
+    
+        """
+        isValid = (self.checksum % 10 == 0)
+        return isValid
---
+++ packages/iduate-bank-card/iduate_bank_card-1.0.tar.gz/iduate_bank_card-1.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='iduate_bank_card',
+      version='1.0',
+      description='Bank Card - Luhn Algorithm',
+      packages=['iduate_bank_card'],
+      zip_safe=False)
