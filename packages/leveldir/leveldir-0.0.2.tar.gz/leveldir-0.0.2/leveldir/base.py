f7d049dad8126729e6654f55e89eb6543e933f51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:15:34 +0000

    Add package leveldir/0.0.2/leveldir-0.0.2.tar.gz

---
+++ packages/leveldir/leveldir-0.0.2.tar.gz/leveldir-0.0.2/leveldir/base.py
@@ -0,0 +1,45 @@
+# standard imports
+import os
+import stat
+
+
+class LevelDir:
+
+    def __init__(self, root_path, levels, entry_length):
+        self.path = root_path
+        self.levels = levels 
+        self.entry_length = entry_length
+        fi = None
+        try:
+            fi = os.stat(self.path)
+            self.__verify_directory()
+        except FileNotFoundError:
+            LevelDir.__prepare_directory(self.path)
+        self.master_file = os.path.join(self.path, 'master')
+
+
+    def __verify_directory(self):
+        fi = os.stat(self.path)
+        if not stat.S_ISDIR(fi.st_mode):
+            raise ValueError('{} is not a directory'.format(self.path))
+        #f = os.listdir(self.path)
+        #os.listdir(self.path)
+        #f.close()
+        return True
+
+
+    def count(self):
+        fi = os.stat(self.master_file)
+        c = fi.st_size / self.entry_length
+        r = int(c)
+        if r != c: # TODO: verify valid for check if evenly divided
+            raise IndexError('master file not aligned')
+        return r
+
+
+    @staticmethod
+    def __prepare_directory(path):
+        os.makedirs(path, exist_ok=True)
+        state_file = os.path.join(path, 'master')
+        f = open(state_file, 'w')
+        f.close()
