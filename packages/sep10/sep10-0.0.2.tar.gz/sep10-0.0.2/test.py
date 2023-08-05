9673c3f3719457803275e9a956174cbd6443d499
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:16:03 +0000

    Add package sep10/0.0.2/sep10-0.0.2.tar.gz

---
+++ packages/sep10/sep10-0.0.2.tar.gz/sep10-0.0.2/test.py
@@ -0,0 +1,13 @@
+from sep10 import Sep10
+
+from pathlib import Path
+
+
+secret = Path(".secrets").read_text()
+
+
+
+
+webauth = Sep10("TZS", "GA2MSSZKJOU6RNL3EJKH3S5TB5CDYTFQFWRYFGUJVIN5I6AOIRTLUHTO", secret)
+
+print(webauth.run_auth())
