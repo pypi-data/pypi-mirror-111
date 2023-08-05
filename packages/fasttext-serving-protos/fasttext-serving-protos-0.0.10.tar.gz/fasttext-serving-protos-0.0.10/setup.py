e2428707d542f7ca897455ebe969c531c2f23f5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:37 +0000

    Add package fasttext-serving-protos/0.0.10/fasttext-serving-protos-0.0.10.tar.gz

---
+++ packages/fasttext-serving-protos/fasttext-serving-protos-0.0.10.tar.gz/fasttext-serving-protos-0.0.10/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="fasttext-serving-protos",
+    version="0.0.10",
+    author="NielsenIQ",
+    author_email="gonzalo.delafuente.consultant@nielseniq.com",
+    description=" FastText Serving Protocol Bufers Python implementation",
+    packages=["fasttext_serving_protos"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.0",
+)
