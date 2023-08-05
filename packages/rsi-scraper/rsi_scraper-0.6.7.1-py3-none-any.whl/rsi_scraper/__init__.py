d496bc860dd0f8525d30cbfcaf261f83d4d0b887
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:45 +0000

    Add package rsi-scraper/0.6.7.1/rsi_scraper-0.6.7.1-py3-none-any.whl

---
+++ packages/rsi-scraper/rsi_scraper-0.6.7.1-py3-none-any.whl/rsi_scraper/__init__.py
@@ -0,0 +1,32 @@
+from .connector import Connector
+from .interface import ICommand
+from .organization import Organization, OrganizationMembers
+from .progress_tracker import ProgressTracker, ProgressTrackerInfo
+from .roadmap import Roadmap
+from .ship import Ship
+from .starmap import (
+    StarmapSystems, StarmapTunnels, StarmapSpecies,
+    StarmapAffiliations, StarmapStarSystems,
+    StarmapCelestialObjects, StarmapSearch)
+from .stats import Stats
+from .telemetry import Telemetry
+from .user import User
+from .version import Version
+
+__all__ = [
+    'Connector',
+    'ICommand',
+    'Organization', 'OrganizationMembers',
+    'ProgressTracker', 'ProgressTrackerInfo',
+    'Roadmap',
+    'Ship',
+    'StarmapSystems', 'StarmapTunnels', 'StarmapSpecies',
+    'StarmapAffiliations', 'StarmapStarSystems',
+    'StarmapCelestialObjects', 'StarmapSearch',
+    'Stats',
+    'Telemetry',
+    'User',
+    'Version',
+]
+
+__version__ = '0.6.7.1'
---
+++ packages/rsi-scraper/rsi_scraper-0.6.7.1-py3-none-any.whl/tests/test_user.py
@@ -0,0 +1,13 @@
+import pytest
+from .validator import assert_valid_schema
+
+
+@pytest.mark.user
+def test_request():
+    from rsi_scraper import User
+
+    object = User('dymerz')
+    data = object.execute()
+    assert data is not None
+
+    assert_valid_schema(data, 'user.json')
