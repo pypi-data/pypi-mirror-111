dffa930a1309fd0453afc6cbf6f99647af6eb727
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:33 +0000

    Add package csvtea-elunico/0.1.0/csvtea_elunico-0.1.0-py3-none-any.whl

---
+++ packages/csvtea-elunico/csvtea_elunico-0.1.0-py3-none-any.whl/csvtea/csvparser.py
@@ -0,0 +1,27 @@
+from typing import TextIO
+
+from cells import DefaultCellParser, CellParser
+from format import DefaultCellFormatter, CellFormatter
+
+
+class CSVParser:
+    def __init__(self, file: TextIO, parser: CellParser = DefaultCellParser(),
+                 formatter: CellFormatter = DefaultCellFormatter()):
+        self.parser = parser
+        self.formatter = formatter
+
+        self.text = file.read().strip('\r\n') + '\0'
+
+    def parse(self) -> list[list[str]]:
+        cells = []
+        row = []
+        text = self.text
+        while text:
+            cell, cell_end, row_done = self.parser.parse(text)
+            cell = self.formatter.format(cell)
+            row.append(cell)
+            if row_done:
+                cells.append(row)
+                row = []
+            text = text[cell_end:]
+        return cells
