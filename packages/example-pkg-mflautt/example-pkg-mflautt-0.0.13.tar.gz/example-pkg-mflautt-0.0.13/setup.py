098bcc3345bed153207e5b2c633b4f0804c86d81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:05 +0000

    Add package example-pkg-mflautt/0.0.13/example-pkg-mflautt-0.0.13.tar.gz

---
+++ packages/example-pkg-mflautt/example-pkg-mflautt-0.0.13.tar.gz/example-pkg-mflautt-0.0.13/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.13'
+DESCRIPTION = 'Test package'
+LONG_DESCRIPTION = 'A test package demoing use of PyPI and JFROG'
+
+# Setting up
+setup(
+    name="example-pkg-mflautt",
+    version=VERSION,
+    author="Mitchell Flautt",
+    author_email="<mitchell.flautt@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
