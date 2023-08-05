155fbbccaefc8ffa6ea84977c7cd805827f988b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:01 +0000

    Add package lunchpy/1.0.0b1/lunchpy-1.0.0b1.tar.gz

---
+++ packages/lunchpy/lunchpy-1.0.0b1.tar.gz/lunchpy-1.0.0b1/lunchpy/__init__.py
@@ -0,0 +1,10 @@
+"""
+lunchpy API wrapper for the Lunch Money API
+"""
+
+__title__ = 'lunchpy'
+__version__ = '1.0.0-beta.1'
+__author__ = 'Vyryn'
+__license__ = 'GNU General Public License v3 (GPLv3)'
+
+from .eat import Eat
---
+++ packages/lunchpy/lunchpy-1.0.0b1.tar.gz/lunchpy-1.0.0b1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+from lunchpy import __version__, __title__, __author__, __license__
+
+setup(
+    name=__title__,
+    version=__version__,
+    author=__author__,
+    author_email=f'{__author__}@protonmail.com',
+    url=f'https://github.com/{__author__}/{__title__}',
+    description='An API wrapper for Lunch Money',
+    packages=[__title__],
+    install_requires=[
+        'requests~=2.25.1',
+    ],
+    license=__license__,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Environment :: Console',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3.9',
+    ]
+)
