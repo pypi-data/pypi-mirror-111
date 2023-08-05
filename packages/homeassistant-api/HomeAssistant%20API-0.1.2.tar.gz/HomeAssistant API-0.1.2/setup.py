884ddd3dd908ff721f55e3039bbaefdcb40e9863
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:56 +0000

    Add package homeassistant-api/0.1.2/HomeAssistant%20API-0.1.2.tar.gz

---
+++ packages/homeassistant-api/HomeAssistant%20API-0.1.2.tar.gz/HomeAssistant API-0.1.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+with open('README.md', 'r') as f:
+    read = f.read()
+
+
+setup(
+    name="HomeAssistant API",
+    description="Python Wrapper for Homeassistant's REST API",
+    version='0.1.2',
+    packages=[
+        'homeassistant_api',
+        'homeassistant_api.servicedomains',
+        'homeassistant_api.states',
+        'homeassistant_api.models'
+    ],
+    install_requires=['requests'],
+    long_description=read,
+    long_description_content_type='text/markdown'
+)
