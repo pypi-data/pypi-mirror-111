1a4bc92d51af51ca0b4e49be7ca45e6da9c131c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:24 +0000

    Add package iduate-bank-card/4.0/iduate_bank_card-4.0.tar.gz

---
+++ packages/iduate-bank-card/iduate_bank_card-4.0.tar.gz/iduate_bank_card-4.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='iduate_bank_card',
+      version='4.0',
+      description='Bank Card - Luhn Algorithm',
+      packages=['bank_card'],
+      zip_safe=False)
