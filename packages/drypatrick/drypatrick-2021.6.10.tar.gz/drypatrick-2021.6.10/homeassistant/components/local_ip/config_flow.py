045eeb514f09340fbfe64eef4ae010db217a6b94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:03 +0000

    Add package drypatrick/2021.6.10/drypatrick-2021.6.10.tar.gz

---
+++ packages/drypatrick/drypatrick-2021.6.10.tar.gz/drypatrick-2021.6.10/homeassistant/components/local_ip/config_flow.py
@@ -0,0 +1,22 @@
+"""Config flow for local_ip."""
+
+from homeassistant import config_entries
+
+from .const import DOMAIN
+
+
+class SimpleConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for local_ip."""
+
+    VERSION = 1
+
+    async def async_step_user(self, user_input=None):
+        """Handle the initial step."""
+
+        if self._async_current_entries():
+            return self.async_abort(reason="single_instance_allowed")
+
+        if user_input is None:
+            return self.async_show_form(step_id="user")
+
+        return self.async_create_entry(title=DOMAIN, data=user_input)
