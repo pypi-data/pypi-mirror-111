83e9acec21493145b89dc79eeab4e57a98e8a838
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:09 +0000

    Add package odoo12-addon-sm-partago-usage/12.0.0.0.3/odoo12_addon_sm_partago_usage-12.0.0.0.3-py2.py3-none-any.whl

---
+++ packages/odoo12-addon-sm-partago-usage/odoo12_addon_sm_partago_usage-12.0.0.0.3-py2.py3-none-any.whl/odoo/addons/sm_partago_usage/models/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+from . import models_smp_reservation_compute
+from . import models_smp_reservation_wizard
+from . import models_smp_edit_reservation_compute_wizard
+from . import models_smp_usage_cron
