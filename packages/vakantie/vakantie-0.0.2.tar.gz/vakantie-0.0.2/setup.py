ef0c5525d200f3d9a8773d2e27c48295d8885bf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:27 +0000

    Add package vakantie/0.0.2/vakantie-0.0.2.tar.gz

---
+++ packages/vakantie/vakantie-0.0.2.tar.gz/vakantie-0.0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+
+setuptools.setup(
+    name='vakantie',
+    version='0.0.2',
+    # scripts=['yearlyholidays'],
+    author="Labiba Kanij Rupty",
+    author_email="labibakanij@gmail.com",
+    description="A Package to get all holidays starting from 2015",
+    long_description=open('README.md').read() + '\n\n' + open('CHANGELOG.txt').read(),
+    License='MIT',
+    long_description_content_type="text/markdown",
+    # url="https://github.com/rupaai/happydays",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['pandas','beautifulsoup4','requests', 'lxml']
+)
