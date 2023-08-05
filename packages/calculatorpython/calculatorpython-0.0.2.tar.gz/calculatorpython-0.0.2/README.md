c0b2592d3c80832018fd7a0b81a1b144666caea7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:27 +0000

    Add package calculatorpython/0.0.2/calculatorpython-0.0.2.tar.gz

---
+++ packages/calculatorpython/calculatorpython-0.0.2.tar.gz/calculatorpython-0.0.2/README.md
@@ -0,0 +1,10 @@
+Release 0.0.2 (calculatorpython):
+    -Added functionality to operate custom numbers to any number.
+
+This package is calculatorpython. You can import it by saying for example
+'from calculatorpython import add'
+Hope you like this release of calculatorpython.
+Thanks,
+Advait,
+Author,
+calculatorpython
---
+++ packages/calculatorpython/calculatorpython-0.0.2.tar.gz/calculatorpython-0.0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+VERSION = '0.0.2'
+DESCRIPTION = 'Calculator with simple operations'
+LONG_DESCRIPTION = 'Note: Argument one in every attribute in operated on with Argument one.'
+
+
+# Setting up
+setup(
+    name="calculatorpython",
+    version=VERSION,
+    author="Advait Shiralkar",
+    author_email="advaitshiralkar2@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description= LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['Calculator'],
+    keywords=[''],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
