120de5dbfb87678ae47f3f0a143a61cd75542cea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:14:42 +0000

    Add package avocado-price-predictor-model/0.0.5/avocado_price_predictor_model-0.0.5-py3-none-any.whl

---
+++ packages/avocado-price-predictor-model/avocado_price_predictor_model-0.0.5-py3-none-any.whl/tests/tests.py
@@ -0,0 +1,51 @@
+import pytest
+import avocado_price_predictor_model.predict as predictor
+
+
+@pytest.fixture
+def data():
+    return {
+        'sold_plu_4046': 5,
+        'sold_plu_4225': 6,
+        'sold_plu_4770': 8,
+        'small_bags': 4,
+        'large_bags': 0,
+        'xlarge_bags': 0,
+        'organic': True,
+        'region': 'Albany',
+        'date': '2020-12-27'
+    }
+
+
+def test_prediction_ok(data):
+    y = predictor.predict(data)
+    assert type(y) == float
+
+
+def test_prediction_empty_data():
+    y = predictor.predict({})
+    assert y == 0
+
+
+def test_prediction_missing_data(data):
+    del data['small_bags']
+    y = predictor.predict(data)
+    assert y == 0
+
+
+def test_prediction_invalid_date(data):
+    data['date'] = 'invalid'
+    y = predictor.predict(data)
+    assert y == 0
+
+
+def test_prediction_invalid_type(data):
+    data['organic'] = 'invalid'
+    y = predictor.predict(data)
+    assert y == 0
+
+
+def test_prediction_invalid_region(data):
+    data['region'] = 'invalid'
+    y = predictor.predict(data)
+    assert y == 0
