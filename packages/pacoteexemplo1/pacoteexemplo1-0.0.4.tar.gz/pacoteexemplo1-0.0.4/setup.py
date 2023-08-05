fa8d5c8d1688a1c1dc6e60ff86cf238dc0f58e82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:15:07 +0000

    Add package pacoteexemplo1/0.0.4/pacoteexemplo1-0.0.4.tar.gz

---
+++ packages/pacoteexemplo1/pacoteexemplo1-0.0.4.tar.gz/pacoteexemplo1-0.0.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    readme = fh.read()
+
+setup(name='pacoteexemplo1',
+    version='0.0.4',
+    license='MIT License',
+    author='Fábio Minutti teixeira',
+    long_description=readme,
+    author_email='fabiomt92@hotmail.com',
+    keywords='Pacote',
+    description=u'Exemplo de pacote PyPI',
+    packages=['pacoteexemplo1'],
+    install_requires=['numpy'],)
+
+
+    
