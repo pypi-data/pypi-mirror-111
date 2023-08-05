060ed6e4013854ed0f91e55172c02158b12f87ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:02 +0000

    Add package mypy-boto3-amp/1.17.101.post1/mypy-boto3-amp-1.17.101.post1.tar.gz

---
+++ packages/mypy-boto3-amp/mypy-boto3-amp-1.17.101.post1.tar.gz/mypy-boto3-amp-1.17.101.post1/mypy_boto3_amp/type_defs.py
@@ -0,0 +1,210 @@
+"""
+Type annotations for amp service type definitions.
+
+[Open documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_amp/type_defs.html)
+
+Usage::
+
+    ```python
+    from mypy_boto3_amp.type_defs import CreateWorkspaceRequestTypeDef
+
+    data: CreateWorkspaceRequestTypeDef = {...}
+    ```
+"""
+import sys
+from datetime import datetime
+from typing import Any, Dict, List
+
+from .literals import WorkspaceStatusCodeType
+
+if sys.version_info >= (3, 8):
+    from typing import TypedDict
+else:
+    from typing_extensions import TypedDict
+
+
+__all__ = (
+    "CreateWorkspaceRequestTypeDef",
+    "CreateWorkspaceResponseResponseTypeDef",
+    "DeleteWorkspaceRequestTypeDef",
+    "DescribeWorkspaceRequestTypeDef",
+    "DescribeWorkspaceResponseResponseTypeDef",
+    "ListWorkspacesRequestTypeDef",
+    "ListWorkspacesResponseResponseTypeDef",
+    "PaginatorConfigTypeDef",
+    "ResponseMetadataTypeDef",
+    "UpdateWorkspaceAliasRequestTypeDef",
+    "WorkspaceDescriptionTypeDef",
+    "WorkspaceStatusTypeDef",
+    "WorkspaceSummaryTypeDef",
+)
+
+CreateWorkspaceRequestTypeDef = TypedDict(
+    "CreateWorkspaceRequestTypeDef",
+    {
+        "alias": str,
+        "clientToken": str,
+    },
+    total=False,
+)
+
+CreateWorkspaceResponseResponseTypeDef = TypedDict(
+    "CreateWorkspaceResponseResponseTypeDef",
+    {
+        "arn": str,
+        "status": "WorkspaceStatusTypeDef",
+        "workspaceId": str,
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+_RequiredDeleteWorkspaceRequestTypeDef = TypedDict(
+    "_RequiredDeleteWorkspaceRequestTypeDef",
+    {
+        "workspaceId": str,
+    },
+)
+_OptionalDeleteWorkspaceRequestTypeDef = TypedDict(
+    "_OptionalDeleteWorkspaceRequestTypeDef",
+    {
+        "clientToken": str,
+    },
+    total=False,
+)
+
+
+class DeleteWorkspaceRequestTypeDef(
+    _RequiredDeleteWorkspaceRequestTypeDef, _OptionalDeleteWorkspaceRequestTypeDef
+):
+    pass
+
+
+DescribeWorkspaceRequestTypeDef = TypedDict(
+    "DescribeWorkspaceRequestTypeDef",
+    {
+        "workspaceId": str,
+    },
+)
+
+DescribeWorkspaceResponseResponseTypeDef = TypedDict(
+    "DescribeWorkspaceResponseResponseTypeDef",
+    {
+        "workspace": "WorkspaceDescriptionTypeDef",
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+ListWorkspacesRequestTypeDef = TypedDict(
+    "ListWorkspacesRequestTypeDef",
+    {
+        "alias": str,
+        "maxResults": int,
+        "nextToken": str,
+    },
+    total=False,
+)
+
+ListWorkspacesResponseResponseTypeDef = TypedDict(
+    "ListWorkspacesResponseResponseTypeDef",
+    {
+        "nextToken": str,
+        "workspaces": List["WorkspaceSummaryTypeDef"],
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+PaginatorConfigTypeDef = TypedDict(
+    "PaginatorConfigTypeDef",
+    {
+        "MaxItems": int,
+        "PageSize": int,
+        "StartingToken": str,
+    },
+    total=False,
+)
+
+ResponseMetadataTypeDef = TypedDict(
+    "ResponseMetadataTypeDef",
+    {
+        "RequestId": str,
+        "HostId": str,
+        "HTTPStatusCode": int,
+        "HTTPHeaders": Dict[str, Any],
+        "RetryAttempts": int,
+    },
+)
+
+_RequiredUpdateWorkspaceAliasRequestTypeDef = TypedDict(
+    "_RequiredUpdateWorkspaceAliasRequestTypeDef",
+    {
+        "workspaceId": str,
+    },
+)
+_OptionalUpdateWorkspaceAliasRequestTypeDef = TypedDict(
+    "_OptionalUpdateWorkspaceAliasRequestTypeDef",
+    {
+        "alias": str,
+        "clientToken": str,
+    },
+    total=False,
+)
+
+
+class UpdateWorkspaceAliasRequestTypeDef(
+    _RequiredUpdateWorkspaceAliasRequestTypeDef, _OptionalUpdateWorkspaceAliasRequestTypeDef
+):
+    pass
+
+
+_RequiredWorkspaceDescriptionTypeDef = TypedDict(
+    "_RequiredWorkspaceDescriptionTypeDef",
+    {
+        "arn": str,
+        "createdAt": datetime,
+        "status": "WorkspaceStatusTypeDef",
+        "workspaceId": str,
+    },
+)
+_OptionalWorkspaceDescriptionTypeDef = TypedDict(
+    "_OptionalWorkspaceDescriptionTypeDef",
+    {
+        "alias": str,
+        "prometheusEndpoint": str,
+    },
+    total=False,
+)
+
+
+class WorkspaceDescriptionTypeDef(
+    _RequiredWorkspaceDescriptionTypeDef, _OptionalWorkspaceDescriptionTypeDef
+):
+    pass
+
+
+WorkspaceStatusTypeDef = TypedDict(
+    "WorkspaceStatusTypeDef",
+    {
+        "statusCode": WorkspaceStatusCodeType,
+    },
+)
+
+_RequiredWorkspaceSummaryTypeDef = TypedDict(
+    "_RequiredWorkspaceSummaryTypeDef",
+    {
+        "arn": str,
+        "createdAt": datetime,
+        "status": "WorkspaceStatusTypeDef",
+        "workspaceId": str,
+    },
+)
+_OptionalWorkspaceSummaryTypeDef = TypedDict(
+    "_OptionalWorkspaceSummaryTypeDef",
+    {
+        "alias": str,
+    },
+    total=False,
+)
+
+
+class WorkspaceSummaryTypeDef(_RequiredWorkspaceSummaryTypeDef, _OptionalWorkspaceSummaryTypeDef):
+    pass
