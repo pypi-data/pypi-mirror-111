32531b2e9ec498fd2773af82b3a019c9c8e2e7d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:49 +0000

    Add package LineGraph/0.0.1/LineGraph-0.0.1-py3-none-any.whl

---
+++ packages/LineGraph/LineGraph-0.0.1-py3-none-any.whl/graph/line draw.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+x = [7, 8, 9]
+y = [8, 5, 4]
+plt.plot(x, y)
+plt.xlabel('x - axis')
+plt.ylabel('y - axis')
+plt.title('Line Draw')
+plt.show()
---
+++ packages/LineGraph/LineGraph-0.0.1-py3-none-any.whl/graph/point plot.py
@@ -0,0 +1,5 @@
+from matplotlib import pyplot as plt
+x = [7, 5, 8, 1, 7]
+y = [5, 6, 8, 9, 2]
+plt.scatter(x, y)
+plt.show()
