c8cd4a0704b943dd00eb10496facd19e52904281
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:34 +0000

    Add package model-regresi-harga-rumah/2.1.1/model-regresi-harga-rumah-2.1.1.tar.gz

---
+++ packages/model-regresi-harga-rumah/model-regresi-harga-rumah-2.1.1.tar.gz/model-regresi-harga-rumah-2.1.1/regression_model/train_pipeline.py
@@ -0,0 +1,32 @@
+import numpy as np
+from config.core import config
+from pipeline import price_pipe
+from processing.data_manager import load_dataset, save_pipeline
+from sklearn.model_selection import train_test_split
+
+
+def run_training() -> None:
+    """Mentraining model kita"""
+
+    # Membaca dataset train kita
+    data = load_dataset(file_name=config.app_config.training_data_file)
+
+    # Membagi training dan test set (menggunakan config)
+    X_train, X_test, y_train, y_test = train_test_split(
+        data[config.model_config.features],  # predictors
+        data[config.model_config.target],
+        test_size=config.model_config.test_size,
+        # Menentukan random seed agar bisa di reproduce (hasilnya sama)
+        random_state=config.model_config.random_state,
+    )
+    y_train = np.log(y_train)
+
+    # fit model kita
+    price_pipe.fit(X_train, y_train)
+
+    # Menyimpan (persist) model yang sudah ditraining
+    save_pipeline(pipeline_to_persist=price_pipe)
+
+
+if __name__ == "__main__":
+    run_training()
