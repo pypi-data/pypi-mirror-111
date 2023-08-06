634679964c9852084a739e5850a14666bae21b48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:17:46 +0000

    Add package QB-solver/0.1.4.7.4/QB-solver-0.1.4.7.4.tar.gz

---
+++ packages/QB-solver/QB-solver-0.1.4.7.4.tar.gz/QB-solver-0.1.4.7.4/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+	name="QB-solver",
+	version="0.1.4.7.4",
+	packages=["qb_solver",],	# name of modules
+	url="https://github.com/a-penton/QB",
+	# license
+	author="Andrew Penton, Heinrich Perez, Steven Perez, Noah Sharpe, Daniel Shinkarow",
+	# author_email
+	description="Rubik's cube simulation and tutorial",
+	install_requires=["ursina", "rubik-cube", "psd-tools3", "os"],
+
+	entry_points = 
+	{ "console_scripts":
+		[
+			"run_simulation=qb_solver.main:main",
+			#"command_name_2 = src.file_name:func_name"
+		]
+	}
+)
