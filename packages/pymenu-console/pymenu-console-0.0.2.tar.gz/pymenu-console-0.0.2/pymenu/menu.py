f6e920af7d5e030e2e9000f58a0258b74f8e68f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:30 +0000

    Add package pymenu-console/0.0.2/pymenu-console-0.0.2.tar.gz

---
+++ packages/pymenu-console/pymenu-console-0.0.2.tar.gz/pymenu-console-0.0.2/pymenu/menu.py
@@ -0,0 +1,64 @@
+import os
+from typing import Callable, List, Tuple
+
+import keyboard
+
+from pymenu.option import Option
+
+
+class Menu:
+    def __init__(self, title: str, options: List[Option] = []) -> None:
+        self.title = title
+        self.options = options
+        self.__selected_index: int = 0
+
+    def add_option(self, option: Tuple[str, Callable]):
+        name, call = option
+        self.options.append(Option(name, call))
+
+    def add_options(self, options: List[Tuple[str, Callable]]):
+        for option in options:
+            self.add_option(option)
+
+    def show(self):
+        self.__update()
+        keyboard.add_hotkey('up', self.up)
+        keyboard.add_hotkey('down', self.down)
+        self.wait_for_command()
+        self.run_selected()
+
+    def clear(self):
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+    def __update(self):
+        self.clear()
+        for i, script in enumerate(self.options):
+            if i == self.__selected_index:
+                try:
+                    print('\033[96m', end='')
+                    print(i + 1, '-', script)
+                finally:
+                    print('\033[0;0m', end='')
+            else:
+                print(i + 1, '-', script)
+
+    def up(self):
+        if self.__selected_index > 0:
+            self.__selected_index -= 1
+            self.__update()
+
+    def down(self):
+        if self.__selected_index < len(self.options) - 1:
+            self.__selected_index += 1
+            self.__update()
+
+    def wait_for_command(self):
+        command = input()
+        if command.isnumeric():
+            option_number = int(command)
+            self.__selected_index = option_number - 1
+
+    def run_selected(self):
+        self.clear()
+        option = self.options[self.__selected_index]
+        option()
