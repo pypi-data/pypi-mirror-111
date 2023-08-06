b8c249adb150f75589e57e7bf945774b3fd0c5c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:36:53 +0000

    Add package datazen/1.7.4/datazen-1.7.4.tar.gz

---
+++ packages/datazen/datazen-1.7.4.tar.gz/datazen-1.7.4/tests/test_parsing.py
@@ -0,0 +1,28 @@
+"""
+datazen - Test functions in the 'parsing' module.
+"""
+
+# built-in
+import os
+
+# module under test
+from datazen.parsing import merge, load
+
+# internal
+from .resources import get_resource
+
+
+def test_bad_overwrite():
+    """Test that if we don't want to overwrite, we don't."""
+
+    dict_a = {"a": "a"}
+    dict_b = {"a": "b"}
+    assert merge(dict_a, dict_b) == dict_a
+    assert merge(dict_a, dict_b, expect_overwrite=True) == dict_b
+
+
+def test_load_bad_template():
+    """Test that loading invalid templates returns correctly."""
+
+    template = get_resource(os.path.join("templates", "bad.j2"), False)
+    assert load(template, {}, {}) == ({}, False)
