8fe48a553eb8baf9083697d41d11a95cf399ad0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:54 +0000

    Add package random-mitype/0.1.0/random_mitype-0.1.0.tar.gz

---
+++ packages/random-mitype/random_mitype-0.1.0.tar.gz/random_mitype-0.1.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+# exec(open("random_mitype/version.py").read())
+__version__ = "0.1.0"
+
+setup(
+    name="random_mitype",
+    version=__version__,
+    py_modules=find_packages(),
+    include_package_data=True,
+    description="Generate random typing tests for mitype.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Cyrus Yip",
+    install_requires=["Click", "mitype"],
+    entry_points={"console_scripts": ["random_mitype=random_mitype.random_mitype:cli", ], },
+    license="MIT",
+    url="https://github.com/RealCyGuy/random-mitype",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Natural Language :: English",
+        "Topic :: Terminals",
+        "Environment :: Console :: Curses",
+        "Development Status :: 4 - Beta",
+    ],
+)
