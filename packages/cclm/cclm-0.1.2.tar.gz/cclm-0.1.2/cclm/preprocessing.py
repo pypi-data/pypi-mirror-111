f07d5d1f05cf11216a297d582c5ffd2874717efa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:59 +0000

    Add package cclm/0.1.2/cclm-0.1.2.tar.gz

---
+++ packages/cclm/cclm-0.1.2.tar.gz/cclm-0.1.2/cclm/preprocessing.py
@@ -0,0 +1,202 @@
+from tqdm import tqdm
+import numpy as np
+from tokenizers import Encoding, Tokenizer
+from tokenizers.models import BPE
+from tokenizers.trainers import BpeTrainer
+from tokenizers.pre_tokenizers import Whitespace
+from tokenizers.normalizers import Lowercase, NFD, StripAccents, Sequence
+from typing import List, Union
+from datasets.arrow_dataset import Dataset
+import json
+import os
+import re
+from collections import Counter
+from typing import Dict, List, Iterable, Optional
+
+PUNCT = "~!@#$%^&*()-+=_.,?\":;'"
+
+
+class Preprocessor:
+    def __init__(
+        self,
+        load_from: str = None,
+        vocab_size: int = 10000,
+        max_example_len: int = 512,
+        downsample_factor: int = 1,
+        add_cls: bool = True,
+    ):
+        """A Preprocessor is responsible for storing the map between
+        characters and integers for embedding. It vectorizes text
+        using that map
+
+        Args:
+            load_from (str, optional): path from which to load Preprocessor. Defaults to None.
+            vocab_size (int, optional): maximum number of characters in vocabulary. Defaults to 10000.
+            max_example_len (int, optional): maximum length of an input. Defaults to 512.
+            downsample_factor (int, optional): factor to ensure downsampling works; will pad/truncate to ensure `len(vector) % self.downsample_len == 0`. Defaults to 1.
+            add_cls (bool, optional): whether or not to add a [CLS] token to the end of vectors. Defaults to True.
+        """
+        self.char_dict: Dict[str, int] = {}
+        self.char_rev: Dict[int, str] = {}
+        self.token_dict: Dict[str, int] = {}
+        self.token_rev: Dict[str, int] = {}
+        self.vocab_size = vocab_size
+        self.max_example_len = max_example_len
+        self.add_cls = add_cls
+        # some default values
+        self.mask_token_ind = 1
+        self.unk_token_ind = 2
+        self.cls_token_ind = 3
+
+        self.downsample_factor = downsample_factor
+        self.tokenizer_is_fit = False
+        self.tokenizer = Tokenizer(BPE(unk_token="[UNK]"))
+        self.tokenizer.pre_tokenizer = Whitespace()
+        self.tokenizer.normalizer = Sequence([NFD(), Lowercase(), StripAccents()])
+        self.tok_trainer = BpeTrainer(
+            special_tokens=["[UNK]", "[MASK]", "[CLS]"], vocab_size=self.vocab_size
+        )
+        if load_from:
+            self._load(load_from)
+
+    def fit(
+        self,
+        data: Union[List[str], Dataset],
+        min_char_count: int = 1,
+        skip_tokenizer: bool = False,
+        progbar: bool = True,
+    ):
+        """
+        Create a character-level dictionary based on a list of strings and train
+        the tokenizer on the dataset. Training the tokenizer allows for using it
+        in the language modeling objective
+        """
+        is_dataset = isinstance(data, Dataset)
+        if not self.tokenizer_is_fit and not skip_tokenizer:
+            print("fitting tokenizer")
+            self.tokenizer.train_from_iterator(data, trainer=self.tok_trainer)
+            self.tokenizer_is_fit = True
+        char_counter: Counter = Counter()
+        token_counter: Counter = Counter()
+        iterator_: Iterable = data
+        if progbar:
+            iterator_ = tqdm(data)
+        print("fitting char_dict")
+        for example in iterator_:
+            if is_dataset:
+                example = example["text"]
+            chars = Counter(example)
+            for char, char_count in chars.items():
+                try:
+                    char_counter[char] += char_count
+                except KeyError:
+                    char_counter[char] = char_count
+
+        counts = [k for k, v in char_counter.items() if v >= min_char_count]
+
+        self.char_rev = {
+            0: "",
+            self.mask_token_ind: "_",
+            self.cls_token_ind: "[CLS]",
+            self.unk_token_ind: "",
+        }
+
+        for c in sorted(counts):
+            n = len(self.char_rev)
+            self.char_rev[n] = c
+            self.char_dict[c] = n
+
+    def tokenize(self, string_to_tokenize: str) -> Encoding:
+        string_to_tokenize = string_to_tokenize
+        return self.tokenizer.encode(string_to_tokenize)
+
+    def string_to_array(
+        self, string_in: str, length: Optional[int] = None, use_max_len: bool = True
+    ):
+        """Turn a string into an array[int]. If length specified, will
+        truncate to that length. Otherwise, the function will infer
+        the proper length. If the Preprocessor has .add_cls == True,
+        then it will also add an int to the end representing [CLS]
+
+        Args:
+            string_in (str): string to vectorize
+            length (Optional[int], optional): specific length of the output. Defaults to None.
+            use_max_len (bool): whether to enforce a Preprocessor.max_example_len as maximum
+
+        Returns:
+            [type]: [description]
+        """
+        len_str = len(string_in)
+        specific_length = True
+        clip_char = False
+        if length is None:
+            specific_length = False
+            length = self.infer_length(string_in, use_max_len)
+
+        # we may need to clip an extra character to make room for [CLS]
+        if self.add_cls and (len_str + 1 * self.add_cls) >= (length):
+            clip_char = True
+        # truncate
+        s = string_in[: (length - 1 * clip_char)]
+        # map char -> int and left-zero-pad
+        mapped = np.zeros((length))
+        for n, char in enumerate(s):
+            try:
+                mapped[n] = self.char_dict[char]
+            except KeyError:
+                pass
+
+        # if we're adding the CLS token,
+        if self.add_cls:
+            mapped[-1] = self.cls_token_ind
+
+        return mapped
+
+    def infer_length(self, inp: str, use_max_len=True) -> int:
+        """infer the length for string_to_array. If use_max_len, honor the maximum length
+        from self.max_example_len. Otherwise, just take into account the necessary
+        downsample_factor and add_cls
+
+        Args:
+            inp (str): input string
+            use_max_len (bool, optional): Whether to honor self.max_example_len. Defaults to True.
+
+        Returns:
+            int: [description]
+        """
+        l = len(inp) + 1 * self.add_cls
+        # possibly enforce max len, possibly truncate to a multiple of downsample_factor
+        if use_max_len and l >= self.max_example_len:
+            return l - (l % self.downsample_factor)
+        # otherwise just enforce downsample_factor
+        mod = l % self.downsample_factor
+        if mod > 0:
+            l += self.downsample_factor - mod
+        return l
+
+    def save(self, path: str):
+        """
+        Write a Preprocessor object to a .JSON config
+        """
+        config = {
+            "char_rev": self.char_rev,
+            "char_dict": self.char_dict,
+            "max_example_len": self.max_example_len,
+        }
+        with open(os.path.join(path, "cclm_config.json"), "w") as f:
+            json.dump(config, f)
+
+    def _load(self, path: str):
+        """
+        Load a Preprocessor object from disk
+        """
+        with open(path, "rb") as f:
+            result = json.load(f)
+        for key, value in result.items():
+            setattr(self, key, value)
+        for key, value in list(self.char_rev.items()):
+            self.char_rev[int(key)] = value
+
+    @property
+    def n_chars(self):
+        return np.max(list(self.char_dict.values())) + 1
---
+++ packages/cclm/cclm-0.1.2.tar.gz/cclm-0.1.2/readme.md
@@ -0,0 +1,96 @@
+## CCLM
+
+### Composable, Character-Level Models
+
+#### Why `cclm`?
+
+
+The goal of `cclm` is to make the deep learning model development process modular by providing abstractions for structuring a computational graph.
+
+If we think of the ML lifecycle as producing a usable class `Model` that consumers can call on `input` to get `output`, then comparing the model training process to human-led software development highlights some big differences. For instance often when we retrain models, we usually change the whole model at once - imagine a developer telling you every commit they made touched every line of code in the package. Similarly, using a pretrained model is like using a 'batteries included' framework: you likely end up inheriting a good deal of functionality you don't require, and it may be hard to customize. These differences suggest that there may be changes that could make it easier to manage deep learning model development, particularly as models continue to explode in size.
+
+#### How does it work?
+
+The way `cclm` aims to achieve the above is by making the model building process composable. There are many ways to pretrain a model on text, and infinite corpora on which to train, and each application has different needs.
+
+`cclm` makes it possible to define a `base` input on which to build many different computational graphs, then combine them. For instance, if there is a standard, published `cclm` model trained with masked language modeling (MLM) on (`wikitext` + `bookcorpus`), you might start with that, but add a second component to that model that uses the same `base`, but is pretrained to extract entities from `wiki-ner`. By combining the two pretrained components with a `ComposedModel`, you get a model with information from both tasks that you can then use as a starting point for your downstream task.
+
+Common model components will be published onto the `cclm-shelf` to make it simple to mix and match capabilities.
+
+The choice to emphasize character-level rather than arbitrary tokenization schemes is to make the input as generically useful across tasks as possible. Character-level input also makes it simpler to add realistic typos/noise to make models more robust to imperfect inputs.
+
+
+#### Basic concepts
+
+The main output of a training job with `cclm` is a `ComposedModel`, which consists of a `Preprocessor` that turns text into a vector[int], a base model that embeds that vector input, and one or more model components that accept the output of the embedder. The `ComposedModel` concatenates the output from those models together to produce its final output.
+
+The package uses `datasets` and `tokenizers` from `huggingface` for a standard interface and to benefit from their great framework. To fit models and preprocessors, you can also pass a `List[str]` directly.
+
+To start, you need a `Preprocessor`.
+
+```python
+from cclm.preprocessing import Preprocessor
+
+prep = Preprocessor()  # set max_example_len to specify a maximum input length
+prep.fit(dataset) # defines the model's vocabulary (character-level)
+```
+
+Once you have that, you can create an `Embedder`, which is the common base on which all the separate models will sit. This is a flexible class primarily responsible for holding a model that embeds a sequence of integers (representing characters) into a space the components expect. For more complicated setups, the `Embedder` could have a `ComposedModel` as its `model`
+
+```python
+from cclm.models import Embedder
+
+embedder = Embedder(prep.max_example_len, prep.n_chars)
+```
+
+The embedder doesn't necessarily need to be fit by itself, as you can fit it while you do your first pretraining task.
+
+Now you're ready to build your first model using a pretraining task (here masked language modeling)
+
+```python
+from cclm.pretraining import MaskedLanguagePretrainer
+
+pretrainer = MaskedLanguagePretrainer(embedder=embedder)
+pretrainer.fit(dataset, epochs=10)
+```
+
+The `MaskedLanguagePretrainer` defines a transformer-based model to do masked language modeling. Calling `.fit()` will use the `Preprocessor` to produce masked inputs and try to identify the missing input token(s) using `sampled_softmax` loss or negative sampling. This is just one example of a pretraining task, but others can be found in `cclm.pretrainers`.
+
+Once you've trained one or more models using `Pretrainer` objects, you can compose them together into one model.
+
+```python
+composed = ComposedModel(embedder, [pretrainer_a.model, pretrainer_b.model])
+```
+
+You can then use `composed.model(x)` to embed input
+
+```python
+x = prep.string_to_array("cclm is neat", prep.max_example_len)
+emb = composed.model(x)   # has shape (1, prep.max_example_len, pretrainer_a_model_shape[-1]+pretrainer_b_model_shape[-1])
+```
+
+... or create a new model with something like
+
+```python
+# pool the output across the character dimension
+gmp = tf.keras.layers.GlobalMaxPool1D()
+# add a classification head on top
+d = tf.keras.layers.Dense(1, activation="sigmoid")
+keras_model = tf.keras.Model(composed.model.input, d(gmp(composed.model.output)))
+```
+
+### Shelf
+
+The `Shelf` class is used to load off-the-shelf components. These are published to a separate repo using git lfs, and are loaded with a specific tag.
+
+```python
+from cclm.shelf import Shelf
+
+shelf = Shelf()
+identifier = "en_wiki_clm_1"
+item_type = "preprocessor"
+shelf.fetch(identifier, item_type, tag="v0.2.1", cache_dir=".cclm")
+prep = Preprocessor(
+    load_from=os.path.join(cache_dir, identifier, item_type, "cclm_config.json")
+)
+```
