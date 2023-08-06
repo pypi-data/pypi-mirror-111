fe1713bb22136e9e9350a17ddda42e0fcbfe6ec5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:38 +0000

    Add package metadata-from-url/0.0.3/metadata_from_url-0.0.3.tar.gz

---
+++ packages/metadata-from-url/metadata_from_url-0.0.3.tar.gz/metadata_from_url-0.0.3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import subprocess
+
+VERSION = '0.0.3'
+DESCRIPTION = 'Extract metadata from url'
+LONG_DESCRIPTION = 'A package that allows you to extract metadata from static webpages.'
+
+subprocess.Popen("make", cwd="metadata_from_url")
+
+setup(
+    name="metadata_from_url",
+    version=VERSION,
+    author="hectortav (Ektoras Tavoularis)",
+    author_email="<tavhector@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'metadata', 'extract'],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+    ]
+)
+
+# python3 setup.py sdist bdist_wheel
+# twine upload dist/*
