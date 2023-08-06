46bf318fa48a41bfc63f6151fa7f5d8b8ed56980
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:14 +0000

    Add package gpgmailencrypt/3.4.7/gpgmailencrypt-3.4.7.tar.gz

---
+++ packages/gpgmailencrypt/gpgmailencrypt-3.4.7.tar.gz/gpgmailencrypt-3.4.7/gmeutils/version.py
@@ -0,0 +1,26 @@
+#License GPL v3
+#Author Horst Knorr <gpgmailencrypt@gmx.de>
+VERSION="3.4.7"
+DATE="29.06.2021"
+COPYRIGHTYEAR="2021"
+unicodeerror="replace"
+#346 restart checking for virusscanners if no virus scanner was found
+#345 bugfix: doesn't crash any more when virus scanner is not available
+#342 pgpmime: encrypt subject
+#341 bugfix: doesn't add headers multiple times
+#340 pgpinline put attachments in zip container and add pdf content attachment
+#332
+#331 securitylevel=script and pdfpasswordmode=script, starting windows support 
+#330 mail decryption added
+#320 use additional keys for encryption
+#passwords user better hashing algorithms via passlib
+#sercurelevels may|redirect|bounce
+#database improvements
+#create tables
+#add pdf password storage in database
+#working mysql, sqlite3 and postgres.
+#MSSQL remains untested
+#Admin console supports createtable command
+#Admin console supports resetmessages command
+#gpgclass can extract public keys
+
