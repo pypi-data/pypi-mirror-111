0742c63c952712c74d2000f1a0eb263518ef2c31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:19 +0000

    Add package pastebinapi/0.0.2/pastebinapi-0.0.2.tar.gz

---
+++ packages/pastebinapi/pastebinapi-0.0.2.tar.gz/pastebinapi-0.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  "Development Status :: 5 - Production/Stable",
+  "Intended Audience :: Education",
+  "Operating System :: Microsoft :: Windows :: Windows 10",
+  "License :: OSI Approved :: MIT License",
+  "Programming Language :: Python :: 3.9"
+]
+ 
+setup(
+  name="pastebinapi",
+  version="0.0.2",
+  description="This module allows to use Pastebin API very easily.",
+  long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+  url="https://github.com/venaxyt/pastebinapi",  
+  author="Venax",
+  author_email="venaxumofficial@gmail.com",
+  license="MIT", 
+  classifiers=classifiers,
+  keywords="pastebin", 
+  install_requires=["requests"]
+)
