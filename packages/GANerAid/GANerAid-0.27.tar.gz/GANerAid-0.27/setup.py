c079447c6006e8a597fa77c184ae6d2b22826b00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:53 +0000

    Add package GANerAid/0.27/GANerAid-0.27.tar.gz

---
+++ packages/GANerAid/GANerAid-0.27.tar.gz/GANerAid-0.27/setup.py
@@ -0,0 +1,37 @@
+from distutils.core import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='GANerAid',
+    version='0.27',
+
+    url='https://github.com/TeamGenerAid/GANerAid'
+        '',
+    license='MIT License',
+    author='TeamGenerAid',
+    author_email='generaid.thu@gmail.com',
+    description='Gan library to create and validate synthetic tabular data',
+    install_requires=[
+        'numpy>=1.19.5',
+        'pandas>=1.2.4',
+        'torch>=1.8.1',
+        'scikit-learn>=0.22.2',
+        'seaborn>=0.11.1',
+        'tqdm>=4.61.1',
+        'tab-gan-metrics >= 1.1.4',
+        'matplotlib>=3.4.0'
+
+
+    ],
+    python_requires='>=3.7',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=['GANerAid'],
+    include_package_data = True,
+    repository = "https://test.pypi.org/legacy/"
+)
