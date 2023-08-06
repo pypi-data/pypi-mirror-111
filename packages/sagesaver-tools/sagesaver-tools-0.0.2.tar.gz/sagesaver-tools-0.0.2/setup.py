b6bbf71bf176864f8d7ad4c7a77eebd6ba4d25fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:39 +0000

    Add package sagesaver-tools/0.0.2/sagesaver-tools-0.0.2.tar.gz

---
+++ packages/sagesaver-tools/sagesaver-tools-0.0.2.tar.gz/sagesaver-tools-0.0.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup(
+    name="sagesaver-tools",
+    version="0.0.2",
+    author="Victor Lin",
+    author_email="vvictor.llin@gmail.com",
+    description="Utils for SageSaver server scripts",
+    url="https://github.com/Cozieee/sagesaver/sagesaver-tools",
+    project_urls={
+        "Project Main": "https://github.com/Cozieee/sagesaver",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
