49bb0aab75e52edfaf1a09962f87eea516aa1e6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:34 +0000

    Add package wdmtoolbox/12.6.0/wdmtoolbox-12.6.0.tar.gz

---
+++ packages/wdmtoolbox/wdmtoolbox-12.6.0.tar.gz/wdmtoolbox-12.6.0/tests/test_rdii.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+"""
+test_rdii.py
+----------------------------------
+
+Tests for `wdmtoolbox` module.
+"""
+
+import os
+import sys
+import tempfile
+
+try:
+    from cStringIO import StringIO
+except:
+    from io import StringIO
+
+from unittest import TestCase
+
+from wdmtoolbox import wdmtoolbox
+
+
+def capture(func, *args, **kwds):
+    sys.stdout = StringIO()  # capture output
+    out = func(*args, **kwds)
+    out = sys.stdout.getvalue()  # release output
+    try:
+        out = bytes(out, "utf-8")
+    except:
+        pass
+    return out
+
+
+class TestDescribe(TestCase):
+    def setUp(self):
+        self.fd, self.wdmname = tempfile.mkstemp(suffix=".wdm")
+        os.close(self.fd)
+
+    def tearDown(self):
+        os.remove(self.wdmname)
+
+    def test_rdii(self):
+        wdmtoolbox.createnewwdm(self.wdmname, overwrite=True)
+        wdmtoolbox.createnewdsn(self.wdmname, 101, tcode=2, base_year=1970, tsstep=15)
+        wdmtoolbox.csvtowdm(self.wdmname, 101, input_ts="tests/nwisiv_02246000.csv")
+        wdmtoolbox.copydsn(self.wdmname, 101, self.wdmname, 1101)
+        wdmtoolbox.wdmtoswmm5rdii(self.wdmname, 101, 1101)
