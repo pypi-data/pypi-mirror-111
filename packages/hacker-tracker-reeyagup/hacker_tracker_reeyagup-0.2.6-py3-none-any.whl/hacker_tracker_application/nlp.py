570ab26cb386211e12d9d060328afa33e2963671
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:17:18 +0000

    Add package hacker-tracker-reeyagup/0.2.6/hacker_tracker_reeyagup-0.2.6-py3-none-any.whl

---
+++ packages/hacker-tracker-reeyagup/hacker_tracker_reeyagup-0.2.6-py3-none-any.whl/hacker_tracker_application/nlp.py
@@ -0,0 +1,9 @@
+# [1] https://towardsdatascience.com/synonyms-and-antonyms-in-python-a865a5e14ce8
+# [2] https://spacytextblob.netlify.app/docs/example
+import nltk
+import spacy
+from nltk.corpus import wordnet
+from spacytextblob.spacytextblob import SpacyTextBlob
+import en_core_web_sm
+
+
