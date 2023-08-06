1dd16946dd0131c2ab817e0e2a262da8383853dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:58 +0000

    Add package ml4ir/0.1.4/ml4ir-0.1.4.tar.gz

---
+++ packages/ml4ir/ml4ir-0.1.4.tar.gz/ml4ir-0.1.4/setup.py
@@ -0,0 +1,37 @@
+from setuptools import find_namespace_packages, setup
+
+
+def load_dependencies():
+    with open("requirements.txt") as f:
+        required = f.read().splitlines()
+    return required
+
+
+def getReadMe():
+    with open("README.md", "r") as f:
+        long_description = f.read()
+    return long_description
+
+
+setup(
+    name="ml4ir",
+    packages=find_namespace_packages(include=["ml4ir.*"]),
+    version="0.1.4",
+    description="Machine Learning libraries for Information Retrieval",
+    long_description=getReadMe(),
+    long_description_content_type="text/markdown",
+    author="Search Relevance, Salesforce",
+    author_email="searchrelevancyscrumteam@salesforce.com",
+    url="https://www.salesforce.com/",
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+    include_package_data=True,
+    license="ASL 2.0",
+    python_requires=">=3.7",
+    install_requires=load_dependencies(),
+    extras_require={
+        "all": ["pyspark==3.0.1"]  # Used by ml4ir.base.io.spark_io
+    }
+)
