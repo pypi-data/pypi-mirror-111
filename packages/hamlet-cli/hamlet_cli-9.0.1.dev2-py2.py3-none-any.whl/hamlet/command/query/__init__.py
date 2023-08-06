1f4c8b5891afa930980b26767383c3e04dfddb8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:51 +0000

    Add package hamlet-cli/9.0.1.dev2/hamlet_cli-9.0.1.dev2-py2.py3-none-any.whl

---
+++ packages/hamlet-cli/hamlet_cli-9.0.1.dev2-py2.py3-none-any.whl/hamlet/command/query/__init__.py
@@ -0,0 +1,20 @@
+import click
+
+from hamlet.command import root
+
+
+@root.group(
+    'query',
+    context_settings=dict(
+        max_content_width=240
+    )
+)
+def query_group():
+    """
+    Base command used to set blueprint generation options
+
+    DEPRECATED: the query command group has been replaced by the component group ( hamlet component )
+
+        run hamlet component --help for the new commands
+    """
+    pass
