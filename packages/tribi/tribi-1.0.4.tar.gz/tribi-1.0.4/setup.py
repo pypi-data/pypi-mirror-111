f96d4672dfa1443be2999a67a9d0e3417cc5d283
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:55 +0000

    Add package tribi/1.0.4/tribi-1.0.4.tar.gz

---
+++ packages/tribi/tribi-1.0.4.tar.gz/tribi-1.0.4/setup.py
@@ -0,0 +1,27 @@
+# Copyright (c) 2021 Avery
+# 
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
+from os import path
+from setuptools import setup
+
+with open(path.join(path.abspath(path.dirname(__file__)), 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='tribi',
+      version='1.0.4',
+      description='Prettier error handling for Python',
+      author='starsflower',
+      url='https://github.com/starsflower/python-tri',
+      license="MIT",
+      classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+      ],
+      packages=['tri'],
+      package_dir={'tri': './tri'},
+
+      # Description
+      long_description=long_description,
+      long_description_content_type='text/markdown')
