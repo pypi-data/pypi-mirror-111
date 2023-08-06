3b4f3ad24cd3703e63af8d3dfb3463957838c381
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:35:37 +0000

    Add package google-tx/0.0.1/google_tx-0.0.1.tar.gz

---
+++ packages/google-tx/google_tx-0.0.1.tar.gz/google_tx-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='google_tx',
+    version='0.0.1',
+    packages=['google_tx'],
+    url='',
+    license='MIT',
+    author='lushan88a',
+    author_email='',
+    description='fork of google_tx with bug fix https://github.com/lushan88a/google_trans_new'
+)
