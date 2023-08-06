311000e7f644bf874076365667c58cd99d9a18b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:27 +0000

    Add package geomapviz/0.6.1/geomapviz-0.6.1.tar.gz

---
+++ packages/geomapviz/geomapviz-0.6.1.tar.gz/geomapviz-0.6.1/geomapviz/__init__.py
@@ -0,0 +1,10 @@
+from . import geomapviz
+
+__version__ = "0.6.1"
+__all__ = ["geomapviz"]
+
+# bound to upper level
+from .geomapviz import *
+
+# Author declaration (optional)
+__author__ = "Thomas Bury"
