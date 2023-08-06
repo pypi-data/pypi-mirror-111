b86cb8a31a50a5d7cf8c73c6823684035f992693
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:51 +0000

    Add package magicgoto/1.6.1/magicgoto-1.6.1-py3-none-any.whl

---
+++ packages/magicgoto/magicgoto-1.6.1-py3-none-any.whl/goto/commands/commands.py
@@ -0,0 +1,21 @@
+from . import add
+from . import check_migrate
+from . import list
+from . import migrate
+from . import rename
+from . import rm
+from . import show
+from . import update
+from . import search
+
+commands_list = [
+    add,
+    check_migrate,
+    list,
+    migrate,
+    rename,
+    rm,
+    show,
+    update,
+    search,
+]
