1bf07fa7e779b8fa8a7c6f50147e84abd6d48157
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:27 +0000

    Add package pysellure/0.0.1/pysellure-0.0.1.tar.gz

---
+++ packages/pysellure/pysellure-0.0.1.tar.gz/pysellure-0.0.1/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Python/Selenium/Allure methods'
+LONG_DESCRIPTION = long_description
+
+setuptools.setup(
+    name='pysellure',
+    version=VERSION,
+    author='Dmitry Vinogradov',
+    author_email='dvinogradov@htsts.ru',
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    install_requires=['selenium', 'allure'],
+    keywords=['python', 'selenium', 'allure', 'autotesting'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6"
+)
