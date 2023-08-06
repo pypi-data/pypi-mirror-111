ffbc7d0089879b0bd2a33d5ded0688ad790cacb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:35:34 +0000

    Add package princedctest/0.1/princedctest-0.1.tar.gz

---
+++ packages/princedctest/princedctest-0.1.tar.gz/princedctest-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='princedctest',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['princedctest'],
+      zip_safe=False)
