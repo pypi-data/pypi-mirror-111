23aa1f8623f0455852ff3126ed3e400c1da43f7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:45 +0000

    Add package keytotext/1.4.5/keytotext-1.4.5-py3-none-any.whl

---
+++ packages/keytotext/keytotext-1.4.5-py3-none-any.whl/keytotext/__init__.py
@@ -0,0 +1,10 @@
+"""
+Text Generation Using Keywords
+Keytotext converts a list of Keywords to sentences and larger texts
+"""
+
+
+__version__ = "1.4.5"
+
+from .pipeline import pipeline
+from .trainer import trainer
