12bb8983e32ec0fe1c344294de594608b5ecf4f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:31 +0000

    Add package shameni/1.0.2/shameni-1.0.2.tar.gz

---
+++ packages/shameni/shameni-1.0.2.tar.gz/shameni-1.0.2/tests.py
@@ -0,0 +1,21 @@
+from shameni import Gaze
+
+crypto = Gaze("cached.json")
+print("Cryptocurrencies:", ", ".join(crypto.supported()))
+
+for x in range(5):
+    if crypto.ping():
+        print("\n[Bitcoin]")
+        print("Prediction")
+        predicted = crypto.tomorrow("bitcoin")
+        print(f" - Tomorrow: {predicted:,.2f} USD")
+        predicted = crypto.today("bitcoin")
+        print(f" - Today: {predicted:,.2f} USD")
+
+        print("\nPrediction History")
+        for date, price in crypto.historical("bitcoin").items():
+            print(f"{date}: {price} USD")
+    break
+
+
+
