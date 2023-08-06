c7379f49d199665a4c47bfd9d49daef6d24cea59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:17 +0000

    Add package convokit/2.3.2.3/convokit-2.3.2.3-py3.6.egg

---
+++ packages/convokit/convokit-2.3.2.3-py3.6.egg/convokit/bag_of_words/bow_transformer.py
@@ -0,0 +1,100 @@
+from convokit import Corpus, CorpusComponent, Transformer
+from typing import Callable
+from sklearn.feature_extraction.text import CountVectorizer as CV
+
+
+class BoWTransformer(Transformer):
+    """
+    Bag-of-Words Transformer for annotating a Corpus's objects with the bag-of-words vectorization
+    of some textual element of the Corpus components.
+
+    Runs on the Corpus's Speakers, Utterances, or Conversations (as specified by `obj_type`).
+    By default, the text used for the different object types:
+
+    - For utterances, this would be the utterance text.
+    - For conversations, this would be joined texts of all the utterances in the conversation
+    - For speakers, this would be the joined texts of all the utterances by the speaker
+
+    Other custom text configurations can be configured using the `text_func` argument
+
+    Compatible with any type of vectorizer (e.g. bag-of-words, TF-IDF, etc)
+
+    :param obj_type: "speaker", "utterance", or "conversation"
+    :param vectorizer: a sklearn vectorizer object; default is CountVectorizer(min_df=10, max_df=.5, ngram_range(1, 1),
+        binary=False, max_features=15000)
+    :param vector_name: the name of the metadata key to store the vector under
+    :param text_func: function for getting text from the Corpus component object. By default, this is configured
+        based on the `obj_type`.
+
+    """
+    def __init__(self, obj_type: str, vector_name="bow_vector",
+                 text_func: Callable[[CorpusComponent], str] = None, vectorizer=None):
+
+        if vectorizer is None:
+            print("Initializing default unigram CountVectorizer...", end="")
+            self.vectorizer = CV(decode_error='ignore', min_df=10, max_df=.5,
+                                 ngram_range=(1, 1), binary=False, max_features=15000)
+            print("Done.")
+        else:
+            self.vectorizer = vectorizer
+
+        self.obj_type = obj_type
+        self.vector_name = vector_name
+
+        if text_func is None:
+            if obj_type == "utterance":
+                self.text_func = lambda utt: utt.text
+            elif obj_type == "conversation":
+                self.text_func = lambda convo: " ".join(utt.text for utt in convo.iter_utterances())
+            elif obj_type == "speaker":
+                self.text_func = lambda speaker: " ".join(utt.text for utt in speaker.iter_utterances())
+            else:
+                raise ValueError("Invalid corpus object type. Use 'utterance', 'conversation', or 'speaker'")
+        else:
+            self.text_func = text_func
+
+    def fit(self, corpus: Corpus, y=None, selector: Callable[[CorpusComponent], bool] = lambda x: True):
+        """
+        Fit the Transformer's internal vectorizer on the Corpus objects' texts, with an optional selector that filters for objects to be fit on.
+
+        :param corpus: the target Corpus
+        :param selector: a (lambda) function that takes a Corpus object and returns True or False (i.e. include / exclude). By default, the selector includes all objects of the specified type in the Corpus.
+        :return: the fitted BoWTransformer
+        """
+        # collect texts for vectorization
+        docs = [self.text_func(obj) for obj in corpus.iter_objs(self.obj_type, selector)]
+        self.vectorizer.fit(docs)
+        return self
+
+    def transform(self, corpus: Corpus, selector: Callable[[CorpusComponent], bool] = lambda x: True) -> Corpus:
+        """
+        Annotate the corpus objects with the vectorized representation of the object's text, with an optional
+        selector that filters for objects to be transformed. Objects that are not selected will get a metadata value
+        of 'None' instead of the vector.
+
+        :param corpus: the target Corpus
+        :param selector: a (lambda) function that takes a Corpus object and returns True or False (i.e. include / exclude). By default, the selector includes all objects of the specified type in the Corpus.
+
+        :return: the target Corpus annotated
+        """
+        objs = list(corpus.iter_objs(self.obj_type, selector))
+        ids = [obj.id for obj in objs]
+        docs = [self.text_func(obj) for obj in objs]
+
+        matrix = self.vectorizer.transform(docs)
+        corpus.set_vector_matrix(self.vector_name, matrix=matrix, ids=ids, columns=self.vectorizer.get_feature_names())
+
+        for obj in objs:
+            obj.add_vector(self.vector_name)
+
+        return corpus
+
+    def fit_transform(self, corpus: Corpus, y=None, selector: Callable[[CorpusComponent], bool] = lambda x: True) -> Corpus:
+        self.fit(corpus, selector=selector)
+        return self.transform(corpus, selector=selector)
+
+    def get_vocabulary(self):
+        """
+        Get the vocabulary of the vectorizer object
+        """
+        return self.vectorizer.get_feature_names()
---
+++ packages/convokit/convokit-2.3.2.3-py3.6.egg/convokit/model/convoKitMeta.py
@@ -0,0 +1,71 @@
+from collections import MutableMapping
+from convokit.util import warn
+from .convoKitIndex import ConvoKitIndex
+import json
+
+# See reference: https://stackoverflow.com/questions/7760916/correct-usage-of-a-getter-setter-for-dictionary-values
+
+
+class ConvoKitMeta(MutableMapping, dict):
+    """
+    ConvoKitMeta is a dictlike object that stores the metadata attributes of a corpus component
+    """
+    def __init__(self, convokit_index, obj_type):
+        self.index: ConvoKitIndex = convokit_index
+        self.obj_type = obj_type
+
+    def __getitem__(self, item):
+        return dict.__getitem__(self, item)
+
+    def __setitem__(self, key, value):
+        if not isinstance(key, str):
+            warn("Metadata attribute keys must be strings. Input key has been casted to a string.")
+            key = str(key)
+
+        if self.index.type_check:
+            if type(value) != type(None): # do nothing to index if value is None
+                if key not in self.index.indices[self.obj_type]:
+                    type_ = _optimized_type_check(value)
+                    self.index.update_index(self.obj_type, key=key, class_type=type_)
+                else:
+                    # entry exists
+                    if self.index.get_index(self.obj_type)[key] != "bin": # if "bin" do no further checks
+                        if str(type(value)) not in self.index.get_index(self.obj_type)[key]:
+                            new_type = _optimized_type_check(value)
+
+                            if new_type == "bin":
+                                self.index.set_index(self.obj_type, key, "bin")
+                            else:
+                                self.index.update_index(self.obj_type, key, new_type)
+        dict.__setitem__(self, key, value)
+
+    def __delitem__(self, key):
+        dict.__delitem__(self, key)
+        self.index.del_from_index(self.obj_type, key)
+
+    def __iter__(self):
+        return dict.__iter__(self)
+
+    def __len__(self):
+        return dict.__len__(self)
+
+    def __contains__(self, x):
+        return dict.__contains__(self, x)
+
+    def to_dict(self):
+        return self.__dict__
+
+
+_basic_types = {type(0), type(1.0), type('str'), type(True)} # cannot include lists or dicts
+
+
+def _optimized_type_check(val):
+    # if type(obj)
+    if type(val) in _basic_types:
+        return str(type(val))
+    else:
+        try:
+            json.dumps(val)
+            return str(type(val))
+        except (TypeError, OverflowError):
+            return "bin"
