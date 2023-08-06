34f50a58761bd226d281f063756535ce38f680fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:11 +0000

    Add package fastapi-framework/1.1.1/fastapi-framework-1.1.1.tar.gz

---
+++ packages/fastapi-framework/fastapi-framework-1.1.1.tar.gz/fastapi-framework-1.1.1/fastapi_framework/redis.py
@@ -0,0 +1,35 @@
+from aioredis import Redis, create_redis_pool, create_redis
+from dotenv import load_dotenv
+from os import getenv
+
+from .modules import disabled_modules
+
+load_dotenv()
+
+REDIS_HOST = getenv("REDIS_HOST", "localhost")
+REDIS_PORT = getenv("REDIS_PORT", "6379")
+
+
+class RedisDependency:
+    """FastAPI Dependency for Redis Connections"""
+
+    redis: Redis = None
+
+    def __call__(self):
+        return self.redis
+
+    async def init(self):
+        """Initialises the Redis Dependency"""
+        if "redis" in disabled_modules:
+            raise Exception("Module Redis is disabled")
+        self.redis: Redis = await create_redis_pool(f"redis://{REDIS_HOST}:{REDIS_PORT}")
+
+
+redis_dependency: RedisDependency = RedisDependency()
+
+
+async def get_redis() -> Redis:
+    """Returns a NEW Redis connection"""
+    if "redis" in disabled_modules:
+        raise Exception("Module Redis is disabled")
+    return await create_redis(f"redis://{REDIS_HOST}:{REDIS_PORT}")
