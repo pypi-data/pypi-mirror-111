f65aca9bfd1a3e7bea0db049bbbcbcc49d61f4b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:35 +0000

    Add package boid-py/0.0.2/boid_py-0.0.2.tar.gz

---
+++ packages/boid-py/boid_py-0.0.2.tar.gz/boid_py-0.0.2/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+# Learn more: https://github.com/kennethreitz/setup.py
+
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='boid_py',
+    version='0.0.2',
+    description='A package for calculating positons of boids',
+    long_description=readme,
+    author='Akira Shioyoke',
+    author_email='s.akira2986@gmail.com',
+    install_requires=['numpy', 'cupy'],
+    url='https://github.com/syoukera/boid_py',
+    license=license,
+    packages=find_packages()
+)
+
