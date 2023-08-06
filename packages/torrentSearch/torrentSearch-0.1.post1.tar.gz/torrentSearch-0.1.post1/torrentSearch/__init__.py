f58d9c98275fb413d8062b484d1969b5a019b2c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:13 +0000

    Add package torrentSearch/0.1.post1/torrentSearch-0.1.post1.tar.gz

---
+++ packages/torrentSearch/torrentSearch-0.1.post1.tar.gz/torrentSearch-0.1.post1/torrentSearch/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3.6
+# -*- encoding: utf-8 -*-
+
+"""Python Torrent Searcher/Scraper."""
+
+from sys import path
+from os.path import dirname
+path.append(dirname(__file__))
+
+__version__ = '0.1.post1'
