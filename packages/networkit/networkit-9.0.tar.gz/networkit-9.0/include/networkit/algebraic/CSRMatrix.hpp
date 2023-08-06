4e2ae8a5836245996107ae9c89d007dc702e4148
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:41 +0000

    Add package networkit/9.0/networkit-9.0.tar.gz

---
+++ packages/networkit/networkit-9.0.tar.gz/networkit-9.0/include/networkit/algebraic/CSRMatrix.hpp
@@ -0,0 +1,13 @@
+#ifndef NETWORKIT_ALGEBRAIC_CSR_MATRIX_HPP_
+#define NETWORKIT_ALGEBRAIC_CSR_MATRIX_HPP_
+
+// networkit-format
+
+#include <networkit/algebraic/CSRGeneralMatrix.hpp>
+
+// For compatibility with the previous CSRMatrix implementation.
+namespace NetworKit {
+using CSRMatrix = CSRGeneralMatrix<double>;
+} // namespace NetworKit
+
+#endif // NETWORKIT_ALGEBRAIC_CSR_MATRIX_HPP_
---
+++ packages/networkit/networkit-9.0.tar.gz/networkit-9.0/include/networkit/distance/AllSimplePaths.hpp
@@ -0,0 +1,7 @@
+// networkit-format
+#ifndef NETWORKIT_DISTANCE_ALL_SIMPLE_PATHS_HPP_
+#define NETWORKIT_DISTANCE_ALL_SIMPLE_PATHS_HPP_
+#warning \
+    "networkit/distance/AllSimplePaths is deprecated, use networkit/reachability/AllSimplePaths"
+#include <networkit/reachability/AllSimplePaths.hpp>
+#endif // NETWORKIT_DISTANCE_ALL_SIMPLE_PATHS_HPP_
