da567314b9b92a01aa7ee791b13191b2691f78a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:57 +0000

    Add package fake_user_agent/0.0.9/fake_user_agent-0.0.9.tar.gz

---
+++ packages/fake_user_agent/fake_user_agent-0.0.9.tar.gz/fake_user_agent-0.0.9/fake_user_agent/settings.py
@@ -0,0 +1,50 @@
+"""setting static data"""
+import os
+import tempfile
+
+from fake_user_agent import __version__
+
+
+DB = os.path.join(
+    tempfile.gettempdir(), "fake_useragent_{version}.json".format(version=__version__)
+)
+
+DB_DIR = tempfile.gettempdir()
+
+BROWSERS_STATS_PAGE = "https://www.w3schools.com/browsers/default.asp"
+
+BROWSER_BASE_PAGE = (
+    "http://useragentstring.com/pages/useragentstring.php?name={browser}"  # noqa
+)
+
+BROWSERS_COUNT_LIMIT = 50
+
+BROWSERS = {
+    "chrome": 80.7,
+    "edge": 5.6,
+    "firefox": 6.1,
+    "safari": 3.7,
+    "opera": 2.4,
+}
+
+
+SHORTCUTS = {
+    "internet explorer": "edge",
+    "ie": "edge",
+    "msie": "edge",
+    "edge": "edge",
+    "google": "chrome",
+    "googlechrome": "chrome",
+    "ff": "firefox",
+}
+
+OVERRIDES = {
+    "Edge/IE": "Internet Explorer",
+    "IE/Edge": "Internet Explorer",
+}
+
+HTTP_TIMEOUT = 10
+
+HTTP_RETRIES = 2
+
+HTTP_DELAY = 0.1
