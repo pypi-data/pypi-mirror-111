4ec5a0bfc13798d338b5061c84b25b0b8088164c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:53 +0000

    Add package georss-ign-sismologia-client/0.4/georss_ign_sismologia_client-0.4.tar.gz

---
+++ packages/georss-ign-sismologia-client/georss_ign_sismologia_client-0.4.tar.gz/georss_ign_sismologia_client-0.4/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+REQUIRES = [
+    "georss_client>=0.14",
+    "dateparser>=0.7.0",
+]
+
+setup(
+    name="georss_ign_sismologia_client",
+    version="0.4",
+    author="Malte Franken",
+    author_email="coding@subspace.de",
+    description="A GeoRSS client library for the IGN Sismologia feed.",
+    license="Apache-2.0",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/exxamalte/python-georss-ign-sismologia-client",
+    packages=find_packages(exclude=["tests"]),
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=REQUIRES,
+)
