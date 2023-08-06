10de3b5dde5b4c29f76b8c0c254014b57ccb17f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:36 +0000

    Add package radgrafter/0.0.2/radgrafter-0.0.2.tar.gz

---
+++ packages/radgrafter/radgrafter-0.0.2.tar.gz/radgrafter-0.0.2/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="radgrafter",
+    version="0.0.2",
+    author="ink",
+    author_email="ink@isafetech.cn",
+    description="Package for request forwarding.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/inksong/radgrafter",
+    project_urls={
+        "Bug Tracker": "https://github.com/inksong/radgrafter/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=2.7",
+)
