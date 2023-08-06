5d0a719d78f09df5e6f65c375770cf7a0d0e53d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:14:52 +0000

    Add package bnpreprocessor/1.0.3/bnpreprocessor-1.0.3.tar.gz

---
+++ packages/bnpreprocessor/bnpreprocessor-1.0.3.tar.gz/bnpreprocessor-1.0.3/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+ 
+with open("README.md", "r",encoding='utf-8') as fh:
+    long_description = fh.read()
+ 
+setuptools.setup(
+    name="bnpreprocessor",
+    version= "1.0.3",
+    author="Abu Kaisar Mohammad Masum",
+ 
+    author_email="abukaisar24@gmail.com",
+ 
+    description="bengali automatic text preprocessor",
+ 
+    long_description=long_description,
+
+    long_description_content_type="text/markdown",
+    url="https://github.com/kaisarmasum/bnpreprocessor",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
