b71fcc69337b3ff84c632ba48c63e58ee9387512
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:32 +0000

    Add package yogendra-fizzbuzz/0.1.0/yogendra_fizzbuzz-0.1.0.tar.gz

---
+++ packages/yogendra-fizzbuzz/yogendra_fizzbuzz-0.1.0.tar.gz/yogendra_fizzbuzz-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['yogendra_fizzbuzz']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['test = scripts:test']}
+
+setup_kwargs = {
+    'name': 'yogendra-fizzbuzz',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Yogendra Rampuria (Yogi)',
+    'author_email': 'yogendrarampuria@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
