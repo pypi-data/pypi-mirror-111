51af3e9b5d3b0a1bd3da1a6e4f34214fd2e6d6e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:19 +0000

    Add package cplot/0.4.1/cplot-0.4.1-py3-none-any.whl

---
+++ packages/cplot/cplot-0.4.1-py3-none-any.whl/cplot/__init__.py
@@ -0,0 +1,22 @@
+from ._colors import get_srgb1, tripcolor
+from ._main import Plot, plot, savefig, show, show_colors, show_contours
+from .benchmark import show_kovesi_test_image, show_test_function
+from .create import create_colormap, find_max_srgb_radius, show_circular, show_linear
+
+__all__ = [
+    "show_test_function",
+    "show_kovesi_test_image",
+    "show_linear",
+    "show_circular",
+    "find_max_srgb_radius",
+    "create_colormap",
+    "get_srgb1",
+    "show_colors",
+    "show_contours",
+    "show",
+    "savefig",
+    "plot",
+    "Plot",
+    #
+    "tripcolor",
+]
