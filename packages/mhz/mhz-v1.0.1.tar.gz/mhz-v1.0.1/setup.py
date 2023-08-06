745572590fd8f5deda82e48451fe149230c91400
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:05 +0000

    Add package mhz/v1.0.1/mhz-v1.0.1.tar.gz

---
+++ packages/mhz/mhz-v1.0.1.tar.gz/mhz-v1.0.1/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+setup(
+    name='mhz',
+    packages=['mhz'],
+    version='v1.0.1',
+    license='GPLv3',
+    description='Just a playground for adding a package in pypi',
+    author='MHZ',
+    author_email='ziyaaddini@gmail.com',
+    url='https://github.com/iamMHZ/mhz',
+    download_url='https://github.com/iamMHZ/mhz/archive/refs/tags/v1.0.1.tar.gz',
+    keywords=['MHZ', 'iam_mhz'],
+    install_requires=[
+
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3',
+    ],
+)
