420768920f056bcb0af9ab72c922182c03888a6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:39 +0000

    Add package a9s/0.5.4/a9s-0.5.4.tar.gz

---
+++ packages/a9s/a9s-0.5.4.tar.gz/a9s-0.5.4/a9s/aws_resources/route53.py
@@ -0,0 +1,97 @@
+import os
+
+import curses
+import boto3
+from colored.colored import bg, fg
+
+from a9s.aws_resources.hud import HUDComponent
+from a9s.components.custom_string import String
+from a9s.components.table import ColSettings, Table
+
+
+IS_LOCAL = os.environ.get('LOCAL', 'false').lower() == 'true'
+
+
+class Route53Table(Table, HUDComponent):
+    SERVICE_NAME = 'Route 53'
+
+    def __init__(self) -> None:
+        self.client = boto3.client(service_name='route53', endpoint_url='http://localhost:54321' if IS_LOCAL else None)
+        self.hosted_zone = None
+        self._selection_stack = []
+        self._filter_stack = []
+        super().__init__([], [])
+        self.data_updating = False
+        self.queue_action(self.list_hosted_zones, self.on_updated_data)
+
+    def on_updated_data(self, data):
+        self.headers, self.data = data
+        self.data_updating = False
+
+    def get_hud_text(self, space_left):
+        if not self.hosted_zone:
+            return super().get_hud_text(space_left)
+
+        return String(self.hosted_zone['Name'], bg=bg('medium_purple_2a'), fg=fg('black')).reset_style_on_end()
+
+    def handle_key(self, key):
+        should_stop_propagate = super().handle_key(key)
+        if key.code == curses.KEY_EXIT and not should_stop_propagate and not self.data_updating:
+            if self.filter:
+                return should_stop_propagate
+
+            if self.hosted_zone is not None:
+                self.queue_action(self.list_hosted_zones, self.on_updated_data)
+                self.hosted_zone = None
+                should_stop_propagate = True
+
+            if len(self._filter_stack) > 0 or len(self._selection_stack) > 0:
+                self.filter = self._filter_stack.pop()
+                self.selected_row = self._selection_stack.pop()
+
+        return should_stop_propagate
+
+    def on_select(self, data):
+        if not self.hosted_zone and not self.data_updating:
+            self._filter_stack.append(self.filter)
+            self._selection_stack.append(self.selected_row)
+            self.hosted_zone = data
+            self.queue_action(self.list_records, self.on_updated_data)
+
+    def list_hosted_zones(self):
+        self.data_updating = True
+        response = self.client.list_hosted_zones()
+        headers = [ColSettings("Name", yank_key='n', stretched=True, min_size=20), ColSettings("ID", yank_key='i'), ColSettings("Records")]
+
+        data = []
+        for item in response['HostedZones']:
+            data.append([item['Name'], item['Id'], str(item['ResourceRecordSetCount'])])
+
+        return headers, data
+
+    def list_records(self):
+        self.data_updating = True
+        headers = [ColSettings('Name', yank_key='n', min_size=20, stretched=True), ColSettings('Type'), ColSettings('TTL', yank_key='t'), ColSettings('Record', yank_key='r')]
+        data = []
+
+        should_continue = True
+        next_record_name = None
+        while should_continue:
+            if next_record_name is None:
+                response = self.client.list_resource_record_sets(HostedZoneId=self.hosted_zone['ID'], MaxItems='300')
+            else:
+                response = self.client.list_resource_record_sets(HostedZoneId=self.hosted_zone['ID'], StartRecordName=next_record_name, MaxItems='300')
+
+            if not response['IsTruncated']:
+                should_continue = False
+
+            else:
+                next_record_name = response['NextRecordName']
+
+            for item in response['ResourceRecordSets']:
+                record = item.get('AliasTarget', {}).get('DNSName')
+                if not record and item.get('ResourceRecords'):
+                    record = ', '.join(r['Value'] for r in item['ResourceRecords'])
+                data.append([item['Name'], item['Type'], str(item.get('TTL', '')), record])
+
+        return headers, data
---
+++ packages/a9s/a9s-0.5.4.tar.gz/a9s-0.5.4/a9s/aws_resources/s3.py
@@ -0,0 +1,115 @@
+import os
+from subprocess import call
+import pathlib
+
+import curses
+import tempfile
+import boto3
+from colored.colored import bg, fg
+
+from a9s.aws_resources.hud import HUDComponent
+from a9s.components.custom_string import String
+from a9s.components.table import ColSettings, Table
+from a9s.components.app import EDITOR
+
+IS_LOCAL = os.environ.get('LOCAL', 'false').lower() == 'true'
+
+
+class S3Table(Table, HUDComponent):
+    SERVICE_NAME = 'S3'
+
+    def __init__(self) -> None:
+        self.client = boto3.client(service_name='s3', endpoint_url='http://localhost:54321' if IS_LOCAL else None)
+        self.bucket = None
+        self.paths = []
+        self._selection_stack = []
+        self._filter_stack = []
+
+        super().__init__([], [])
+        self.data_updating = False
+        self.queue_action(self.list_buckets, self.on_updated_data)
+
+    def on_updated_data(self, data):
+        self.headers, self.data = data
+        self.data_updating = False
+
+    def get_hud_text(self, space_left):
+        if not self.bucket:
+            return super().get_hud_text(space_left)
+
+        return String([String(self.bucket, bg=bg('orange_1'), fg=fg('black')).reset_style_on_end(),
+                       String("/"), String(self.prefix[-(space_left - len(self.bucket) - 1):])])
+    
+    @property
+    def prefix(self):
+        return "".join(self.paths)
+
+    def handle_key(self, key):
+        should_stop_propagate = super().handle_key(key)
+        if key.code == curses.KEY_EXIT and not should_stop_propagate and not self.data_updating:
+            if len(self.paths) > 0:
+                self.paths.pop()
+                self.data_updating = True
+                self.queue_action(self.list_bucket, self.on_updated_data)
+                should_stop_propagate = True
+
+            elif self.bucket is not None:
+                self.data_updating = True
+                self.queue_action(self.list_buckets, self.on_updated_data)
+                self.bucket = None
+                should_stop_propagate = True
+        
+            if len(self._filter_stack) > 0 or len(self._selection_stack) > 0:
+                self.filter = self._filter_stack.pop()
+                self.selected_row = self._selection_stack.pop()
+
+        return should_stop_propagate
+ 
+    def on_select(self, data):
+        if not self.bucket:
+            self._filter_stack.append(self.filter)
+            self._selection_stack.append(self.selected_row)
+            self.bucket = data['Bucket name']
+            self.headers, self.data = self.list_bucket()
+        
+        else:
+            if data['Type'] == "folder":
+                self._filter_stack.append(self.filter)
+                self._selection_stack.append(self.selected_row)
+                self.paths.append(data['Key'])
+                self.headers, self.data = self.list_bucket()
+            
+            else:  # we are a file
+                full_key = self.prefix + data['Key']
+                resp = self.client.get_object(Bucket=self.bucket, Key=full_key)
+                with tempfile.NamedTemporaryFile(suffix="." + full_key.replace("/", "___")) as tf:
+                    for chunk in resp['Body'].iter_chunks():
+                        tf.write(chunk)
+
+                    tf.flush()
+                    call([EDITOR, tf.name])
+
+    def list_buckets(self):
+        response = self.client.list_buckets()
+        headers = [ColSettings("Bucket name", stretched=True, yank_key='n'), ColSettings("Creation date", yank_key='d')]
+        data = []
+        for bucket in response['Buckets']:
+            data.append([bucket['Name'], str(bucket['CreationDate'])])
+
+        return headers, data
+
+    def list_bucket(self):
+        objects = self.client.list_objects_v2(Bucket=self.bucket, Delimiter="/", Prefix=self.prefix)
+        headers = [ColSettings("Key", stretched=True, min_size=15, yank_key='k'), ColSettings("Type"), ColSettings("Last modify"), ColSettings("ETag", yank_key='t'), ColSettings("Size"), ColSettings("Storage Class"), ColSettings("Owner")]
+        data = []
+        for object in objects.get('CommonPrefixes', []):
+            folder_data = [String(str(object['Prefix'].replace(self.prefix, "", 1)), fg=fg('magenta_3c')), "folder"]
+            folder_data += (len(headers) - len(folder_data)) * [""]
+            data.append(folder_data)
+
+        for object in objects.get('Contents', []):
+            name = object['Key'].replace(self.prefix, "", 1)
+            file_type = "file ({})".format(pathlib.Path(name).suffix) if pathlib.Path(name).suffix else "file"
+            data.append([name, file_type, str(object['LastModified']), object['ETag'], str(object['Size']), object['StorageClass'], object.get('Owner', {}).get('DisplayName', "")])
+
+        return headers, data
