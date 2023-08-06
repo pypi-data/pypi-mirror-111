7e94ba90dc66fe490ba0d00ea753d90ec67dd205
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:30 +0000

    Add package distributions-spovi/0.2/distributions_spovi-0.2.tar.gz

---
+++ packages/distributions-spovi/distributions_spovi-0.2.tar.gz/distributions_spovi-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='distributions_spovi',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['distributions_spovi'],
+      author = 'SPoVi',
+      author_email = 'snop.vlc@gmail.com',
+      zip_safe=False)
+
+# Can include author_email = "@gmail.com"
