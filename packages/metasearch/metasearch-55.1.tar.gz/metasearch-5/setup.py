fd25f31ac2bfb55eff8ccb8e6be60696d6bae49f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:55 +0000

    Add package metasearch/5/metasearch-55.1.tar.gz

---
+++ packages/metasearch/metasearch-55.1.tar.gz/metasearch-5/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+ 
+classifiers = []
+ 
+setup(
+  name='metasearch',
+  version='5',
+  description='Pre built metasearch repository for easy uses.',
+  long_description=open('README.md').read() + "\n#### pog",
+  long_description_content_type='text/markdown',
+  url='https://github.com/dumb-stuff/Meta-search',  
+  author='Rukchad Wongprayoon',
+  author_email='contact@biomooping.tk',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='Tools', 
+  packages=find_packages(),
+  install_requires=["flask","requests","googlesearch-python","asgiref"],
+  entry_points={
+    'console_scripts': ['metasearch=metasearch:launch']
+  }
+)
