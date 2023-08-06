4689a74953e41f511e3dde623190cd8c29bc232d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:04 +0000

    Add package druid-data/0.1.19/druid_data-0.1.19-py3-none-any.whl

---
+++ packages/druid-data/druid_data-0.1.19-py3-none-any.whl/druid_data/__init__.py
@@ -0,0 +1,12 @@
+"""Top-level package for druid-data"""  # noqa: F401
+# noqa: F401
+from .main import CrudRepository  # noqa: F401
+from .main import DynamoCrudRepository  # noqa: F401
+from .main import DynamoDBGlobalParameters  # noqa: F401
+from .main import Repository  # noqa: F401
+from .main import SimulatedDynamoDBResource  # noqa: F401
+from .main import SimulatedTable  # noqa: F401
+from .main import dynamo_entity  # noqa: F401; noqa: F401
+
+__version__ = "0.1.19"
+__author__ = """DRUID DEVAX"""
