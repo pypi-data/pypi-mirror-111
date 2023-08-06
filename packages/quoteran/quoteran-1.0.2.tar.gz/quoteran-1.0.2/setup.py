e80d0f8a9eaf0fc5add3db5425b2c8d2aea3cf47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:11 +0000

    Add package quoteran/1.0.2/quoteran-1.0.2.tar.gz

---
+++ packages/quoteran/quoteran-1.0.2.tar.gz/quoteran-1.0.2/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+from quoteran.version import __version__
+
+setup(
+    name="quoteran",
+    version=__version__,
+    description="Random quotes on Terminal",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="UltiRequiem",
+    author_email="eliaz.bobadilladev@gmail.com",
+    url="https://github.com/UltiRequiem/quoteran",
+    license="MIT",
+    scripts=["./bin/quoteran"],
+    packages=["quoteran"],
+    install_requires=["requests==2.25.1"],
+)
