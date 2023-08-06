bf38e9bda39accba62f834e8f58b2eb2ac2777bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:08 +0000

    Add package pymitblod/0.0.7/pymitblod-0.0.7.tar.gz

---
+++ packages/pymitblod/pymitblod-0.0.7.tar.gz/pymitblod-0.0.7/pymitblod/__init__.py
@@ -0,0 +1,11 @@
+'''
+Init file for pymitblod
+'''
+
+from .mitblod import MitBlod
+from .models import Institution
+from .consts import Institutions as _Institutions
+
+
+
+Institutions = _Institutions()
