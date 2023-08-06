e494869f37d5b1067cb35478c74465cab6e1e3ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:34 +0000

    Add package hell-hound/0.1.0/hell_hound-0.1.0-py3-none-any.whl

---
+++ packages/hell-hound/hell_hound-0.1.0-py3-none-any.whl/hell_hound/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""Hell Hound provides type checking and other base functionality out of the box and is designed to be non-blocking and easily and widely extensible, allowing for custom validation. It has no dependencies, but has the potential to become yours."""
+
+from hell_hound import metadata
+
+
+__version__ = metadata.version
+__author__ = metadata.authors[0]
+__license__ = metadata.license
+__copyright__ = metadata.copyright
---
+++ packages/hell-hound/hell_hound-0.1.0-py3-none-any.whl/hell_hound/metadata.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""Project metadata
+
+Information describing the project.
+"""
+
+# The package name, which is also the "UNIX name" for the project.
+package = 'hell_hound'
+project = "Hell Hound is a lightweight and extensible data validation library for Python."
+project_no_spaces = project.replace(' ', '')
+version = '0.1.0'
+description = 'Hell Hound provides type checking and other base functionality out of the box and is designed to be non-blocking and easily and widely extensible, allowing for custom validation. It has no dependencies, but has the potential to become yours.'
+authors = ['Allan']
+authors_string = ', '.join(authors)
+emails = ['allanice001@gmail.com']
+license = 'MIT'
+copyright = '2021 ' + authors_string
+url = 'http://CtrlTilde.com/'
