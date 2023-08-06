a709e49207ee98cfb63a08f18f4721ad526f7ebb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:49 +0000

    Add package SCALECAST/0.1.3/SCALECAST-0.1.3.tar.gz

---
+++ packages/SCALECAST/SCALECAST-0.1.3.tar.gz/SCALECAST-0.1.3/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+setup(
+  name = 'SCALECAST',
+  packages = ['Scalecast'],
+  version = '0.1.3',
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  author = 'Michael Keith',
+  author_email = 'mikekeith52@gmail.com',
+  url = 'https://github.com/mikekeith52/scalecast',
+  keywords = ['FORECAST', 'SCALE', 'FLEXIBLE'],
+  install_requires = [
+    'scikit-learn',
+    'statsmodels',
+    'eli5',
+    'numpy',
+    'pandas',
+    'scipy',
+    'matplotlib',
+    'seaborn',
+  ],
+)
