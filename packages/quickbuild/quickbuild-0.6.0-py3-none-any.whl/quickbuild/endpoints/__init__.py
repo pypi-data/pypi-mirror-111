f95ec5dd644fe0e208240ea3b4f5c70b5f6018ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:47 +0000

    Add package quickbuild/0.6.0/quickbuild-0.6.0-py3-none-any.whl

---
+++ packages/quickbuild/quickbuild-0.6.0-py3-none-any.whl/quickbuild/endpoints/__init__.py
@@ -0,0 +1,23 @@
+from .audits import Audits
+from .builds import Builds
+from .configurations import Configurations
+from .dashboards import Dashboards
+from .groups import Groups
+from .memberships import Memberships
+from .requests import Requests
+from .system import System
+from .tokens import Tokens
+from .users import Users
+
+__all__ = (
+    'Audits',
+    'Builds',
+    'Configurations',
+    'Dashboards',
+    'Groups',
+    'Memberships',
+    'Requests',
+    'System',
+    'Tokens',
+    'Users',
+)
