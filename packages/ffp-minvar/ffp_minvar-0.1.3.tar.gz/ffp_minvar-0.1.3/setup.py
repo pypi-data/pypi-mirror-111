77f4ca18816192c0d16b7e0baacf64150fc8f308
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:34 +0000

    Add package ffp-minvar/0.1.3/ffp_minvar-0.1.3.tar.gz

---
+++ packages/ffp-minvar/ffp_minvar-0.1.3.tar.gz/ffp_minvar-0.1.3/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+from distutils.core import setup, Extension
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+shared_module = Extension('alg_lomv.so',
+                    sources = ['src/alg_lomv.c'],
+                    include_dirs= ['include'],
+                    extra_compile_args=['-lm', '-lgsl', '-lgslcblas', '-g'])
+
+# setuptools.setup
+setup(
+    name="ffp_minvar",
+    version="0.1.3",
+    author="Lucius Luo",
+    author_email="lucius0228@gmail.com",
+    description="rewritten python package of ffp_minvar algorithm",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/luciusluo/ffp_minvar",
+    project_urls={
+        "Bug Tracker": "https://github.com/luciusluo/ffp_minvar/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "lib"},
+    packages=setuptools.find_packages(where="lib"),
+    python_requires=">=3.6",
+    include_package_data=True, # might delete later
+    ext_modules = [shared_module]
+)
