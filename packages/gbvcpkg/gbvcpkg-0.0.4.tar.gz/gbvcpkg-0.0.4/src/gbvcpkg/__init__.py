cc277bbc656fedfb694910aeb2ef8c6f99b02a75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:15:00 +0000

    Add package gbvcpkg/0.0.4/gbvcpkg-0.0.4.tar.gz

---
+++ packages/gbvcpkg/gbvcpkg-0.0.4.tar.gz/gbvcpkg-0.0.4/src/gbvcpkg/__init__.py
@@ -0,0 +1,6 @@
+from gbvcpkg.video_player_class import App1
+from gbvcpkg.video_player_class import handDetector
+from gbvcpkg.video_player_class import MyVideoCapture
+from gbvcpkg.video_player_class import MenuBar
+from gbvcpkg.video_player_class import FrameBox
+from gbvcpkg.video_player_class import App
