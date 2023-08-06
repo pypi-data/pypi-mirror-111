edfd5310739aefad3a689b340a86332535dfebe6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:55 +0000

    Add package corded/1.3.6/corded-1.3.6.tar.gz

---
+++ packages/corded/corded-1.3.6.tar.gz/corded-1.3.6/corded/__init__.py
@@ -0,0 +1,39 @@
+from .http import File, HTTPClient, Route
+from .ws import GatewayClient, Shard
+from .errors import (
+    CordedError,
+    HTTPError,
+    BadRequest,
+    Unauthorized,
+    Forbidden,
+    NotFound,
+    PayloadTooLarge,
+    TooManyRequests,
+    DiscordServerError,
+)
+from .constants import VERSION as __version__
+from .objects import Object, GatewayEvent
+from .client import CordedClient
+from .helpers import BitField
+
+__all__ = (
+    File,
+    HTTPClient,
+    Route,
+    CordedError,
+    HTTPError,
+    BadRequest,
+    Unauthorized,
+    Forbidden,
+    NotFound,
+    PayloadTooLarge,
+    TooManyRequests,
+    DiscordServerError,
+    Object,
+    GatewayEvent,
+    GatewayClient,
+    Shard,
+    CordedClient,
+    BitField,
+    __version__,
+)
