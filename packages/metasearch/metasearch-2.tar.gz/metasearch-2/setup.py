fe7cb85018fb270bbd23a9866b0b78215deded2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:14 +0000

    Add package metasearch/2/metasearch-2.tar.gz

---
+++ packages/metasearch/metasearch-2.tar.gz/metasearch-2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+ 
+classifiers = []
+ 
+setup(
+  name='metasearch',
+  version='2',
+  description='Pre built metasearch repository for easy uses.',
+  long_description=open('pypi support one.md').read() + "\n####pog",
+  long_description_content_type='text/markdown',
+  url='https://github.com/dumb-stuff/Meta-search',  
+  author='Rukchad Wongprayoon',
+  author_email='contact@biomooping.tk',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='Tools', 
+  packages=find_packages(),
+  install_requires=["flask","requests","googlesearch-python","asgiref"],
+  entry_points={
+    'console_scripts': ['metasearch=metasearch.__init__:launch']
+  }
+)
