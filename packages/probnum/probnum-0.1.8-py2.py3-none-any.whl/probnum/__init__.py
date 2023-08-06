b084da7d7d9754694cdf64679b220bf3e4aa362a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:23 +0000

    Add package probnum/0.1.8/probnum-0.1.8-py2.py3-none-any.whl

---
+++ packages/probnum/probnum-0.1.8-py2.py3-none-any.whl/probnum/__init__.py
@@ -0,0 +1,67 @@
+"""ProbNum implements probabilistic numerical methods in Python. Such methods solve
+numerical problems from linear algebra, optimization, quadrature and differential
+equations using probabilistic inference. This approach captures uncertainty arising from
+finite computational resources and stochastic input.
+
++----------------------------------+--------------------------------------------------------------+
+| **Subpackage**                   | **Description**                                              |
++----------------------------------+--------------------------------------------------------------+
+| :mod:`~probnum.diffeq`           | Probabilistic solvers for ordinary differential equations.   |
++----------------------------------+--------------------------------------------------------------+
+| :mod:`~probnum.filtsmooth`       | Bayesian filtering and smoothing.                            |
++----------------------------------+--------------------------------------------------------------+
+| :mod:`~probnum.kernels`          | Kernels / covariance functions.                              |
++----------------------------------+--------------------------------------------------------------+
+| :mod:`~probnum.linalg`           | Probabilistic numerical linear algebra.                      |
++----------------------------------+--------------------------------------------------------------+
+| :mod:`~probnum.linops`           | Finite-dimensional linear operators.                         |
++----------------------------------+--------------------------------------------------------------+
+| :mod:`~probnum.problems`         | Definitions and collection of problems solved by PN methods. |
++----------------------------------+--------------------------------------------------------------+
+| :mod:`~probnum.quad`             | Bayesian quadrature / numerical integration.                 |
++----------------------------------+--------------------------------------------------------------+
+| :mod:`~probnum.randprocs`        | Random processes representing uncertain functions.           |
++----------------------------------+--------------------------------------------------------------+
+| :mod:`~probnum.randvars`         | Random variables representing uncertain values.              |
++----------------------------------+--------------------------------------------------------------+
+| :mod:`~probnum.statespace`       | Probabilistic state space models.                            |
++----------------------------------+--------------------------------------------------------------+
+| :mod:`~probnum.utils`            | Utility functions.                                           |
++----------------------------------+--------------------------------------------------------------+
+"""
+
+from pkg_resources import DistributionNotFound, get_distribution
+
+from . import (
+    diffeq,
+    filtsmooth,
+    kernels,
+    linalg,
+    linops,
+    problems,
+    quad,
+    randprocs,
+    randvars,
+    statespace,
+    utils,
+)
+from ._probabilistic_numerical_method import ProbabilisticNumericalMethod
+from .randvars import asrandvar
+
+# Public classes and functions. Order is reflected in documentation.
+__all__ = [
+    "asrandvar",
+    "ProbabilisticNumericalMethod",
+]
+
+# Set correct module paths. Corrects links and module paths in documentation.
+ProbabilisticNumericalMethod.__module__ = "probnum"
+
+try:
+    # Change here if project is renamed and does not equal the package name
+    dist_name = __name__
+    __version__ = get_distribution(dist_name).version
+except DistributionNotFound:
+    __version__ = "unknown"
+finally:
+    del get_distribution, DistributionNotFound
---
+++ packages/probnum/probnum-0.1.8-py2.py3-none-any.whl/probnum/utils/argutils.py
@@ -0,0 +1,85 @@
+"""Utility functions for argument types."""
+
+import numbers
+from typing import Optional
+
+import numpy as np
+import scipy._lib._util
+
+from probnum.type import (
+    DTypeArgType,
+    RandomStateArgType,
+    RandomStateType,
+    ScalarArgType,
+    ShapeArgType,
+    ShapeType,
+)
+
+__all__ = ["as_shape", "as_random_state", "as_numpy_scalar"]
+
+
+def as_random_state(seed: RandomStateArgType) -> RandomStateType:
+    """Turn ``seed`` into a np.random.RandomState instance.
+
+    Parameters
+    ----------
+    seed
+        If seed is None, return the RandomState singleton used by np.random. If seed is
+        an int, return a new RandomState instance seeded with seed. If seed is already a
+        RandomState instance, return it.
+
+    Raises
+    -------
+    ValueError
+        If seed is neither None, an int or a RandomState instance.
+    """
+    return scipy._lib._util.check_random_state(seed)
+
+
+def as_shape(x: ShapeArgType, ndim: Optional[numbers.Integral] = None) -> ShapeType:
+    """Convert a shape representation into a shape defined as a tuple of ints.
+
+    Parameters
+    ----------
+    x
+        Shape representation.
+    """
+    if isinstance(x, (int, numbers.Integral, np.integer)):
+        shape = (int(x),)
+    elif isinstance(x, tuple) and all(isinstance(item, int) for item in x):
+        shape = x
+    else:
+        try:
+            _ = iter(x)
+        except TypeError as e:
+            raise TypeError(
+                f"The given shape {x} must be an integer or an iterable of integers."
+            ) from e
+
+        if not all(isinstance(item, (int, numbers.Integral, np.integer)) for item in x):
+            raise TypeError(f"The given shape {x} must only contain integer values.")
+
+        shape = tuple(int(item) for item in x)
+
+    if isinstance(ndim, numbers.Integral):
+        if len(shape) != ndim:
+            raise TypeError(f"The given shape {shape} must have {ndim} dimensions.")
+
+    return shape
+
+
+def as_numpy_scalar(x: ScalarArgType, dtype: DTypeArgType = None) -> np.generic:
+    """Convert a scalar into a NumPy scalar.
+
+    Parameters
+    ----------
+    x
+        Scalar value.
+    dtype
+        Data type of the scalar.
+    """
+
+    if np.ndim(x) != 0:
+        raise ValueError("The given input is not a scalar.")
+
+    return np.asarray(x, dtype=dtype)[()]
