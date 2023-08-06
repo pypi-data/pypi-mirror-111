a1ffdcc9a054b8c29560955ed59fb27beb4cab3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:23 +0000

    Add package DatasetsEvaluator/0.0.2/DatasetsEvaluator-0.0.2.tar.gz

---
+++ packages/DatasetsEvaluator/DatasetsEvaluator-0.0.2.tar.gz/DatasetsEvaluator-0.0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='DatasetsEvaluator',
+	version='0.0.2',
+	author='Brett Kennedy',
+	author_email='wm.brett.kennedy@gmail.com',
+	packages=find_packages(),
+)
