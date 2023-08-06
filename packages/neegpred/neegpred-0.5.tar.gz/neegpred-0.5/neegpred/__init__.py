a75e456e4e75d78eac5506a2bf046466d7453138
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:04 +0000

    Add package neegpred/0.5/neegpred-0.5.tar.gz

---
+++ packages/neegpred/neegpred-0.5.tar.gz/neegpred-0.5/neegpred/__init__.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+import mat73
+from matplotlib import pyplot as plt
+import scipy.signal as signal
+import math
+import scipy.stats as stats
+import tqdm
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegressionCV
+from sklearn import metrics
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import GroupKFold
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.model_selection import GridSearchCV
+import pytest
+import pathlib
+from classes_and_functions import *
+from cleanup_func import *
+from tests import *
---
+++ packages/neegpred/neegpred-0.5.tar.gz/neegpred-0.5/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+
+setup(name='neegpred',
+      packages = ['neegpred'],
+      version='0.5',
+      description='predicit liking and BDM results from EEG signals',
+      url='https://github.com/KeshetTadmor/neegpred', 
+      download_url = 'https://github.com/KeshetTadmor/neegpred/archive/refs/tags/0.1.tar.gz', #FILL IN LATER
+      author='Adi Cohen, Nimrod Leberstein & Keshet Tadmor',
+      author_email='nimrodlbr@gmail.com',
+      keywords = ['EEG', 'ML', 'neuro economy'],
+      license='GNU General Public License v3.0',
+      long_description='This package is intended to be used by Prof. Dino Levy, It offers a platform to train\n'
+                       'models that can predict the effect of a given commercial by EEG signal processing'
+                       'and Machine learning',
+
+      install_requires=['pandas','numpy','mat73','matplotlib','scipy','tqdm','seaborn','sklearn','shap','pytest'],
+  
+
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3',  
+        'Programming Language :: Python :: 3.7',
+        ],
+)
