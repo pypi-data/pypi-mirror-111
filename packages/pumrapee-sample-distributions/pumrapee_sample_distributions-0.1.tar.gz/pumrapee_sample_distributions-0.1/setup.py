064ee30600e7ca989b18ca41078cd094539a2f68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:20 +0000

    Add package pumrapee-sample-distributions/0.1/pumrapee_sample_distributions-0.1.tar.gz

---
+++ packages/pumrapee-sample-distributions/pumrapee_sample_distributions-0.1.tar.gz/pumrapee_sample_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='pumrapee_sample_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['pumrapee_sample_distributions'],
+      author = 'Pumrapee Poomka',
+      author_email = 'pumrapee2599@gmail.com',
+      zip_safe=False)
