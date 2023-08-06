9e52b91750eef6e61578460a7d18fb4d4c5ffa5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:58 +0000

    Add package NlpToolkit-SentiNet/1.0.11/NlpToolkit-SentiNet-1.0.11.tar.gz

---
+++ packages/NlpToolkit-SentiNet/NlpToolkit-SentiNet-1.0.11.tar.gz/NlpToolkit-SentiNet-1.0.11/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='NlpToolkit-SentiNet',
+    version='1.0.11',
+    packages=['SentiNet'],
+    url='https://github.com/StarlangSoftware/TurkishSentiNet-Py',
+    license='',
+    author='olcaytaner',
+    author_email='olcay.yildiz@ozyegin.edu.tr',
+    description='Turkish SentiNet'
+)
