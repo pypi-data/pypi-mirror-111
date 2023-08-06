c15db8691a91e9a87b44ef79fd9d3b0ba85a8f81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:26 +0000

    Add package slp/1.2.0/slp-1.2.0-py3-none-any.whl

---
+++ packages/slp/slp-1.2.0-py3-none-any.whl/slp/modules/__init__.py
@@ -0,0 +1,12 @@
+from slp.modules.attention import Attention, MultiheadAttention, attention_scores
+from slp.modules.classifier import Classifier
+from slp.modules.embed import Embed, PositionalEncoding
+from slp.modules.feedforward import FF, MultilayerFF, PositionwiseFF
+from slp.modules.norm import LayerNorm
+from slp.modules.regularization import GaussianNoise
+from slp.modules.rnn import RNN, AttentiveRNN, TokenRNN
+from slp.modules.transformer import (
+    Transformer,
+    TransformerSequenceEncoder,
+    TransformerTokenSequenceEncoder,
+)
