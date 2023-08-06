d1fff1f63ba31678fd1fce0b499937209790e7e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:56 +0000

    Add package capsule-vacuum-vessel/0.0.2/capsule_vacuum_vessel-0.0.2.tar.gz

---
+++ packages/capsule-vacuum-vessel/capsule_vacuum_vessel-0.0.2.tar.gz/capsule_vacuum_vessel-0.0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='capsule_vacuum_vessel',
+    author='Keir',
+    version='0.0.2',
+    author_email='keirhearn@gmail.com',
+    license='MIT',
+    url='https://github.com/Blitan/capsule_vacuum_vessel',
+    description='Package includes a function which returns from 3 given parameters, the coordinates for a capsule vacuum vessel.',
+    packages=["capsule_vacuum_vessel"],
+
+)
