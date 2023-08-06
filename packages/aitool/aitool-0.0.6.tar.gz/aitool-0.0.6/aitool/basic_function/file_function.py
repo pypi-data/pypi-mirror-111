9c8ba05eb8b8b8c7fc58d2a76d7b98dfbf2327e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:11 +0000

    Add package aitool/0.0.6/aitool-0.0.6.tar.gz

---
+++ packages/aitool/aitool-0.0.6.tar.gz/aitool-0.0.6/aitool/basic_function/file_function.py
@@ -0,0 +1,63 @@
+# -*- coding: UTF-8 -*-
+# @Time    : 2020/10/29
+# @Author  : xiangyuejia@qq.com
+import os
+import json
+import pickle
+import pandas as pd
+from typing import Any, List
+
+
+def file_exist(file: str):
+    return os.path.exists(file)
+
+
+def save_json(file: str, obj: Any) -> None:
+    path = os.path.split(file)
+    if not os.path.exists(path):
+        os.makedirs(path)
+    with open(file, 'w', encoding='utf-8') as fw:
+        json.dump(obj, fw)
+
+
+def load_json(file: str) -> Any:
+    if not os.path.isfile(file):
+        print('incorrect file path')
+        raise FileExistsError
+    with open(file, 'r', encoding='utf-8') as fr:
+        return json.load(fr)
+
+
+def save_pickle(obj: Any, file: str) -> None:
+    with open(file, 'wb') as fw:
+        pickle.dump(obj, fw)
+
+
+def load_pickle(file: str) -> Any:
+    if not os.path.isfile(file):
+        print('incorrect file path')
+        raise Exception
+    with open(file, 'rb') as fr:
+        return pickle.load(fr)
+
+
+def read_lines(file: str) -> List[Any]:
+    with open(file, 'r', encoding='utf8') as fin:
+        data = [d.strip() for d in fin.readlines()]
+    return data
+
+
+def write_lines(data: List[Any], file: str) -> None:
+    with open(file, 'w', encoding='utf8') as fout:
+        for d in data:
+            print(d, file=fout)
+
+
+def save_excel(
+        data: List[Any],
+        file: str,
+        index=False,
+        header=False
+) -> None:
+    df = pd.DataFrame(data)
+    df.to_excel(file, index=index, header=header)
