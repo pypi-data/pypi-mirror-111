960b56c234c85d2e55090219363df115cae176fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:14 +0000

    Add package afmformats/0.14.3/afmformats-0.14.3-py3-none-any.whl

---
+++ packages/afmformats/afmformats-0.14.3-py3-none-any.whl/afmformats/fmt_workshop/__init__.py
@@ -0,0 +1,19 @@
+from .ws_single import load_csv
+from .ws_map import load_map
+
+
+recipe_workshop_map = {
+    "descr": "zipped comma-separated values",
+    "loader": load_map,
+    "suffix": ".zip",
+    "modality": "force-distance",
+    "maker": "AFM workshop",
+}
+
+recipe_workshop_single = {
+    "descr": "comma-separated values",
+    "loader": load_csv,
+    "suffix": ".csv",
+    "modality": "force-distance",
+    "maker": "AFM workshop",
+}
