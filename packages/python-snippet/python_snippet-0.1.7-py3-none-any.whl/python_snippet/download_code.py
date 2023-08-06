9f3a6795331cf9033fbf8b58d32d32112bd8a8b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:51 +0000

    Add package python-snippet/0.1.7/python_snippet-0.1.7-py3-none-any.whl

---
+++ packages/python-snippet/python_snippet-0.1.7-py3-none-any.whl/python_snippet/download_code.py
@@ -0,0 +1,43 @@
+import sys
+
+import requests
+from rich.console import Console
+from rich.syntax import Syntax
+
+console = Console()
+
+
+def get_raw_url(url: str):
+    url = url.replace(
+        "https://github.com/", "https://raw.githubusercontent.com/"
+    )
+    url = url.replace("/blob", "")
+    url = url.replace("/tree", "")
+    return url
+
+
+def get_file_name(url: str):
+    return url.split("/")[-1]
+
+
+def get_extension(filename: str):
+    if filename.endswith(".sh"):
+        return "bash"
+    else:
+        return "python"
+
+
+def print_code(url: str, filename: str):
+    raw_url = get_raw_url(url)
+    r = requests.get(raw_url)
+    if r.status_code == 404:
+        console.print(
+            f"Here is the link to the source code: [bold magenta]{url}[/bold magenta]"
+        )
+        sys.exit(1)
+    else:
+        code = r.text
+        console.print("Here is the code for you to copy: :books: \n")
+        console.print(Syntax(code, get_extension(filename)))
+        console.print("\n")
+        return code
---
+++ packages/python-snippet/python_snippet-0.1.7-py3-none-any.whl/python_snippet/utils.py
@@ -0,0 +1,126 @@
+import sys
+
+import questionary
+from decorator import decorator
+from questionary import Choice
+from rich.console import Console
+
+from .download_code import get_file_name, print_code
+
+console = Console()
+
+CATEGORY_TO_SUBCATEGORY = {
+    "Python": [
+        "Number",
+        "Boolean",
+        "String",
+        "List",
+        "Tuple",
+        "Dictionary",
+        "Datetime",
+        "Function",
+        "Classes",
+        "Files",
+        "Error handling",
+        "Interact with terminal",
+        "Best practices",
+        "Code speed",
+    ],
+    "Pandas": ["Change Values", "Get Values", "Testing"],
+    "NumPy": [],
+    "Data Science Tools": [
+        "Testing",
+        "Data",
+        "Feature extraction",
+        "Visualization",
+        "Sharing and downloading",
+        "Natural language processing",
+        "Tools for best Python practices",
+        "Speed up code",
+        "Better Pandas",
+        "Machine learning",
+    ],
+    "Terminal": [
+        "Text",
+        "Files",
+        "Tracking",
+        "Python",
+        "Prettify Terminal",
+        "Sharing",
+        "Productive hacks",
+    ],
+    "Cool Tools": [
+        "Better output",
+        "Tracking",
+        "Data",
+        "Automation",
+        "Git and GitHub",
+        "Alternative Approach",
+    ],
+    "Jupyter Notebook": [],
+}
+
+
+@decorator
+def handle_exception(func, *args, **kwargs):
+    try:
+        val = func(*args, **kwargs)
+        if val is None:
+            sys.exit(1)
+        return val
+    except KeyboardInterrupt:
+        console.print("Cancelled by user")
+    except ValueError:
+        console.print("No results found")
+        sys.exit(1)
+
+
+@handle_exception
+def get_post_from_questionary(choices: list):
+    return questionary.select(
+        "Which post do you want to choose?", choices=choices
+    ).ask()
+
+
+@handle_exception
+def get_save_code_from_questionary():
+    return questionary.select(
+        "Would you like to save the code snippet?", choices=["Yes", "No"]
+    ).ask()
+
+
+def get_code(code_link: str):
+    if code_link == "":
+        return
+
+    file_name = get_file_name(code_link)
+    if file_name.endswith(".ipynb"):
+        console.print(
+            "This is a notebook so the source code will not be printed on the terminal."
+        )
+    else:
+        code = print_code(code_link, file_name)
+
+    # if code is not None:
+
+    save_code = get_save_code_from_questionary()
+
+    if save_code == "Yes":
+        with open(file_name, "w") as f:
+            f.write(code)
+        console.print(
+            f"The file {file_name} is saved to your current directory :sparkles:"
+        )
+
+
+def get_color(num: int):
+    if num % 2 == 0:
+        return "fg:#809bce bold"
+    return "fg:#d6eadf"
+
+
+def get_choices(titles: list):
+    return [
+        Choice(title=[(get_color(i), choice)])
+        for i, choice in enumerate(titles)
+    ]
