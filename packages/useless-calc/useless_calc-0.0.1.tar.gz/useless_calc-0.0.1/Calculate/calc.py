0a1fcc50c954134f6daa0da9541fabff3be72b08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:47:52 +0000

    Add package useless-calc/0.0.1/useless_calc-0.0.1.tar.gz

---
+++ packages/useless-calc/useless_calc-0.0.1.tar.gz/useless_calc-0.0.1/Calculate/calc.py
@@ -0,0 +1,14 @@
+import os
+class Calc:
+    def __init__(self):
+        pass
+    def sum(self,a,b):
+        return a+b
+    def subtract(self,a,b):
+        return a-b 
+    def multiply(self,a,b):
+        return a*b
+    def divide(self,a,b):
+        return a/b
+
+calculator = Calc()
---
+++ packages/useless-calc/useless_calc-0.0.1.tar.gz/useless_calc-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='useless_calc',
+    url='https://github.com/jagadeesh-r1/calculate_silly',
+    author='Jagadeesh Reddy',
+    author_email='mail4jagadeeshreddy@gmail.com',
+    packages=['Calculate'],
+    version='0.0.1',
+    license='GNU General Public License v3.0',
+    description='dont use this.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown'
+)
