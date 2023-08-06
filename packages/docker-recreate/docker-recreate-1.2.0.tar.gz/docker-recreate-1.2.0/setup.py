3fcc2c2165b6cfb72c240330883637702931e5b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:15 +0000

    Add package docker-recreate/1.2.0/docker-recreate-1.2.0.tar.gz

---
+++ packages/docker-recreate/docker-recreate-1.2.0.tar.gz/docker-recreate-1.2.0/setup.py
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+from setuptools import setup
+import os
+import subprocess
+
+
+def get_version():
+    version = os.environ.get('VERSION_NAME', None)
+    if version:
+        return version
+    output = subprocess.check_output(['git', 'describe', '--tags'])
+    output = output.decode('utf8')
+    return output.strip()
+
+
+setup(
+    name='docker-recreate',
+    version=get_version(),
+    description='Get docker run command from container',
+    author='Jeffrey Zhang',
+    author_email='zhang.lei.fly@gmail.com',
+    url='https://www.github.com/jeffrey4l/docker-recreate',
+    home_page='https://www.github.com/jeffrey4l/docker-recreate',
+    entry_points={
+        'console_scripts': [
+            'docker-recreate=main:main'
+        ]},
+    py_modules=['main'],
+    )
