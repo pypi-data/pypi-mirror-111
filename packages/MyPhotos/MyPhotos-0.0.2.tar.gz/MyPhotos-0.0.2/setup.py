1df875c917b40bed6d4e975429e00d1c27abed43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:42 +0000

    Add package MyPhotos/0.0.2/MyPhotos-0.0.2.tar.gz

---
+++ packages/MyPhotos/MyPhotos-0.0.2.tar.gz/MyPhotos-0.0.2/setup.py
@@ -0,0 +1,50 @@
+# setup.py for MyPhotos
+#
+# Direct install (all systems):
+#   "python setup.py install"
+#
+# For Python 3.x use the corresponding Python executable,
+# e.g. "python3 setup.py ..."
+#
+# (C) 2021 ZouMingzhe <zoumingzhe@qq.com>
+#
+# SPDX-License-Identifier:    MIT
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name="MyPhotos",
+    description="Archive photos for quick search.",
+    version="0.0.2",
+    author="Zou Mingzhe",
+    author_email="zoumingzhe@qq.com",
+    url="https://github.com/zoumingzhe/MyPhotos",
+    packages=[
+        'MyPhotos',
+        'MyPhotos.config',
+        'MyPhotos.data',
+    ],
+    license="MIT",
+    long_description=open('..\README.rst', mode='r', encoding='UTF-8').read(),
+    install_requires=[
+        'xlrd',
+        'xlwt',
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: Chinese (Simplified)',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    platforms='any',
+)
