7c5f6ee14bf966997dea915462ef638974d0a730
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:32 +0000

    Add package pyjamalib/1.0.0/pyjamalib-1.0.0.tar.gz

---
+++ packages/pyjamalib/pyjamalib-1.0.0.tar.gz/pyjamalib-1.0.0/pyjamalib/__init__.py
@@ -0,0 +1,10 @@
+from pyjamalib.DataHandler import *
+from pyjamalib.DataProcessing import *
+from pyjamalib.DataVisualisation import *
+from pyjamalib.DataAnalysis import *
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import scipy
+import csv
---
+++ packages/pyjamalib/pyjamalib-1.0.0.tar.gz/pyjamalib-1.0.0/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+ 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+  
+setuptools.setup(
+
+    name="pyjamalib",
+    packages = ['pyjamalib'],
+    version="1.0.0",
+    author="Túlio 1F. Almeida",
+    author_email="tuliofalmeida@hotmail.com",
+    description="A library for analyze joint angles from IMU data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tuliofalmeida/pyjama",
+    install_requires= ['numpy',
+                      'pandas',
+                      'matplotlib'],
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
