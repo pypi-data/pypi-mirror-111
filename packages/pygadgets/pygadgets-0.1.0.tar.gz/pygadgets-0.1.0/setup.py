dbcea1fc5d4bdddad8ed81a8501230d3e7ee9a2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:30 +0000

    Add package pygadgets/0.1.0/pygadgets-0.1.0.tar.gz

---
+++ packages/pygadgets/pygadgets-0.1.0.tar.gz/pygadgets-0.1.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.1.0'
+DESCRIPTION = 'programming resources and utils'
+LONG_DESCRIPTION = 'package to maintain my programming resources and utilities'
+
+setup(
+    name="pygadgets",
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    author="pnowk",
+    author_email="pnowk123@gmail.com",
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'twine', 'python-dotenv', 'wheel', 'black', 'bumpversion', 'pytest'
+    ],
+    keywords='python',
+    classifiers= [
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        'License :: OSI Approved :: MIT License',
+        "Programming Language :: Python :: 3",
+    ]
+)
