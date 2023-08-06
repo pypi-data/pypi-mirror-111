3e474907f4ed9baa1e5a15f4ecd4cc57a0ddbbff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:52 +0000

    Add package backfillz/0.1.4/backfillz-0.1.4-py3-none-any.whl

---
+++ packages/backfillz/backfillz-0.1.4-py3-none-any.whl/backfillz/data.py
@@ -0,0 +1,64 @@
+from dataclasses import dataclass
+from math import floor
+from typing import Any, Dict, List
+
+import numpy as np
+from stan.fit import Fit  # type: ignore
+
+
+@dataclass
+class MCMCRun:
+    """A Stan fit, plus some derived data."""
+
+    samples: Fit
+
+    def iter_chains(self, param: str, index: int = 0) -> np.ndarray:
+        """Return (n_chains × n_samples) matrix of draws for a given parameter."""
+        n_chains, n_samples = self.samples.num_chains, self.samples.num_samples
+        xss = np.zeros((n_chains, n_samples))
+        for n in range(0, n_chains):
+            xss[n] = self.samples[param][index][n * n_samples: (n + 1) * n_samples]
+        return xss
+
+    @property
+    def params(self) -> List[str]:
+        return list(self.samples.param_names)
+
+
+@dataclass
+class Slice:
+    """A slice of an MCMC trace."""
+
+    lower: float
+    upper: float
+
+
+Param = str
+Slices = Dict[Param, List[Slice]]
+Props = Dict[str, Any]
+
+
+@dataclass
+class ParameterSlices:
+    """The MCMC data being presented."""
+
+    slcs: List[Slice]
+    param: str
+    chains: np.ndarray  # shape is [n, n_iter] where n is number of chains
+    max_sample: float
+    min_sample: float
+
+    @property
+    def n_iter(self) -> int:
+        """Return number of MCMC iterations per chain."""
+        return int(self.chains.shape[1])
+
+    def chain_slices(self, slc: Slice) -> List[np.ndarray]:
+        """The specified slice of each chain."""
+        return [
+            self.chains[
+                n,
+                floor(slc.lower * self.n_iter):floor(slc.upper * self.n_iter)
+            ]
+            for n, _ in enumerate(self.chains)
+        ]
