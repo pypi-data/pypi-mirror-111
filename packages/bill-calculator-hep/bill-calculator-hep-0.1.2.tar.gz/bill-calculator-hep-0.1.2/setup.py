aa6f0cc9247397077db1ef788beee2ac6302198d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:14:22 +0000

    Add package bill-calculator-hep/0.1.2/bill-calculator-hep-0.1.2.tar.gz

---
+++ packages/bill-calculator-hep/bill-calculator-hep-0.1.2.tar.gz/bill-calculator-hep-0.1.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bill-calculator-hep", # This needs to change soon
+    version="0.1.2",
+    author="Maria P. Acosta F./HEPCloud project",
+    author_email="macosta@fnal.gov",
+    description="Billing calculations and threshold alarms for hybrid cloud setups",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    url="https://github.com/HEPCloud/billing-calculator",
+    packages=setuptools.find_packages(where='src'),
+    package_dir={'': 'src', },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.4',
+)
