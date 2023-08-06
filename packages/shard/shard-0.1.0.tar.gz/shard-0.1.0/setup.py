9aacbcbc1465dc2f3876615c77a69cbe5b898abb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:18:59 +0000

    Add package shard/0.1.0/shard-0.1.0.tar.gz

---
+++ packages/shard/shard-0.1.0.tar.gz/shard-0.1.0/setup.py
@@ -0,0 +1,21 @@
+import pathlib
+
+from setuptools import setup
+
+setup(
+    name="shard",
+    version="0.1.0",
+    url="https://github.com/shard-dev/shard",
+    author="Bruno Amaral",
+    author_email="amaralbf@gmail.com",
+    license="BSD-3-Clause",
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    packages=["shard"],
+)
