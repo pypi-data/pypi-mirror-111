f3ee3b9c9d78e38fee4d7aa48ebac2365122bdaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:38 +0000

    Add package dataplotter/0.0.4/dataplotter-0.0.4-py3-none-any.whl

---
+++ packages/dataplotter/dataplotter-0.0.4-py3-none-any.whl/dataplotter/__main__.py
@@ -0,0 +1,31 @@
+# argparse for parsing of incoming arguments
+import argparse
+
+# argparse to parse arguments
+from DataPlotter.Models import DataSet
+from DataPlotter.Models import DataPlotter
+
+
+# parse input with the help of an argument parser
+parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter)
+# input path to specimen folder
+parser.add_argument('-file', help='Path to file')
+
+# read arguments
+args = parser.parse_args()
+
+# get the file path from arguments
+filePath = args.file
+
+# create the dataplotter
+plotter = DataPlotter(filePath)
+
+# plot the data
+plotter.plot()
+
+# calculate the difference of first columns
+#   start with 1, bc 0 is x value
+plotter.filterData(lambda l: l+ [l[2]-l[1]])
+
+# plot the data
+plotter.plot()
