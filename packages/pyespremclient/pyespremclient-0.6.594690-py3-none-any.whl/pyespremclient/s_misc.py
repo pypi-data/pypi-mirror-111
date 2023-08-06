141cb6a9800b2d6a6853c88cccda1b07089fb13d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:26 +0000

    Add package pyespremclient/0.6.594690/pyespremclient-0.6.594690-py3-none-any.whl

---
+++ packages/pyespremclient/pyespremclient-0.6.594690-py3-none-any.whl/pyespremclient/s_misc.py
@@ -0,0 +1,29 @@
+
+#####################################################################
+#
+# ESPREM Client Misc
+#
+# Project   : PYESPREMCLIENT
+# Author(s) : Zafar Iqbal < zaf@saparc.gr >
+# Copyright : (C) 2021 SPARC PC < https://sparc.space/ >
+#
+# All rights reserved. No warranty, explicit or implicit, provided.
+# SPARC PC is and remains the owner of all titles, rights
+# and interests in the Software.
+#
+#####################################################################
+
+import atexit
+
+from . import s_log  
+
+#####################################################################
+
+def cleanup( ) :
+    s_log.write_msg( "CLEANUP" )
+
+#####################################################################
+
+atexit.register( cleanup )
+s_log.write_msg( "Registered cleanup" )
+    
