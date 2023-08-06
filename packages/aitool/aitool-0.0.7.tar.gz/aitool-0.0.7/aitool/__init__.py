ef925b6feb7d362e27d10c00741bbc7162a22dd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:58 +0000

    Add package aitool/0.0.7/aitool-0.0.7.tar.gz

---
+++ packages/aitool/aitool-0.0.7.tar.gz/aitool-0.0.7/aitool/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: UTF-8 -*-
+# @Time    : 2020/10/22
+# @Author  : xiangyuejia@qq.com
+
+from aitool.basic_function.file_function import file_exist as file_exist
+from aitool.basic_function.file_function import save_json as save_json
+from aitool.basic_function.file_function import load_json as load_json
+from aitool.basic_function.singleton import singleton as singleton
+from aitool.basic_function.time_function import exe_time as exe_time
