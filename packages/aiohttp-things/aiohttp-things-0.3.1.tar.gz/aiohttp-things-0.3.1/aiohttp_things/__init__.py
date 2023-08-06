7a2569e9c0acd0ee216d8c446cdbf870bdb270a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:50 +0000

    Add package aiohttp-things/0.3.1/aiohttp-things-0.3.1.tar.gz

---
+++ packages/aiohttp-things/aiohttp-things-0.3.1.tar.gz/aiohttp-things-0.3.1/aiohttp_things/__init__.py
@@ -0,0 +1,4 @@
+from aiohttp_things import views
+
+__version__ = '0.3.1'
+__all__ = ['views']
---
+++ packages/aiohttp-things/aiohttp-things-0.3.1.tar.gz/aiohttp-things-0.3.1/aiohttp_things/views.py
@@ -0,0 +1,37 @@
+from abc import ABCMeta
+from typing import Any, Callable, Mapping
+
+from aiohttp.abc import AbstractView
+from aiohttp.web import Request
+
+
+class ContextMixin(AbstractView, metaclass=ABCMeta):
+    """
+    Class based view mixin with context attribute.
+    """
+    #: Dictionary for JSON response or HTML template response.
+    context: Mapping[str, Any]
+
+    def __init__(self, request: Request) -> None:
+        super().__init__(request)
+        self.context = {}
+
+
+class ItemMixin(ContextMixin, metaclass=ABCMeta):
+    """
+    Class based view mixin with primary key attribute.
+    """
+    #: Primary key from path variable for select an object from database.
+    pk: Any
+
+    #: Callable object for converting a primary key.
+    pk_factory: Callable[..., Any] = lambda pk: pk
+
+    def __init__(self, request: Request) -> None:
+        super().__init__(request)
+        # Use regular expressions in routes for safe input
+        self.pk = self.pk_factory(self.request.match_info.get('pk'))
+
+
+# synonyms
+PrimaryKeyMixin = ItemMixin
