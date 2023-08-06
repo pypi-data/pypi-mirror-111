a3f4099a988f2a35783231e1163136f0ecfdb64e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:28 +0000

    Add package sparkautomapper.fhir/0.2.21b8/sparkautomapper.fhir-0.2.21b8-py3-none-any.whl

---
+++ packages/sparkautomapper.fhir/sparkautomapper.fhir-0.2.21b8-py3-none-any.whl/spark_auto_mapper_fhir/generator/test_generator.py
@@ -0,0 +1,14 @@
+from pprint import pprint
+
+from spark_auto_mapper_fhir.generator.fhir_xml_schema_parser import FhirXmlSchemaParser
+
+
+def test_generator() -> None:
+    # fhir_entities = FhirXmlSchemaParser.generate_classes(
+    #     filter_to_resource="appointment"
+    # )
+    fhir_entities = FhirXmlSchemaParser.generate_classes()
+
+    # now print the result
+    for fhir_entity in fhir_entities:
+        pprint(fhir_entity)
