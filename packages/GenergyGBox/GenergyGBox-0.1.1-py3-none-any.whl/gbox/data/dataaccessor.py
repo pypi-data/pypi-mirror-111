a3aa60a9093f519e4366ffd47482fcebb9edb023
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:15 +0000

    Add package GenergyGBox/0.1.1/GenergyGBox-0.1.1-py3-none-any.whl

---
+++ packages/GenergyGBox/GenergyGBox-0.1.1-py3-none-any.whl/gbox/data/dataaccessor.py
@@ -0,0 +1,7 @@
+class DataAccessor(object):
+    def __init__(self):
+        print("This is a DataAccessor init function")
+
+    @staticmethod
+    def method():
+        print("This is DataAccessor static method")
---
+++ packages/GenergyGBox/GenergyGBox-0.1.1-py3-none-any.whl/gbox/gpio/gpiomanager.py
@@ -0,0 +1,7 @@
+class GPIOController(object):
+    def __init__(self):
+        print("This is a GPIOController function")
+
+    @staticmethod
+    def method():
+        print("This is GPIOController method")
