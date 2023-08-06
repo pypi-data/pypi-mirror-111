69684eca53f44ff3c9275efc2700c8522f620a9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:35:57 +0000

    Add package ArcProbeInterface/0.1.1/ArcProbeInterface-0.1.1.tar.gz

---
+++ packages/ArcProbeInterface/ArcProbeInterface-0.1.1.tar.gz/ArcProbeInterface-0.1.1/src/ArcProbeInterface/syncapi.py
@@ -0,0 +1,16 @@
+from websocket import create_connection
+
+from src.ArcProbeInterface.baseapi import API
+
+
+class SyncAPI(API):
+
+    def fetch_data(self):
+        result = {"scores": []}
+        conn = create_connection("wss://arc.estertion.win:616", timeout=self.timeout)
+        conn.send(f"{self.user_code} {self.start_const} {self.end_const}")
+        while True:
+            _r = conn.recv()
+            if self.manage_data(_r, result):
+                break
+        return result
