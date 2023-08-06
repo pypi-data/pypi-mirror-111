c4717c45995ebf5d23e4649d7381bd9a310ae11b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:02 +0000

    Add package xieminxuan10/1.0.2/xieminxuan10-1.0.2.tar.gz

---
+++ packages/xieminxuan10/xieminxuan10-1.0.2.tar.gz/xieminxuan10-1.0.2/README.md
@@ -0,0 +1,8 @@
+# 玩玩而已
+
+#删除了suiji()，random.randint()的镜像
+
+#xuanze()同理
+
+
+
---
+++ packages/xieminxuan10/xieminxuan10-1.0.2.tar.gz/xieminxuan10-1.0.2/xieminxuan10.py
@@ -0,0 +1,14 @@
+#这是xieminxuan10模块，version==1.0.1
+def out(a):
+    #一镜像
+    return a
+def mathplus(c,plus=1):
+    return c + plus
+def bey(sleep=0):
+    import time
+    time.sleep(sleep)
+    quit()
+def initme():
+    return "测试"
+def mathsubtract(a,subtract=1):
+    return a-subtract
