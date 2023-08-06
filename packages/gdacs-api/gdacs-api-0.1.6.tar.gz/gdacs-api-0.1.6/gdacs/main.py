47dd5867e773c387941f284597dc8a3268b0a836
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:12 +0000

    Add package gdacs-api/0.1.6/gdacs-api-0.1.6.tar.gz

---
+++ packages/gdacs-api/gdacs-api-0.1.6.tar.gz/gdacs-api-0.1.6/gdacs/main.py
@@ -0,0 +1,15 @@
+from api import GDACSAPIReader
+
+client = GDACSAPIReader()
+
+# get latest events
+# latest_events = client.latest_events()
+# latest_events = client.latest_events(event_type="FL", historical="7d")
+# latest_events = client.latest_events(event_type="EQ", historical="24h", limit=1000)
+# print(len(latest_events))
+
+# get single event
+#event = client.get_event(event_type='TC', event_id='1000132', episode_id='8')
+# event = client.get_event(event_type='DR', event_id='1012428', episode_id='9', source_format='geojson', cap_file=False)
+event = client.get_event(event_type='TC', event_id='1000132', episode_id='8', source_format='shp')
+print(event)
