6e29e72948ab4e6c15b90c49d480f570c286d817
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:39 +0000

    Add package deepmd-kit/2.0.0b3/deepmd_kit-2.0.0b3-cp38-cp38-manylinux2010_x86_64.whl

---
+++ packages/deepmd-kit/deepmd_kit-2.0.0b3-cp38-cp38-manylinux2010_x86_64.whl/deepmd/entrypoints/__init__.py
@@ -0,0 +1,24 @@
+"""Submodule that contains all the DeePMD-Kit entry point scripts."""
+
+from .compress import compress
+from .config import config
+from .doc import doc_train_input
+from .freeze import freeze
+from .test import test
+from .train import train
+from .transfer import transfer
+from ..infer.model_devi import make_model_devi
+from .convert import convert
+
+__all__ = [
+    "config",
+    "doc_train_input",
+    "freeze",
+    "test",
+    "train",
+    "transfer",
+    "compress",
+    "doc_train_input",
+    "make_model_devi",
+    "convert",
+]
