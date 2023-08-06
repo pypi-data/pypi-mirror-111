1245dd5df53a9fbd02340de53897cf36134b8d99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:18 +0000

    Add package sylo/1.1.8/sylo-1.1.8.tar.gz

---
+++ packages/sylo/sylo-1.1.8.tar.gz/sylo-1.1.8/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+
+VERSION = "1.1.8"
+
+
+with open("README.md") as f:
+    readme = f.read()
+
+
+setup(
+    name="sylo",
+    version=VERSION,
+    description="SYLO Python Pomodoro Timer",
+    long_description=readme,
+    long_description_content_type="text/markdown",  # This is important!
+    url="http://github.com/rob-parker-what/sylo",
+    author="Rob Parker",
+    author_email="robparkerwhat.dev@gmail.com",
+    license="MIT",
+    packages=["sylo"],
+    entry_points={
+        "console_scripts": ["sylo=sylo.cmdline:main"],
+    },
+    install_requires=[
+        "colorama==0.4.4",
+        "simpleaudio==1.0.4",
+        "beepy==1.0.7",
+    ],
+    package_data={"sylo": ["audio/*.wav"]},
+    zip_safe=False,
+    python_requires='>=3',
+)
---
+++ packages/sylo/sylo-1.1.8.tar.gz/sylo-1.1.8/sylo/cmdline.py
@@ -0,0 +1,7 @@
+from sylo.timer import run
+from sylo.args import get_arguments
+
+
+def main():
+    args = get_arguments()
+    run(args)
