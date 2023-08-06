7114da4ef871d2983fc6a0b3e5d5761da3d49430
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:38 +0000

    Add package zohobooks-api/1.0/zohobooks-api-1.0.tar.gz

---
+++ packages/zohobooks-api/zohobooks-api-1.0.tar.gz/zohobooks-api-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='zohobooks-api',
+    version='1.0',
+    packages=['zohobooks', 'zohobooks.api', 'zohobooks.util', 'zohobooks.model', 'zohobooks.parser', 'zohobooks.service', 'zohobooks.exception'],
+    url='',
+    license='MIT',
+    author='Sagar',
+    author_email='sagarnrng17@gmail.com',
+    description='Zoho Books Python Client Library'
+)
---
+++ packages/zohobooks-api/zohobooks-api-1.0.tar.gz/zohobooks-api-1.0/zohobooks/model/PlaceholderAddressFormat.py
@@ -0,0 +1,49 @@
+# $Id$
+
+from zohobooks.model.Customer import Customer
+from zohobooks.model.Organization import Organization
+
+
+class PlaceholderAddressFormat:
+    """This class is used to create object for Palceholders Address Format."""
+
+    def __init__(self):
+        """Initialize parameters for placeholders address format."""
+        self.organization = Organization()
+        self.customer = Customer()
+
+    def set_organization(self, organization):
+        """Set organization.
+
+        Args:
+            organization(instance): Organization object.
+
+        """
+        self.organization = Organization()
+
+    def get_organization(self):
+        """Get organization.
+
+        Returns:
+            instance: Organization object.
+
+        """
+        return self.organization
+
+    def set_customer(self, customer):
+        """Set customer.
+
+        Args:
+            customer(instance):Customer object.
+
+        """
+        self.customer = customer
+
+    def get_customer(self):
+        """Get customer.
+
+        Returns:
+            instance: Customer object.
+
+        """
+        return self.customer
