8adc468d696cc700f5485b6b977d6a0c3822c6d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:58 +0000

    Add package simplechinese/0.2.10/simplechinese-0.2.10.tar.gz

---
+++ packages/simplechinese/simplechinese-0.2.10.tar.gz/simplechinese-0.2.10/simplechinese/pinyin.py
@@ -0,0 +1,9 @@
+import simplepinyin as sp
+
+lm = sp.Languagle_Model()
+
+def str2pinyin(words, hasTone=False):
+    return lm.translate_c2p(words, hasTone=hasTone)
+
+def pinyin2str(pinyins):
+    return lm.translate_p2c(pinyins.strip().split())
