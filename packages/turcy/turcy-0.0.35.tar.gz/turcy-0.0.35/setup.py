c560a10bdf66b150f72cb05d9f0d583592fb3527
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:14:10 +0000

    Add package turcy/0.0.35/turcy-0.0.35.tar.gz

---
+++ packages/turcy/turcy-0.0.35.tar.gz/turcy-0.0.35/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+# python -m build
+# twine upload dist/*
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="turcy", # Replace with your own username
+    version="0.0.35",
+    author="Christian Klose",
+    author_email="chris.klose@gmx.net",
+    description="A package for German Open Informtion Extraction",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ChrisChross/turCy",
+    packages=setuptools.find_packages(),
+    data_files=[('', ['turcy/patterns_small.jsonl', 'turcy/patterns_large.jsonl'])],
+    include_package_data=True,
+    keywords="openie turcy information extraction spacy",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
