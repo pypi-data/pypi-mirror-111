0f7369192fc58255455c70b57ed86cb7aad3b419
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:38 +0000

    Add package MyPhotos/0.0.1/MyPhotos-0.0.1.tar.gz

---
+++ packages/MyPhotos/MyPhotos-0.0.1.tar.gz/MyPhotos-0.0.1/MyPhotos/__init__.py
@@ -0,0 +1,15 @@
+# Copyright (c) 2021 ZouMingzhe <zoumingzhe@qq.com>
+# This module is part of the MyPhotos package, which is released under a MIT licence.
+
+"""
+for config
+"""
+from .config.selfconfig     import (selfconfig)
+
+
+"""
+for data
+"""
+from .data.fbasic           import (fbasic)
+from .data.hash             import (hash)
+from .data.xls              import (xls)
