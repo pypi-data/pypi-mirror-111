908e2472a60edabe4a2166ebf299463d0b5edc77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:42 +0000

    Add package Sansorchi/3.1/Sansorchi-3.1.tar.gz

---
+++ packages/Sansorchi/Sansorchi-3.1.tar.gz/Sansorchi-3.1/Sansorchi/__init__.py
@@ -0,0 +1,26 @@
+from .data import fa
+
+Json = fa["word"]
+
+
+def sansor(txt: str, bad=Json, lang="fa", **kwargs) -> str:
+    """This function takes an obligatory input called
+    txt and deletes it if there is an insult in it
+    >>> sansor('موز خر است') == "م**ز خ**ر است' # Farsi
+    """
+
+    if lang == "fa":
+        txt = txt.replace("‌", "")
+
+    split = txt.split(" ")
+
+    a = ""
+    for i in split:
+        if i in bad:
+            badi = str(i[0]) + "**" + str(i[-1])
+            a += badi
+        else:
+            a += i
+        if split[-1] != i:
+            a += " "
+    return a
---
+++ packages/Sansorchi/Sansorchi-3.1.tar.gz/Sansorchi-3.1/test/test.py
@@ -0,0 +1,18 @@
+from Sansorchi import data, sansor
+
+txts = ["تو موزی موز", "خنگ خدا", "خر به پیش", "حیوانی وحشی"]
+outputs = ["تو موزی م**ز", "خ**گ خدا", "خ**ر به پیش", "ح**ی وحشی"]
+
+
+def test_sentences():
+    i = 0
+    for txt in txts:
+        i += 1
+        assert sansor(txt) == outputs[i - 1]
+
+
+def test_word():
+    words = data.fa["word"]
+    for word in words:
+
+        assert sansor(word) == word[0] + "**" + word[-1]
