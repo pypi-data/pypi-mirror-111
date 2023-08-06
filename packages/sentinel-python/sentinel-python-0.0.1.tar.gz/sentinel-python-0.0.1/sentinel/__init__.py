8f82ddd9395489f36d6dc316fa6ed2f90f567c89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:36 +0000

    Add package sentinel-python/0.0.1/sentinel-python-0.0.1.tar.gz

---
+++ packages/sentinel-python/sentinel-python-0.0.1.tar.gz/sentinel-python-0.0.1/sentinel/__init__.py
@@ -0,0 +1,13 @@
+"""
+Sentinel
+~~~~~~~
+
+⚡️ Package for using Discord slash commands
+
+:copyright: (c) 2021 xezzz
+:license: MIT, see LICENSE for more details.
+"""
+
+from .rest import *
+from .struct.client import SentinelClient
+from .struct.types import Embed, User, Context, Activity, Status
