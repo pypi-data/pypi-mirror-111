72cecd2d091da7504271ddfc0fac8bf258ffcc50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:47 +0000

    Add package epath/0.5/epath-0.5.tar.gz

---
+++ packages/epath/epath-0.5.tar.gz/epath-0.5/setup.py
@@ -0,0 +1,27 @@
+#from distutils.core import setup
+from setuptools import setup, find_packages
+
+setup(
+  name = 'epath',          
+  version = '0.5',      
+  license='MIT',
+  description = 'Easy path for python import/save/load process',   
+  author = 'A.Akdogan',                   
+  author_email = 'adem.akdogan92@gmail.com',      
+  url = 'https://github.com/ademakdogan/epath',   
+  #download_url = 'https://github.com/.../v_01.tar.gz', 
+  keywords = ['PATH', 'ROUTE'],  
+  packages = find_packages(),
+  classifiers=[
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+  ],
+)
