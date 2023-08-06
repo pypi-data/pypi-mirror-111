3966469f2a4b6aef9fb1d4dc4ba630c03e67283c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:21 +0000

    Add package nwpm/0.1.12/nwpm-0.1.12.tar.gz

---
+++ packages/nwpm/nwpm-0.1.12.tar.gz/nwpm-0.1.12/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1.12'
+
+setup(name='nwpm',
+      version=version,
+      description="Navigator Workspace Pack Manager",
+      long_description="""\
+nwpm is a Workspace Pack Manager for Navigator like npm for node.js""",
+      classifiers=[],
+      keywords=["nwpm", "navigator", "workspace_pack"],
+      author='Alan Hsu',
+      author_email='xkevas@qq.com',
+      url='https://github.com/xkevas24/nwpm',
+      license='Apache-2.0 License',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[],
+      python_requires='>=3.6',
+      )
