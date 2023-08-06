b31eaae49761acdf06dc394d3c6ce2262767179c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:17:13 +0000

    Add package sdc-api-py/1.2.8/sdc_api_py-1.2.8.tar.gz

---
+++ packages/sdc-api-py/sdc_api_py-1.2.8.tar.gz/sdc_api_py-1.2.8/sdc_api_py/classes/Bots.py
@@ -0,0 +1,30 @@
+from discord.ext.commands import Bot
+
+
+class Global:
+    SDC_token = ""
+    _time = 60
+
+
+class Bots:
+
+    def __init__(self, bot: Bot, token):
+        if not token.startswith("SDC "):
+            token = "SDC " + token
+        self.SDC_token = token
+        self.bot = bot
+        Global.SDC_token = self.SDC_token
+
+    def create_loop(self, time=60):
+        time = int(time)
+
+        if time < 30:
+            time = 30
+
+        Global._time = time
+        try:
+            self.bot.load_extension(".classes.extension", package="sdc_api_py")
+        except Exception as e:
+            print(e)
+        else:
+            print(f"SDC_API: Цикл успешно запущен. Статистика будет обновляться каждые {Global._time} минут")
