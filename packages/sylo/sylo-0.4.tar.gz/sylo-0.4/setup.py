d778e3cf3682e26b89b7e00dff3c95d1ea648a3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:14 +0000

    Add package sylo/0.4/sylo-0.4.tar.gz

---
+++ packages/sylo/sylo-0.4.tar.gz/sylo-0.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name='sylo',
+      version='0.4',
+      description='SYLO Python Pomodoro Timer',
+      url='http://github.com/rob-parker-what/sylo',
+      author='Rob Parker',
+      author_email='robparkerwhat.dev@gmail.com',
+      license='MIT',
+      packages=['sylo'],
+      entry_points={
+          'console_scripts': ['sylo=sylo.cmdline:main'],
+      },
+      install_requires=[
+            'colorama==0.4.4',
+            'simpleaudio==1.0.4',
+            'beepy==1.0.7',
+      ],
+      zip_safe=False)
