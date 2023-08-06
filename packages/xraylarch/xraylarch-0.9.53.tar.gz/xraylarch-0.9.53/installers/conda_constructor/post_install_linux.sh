6b30cf146d50689311fa71dd8ff960f08a41e166
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:53 +0000

    Add package xraylarch/0.9.53/xraylarch-0.9.53.tar.gz

---
+++ packages/xraylarch/xraylarch-0.9.53.tar.gz/xraylarch-0.9.53/installers/conda_constructor/post_install_linux.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# $2 is the install location, ($HOME by default)
+if [ xx$PREFIX == 'xx' ]; then
+    PREFIX=$(echo "$2/__NAME_LOWER__" | sed -e 's,//,/,g')
+fi
+
+echo " THIS IS POST INSTALL LINUX  PREFIX " $PREFIX
+# force install of packages from conda-forge
+echo "### $PREFIX/bin/conda install -yc conda-forge wxpython tomopy "
+
+$PREFIX/bin/conda install --force-reinstall -yc conda-forge  wxpython tomopy pymatgen pycifrw
+
+# use pip to install some known-safe-for-pip packages
+$PREFIX/bin/pip install xraylarch pyepics epicsapps psycopg2-binary pyFAI numdifftools
+
+# make desktop icons
+$PREFIX/bin/python $PREFIX/bin/larch -m
+
+echo '# Larch post install done!'
+sleep 1
---
+++ packages/xraylarch/xraylarch-0.9.53.tar.gz/xraylarch-0.9.53/installers/conda_constructor/post_install_windows.bat
@@ -0,0 +1,12 @@
+
+REM # install wxpython and tomopy from conda-forge
+call %PREFIX%\condabin\conda install --force-reinstall -yc conda-forge wxpython tomopy pymatgen pycifrw
+
+REM # use pip to install some known-safe-for-pip packages
+%PREFIX%\Scripts\pip.exe install xraylarch pyepics epicsapps psycopg2-binary pyFAI numdifftools
+
+REM # make desktop icons
+%PREFIX%\Scripts\larch.exe -m
+
+echo '# Larch post install done!'
+timeout /t 10
