46bbd4bb5570a7d2561513f80e1cb98cbef4ecb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:30 +0000

    Add package druid-data/0.1.20/druid_data-0.1.20.tar.gz

---
+++ packages/druid-data/druid_data-0.1.20.tar.gz/druid_data-0.1.20/druid_data/__init__.py
@@ -0,0 +1,12 @@
+"""Top-level package for druid-data"""  # noqa: F401
+# noqa: F401
+from .main import CrudRepository  # noqa: F401
+from .main import DynamoCrudRepository  # noqa: F401
+from .main import DynamoDBGlobalConfiguration  # noqa: F401
+from .main import MultiTableDynamoCrudRepository  # noqa: F401
+from .main import Repository  # noqa: F401
+from .main import SingleTableDynamoCrudRepository  # noqa: F401
+from .main import dynamo_entity  # noqa: F401; noqa: F401
+
+__version__ = "0.1.20"
+__author__ = """DRUID DEVAX"""
