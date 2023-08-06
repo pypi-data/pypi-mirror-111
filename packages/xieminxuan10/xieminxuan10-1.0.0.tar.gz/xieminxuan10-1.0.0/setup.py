57d165bb6912f31f9544801d773dcb07b994d9d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:32 +0000

    Add package xieminxuan10/1.0.0/xieminxuan10-1.0.0.tar.gz

---
+++ packages/xieminxuan10/xieminxuan10-1.0.0.tar.gz/xieminxuan10-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+from setuptools import find_packages
+setup(
+    name= 'xieminxuan10',
+    version= '1.0.0',
+    py_modules =['xieminxuan10'],
+    packages=find_packages(),
+    author= 'xieminxuan',
+    author_email='2091549152@qq.com',
+    url='https://gitee.com/',
+    description= '小尝试哦'
+
+)
