dbff76e913d48fe3dd10a18bba7a1a09d17720f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:44 +0000

    Add package qubo-nn/0.2.1/qubo-nn-0.2.1.tar.gz

---
+++ packages/qubo-nn/qubo-nn-0.2.1.tar.gz/qubo-nn-0.2.1/qubo_nn/data.py
@@ -0,0 +1,132 @@
+import gzip
+import pickle
+
+import pyxis as px
+import torch
+from torch.utils.data import DataLoader
+from torch.utils.data import TensorDataset
+
+
+class ChunkedDataLoader:
+    # TODO NOT FINISHED
+
+    def __init__(self, cfg):
+        self.cfg = cfg
+        self.chunks = cfg['problems']['chunks']
+        self.batch_size = cfg['model']['batch_size']
+        self.train_eval_split = cfg['model']['train_eval_split']
+        self.dataset_id = cfg['dataset_id']
+        self.current_chunk = 0
+        self.last_loaded_chunk = -1
+
+    def _load_chunk(self):
+        fname = 'datasets/%s.%d.pickle.gz' % (
+            self.dataset_id, self.current_chunk
+        )
+        with gzip.open(fname, 'rb') as f:
+            data, labels = pickle.load(f)
+
+        tensor_x = torch.Tensor(data)
+        tensor_y = torch.Tensor(labels)
+        dataset = TensorDataset(tensor_x, tensor_y.long())
+
+        train_size = int(self.train_eval_split * len(dataset))
+        test_size = len(dataset) - train_size
+        train_dataset, test_dataset = torch.utils.data.random_split(
+            dataset, [train_size, test_size]
+        )
+
+        self.train_data_loader = DataLoader(
+            train_dataset,
+            batch_size=self.batch_size,
+            shuffle=self.cfg['model']['shuffle_data']
+        )
+        self.test_data_loader = DataLoader(
+            test_dataset,
+            batch_size=1
+        )
+        self.last_loaded_chunk = self.current_chunk
+        return self.train_data_loader, self.test_data_loader
+
+    def get_current_chunk(self):
+        if self.current_chunk != self.last_loaded_chunk:
+            return self._load_chunk()
+        return self.train_data_loader, self.test_data_loader
+
+    def increment(self):
+        self.current_chunk += 1
+        if self.current_chunk == self.chunks:
+            return False
+        return True
+
+
+# Environment can't be pickled.
+# class LMDBDataSet(pxt.TorchDataset):
+#     def __init__(self, cfg):
+#         self.dataset_id = cfg['dataset_id']
+#         super(LMDBDataSet, self).__init__('datasets/' + self.dataset_id)
+#
+#     def __getitem__(self, idx):
+#         sample = self.db[idx]
+#         return sample['input'], sample['target']
+
+
+class LMDBDataSet(torch.utils.data.Dataset):
+    def __init__(self, cfg, reverse, base_path=''):
+        dirpath = base_path + 'datasets/'
+        if cfg["use_big"]:
+            dirpath = '/big/r/ratke/qubo_datasets/'
+        self.dataset_id = cfg['dataset_id']
+        self.dirpath = dirpath + self.dataset_id
+        self.reverse = reverse
+
+    def __len__(self):
+        if not hasattr(self, "len_"):
+            db = px.Reader(self.dirpath, lock=False)
+            self.len_ = len(db)
+            db.close()
+        return self.len_
+
+    def __getitem__(self, key):
+        if not hasattr(self, "db"):
+            self.db = px.Reader(self.dirpath, lock=False)
+
+        sample = self.db[key]
+        if self.reverse:
+            if isinstance(sample, list):
+                return [(s['input'], s['target'], s['prob']) for s in sample]
+            return sample['input'], sample['target'], sample['prob']
+        else:
+            return sample['input'], sample['target']
+
+    def __repr__(self):
+        return str(self.db)
+
+
+class LMDBDataLoader:
+    def __init__(self, cfg, reverse=False, part=None, base_path=''):
+        self.cfg = cfg
+        self.dataset_id = cfg['dataset_id']
+        self.dataset = LMDBDataSet(cfg, reverse, base_path)
+        if part is not None:
+            self.dataset = self.dataset[slice(*part)]
+        self.batch_size = cfg['model']['batch_size']
+        self.train_eval_split = cfg['model']['train_eval_split']
+
+        train_size = int(self.train_eval_split * len(self.dataset))
+        test_size = len(self.dataset) - train_size
+        train_dataset, test_dataset = torch.utils.data.random_split(
+            self.dataset, [train_size, test_size]
+        )
+
+        self.train_data_loader = DataLoader(
+            train_dataset,
+            batch_size=self.batch_size,
+            shuffle=cfg['model']['shuffle_data'],
+            num_workers=4
+        )
+        self.test_data_loader = DataLoader(
+            test_dataset,
+            batch_size=1,
+            num_workers=4
+        )
---
+++ packages/qubo-nn/qubo-nn-0.2.1.tar.gz/qubo-nn-0.2.1/qubo_nn/main.py
@@ -0,0 +1,95 @@
+from qubo_nn.pipeline import Classification
+from qubo_nn.pipeline import DefeatClassification
+from qubo_nn.pipeline import ReverseRegression
+from qubo_nn.pipeline import A3
+from qubo_nn.pipeline import R1
+from qubo_nn.pipeline import R2
+from qubo_nn.pipeline import QbsolvRegression
+from qubo_nn.pipeline import RedAERegression
+from qubo_nn.config import Config
+import argparse
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--type", nargs=None, help="Type (classify, reverse)", default='classify')
+    parser.add_argument("--eval", action="store_true")
+    parser.add_argument("--gendata", action="store_true")
+    parser.add_argument("--train", action="store_true")
+    parser.add_argument("--big", action="store_true")
+    parser.add_argument("-c", "--cfg_id", nargs=None, help="cfg_id")
+    parser.add_argument("-m", "--model", nargs="?")
+    parser.add_argument("-n", "--nruns", nargs="?", type=int, default=3)
+    args = parser.parse_args()
+
+    # TODO: Some optionals are 'too optional' - i.e. I use them but they're optional
+
+    cfg = Config().get_cfg(args.cfg_id)
+    cfg["use_big"] = args.big
+
+    if args.type == 'classify':
+        if args.gendata:
+            c = Classification(cfg)
+            c.gen_data_lmdb()
+        elif args.train:
+            c = Classification(cfg)
+            c.run_experiment(args.nruns)
+        elif args.eval:
+            c = Classification(cfg)
+            c.eval(args.model)
+    elif args.type == 'autoencode':
+        if args.train:
+            c = Classification(cfg)
+            c.auto_encoder_prototype()
+    elif args.type == 'reverse':
+        if args.gendata:
+            c = ReverseRegression(cfg)
+            c.gen_data_lmdb()
+        elif args.train:
+            c = ReverseRegression(cfg)
+            c.run_experiment(args.nruns)
+        elif args.eval:
+            c = ReverseRegression(cfg)
+            c.eval(args.model)
+    elif args.type == 'defeat':
+        if args.gendata:
+            c = DefeatClassification(cfg)
+            c.gen_data_lmdb()
+        elif args.train:
+            c = DefeatClassification(cfg)
+            c.run_experiment(args.nruns)
+    elif args.type == 'a3':
+        if args.gendata:
+            c = A3(cfg)
+            c.gen_data_lmdb()
+        elif args.train:
+            c = A3(cfg)
+            c.run_experiment(args.nruns)
+    elif args.type == 'r1':
+        if args.gendata:
+            c = R1(cfg)
+            c.gen_data_lmdb()
+        elif args.train:
+            c = R1(cfg)
+            c.run_experiment(args.nruns)
+    elif args.type == 'r2':
+        if args.gendata:
+            c = R2(cfg)
+            c.gen_data_lmdb()
+        elif args.train:
+            c = R2(cfg)
+            c.run_experiment(args.nruns)
+    elif args.type == 'qbsolv':
+        if args.gendata:
+            c = QbsolvRegression(cfg)
+            c.gen_data_lmdb()
+        elif args.train:
+            c = QbsolvRegression(cfg)
+            c.run_experiment(args.nruns)
+    elif args.type == 'red':
+        if args.gendata:
+            c = RedAERegression(cfg)
+            c.gen_data_lmdb()
+        elif args.train:
+            c = RedAERegression(cfg)
+            c.run_experiment(args.nruns)
