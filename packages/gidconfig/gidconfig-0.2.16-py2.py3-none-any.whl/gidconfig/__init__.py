b4fe44102ac9e1c962b2222d151e9412b2f7730f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:30 +0000

    Add package gidconfig/0.2.16/gidconfig-0.2.16-py2.py3-none-any.whl

---
+++ packages/gidconfig/gidconfig-0.2.16-py2.py3-none-any.whl/gidconfig/__init__.py
@@ -0,0 +1,13 @@
+"""
+Configuration provider package, that exposes the section as attribute and also automatically keeps the config file and object in sync.
+Through the Factory/Strategy the config objects are singletons as long as they point to the same file.
+"""
+
+__version__ = '0.2.16'
+
+from .standard import *
+from .data import *
+import logging
+import gidlogger as glog
+
+log = glog.library_base_logger(__name__)
