6920432366827caa8d0007d4439d5c10ae4e65db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:39 +0000

    Add package a9s/0.5.4/a9s-0.5.4-py3-none-any.whl

---
+++ packages/a9s/a9s-0.5.4-py3-none-any.whl/a9s/components/app.py
@@ -0,0 +1,94 @@
+import asyncio
+
+import os
+import sys
+from typing import List
+import tempfile
+from subprocess import call
+
+from blessed import Terminal
+from colored import attr
+
+from a9s.components.custom_string import String
+from a9s.components.renderer import Renderer
+
+
+EDITOR = os.environ.get('EDITOR', 'vim')
+
+
+def flush():
+    sys.stdout.flush()
+
+
+class App:
+    def __init__(self):
+        self.term = Terminal()
+        self.render_list = []
+        self.should_run = True
+
+        self.buffer = [[' ' for x in range(self.term.width)] for y in range(self.term.height)]
+
+    def dump_to_screen(self):
+        for line_num, line in enumerate(self.buffer):
+            sys.stdout.write(self.term.move_xy(0, line_num) + "".join(line) + attr("reset"))
+
+        sys.stdout.write(attr("reset"))
+
+    def resize(self):
+        self.buffer = [[' ' for x in range(self.term.width)] for y in range(self.term.height)]
+
+    def echo(self, x, y, to_print):
+        if isinstance(to_print, str):
+            to_print = String(to_print)
+
+        if y >= len(self.buffer) or x + len(to_print) > len(self.buffer[0]):
+            return
+
+        line = self.buffer[y]
+        to_print.reset_style_on_end()
+        for i, char in enumerate(to_print):
+            actual_x = x + i
+            line[actual_x] = str(char)
+
+    def clear(self):
+        sys.stdout.write(self.term.clear)
+        self.buffer = [[' ' for x in range(self.term.width)] for y in range(self.term.height)]
+
+    def add_to_render(self, renderer: Renderer):
+        self.render_list.append(renderer)
+
+    def add_multiple(self, renders: List[Renderer]):
+        self.render_list.extend(renders)
+
+    @classmethod
+    def get_editor_input(cls, initial_text: str = ""):
+        with tempfile.NamedTemporaryFile(suffix=".tmp") as tf:
+            tf.write(initial_text.encode())
+            tf.flush()
+            call([EDITOR, tf.name])
+
+            # do the parsing with `tf` using regular File operations.
+            # for instance:
+            tf.seek(0)
+            return tf.read().decode()
+
+    async def interactive_run(self):
+        with self.term.fullscreen():
+            with self.term.cbreak(), self.term.raw():
+                while self.should_run:
+                    # self.echo(0, 0, self.term.clear)
+                    with self.term.hidden_cursor():
+                        await asyncio.gather(*[renderer.update_data() for renderer in self.render_list])
+                        for renderer in self.render_list:
+                            renderer.set_echo_func(self.echo)
+                            renderer.draw()
+                            renderer.fill_empty()
+
+                        self.dump_to_screen()
+                        flush()
+
+                        key = self.term.inkey(timeout=0.1)
+                        if key in [chr(3)]:
+                            break
+
+                        yield key
---
+++ packages/a9s/a9s-0.5.4-py3-none-any.whl/a9s/main.py
@@ -0,0 +1,164 @@
+import asyncio
+from dataclasses import dataclass
+from typing import Callable
+
+import signal
+import curses
+import pyperclip
+
+from a9s.components.renderer import ScrollableRenderer
+from a9s.aws_resources.help import Help
+from a9s.aws_resources.logo import Logo
+from a9s.aws_resources.hud import HUD
+from a9s.aws_resources.services import ServicesSelector
+from a9s.components.app import App
+from a9s.components.logger import logger
+from a9s.components.mode import KeyMode, Mode
+from a9s.components.autocomplete import AutoComplete
+from a9s import __version__
+
+
+@dataclass
+class Context:
+    mode: KeyMode
+    focused: Callable[[], ScrollableRenderer]
+
+    def __eq__(self, other):
+        return self.mode == other.mode and self.focused() == other.focused()
+
+
+class MainApp(App):
+    def __init__(self):
+        super(MainApp, self).__init__()
+        self.logo = Logo(__version__)
+        self.help = Help()
+        self.hud = HUD()
+        self.services_selector = ServicesSelector(hud=self.hud)
+        self.mode_renderer = Mode()
+
+        self.auto_complete = AutoComplete(commands=self.commands)
+        self.add_multiple([self.logo, self.help, self.services_selector, self.mode_renderer, self.auto_complete, logger, self.hud])
+
+        self.on_resize()
+        self.context_stack = [Context(mode=KeyMode.Navigation, focused=lambda: self.services_selector.current_service)]
+
+    @property
+    def commands(self):
+        return {
+            'debug': self.on_debug_command,
+            'hideDebug': self.on_hide_debug_command,
+            'quit': self.on_quit_command,
+
+            's3': lambda: self.services_selector.set_service('s3'),
+            'route53': lambda: self.services_selector.set_service('route53'),
+        }
+
+    def on_debug_command(self):
+        current_context = self.context_stack[-1]
+        debug_context = Context(mode=KeyMode.Navigation, focused=lambda: logger)
+        if current_context != debug_context:
+            self.context_stack.append(debug_context)
+
+        logger.halt_debug()
+        logger.shown = True
+
+    def on_hide_debug_command(self):
+        current_context = self.context_stack[-1]
+        if current_context.focused() == logger:
+            self.context_stack.pop()
+
+        logger.shown = False
+
+    def on_quit_command(self):
+        self.should_run = False
+
+    def on_resize(self, *args):
+        self.help.set_pos(x=25, to_x=self.term.width, y=0, to_y=9)
+        self.logo.set_pos(x=3, y=0, to_x=24, to_y=6)
+        self.hud.set_pos(x=0, y=9, to_x=self.term.width)
+        self.mode_renderer.set_pos(x=0, y=self.term.height - 1, to_x=10)
+        self.auto_complete.set_pos(x=11, y=self.term.height - 1, to_x=self.term.width)
+        self.services_selector.set_pos(x=0, y=10, to_x=self.term.width, to_y=self.term.height - 1)
+        logger.set_pos(x=max(self.term.width - 40, 0), y=0, to_x=self.term.width, to_y=10)
+        self.services_selector.onresize()
+        self.resize()
+        self.auto_complete.to_x = self.term.width
+        self.clear()
+
+    async def run(self):
+        async for key in self.interactive_run():
+            current_context = self.context_stack[-1]
+            self.mode_renderer.mode = current_context.mode
+            focused = current_context.focused()
+            if key:
+                logger.debug("pressed key: " + repr(key))
+
+            if current_context.mode == KeyMode.Navigation:
+                should_stop_propagate = focused.handle_key(key)
+                self.auto_complete.text = ("/" + focused.filter) if focused.filter else ""
+                if key.code == curses.KEY_EXIT and not should_stop_propagate:
+                    if len(self.context_stack) > 1:
+                        self.context_stack.pop()
+                        logger.debug("Popping context")
+
+                if key == "/":
+                    self.context_stack.append(Context(mode=KeyMode.Search, focused=current_context.focused))
+                    logger.debug("Switching to Search mode")
+                elif key == ":":
+                    self.context_stack.append(Context(mode=KeyMode.Command, focused=current_context.focused))
+                    logger.debug("Switching to Command mode")
+
+                if key in ["/", ":"]:
+                    self.auto_complete.text = str(key)
+
+            if current_context.mode in [KeyMode.Search, KeyMode.Command]:
+                if key.code == curses.KEY_EXIT:
+                    self.context_stack.pop()
+                    logger.debug("Popping context and switching to Navigate mode")
+
+                if key == "\x16":  # ctrl + v
+                    self.auto_complete.text += pyperclip.paste().replace("\n", "").replace("\r", "")
+
+                elif key.code == curses.KEY_BACKSPACE:
+                    self.auto_complete.delete_char()
+
+                elif key.code == curses.KEY_ENTER:
+                    self.context_stack.pop()
+                    logger.debug("Popping context and switching to Navigate mode")
+                    if self.auto_complete.get_actual_text() == "":
+                        self.auto_complete.text = ""
+
+                    self.auto_complete.execute_command_if_matched()
+
+                elif key.isprintable():
+                    self.auto_complete.text += key
+
+                if current_context.mode == KeyMode.Search:
+                    focused.filter = self.auto_complete.get_actual_text()
+
+                if current_context.mode == KeyMode.Command:
+                    self.auto_complete.handle_key(key)
+
+            if current_context.mode == KeyMode.Command:
+                if key.code == curses.KEY_ENTER:
+                    self.auto_complete.text = ""
+
+
+async def main_loop():
+    app = MainApp()
+
+    try:
+        signal.signal(signal.SIGWINCH, app.on_resize)
+
+    except Exception:
+        pass
+
+    await app.run()
+
+
+def main():
+    asyncio.run(main_loop())
+
+
+if __name__ == '__main__':
+    main()
