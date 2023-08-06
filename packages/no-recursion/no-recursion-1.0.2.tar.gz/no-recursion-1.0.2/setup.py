0f4ac4a11282e21cc8875b91ad546f9c778d3298
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:14:12 +0000

    Add package no-recursion/1.0.2/no-recursion-1.0.2.tar.gz

---
+++ packages/no-recursion/no-recursion-1.0.2.tar.gz/no-recursion-1.0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+__version__ = "1.0.2"
+
+with open("README.md") as f:
+    long_description = f.read()
+
+setup(
+    name="no-recursion",
+    version=__version__,
+    author="Jonathan",
+    author_email="pybots.il@gmail.com",
+    description="This module helps you to get rid of recursions when you want"
+                "to rename a builtin function, or a function that already exists.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jonatan1609/noRecursion",
+    packages=find_packages(),
+    classifiers=[
+        "License :: OSI Approved :: The Unlicense (Unlicense)",
+        "Programming Language :: Python :: 3.5",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.5"
+)
