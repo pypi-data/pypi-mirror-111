b8f92a7dd6ebffad59f4b31fadf5ee8f4f32261f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:07 +0000

    Add package pipython3/0.1.5/pipython3-0.1.5-py2-none-any.whl

---
+++ packages/pipython3/pipython3-0.1.5-py2-none-any.whl/pipython/pitools/common/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from . import *
+"""Collection of common gcs tools."""
+__signature__ = 0x6b9d0bfdfd318773e441964d24bd6df6
---
+++ packages/pipython3/pipython3-0.1.5-py2-none-any.whl/pipython/version.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+""" Versions informations."""
+
+__version__ = '2.5.1.3'
+__signature__ = 0x170eee0a3ddf54d6363d2c5ec9399a3a
