b46278b707b53d596320d6567e3af2908054a800
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:28 +0000

    Add package build-dashboard/0.2.6/build-dashboard-0.2.6.tar.gz

---
+++ packages/build-dashboard/build-dashboard-0.2.6.tar.gz/build-dashboard-0.2.6/build_dashboard/screen.py
@@ -0,0 +1,28 @@
+from asciimatics.scene import Scene
+from asciimatics.screen import Screen
+from asciimatics.exceptions import ResizeScreenError, StopApplication
+from build_dashboard.views import BuildersView, BuildsView, StepsView, LogView
+from build_dashboard import logger
+
+def draw_screen(model, loop, update_secs=5):
+    screen = Screen.open()
+    scenes = []
+    scenes.append(Scene([BuildersView(screen, model)], -1, name="BuildersView"))
+    scenes.append(Scene([BuildsView(screen, model)], -1, name="BuildsView"))
+    scenes.append(Scene([StepsView(screen, model)], -1, name="StepsView"))
+    scenes.append(Scene([LogView(screen, model)], -1, name="LogView"))
+    screen.set_scenes(scenes)
+
+    while True:
+        try:
+            loop.run_until_complete(model.update())
+            screen.force_update()
+            screen.draw_next_frame(repeat=True)
+            screen.wait_for_input(update_secs)
+        except KeyboardInterrupt as e:
+            screen.close()
+            break
+        except Exception as e:
+            screen.close()
+            logger.error(e)
+            break
