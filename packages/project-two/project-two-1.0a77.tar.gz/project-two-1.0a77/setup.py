f707de188376c3a0aba9f3d2f23c903c1e437622
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:33 +0000

    Add package project-two/1.0a77/project-two-1.0a77.tar.gz

---
+++ packages/project-two/project-two-1.0a77.tar.gz/project-two-1.0a77/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='project-two',
+    version='1.0a77',
+    packages=['v1'],
+    url='https://google.com',
+    author='ML',
+    author_email='',
+    install_requires=[
+        "numpy==1.20",
+    ]
+)
---
+++ packages/project-two/project-two-1.0a77.tar.gz/project-two-1.0a77/v1/main.py
@@ -0,0 +1,20 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import numpy as numpyv1
+
+
+class Pepe():
+    def print_hi(name):
+        # Use a breakpoint in the code line below to debug your script.
+        print(f'Soy la version de numpy, {numpyv1.__version__}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    p = Pepe
+    p.print_hi('asd')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
