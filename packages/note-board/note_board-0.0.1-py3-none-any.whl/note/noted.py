15f8e33e2058ab9ef119924384a559f1caf464ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:46 +0000

    Add package note-board/0.0.1/note_board-0.0.1-py3-none-any.whl

---
+++ packages/note-board/note_board-0.0.1-py3-none-any.whl/note/noted.py
@@ -0,0 +1,7 @@
+import note
+
+def main():
+    print('noted', note.__version__)
+
+if __name__ == '__main__':
+    main()
---
+++ packages/note-board/note_board-0.0.1-py3-none-any.whl/note_board/noted.py
@@ -0,0 +1,7 @@
+import note_board
+
+def main():
+    print('noted', note_board.__version__)
+
+if __name__ == '__main__':
+    main()
