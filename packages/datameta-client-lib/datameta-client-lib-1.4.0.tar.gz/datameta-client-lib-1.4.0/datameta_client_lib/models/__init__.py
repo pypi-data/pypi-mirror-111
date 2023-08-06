c08bcae37423f01d44b145115187bf17cc096329
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:07 +0000

    Add package datameta-client-lib/1.4.0/datameta-client-lib-1.4.0.tar.gz

---
+++ packages/datameta-client-lib/datameta-client-lib-1.4.0.tar.gz/datameta-client-lib-1.4.0/datameta_client_lib/models/__init__.py
@@ -0,0 +1,51 @@
+# flake8: noqa
+
+# import all models into this package
+# if you have many models here with many references from one model to another this may
+# raise a RecursionError
+# to avoid this, import only the models that you directly need like:
+# from from datameta_client_lib.model.pet import Pet
+# or import this package, but before doing it, use:
+# import sys
+# sys.setrecursionlimit(n)
+
+from datameta_client_lib.model.api_key_list import ApiKeyList
+from datameta_client_lib.model.app_settings_response import AppSettingsResponse
+from datameta_client_lib.model.app_settings_update_request import AppSettingsUpdateRequest
+from datameta_client_lib.model.create_token_request import CreateTokenRequest
+from datameta_client_lib.model.error import Error
+from datameta_client_lib.model.error_model import ErrorModel
+from datameta_client_lib.model.file_announcement import FileAnnouncement
+from datameta_client_lib.model.file_response import FileResponse
+from datameta_client_lib.model.file_update_request import FileUpdateRequest
+from datameta_client_lib.model.file_upload_response import FileUploadResponse
+from datameta_client_lib.model.file_url import FileUrl
+from datameta_client_lib.model.group_response import GroupResponse
+from datameta_client_lib.model.group_submissions import GroupSubmissions
+from datameta_client_lib.model.group_update_request import GroupUpdateRequest
+from datameta_client_lib.model.identifier import Identifier
+from datameta_client_lib.model.meta_data_response import MetaDataResponse
+from datameta_client_lib.model.meta_data_set import MetaDataSet
+from datameta_client_lib.model.meta_data_set_response import MetaDataSetResponse
+from datameta_client_lib.model.meta_data_set_service_execution import MetaDataSetServiceExecution
+from datameta_client_lib.model.meta_datum import MetaDatum
+from datameta_client_lib.model.meta_datum_response import MetaDatumResponse
+from datameta_client_lib.model.nullable_identifier import NullableIdentifier
+from datameta_client_lib.model.password_change import PasswordChange
+from datameta_client_lib.model.register_settings_response import RegisterSettingsResponse
+from datameta_client_lib.model.registration_request import RegistrationRequest
+from datameta_client_lib.model.server_info_response import ServerInfoResponse
+from datameta_client_lib.model.service_execution import ServiceExecution
+from datameta_client_lib.model.service_request import ServiceRequest
+from datameta_client_lib.model.service_response import ServiceResponse
+from datameta_client_lib.model.service_update_request import ServiceUpdateRequest
+from datameta_client_lib.model.services import Services
+from datameta_client_lib.model.staged_files import StagedFiles
+from datameta_client_lib.model.staged_meta_data_sets import StagedMetaDataSets
+from datameta_client_lib.model.submission_request import SubmissionRequest
+from datameta_client_lib.model.submission_response import SubmissionResponse
+from datameta_client_lib.model.user_response import UserResponse
+from datameta_client_lib.model.user_session import UserSession
+from datameta_client_lib.model.user_update_request import UserUpdateRequest
+from datameta_client_lib.model.whoami_response import WhoamiResponse
+from datameta_client_lib.model.whoami_response_group import WhoamiResponseGroup
