f8717a78276e3eb09ce06a174fa456bd9a347857
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:12 +0000

    Add package discord-together/1.0.8/discord_together-1.0.8-py3-none-any.whl

---
+++ packages/discord-together/discord_together-1.0.8-py3-none-any.whl/discordTogether/__init__.py
@@ -0,0 +1,14 @@
+"""
+discord-together
+~~~~~~~~~~~~~~~~
+
+An unofficial module that allows the use of the Discord's BETA feature, Party Games - like YouTube together or Poker Night. 
+"""
+
+__name__ = "discord-together"
+__author__ = "apurv-r"
+__version__ = "1.0.8"
+__license__ = "MIT"
+
+from .discordTogetherMain import DiscordTogether
+from .errors import *
