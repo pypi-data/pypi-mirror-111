37dbc95457ad485249eac297e7138c436d8b857d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:53 +0000

    Add package external-site-parser/0.4.2/external_site_parser-0.4.2.tar.gz

---
+++ packages/external-site-parser/external_site_parser-0.4.2.tar.gz/external_site_parser-0.4.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='external_site_parser',
+    version='0.4.2',
+    packages=['external_site_parser'],
+    url='',
+    license='',
+    author='Cathal A. Dinneen',
+    author_email='cathal.a.dinneen@gmail.com',
+    install_requires=['requests', 'BeautifulSoup4', 'lxml', 'xlrd'],
+    description=''
+)
+
---
+++ packages/external-site-parser/external_site_parser-0.4.2.tar.gz/external_site_parser-0.4.2/src/external_site_parser/utils.py
@@ -0,0 +1,3 @@
+X_KEY_VALUE_PATTERN = "[x|X]-[f|F]ixlet-(.*?): (.*)\n"  # todo: fix rege
+CIS_RULE_PATTERN = "([0-9\.]{3,7})"  # Matches rules with names like 1.2, 5.1.3 and 5.2.3.1
+BASELINE_CONTENT_TYPE_PATTERN = 'Content-Type: multipart/digest; boundary="(.*?)"'
