98980a280974f7628200b05362e50877039af887
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:28 +0000

    Add package gausbinom-distributions-nraichur/0.1/gausbinom_distributions_nraichur-0.1.tar.gz

---
+++ packages/gausbinom-distributions-nraichur/gausbinom_distributions_nraichur-0.1.tar.gz/gausbinom_distributions_nraichur-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gausbinom_distributions_nraichur',
+      version='0.1',
+      description='Gaussian-Binomian distributions',
+      packages=['gausbinom_distributions_nraichur'],
+      author = "Nisha",
+      author_email = "nishalraichur@gmail.com",
+      zip_safe=False)
