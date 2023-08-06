0184baa8e2947d423379a5276f1970ed68221ae4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:33 +0000

    Add package rpycore/0.1.1/rpycore-0.1.1.tar.gz

---
+++ packages/rpycore/rpycore-0.1.1.tar.gz/rpycore-0.1.1/rpycore/libraries/decorators/decorators.py
@@ -0,0 +1,3 @@
+# Library of decorators used in RocketPy applications (incl. not implemented, debug, etc)
+# https://realpython.com/primer-on-python-decorators/ for reference
+# Maintained by Raihaan Usman
