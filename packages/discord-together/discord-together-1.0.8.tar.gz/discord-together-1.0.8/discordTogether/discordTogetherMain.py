594ced721328502d36ce662a80601ca9d98f6ef1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:12 +0000

    Add package discord-together/1.0.8/discord-together-1.0.8.tar.gz

---
+++ packages/discord-together/discord-together-1.0.8.tar.gz/discord-together-1.0.8/discordTogether/discordTogetherMain.py
@@ -0,0 +1,134 @@
+from discord import Client, AutoShardedClient, InvalidArgument
+from discord.ext.commands import Bot, AutoShardedBot, BotMissingPermissions
+from discord.http import Route
+from typing import Union
+from .errors import InvalidChannelID, InvalidActivityChoice
+
+
+defaultApplications = {  # Credits to RemyK888
+    'youtube': '755600276941176913',
+    'poker': '755827207812677713',
+    'betrayal': '773336526917861400',
+    'fishing': '814288819477020702',
+    'chess': '832012586023256104'
+}
+
+class DiscordTogether:
+    """
+    Controls invite generations.
+    ----------
+    Attributes
+    - client/bot : discord.Client | discord.AutoShardedClient | commands.Bot | commands.AutoShardedBot
+    - debug (bool) (default = False) : Debug mode toggle
+    -------
+    Methods
+    - create_link(voiceChannelID : int, option : str) :
+    """
+
+    def __init__(self, client: Union[Client, Bot, AutoShardedClient, AutoShardedBot], **kwargs):
+        """
+        Controls invite generations.
+        ----------
+        Attributes
+        - client/bot : The client/bot variable used for your project
+        - debug (bool) (default = False) : Debug mode toggle => used to identify error code in case of invalid invite links
+        -------
+        Methods
+        - create_link(voiceChannelID : int, option : str) : Creates a invite link
+        """
+
+        if isinstance(client, Client) or isinstance(client, AutoShardedClient):
+            self.client = client
+        else:
+            raise ValueError("The client/bot object parameter is not valid.")
+        
+        debug = kwargs.get("debug",False)
+        if isinstance(debug,bool):
+            self.debug = debug
+        else:
+            self.debug = False
+            print('\033[93m'+"[WARN] (discord-together) Debug parameter did not recieve a bool object. Reverting to Debug = False."+'\033[0m') 
+
+
+    async def create_link(self, voiceChannelID: int, option: str) -> str:
+        """
+        Generates a invite link to a VC with the Discord Party VC Feature.
+        ----------
+        Parameters:
+            - voiceChannelID (int): ID of the voice channel to create the activity for
+            - option (str): A option amongst the predefined choices ("youtube","poker","betrayal","fishing","chess") or a custom ID (as str)
+        ----------
+        Returns:
+            - invite_link (str): A discord invite link which, upon clicked, starts the custom activity in the VC.
+        """
+
+        # Pre Defined Application ID
+        if option and (str(option).lower().replace(" ", "") in defaultApplications.keys()):   
+
+            data = {
+                'max_age': 86400,
+                'max_uses': 0,
+                'target_application_id': defaultApplications[option],
+                'target_type': 2,
+                'temporary': False,
+                'validate': None
+            }
+            
+            try:
+                result = await self.client.http.request(
+                    Route("POST", f"/channels/{voiceChannelID}/invites"), json = data
+                )
+            #Error Handling
+            except Exception as e:
+                if "10003" in str(e):
+                    raise InvalidChannelID("Voice Channel ID is invalid.")
+                elif "50013" in str(e):
+                    raise BotMissingPermissions(["CREATE_LINK"])  
+                elif "130000" in str(e):
+                    raise ConnectionError("API resource is currently overloaded. Try again a little later.")      
+                else:
+                    raise ConnectionError("An error occurred while retrieving data from Discord API.")
+
+            if self.debug:
+                print('\033[95m'+'\033[1m'+'[DEBUG] (discord-together) Response Output:\n'+'\033[0m'+str(result))
+            
+            return f"https://discord.com/invite/{result['code']}"
+
+
+
+        # User Defined Application ID
+        elif option and (str(option).lower().replace("", " ") not in defaultApplications.keys()) and option.isnumeric():
+            
+            data = {
+                'max_age': 86400,
+                'max_uses': 0,
+                'target_application_id': str(option),
+                'target_type': 2,
+                'temporary': False,
+                'validate': None
+            }
+
+            try:
+                result = await self.client.http.request(
+                    Route("POST", f"/channels/{voiceChannelID}/invites"), json = data
+                )
+            #Error Handling
+            except Exception as e:
+                if "10003" in str(e):
+                    raise InvalidChannelID("Voice Channel ID is invalid.")
+                elif "target_application_id" in str(e):
+                    raise InvalidArgument(f"\"{str(option)}\" is an invalid custom application ID.")
+                elif "50013" in str(e):
+                    raise BotMissingPermissions(["CREATE_LINK"])  
+                elif "130000" in str(e):
+                    raise ConnectionError("API resource is currently overloaded. Try again a little later.")      
+                else:
+                    raise ConnectionError("An error occurred while retrieving data from Discord API.")
+
+            if self.debug:
+                print('\033[95m'+'\033[1m'+'[DEBUG] (discord-together) Response Output:\n'+'\033[0m'+str(result))
+
+            return f"https://discord.com/invite/{result['code']}"
+        
+        else:
+            raise InvalidActivityChoice("Invalid activity option chosen. You may only choose between (\"youtube\",\"poker\",\"chess\",\"fishing\",\"betrayal\") or input a custom application ID as a string.")
---
+++ packages/discord-together/discord-together-1.0.8.tar.gz/discord-together-1.0.8/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+
+readme = ''
+with open("README.md", "r", encoding="utf-8") as fh:
+    readme = fh.read()
+
+
+setup(name='discord-together',
+      packages = ['discordTogether'],
+      author='Apurv R',
+      url='https://github.com/apurv-r/discord-together',
+      project_urls={
+        "Issue tracker": "https://github.com/apurv-r/discord-together/issues",
+      },
+      download_url="https://github.com/apurv-r/discord-together/archive/refs/tags/1.0.8.tar.gz",
+      version='1.0.8',
+      license='MIT',
+      description='Utilize the BETA Discord VC Party Games feature!',
+      long_description=readme,
+      long_description_content_type="text/markdown",
+      include_package_data=True,
+      install_requires=[
+        "discord.py>=1.5",
+      ],
+      keywords = ['discord.py','discord together','youtube together','party games','discord activities','discord games'],
+      python_requires='>=3.8.0',
+      classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+      ],
+)
