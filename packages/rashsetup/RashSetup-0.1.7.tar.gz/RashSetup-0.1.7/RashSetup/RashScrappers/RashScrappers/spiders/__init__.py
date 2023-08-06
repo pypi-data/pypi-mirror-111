71573ef11cad28408ca08dd9071c2d582dd87cc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:16 +0000

    Add package rashsetup/0.1.7/RashSetup-0.1.7.tar.gz

---
+++ packages/rashsetup/RashSetup-0.1.7.tar.gz/RashSetup-0.1.7/RashSetup/RashScrappers/RashScrappers/spiders/__init__.py
@@ -0,0 +1,10 @@
+from .Settings import SettingsSpider
+from .Repo import RepoSpider, TempHandler
+from .ModuleChecker import ModuleCheckerSpider
+
+__all__ = [
+    "SettingsSpider",
+    "RepoSpider",
+    "TempHandler",
+    "ModuleCheckerSpider"
+]
