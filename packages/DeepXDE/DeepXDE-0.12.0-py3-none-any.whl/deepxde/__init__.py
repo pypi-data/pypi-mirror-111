b84a5b2107912960805da55778b7cecfe8db3494
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:46 +0000

    Add package DeepXDE/0.12.0/DeepXDE-0.12.0-py3-none-any.whl

---
+++ packages/DeepXDE/DeepXDE-0.12.0-py3-none-any.whl/deepxde/__init__.py
@@ -0,0 +1,43 @@
+from __future__ import absolute_import
+
+from .__about__ import __version__
+
+# Should import backend before importing anything else
+from . import backend
+
+from . import boundary_conditions as bc
+from . import callbacks
+from . import data
+from . import geometry
+from . import gradients as grad
+from . import maps
+from .boundary_conditions import DirichletBC
+from .boundary_conditions import NeumannBC
+from .boundary_conditions import OperatorBC
+from .boundary_conditions import PeriodicBC
+from .boundary_conditions import RobinBC
+from .boundary_conditions import PointSetBC
+from .initial_condition import IC
+from .model import Model
+from .postprocessing import saveplot
+from .utils import apply
+
+
+__all__ = [
+    "bc",
+    "callbacks",
+    "data",
+    "geometry",
+    "grad",
+    "maps",
+    "DirichletBC",
+    "NeumannBC",
+    "OperatorBC",
+    "PeriodicBC",
+    "RobinBC",
+    "PointSetBC",
+    "IC",
+    "Model",
+    "saveplot",
+    "apply",
+]
