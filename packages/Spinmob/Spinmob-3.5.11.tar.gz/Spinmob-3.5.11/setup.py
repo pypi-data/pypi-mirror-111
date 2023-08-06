e15b04684a2628aa8a3363f660537c1b09b67051
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:19 +0000

    Add package Spinmob/3.5.11/Spinmob-3.5.11.tar.gz

---
+++ packages/Spinmob/Spinmob-3.5.11.tar.gz/Spinmob-3.5.11/setup.py
@@ -0,0 +1,35 @@
+__version__ = '3.5.11' # Keep this on the first line so it's easy for __init__.py to grab.
+
+
+
+from setuptools import setup
+setup(name           = 'Spinmob',
+      version        = __version__,
+      description    = 'Data handling, plotting, analysis, and GUI building for scientific labs',
+      author         = 'Jack Sankey',
+      author_email   = 'jack.sankey@gmail.com',
+      url            = 'https://github.com/Spinmob/spinmob',
+      packages       = [
+           'spinmob',
+           'spinmob.tests',
+           'spinmob.egg',
+           ],
+      package_dir = {
+          'spinmob'          : '.',
+          'tests'            : './tests',
+          'egg'              : './egg',
+          },
+      package_data={
+          ''  : [
+              './setup.py',
+              './tests/fixtures/*.*',
+              './egg/DataboxProcessor/*.*',
+            ],
+          },
+      install_requires=[
+          "scipy",
+          "matplotlib",
+          "lmfit",
+          "pyqtgraph>=0.11",
+          ],
+     )
