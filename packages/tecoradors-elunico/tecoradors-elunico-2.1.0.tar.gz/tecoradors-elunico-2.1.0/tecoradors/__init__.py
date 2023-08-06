730e8c1cad938531d814d2821046ee8db678ab62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:35 +0000

    Add package tecoradors-elunico/2.1.0/tecoradors-elunico-2.1.0.tar.gz

---
+++ packages/tecoradors-elunico/tecoradors-elunico-2.1.0.tar.gz/tecoradors-elunico-2.1.0/tecoradors/__init__.py
@@ -0,0 +1,19 @@
+from tecoradors.tecoradors import *
+
+__all__ = [
+    "accepts",
+    "json_serializable",
+    "spread",
+    "timed",
+    "squash",
+    "stringable",
+    "equatable",
+    "hashable",
+    "dataclass",
+    "Self",
+    "final",
+    "deprecated",
+    "freeze",
+    "FrozenClassError"
+]
+
