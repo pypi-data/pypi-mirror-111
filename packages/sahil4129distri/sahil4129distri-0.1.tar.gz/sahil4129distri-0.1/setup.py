b49a145a4ec0d29495f13fc768ddd0525f9e4054
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:28 +0000

    Add package sahil4129distri/0.1/sahil4129distri-0.1.tar.gz

---
+++ packages/sahil4129distri/sahil4129distri-0.1.tar.gz/sahil4129distri-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='sahil4129distri',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['sahil4129distri'],
+      zip_safe=False)
