36bd15e4bc52c94fd3420ad4cd54cb20f6ca4c55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:27 +0000

    Add package classopt/0.1.1/classopt-0.1.1.tar.gz

---
+++ packages/classopt/classopt-0.1.1.tar.gz/classopt-0.1.1/classopt/classopt.py
@@ -0,0 +1,37 @@
+from argparse import ArgumentParser
+from dataclasses import dataclass
+
+
+def ClassOpt(cls):
+    @classmethod
+    def from_args(cls):
+        parser = ArgumentParser()
+
+        for arg_name, arg_field in cls.__dataclass_fields__.items():
+            kwargs = {}
+            kwargs.update(arg_field.metadata)
+            kwargs["type"] = arg_field.type
+
+            name_or_flags = []
+            if "name_or_flags" in arg_field.metadata:
+                name_or_flags.append(arg_field.metadata["name_or_flags"])
+                if arg_field.metadata["name_or_flags"].startswith("-"):
+                    name_or_flags.append(f"--{arg_name}")
+                kwargs.pop("name_or_flags")
+            else:
+                name_or_flags.append(f"--{arg_name}")
+
+            if "action" in arg_field.metadata:
+                kwargs.pop("type")
+
+            if "type" in arg_field.metadata:
+                kwargs["type"] = arg_field.metadata["type"]
+
+            parser.add_argument(*name_or_flags, **kwargs)
+
+        args = parser.parse_args()
+        return cls(**vars(args))
+
+    setattr(cls, "from_args", from_args)
+
+    return dataclass(cls)
---
+++ packages/classopt/classopt-0.1.1.tar.gz/classopt-0.1.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['classopt']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'classopt',
+    'version': '0.1.1',
+    'description': 'Arguments parser with class for Python, inspired by StructOpt',
+    'long_description': '<h1 align="center">Welcome to ClassOpt 👋</h1>\n<p>\n  <img alt="Version" src="https://img.shields.io/badge/version-0.1.1-blue.svg?cacheSeconds=2592000" />\n  <a href="https://github.com/moisutsu/classopt/blob/main/LICENSE" target="_blank">\n    <img alt="License: MIT" src="https://img.shields.io/badge/License-MIT-yellow.svg" />\n  </a>\n  <a href="https://twitter.com/moisutsu" target="_blank">\n    <img alt="Twitter: moisutsu" src="https://img.shields.io/twitter/follow/moisutsu.svg?style=social" />\n  </a>\n</p>\n\n> Arguments parser with class for Python, inspired by [StructOpt](https://github.com/TeXitoi/structopt)\n\n## Install\n\n```sh\npip install classopt\n```\n\n## Usage\n\nImport `ClassOpt` and define the Opt class with decorator.\n\n```python\nfrom classopt import ClassOpt\n\n@ClassOpt\nclass Opt:\n    arg_int: int\n    arg_str: str\n\nif __name__ == "__main__":\n    opt = Opt.from_args()\n    print(opt.arg_int, opt.arg_str)\n```\n\nRun with command line arguments.\n\n```bash\n$ python main.py --arg_int 5 --arg_str hello\n5 hello\n```\n\n## Advanced Usage\n\n`ClassOpt` internally uses the standard libraries [dataclasses](https://docs.python.org/ja/3/library/dataclasses.html) and [argparse](https://docs.python.org/ja/3/library/argparse.html).\nAnd you can specify the argument of `argparse.ArgumentParser.add_argument` with the `metadata` of `dataclasses.field`\n\n```python\nfrom dataclasses import field\nfrom classopt import ClassOpt\n\n@ClassOpt\nclass Opt:\n    positional_arguments: str = field(\n        metadata={"name_or_flags": "positional_arguments"}\n    )\n    short_arg: str = field(metadata={"name_or_flags": "-s"})\n    default_int: int = field(metadata={"default": 3})\n    store_true: bool = field(metadata={"action": "store_true"})\n    nargs: list = field(metadata={"nargs": "+", "type": int})\n\nif __name__ == "__main__":\n    opt = Opt.from_args()\n    print(opt)\n```\n\n```bash\n$ python main.py positional_arguments -s short_arg --store_true --nargs 1 2 3\nOpt(positional_arguments=\'positional_arguments\', short_arg=\'short_arg\', default_int=3, store_true=True, nargs=[1, 2, 3])\n```\n\n## Run tests\n\n```sh\npoetry run pytest\n```\n\n## Author\n\n👤 **moisutsu**\n\n* Twitter: [@moisutsu](https://twitter.com/moisutsu)\n* Github: [@moisutsu](https://github.com/moisutsu)\n\n## Show your support\n\nGive a ⭐️ if this project helped you!\n\n## 📝 License\n\nCopyright © 2021 [moisutsu](https://github.com/moisutsu).<br />\nThis project is [MIT](https://github.com/moisutsu/classopt/blob/main/LICENSE) licensed.\n\n***\n_This README was generated with ❤️ by [readme-md-generator](https://github.com/kefranabg/readme-md-generator)_\n',
+    'author': 'moisutsu',
+    'author_email': 'moisutsu@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/moisutsu/classopt',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
