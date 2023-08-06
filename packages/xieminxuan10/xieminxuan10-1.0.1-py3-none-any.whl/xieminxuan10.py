bc4ed198930099c912e00f342935996071dbf431
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:00 +0000

    Add package xieminxuan10/1.0.1/xieminxuan10-1.0.1-py3-none-any.whl

---
+++ packages/xieminxuan10/xieminxuan10-1.0.1-py3-none-any.whl/xieminxuan10.py
@@ -0,0 +1,23 @@
+#这是xieminxuan10模块，version==1.0.1
+def out(a):
+    #一镜像
+    return a
+def mathplus(c,plus=1):
+    return c + plus
+def bey(sleep=0):
+    import time
+    time.sleep(sleep)
+    quit()
+def initme():
+    return "测试"
+def suiji(a,b):
+    #一镜像
+    import random.randint as g
+    return g(a,b)
+def xuanze(a):
+    #a必须为列表
+    #一镜像
+    import random.choice as p
+    return p(a)
+def mathsubtract(a,subtract=1):
+    return a-subtract
