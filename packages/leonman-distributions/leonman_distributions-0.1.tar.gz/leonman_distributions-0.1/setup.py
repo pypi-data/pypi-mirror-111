7afe133ab92e3a530bb0dfec5245c8b3880e4c1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:41 +0000

    Add package leonman-distributions/0.1/leonman_distributions-0.1.tar.gz

---
+++ packages/leonman-distributions/leonman_distributions-0.1.tar.gz/leonman_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='leonman_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['leonman_distributions'],
+      zip_safe=False)
