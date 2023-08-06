41fd8efd73298a429966b970528fdbd91905ae17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:03 +0000

    Add package bln-tools/0.0.1/bln-tools-0.0.1.tar.gz

---
+++ packages/bln-tools/bln-tools-0.0.1.tar.gz/bln-tools-0.0.1/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='bln-tools',
+    version='0.0.1',
+    author='Daniel Jenson',
+    author_email='djenson@stanford.edu',
+    description='Big Local News Tools',
+    license='GNU GPLv3',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    platform='OS Independent',
+    url='https://github.com/biglocalnews/tools',
+    packages=setuptools.find_packages(),
+    scripts=[
+        'scripts/harmonizer',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'fuzzywuzzy',
+        'numpy',
+        'pandas',
+        'python-Levenshtein',
+        'sklearn',
+    ],
+)
