dcc83cf18f10881601143cda9b83a46b2d897428
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:40 +0000

    Add package aws-ssooidc/2021.1.0.1/aws_ssooidc-2021.1.0.1.tar.gz

---
+++ packages/aws-ssooidc/aws_ssooidc-2021.1.0.1.tar.gz/aws_ssooidc-2021.1.0.1/README.rst
@@ -0,0 +1,73 @@
+===============
+**aws_ssooidc**
+===============
+
+Overview
+--------
+
+Create temporary credentials for AWS SSO-OIDC.
+
+Prerequisites
+-------------
+
+- *Python >= 3.6*
+- *boto3 >= 1.17.78* (installed as a dependency)
+
+Required (Positional) Arguments
+-------------------------------
+
+- Position 1: start_url (the start URL for your AWS SSO login)
+
+Optional (Keyword) Arguments
+----------------------------
+
+- client_name
+    - Description: Arbitrary name of the SSO client to create.
+    - Type: String
+    - Default: ssoclient
+- region
+    - Description: Your AWS region.
+    - Type: String
+    - Default: us-east-1
+
+Usage
+-----
+
+Installation:
+
+.. code-block:: BASH
+
+   pip3 install aws_ssooidc
+   # or
+   python3 -m pip install aws_ssooidc
+
+In Python3:
+
+.. code-block:: BASH
+
+   import aws_ssooidc.aws_ssooidc as sso
+   response = sso.gettoken('<start_url>')
+   access_token = response['accessToken']
+
+In BASH:
+
+.. code-block:: BASH
+
+   python3 -c "
+       import aws_ssooidc.aws_ssooidc as sso
+       response = sso.gettoken('<start_url>')
+       access_token = response['accessToken']
+   "
+
+Changelog
+---------
+
+2021.1.0.1
+
+- Added verification URI printout for devices which cannot launch browsers automatically.
+
+2021.1.0.0
+
+- Initial release.
+
+*Current version: 2021.1.0.1*
---
+++ packages/aws-ssooidc/aws_ssooidc-2021.1.0.1.tar.gz/aws_ssooidc-2021.1.0.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['aws_ssooidc']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['boto3>=1.17.78,<2.0.0']
+
+setup_kwargs = {
+    'name': 'aws-ssooidc',
+    'version': '2021.1.0.1',
+    'description': 'Create temporary credentials for AWS SSO-OIDC.',
+    'long_description': '===============\n**aws_ssooidc**\n===============\n\nOverview\n--------\n\nCreate temporary credentials for AWS SSO-OIDC.\n\nPrerequisites\n-------------\n\n- *Python >= 3.6*\n- *boto3 >= 1.17.78* (installed as a dependency)\n\nRequired (Positional) Arguments\n-------------------------------\n\n- Position 1: start_url (the start URL for your AWS SSO login)\n\nOptional (Keyword) Arguments\n----------------------------\n\n- client_name\n    - Description: Arbitrary name of the SSO client to create.\n    - Type: String\n    - Default: ssoclient\n- region\n    - Description: Your AWS region.\n    - Type: String\n    - Default: us-east-1\n\nUsage\n-----\n\nInstallation:\n\n.. code-block:: BASH\n\n   pip3 install aws_ssooidc\n   # or\n   python3 -m pip install aws_ssooidc\n\nIn Python3:\n\n.. code-block:: BASH\n\n   import aws_ssooidc.aws_ssooidc as sso\n   response = sso.gettoken(\'<start_url>\')\n   access_token = response[\'accessToken\']\n\nIn BASH:\n\n.. code-block:: BASH\n\n   python3 -c "\n       import aws_ssooidc.aws_ssooidc as sso\n       response = sso.gettoken(\'<start_url>\')\n       access_token = response[\'accessToken\']\n   "\n\nChangelog\n---------\n\n2021.1.0.1\n\n- Added verification URI printout for devices which cannot launch browsers automatically.\n\n2021.1.0.0\n\n- Initial release.\n\n*Current version: 2021.1.0.1*\n',
+    'author': 'Ahmad Ferdaus Abd Razak',
+    'author_email': 'ahmad.ferdaus.abd.razak@ni.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/fer1035/pypi-ssooidc',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
