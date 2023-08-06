22028997373ca0a876c3df1b59c9b06dcf54deec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:14:23 +0000

    Add package apitorch/0.0.18/apitorch-0.0.18.tar.gz

---
+++ packages/apitorch/apitorch-0.0.18.tar.gz/apitorch-0.0.18/apitorch/routes.py
@@ -0,0 +1,10 @@
+def classify_image_route():
+    return 'classify'
+
+
+def ping_route():
+    return 'ping'
+
+
+def training_set_images_route(training_set_id):
+    return f'training-set/{training_set_id}/images'
