b360f676b48ba4989511153ca4ed8615de557ffd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:19 +0000

    Add package vsource-algorithm/1.0.9/vsource_algorithm-1.0.9.tar.gz

---
+++ packages/vsource-algorithm/vsource_algorithm-1.0.9.tar.gz/vsource_algorithm-1.0.9/vsource_algorithm/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @Author   : Ecohnoch(xcy)
+# @File     : __init__.py.py
+# @Function : TODO
+
+from vsource_algorithm.loginInstance import login
+from vsource_algorithm.algorithms.face_recognition import face_recognition
+from vsource_algorithm.algorithms.face_detection import face_detection
+from vsource_algorithm.algorithms.face_attribute import face_attribute
+from vsource_algorithm.algorithms.speaker_recognition import speaker_recognition
+
+from vsource_algorithm.scripts.algorithm_scripts.algorithms import parse_algorithm
+
