f37aace5419e2616601689c15be554be2ea32bfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:07 +0000

    Add package robyn/0.5.0/robyn-0.5.0-cp37-cp37m-manylinux_2_12_i686.manylinux2010_i686.whl

---
+++ packages/robyn/robyn-0.5.0-cp37-cp37m-manylinux_2_12_i686.manylinux2010_i686.whl/robyn/__init__.py
@@ -0,0 +1,54 @@
+from .robyn import Server
+from asyncio import iscoroutinefunction
+
+class Robyn:
+    """This is the python wrapper for the Robyn binaries.
+    """
+    def __init__(self) -> None:
+        self.server = Server()
+
+    def add_route(self, route_type, endpoint, handler):
+        handler = {
+            "is_async": iscoroutinefunction(handler),
+            "handler": handler
+        }
+        self.server.add_route(route_type, endpoint, handler)
+
+    def start(self, port):
+        print(f"Starting the server at port: {port}")
+        self.server.start(port)
+
+    def get(self, endpoint):
+        def inner(handler):
+            self.add_route("GET", endpoint, handler)
+        return inner
+    
+    def post(self, endpoint):
+        def inner(handler):
+            self.add_route("POST", endpoint, handler)
+        return inner
+
+    def put(self, endpoint):
+        def inner(handler):
+            self.add_route("PUT", endpoint, handler)
+        return inner
+
+    def update(self, endpoint):
+        def inner(handler):
+            self.add_route("UPDATE", endpoint, handler)
+        return inner
+
+    def delete(self, endpoint):
+        def inner(handler):
+            self.add_route("DELETE", endpoint, handler)
+        return inner
+
+    def patch(self, endpoint):
+        def inner(handler):
+            self.add_route("PATCH", endpoint, handler)
+        return inner
+
+
+
+
+
