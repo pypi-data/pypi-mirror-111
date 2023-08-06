e2707e55963236027b2ce86b0154fbe2a0f395ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:40 +0000

    Add package dsFramework/0.1.47/dsFramework-0.1.47.tar.gz

---
+++ packages/dsFramework/dsFramework-0.1.47.tar.gz/dsFramework-0.1.47/cli/tester/__init__.py
@@ -0,0 +1,6 @@
+from cli.tester.dataset_template import generatedClass
+from cli.tester.reporter_template import generatedClass
+from cli.tester.tester_template import generatedClass
+from cli.tester.server_run_template import generatedClass
+from cli.tester.pool_template import generatedClass
+
