c7c3ad17d3572fafc3af9d3b3faf5a662500afad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:24 +0000

    Add package imblog/0.2/imblog-0.2-py3-none-any.whl

---
+++ packages/imblog/imblog-0.2-py3-none-any.whl/imblog/common/utils.py
@@ -0,0 +1,66 @@
+import requests
+import os
+import tempfile
+import datetime,shutil
+import uuid
+from imblog.common import config
+__all__ =["load_cookie","save_cookie"]
+
+def get_dir(dir):
+    tmp_download_path = os.path.join(tempfile.gettempdir(), dir)
+    if not os.path.exists(tmp_download_path):
+        os.makedirs(tmp_download_path)
+    return tmp_download_path
+def download_img(image_url):
+    try:
+        download_path=get_dir("download")
+        img_path=os.path.join(download_path, f"img-{uuid.uuid4().hex}.jpg")
+        img_data = requests.get(image_url).content
+        with open(img_path, 'wb') as handler:
+            handler.write(img_data)
+    except:
+        img_path = None
+        pass
+    return img_path
+
+def make_folder(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+def load_cookie(cookie_folder,email):
+    api_address = os.getenv("MAIL_SERVER", config.ServerAdress.MAIL_SERVER)
+    r = requests.get(api_address+"/automail/api/cookie/get/"+email)
+    make_folder(cookie_folder)
+    with open(cookie_folder+"/cookies.sqlite", 'wb') as f:
+        f.write(r.content)
+
+def save_cookie(cookie_folder,email):
+    with open(cookie_folder+"/cookies.sqlite", 'rb') as f:
+        files = {'file':('cookies.sqlite',f)}
+        data={'fileName':email}
+        api_address=os.getenv("MAIL_SERVER", config.ServerAdress.MAIL_SERVER)
+        requests.post(api_address+"/automail/api/mail/upload/", data=data,files=files)
+def call_success(server_address,job_id,gmail,video_id):
+    log_data="s;;"+ str(datetime.datetime.now())+";;"+gmail+";;"+video_id;
+    data={'id':job_id,'log_data':log_data}
+    requests.post(server_address,data=None,json=data)
+def clear_data():
+    cmd= "rm -rf "+get_dir('download')
+    try:
+        shutil.rmtree(get_dir('download'))
+    except:
+        os.system(cmd)
+        pass
+def unlock_process(server_address,job_id):
+    log_data="f;;"+ str(datetime.datetime.now());
+    data={'id':job_id,'log_data':log_data}
+    requests.post(server_address,data=None,json=data)
+
+def call_error(server_address,job_id,gmail,log):
+    log_data="e;;"+ str(datetime.datetime.now())+";;"+gmail+";;"+log;
+    data={'id':job_id,'log_data':log_data}
+    requests.post(server_address,data=None,json=data)
+
+def get_video():
+    return requests.get("http://news.singerchart.com/msn/link/get").text;
+
+
