b777a1b16874b46bc5d0417f86fb77c950d43d7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:00 +0000

    Add package graia-application-mirai/0.20.1/graia-application-mirai-0.20.1.tar.gz

---
+++ packages/graia-application-mirai/graia-application-mirai-0.20.1.tar.gz/graia-application-mirai-0.20.1/src/graia/application/event/lifecycle.py
@@ -0,0 +1,51 @@
+from typing import Any
+import typing
+from graia.broadcast.entities.event import Dispatchable, BaseDispatcher
+from graia.broadcast.interfaces.dispatcher import DispatcherInterface
+
+if typing.TYPE_CHECKING:
+    from graia.application import GraiaMiraiApplication
+
+class ApplicationLaunched(Dispatchable):
+    app: "GraiaMiraiApplication"
+
+    def __init__(self, app) -> None:
+        self.app = app
+
+    class Dispatcher(BaseDispatcher):
+        @staticmethod
+        async def catch(interface: "DispatcherInterface"):
+            from .. import GraiaMiraiApplication
+
+            if interface.annotation is GraiaMiraiApplication:
+                return interface.event.app
+
+
+class ApplicationLaunchedBlocking(Dispatchable):
+    app: "GraiaMiraiApplication"
+
+    def __init__(self, app) -> None:
+        self.app = app
+
+    class Dispatcher(BaseDispatcher):
+        @staticmethod
+        async def catch(interface: "DispatcherInterface"):
+            from .. import GraiaMiraiApplication
+
+            if interface.annotation is GraiaMiraiApplication:
+                return interface.event.app
+
+
+class ApplicationShutdowned(Dispatchable):
+    app: "GraiaMiraiApplication"
+
+    def __init__(self, app) -> None:
+        self.app = app
+
+    class Dispatcher(BaseDispatcher):
+        @staticmethod
+        async def catch(interface: "DispatcherInterface"):
+            from .. import GraiaMiraiApplication
+
+            if interface.annotation is GraiaMiraiApplication:
+                return interface.event.app
