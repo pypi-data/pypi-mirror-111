611f1caa3d2bbdf6adf889ee319c4c7c9760e8e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:21 +0000

    Add package addoneaddmat/0.0.4/addoneaddmat-0.0.4.tar.gz

---
+++ packages/addoneaddmat/addoneaddmat-0.0.4.tar.gz/addoneaddmat-0.0.4/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.4'
+DESCRIPTION = 'My first Python package'
+LONG_DESCRIPTION = 'Same as regular description'
+
+setup(
+    name="addoneaddmat",
+    version=VERSION,
+    author="Wayne Lam",
+    author_email="",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['numpy'],
+    keywords=['test', 'first package'],
+)
