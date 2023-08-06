4c48b03667c16b08ed1ba3067fcca271c0c86306
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:33 +0000

    Add package abcdijklm.py/1.0.3/abcdijklm.py-1.0.3.tar.gz

---
+++ packages/abcdijklm.py/abcdijklm.py-1.0.3.tar.gz/abcdijklm.py-1.0.3/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*
+
+from __future__ import absolute_import
+
+import os
+from codecs import open
+
+from setuptools import find_packages, setup
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+with open('requirements.txt') as f:
+    install_requires = f.read().splitlines()
+
+readme = "Just Use yem"
+history = "Nothing"
+
+setup(name='abcdijklm.py',
+      version='1.0.3',
+      packages=find_packages('src', exclude=('tests', )),
+      package_dir={'': 'src'},
+      include_package_data=True,
+      zip_safe=False,
+      description='test',
+      long_description=readme + '\n\n' + history,
+      long_description_content_type='text/markdown',
+      author='Me',
+      author_email='albert@trashemail.in',
+      license='Creative Commons Attribution-Noncommercial-Share Alike license',
+      install_requires=install_requires,
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3',
+          'Topic :: Internet :: WWW/HTTP',
+      ])
