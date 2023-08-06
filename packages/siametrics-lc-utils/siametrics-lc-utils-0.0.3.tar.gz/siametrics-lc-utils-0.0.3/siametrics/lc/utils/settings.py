62bb864eca25cee728c099c5ba8e238e03d1f365
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:17 +0000

    Add package siametrics-lc-utils/0.0.3/siametrics-lc-utils-0.0.3.tar.gz

---
+++ packages/siametrics-lc-utils/siametrics-lc-utils-0.0.3.tar.gz/siametrics-lc-utils-0.0.3/siametrics/lc/utils/settings.py
@@ -0,0 +1,21 @@
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    AWS_REGION: str = 'ap-southeast-1'
+    AWS_SNS_KEY: str = None
+    AWS_SNS_SECRET: str = None
+    AWS_SNS_PATH: str = 'arn:aws:sns:ap-southeast-1:580482583062:'
+
+    class Config:
+        case_sensitive = True
+
+        fields = {
+            'AWS_REGION': {'env': 'AWS_REGION'},
+            'AWS_SNS_KEY': {'env': 'AWS_SNS_KEY'},
+            'AWS_SNS__SECRET': {'env': 'AWS_SNS__SECRET'},
+            'AWS_SNS_PATH': {'env': 'AWS_SNS_PATH'},
+        }
+
+
+settings = Settings()
