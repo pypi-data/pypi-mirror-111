d38609b91379d9a5639044fee473f80c9d52f592
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:44 +0000

    Add package oknlp/0.0.4rc2/oknlp-0.0.4rc2-cp37-cp37m-macosx_10_14_x86_64.whl

---
+++ packages/oknlp/oknlp-0.0.4rc2-cp37-cp37m-macosx_10_14_x86_64.whl/oknlp/algorithm/cws/BaseCWS.py
@@ -0,0 +1,23 @@
+from typing import List
+from ..abc import Algorithm
+
+
+class BaseCWS(Algorithm):
+
+    def __call__(self, sents: List[str]) -> List[List[str]]:
+        """
+        Args:
+            sents: 输入的句子列表。
+        Returns:
+            返回一个和输入相同长度的列表，列表中的每一项都是对应输入的分词结果。
+        
+        更多信息请参考 :doc:`中文分词 - 示例</examples/cws>`
+
+        Examples:    
+            >>> import oknlp
+            >>> cws = oknlp.cws.get_by_name()
+            >>> cws(['我爱北京天安门'])
+            [['我', '爱', '北京', '天安门']]
+        
+        """
+        return super().__call__(sents)
