de9d41a3f90f37ce731cce6b48f4aad6c0bf55ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:17 +0000

    Add package birria/0.1.0/birria-0.1.0-py3-none-any.whl

---
+++ packages/birria/birria-0.1.0-py3-none-any.whl/birria/__init__.py
@@ -0,0 +1,23 @@
+from birria._birria import (
+    cook,
+    ingredient,
+    ingredients,
+    is_cooked,
+    is_cooked_instance,
+    is_cooked_class,
+    serve,
+    _match_opt_strings
+)
+
+__version__ = "0.1.0"
+
+__all__ = [
+    "cook",
+    "ingredient",
+    "ingredients",
+    "is_cooked",
+    "is_cooked_instance",
+    "is_cooked_class",
+    "serve",
+    "_match_opt_strings"
+]
