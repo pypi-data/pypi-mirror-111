0962ff7b8262ad04519fb38f160ac7c0de30f858
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:53 +0000

    Add package text2text/0.4.7/text2text-0.4.7-py3-none-any.whl

---
+++ packages/text2text/text2text-0.4.7-py3-none-any.whl/text2text/handler.py
@@ -0,0 +1,34 @@
+import text2text as t2t
+
+class Handler(object):
+  """
+  Text Handler unified API for text transformers
+  """
+
+  EXPOSED_TRANSFORMERS = {
+    "answer": t2t.Answerer,
+    "count": t2t.Counter,
+    "measure": t2t.Measurer,
+    "question": t2t.Questioner,
+    "search": t2t.Searcher,
+    "summarize": t2t.Summarizer,
+    "tfidf": t2t.Tfidfer,
+    "tokenize": t2t.Tokenizer,
+    "translate": t2t.Translator,
+    "variate": t2t.Variator,
+    "vectorize": t2t.Vectorizer,
+  }
+
+  def _transformer_handler(self, transformation, **kwargs):
+    transformer = self.__class__.transformer_instances.get(transformation, self.__class__.EXPOSED_TRANSFORMERS[transformation]())
+    self.__class__.transformer_instances[transformation] = transformer
+    return transformer.transform(input_lines=self.input_lines, src_lang=self.src_lang, **kwargs)
+    
+  def __init__(self, input_lines=[], src_lang='en', **kwargs):
+    self.input_lines = input_lines
+    self.src_lang = src_lang
+    for k in self.__class__.EXPOSED_TRANSFORMERS:
+      handler = lambda x: lambda **kwargs: self._transformer_handler(transformation=x, **kwargs)
+      handler = handler(k)
+      setattr(self, k, handler)
+    self.__class__.transformer_instances = {}
