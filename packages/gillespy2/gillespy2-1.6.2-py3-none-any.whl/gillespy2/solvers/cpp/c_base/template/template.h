f3ba109b926a1293c2a9f20d311e6b9b490884c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:07 +0000

    Add package gillespy2/1.6.2/gillespy2-1.6.2-py3-none-any.whl

---
+++ packages/gillespy2/gillespy2-1.6.2-py3-none-any.whl/gillespy2/solvers/cpp/c_base/template/template.h
@@ -0,0 +1,40 @@
+/*
+ * GillesPy2 is a modeling toolkit for biochemical simulation.
+ * Copyright (C) 2019-2021 GillesPy2 developers.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* TEMPLATE.H
+ *
+ * This is the header file which defines the interface of the template.
+ * Includes functions for loading and defining simulation parameters.
+ */
+
+#include "model.h"
+
+namespace Gillespy
+{
+	extern std::vector<unsigned int> species_populations;
+	extern std::vector<std::string> species_names;
+	extern std::vector<std::string> reaction_names;
+
+	double map_propensity(int reaction_id, const std::vector<int> &state);
+	double map_propensity(int reaction_id, unsigned int *S);
+	double map_ode_propensity(int reaction_id, const std::vector<double> &state);
+	void add_reactions(Model &model);
+
+	void map_variable_parameters(std::stringstream &stream);
+	void map_variable_populations(std::stringstream &stream);
+}
