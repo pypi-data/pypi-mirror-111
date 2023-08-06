faaf1a80ec00b739c4b27f5ec83e4f8520edf147
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:40 +0000

    Add package unimatrix.ext.crypto/0.8.3/unimatrix.ext.crypto-0.8.3.tar.gz

---
+++ packages/unimatrix.ext.crypto/unimatrix.ext.crypto-0.8.3.tar.gz/unimatrix.ext.crypto-0.8.3/unimatrix/ext/crypto/conf.py
@@ -0,0 +1,39 @@
+"""Provides the interface to configure the :mod:`unimatrix.ext.crypto`
+package at runtime.
+"""
+import asyncio
+import concurrent.futures
+
+import ioc.loader
+import unimatrix.lib.etc
+from unimatrix.lib import meta
+from unimatrix.lib.datastructures import ImmutableDTO
+
+from .keychain import chain
+
+
+@meta.allow_sync
+async def configure(**config):
+    """Configure the :mod:`unimatrix.ext.crypto` package."""
+    config = ImmutableDTO.fromdict(config)
+
+    # Run all loaders if they are configured.
+    futures = []
+    for loader_config in (config.get('loaders') or []):
+        Loader = ioc.loader.import_symbol(loader_config.loader)
+        instance = Loader(
+            loader_config.options,
+            public_only=loader_config.get('public_only', False)
+        )
+        futures.append(instance.load())
+
+    if futures:
+        await asyncio.gather(*futures)
+
+
+def load(fp):
+    """Load keys from a YAML configuration file."""
+    config = unimatrix.lib.etc.load(fp)
+    with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+        for k in config.get('keys') or {}:
+            executor.submit(chain.register_deferred, config['keys'][k])
