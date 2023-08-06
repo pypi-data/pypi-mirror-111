f2d0dbd935be48f3c0a926f8d625afa688b596ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:14 +0000

    Add package django-expression-index/0.2.1/django-expression-index-0.2.1.tar.gz

---
+++ packages/django-expression-index/django-expression-index-0.2.1.tar.gz/django-expression-index-0.2.1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    author='Kamil Mierzejewski',
+    name='django-expression-index',
+    version='0.2.1',
+    description='Subclass of django.db.models.Index, which enables indexing on expressions.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/kmierzeje/django-expression-index',
+    packages=setuptools.find_packages(),
+    install_requires=['django>=2.2.13'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
