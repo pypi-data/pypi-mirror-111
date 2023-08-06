32cf51509777419a03c28070fb6ad0ce03e8a176
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:08 +0000

    Add package py-snatch/2.8.0/py_snatch-2.8.0-py3-none-any.whl

---
+++ packages/py-snatch/py_snatch-2.8.0-py3-none-any.whl/snatch/__init__.py
@@ -0,0 +1,8 @@
+from snatch.banco_central import BancoCentral  # noqa
+from snatch.boa_vista import BoaVista  # noqa
+from snatch.cgu_pep import PEP  # noqa
+from snatch.data_risk import DataRisk  # noqa
+from snatch.qsa import QSA  # noqa
+from snatch.receita_federal_pj import ReceitaFederalPJ  # noqa
+
+__version__ = "2.8.0"
