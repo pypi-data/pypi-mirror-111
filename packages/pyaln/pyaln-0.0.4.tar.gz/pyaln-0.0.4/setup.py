f3da1344fe79119235e09639476b5b0e2d750929
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:53 +0000

    Add package pyaln/0.0.4/pyaln-0.0.4.tar.gz

---
+++ packages/pyaln/pyaln-0.0.4.tar.gz/pyaln-0.0.4/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+if __name__ == "__main__":
+    setup(
+# install_requires =
+        #     Bio >=1.46
+        #     pandas >=1.2
+        #     numpy >=1.16
+        install_requires = [
+            'Bio',
+            'ajsdlhabsdlihbasd',
+            'easyterm'
+        ]
+        
+
+    )
