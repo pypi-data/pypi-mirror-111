0e4696f9ede3f37aa79f1d1142510dde2405e7c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:05 +0000

    Add package tangata/0.2.0/tangata-0.2.0.tar.gz

---
+++ packages/tangata/tangata-0.2.0.tar.gz/tangata-0.2.0/setup.py
@@ -0,0 +1,42 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='tangata',
+    url='https://github.com/ciejer/tangata_local',
+    author="Chris Jenkins",
+    author_email='chris@chrisjenkins.nz',
+    description='An interactive catalog for dbt_',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Topic :: Database :: Front-Ends",
+        "Framework :: Flask",
+        "Development Status :: 4 - Beta",
+    ],
+    version='0.2.0',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': ['tangata=tangata.tangata:tangata',
+        ],
+    },
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'flask',
+        'Flask-SocketIO',
+        'PyYAML',
+        'PyDriller',
+        'python-dateutil',
+        'simple-websocket',
+        'whoosh',
+        'GitPython',
+        'apscheduler',
+        'ruamel.yaml',
+    ],
+)
