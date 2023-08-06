7627b3f77d82043446369b0cbdda121684db1ac8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:30 +0000

    Add package redata/0.4.2/redata-0.4.2.tar.gz

---
+++ packages/redata/redata-0.4.2.tar.gz/redata-0.4.2/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_namespace_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r") as fr:
+    requirements = fr.read().splitlines()
+
+setup(
+    name='redata',
+    version='0.4.2',
+    packages=find_namespace_packages(),
+    url='https://github.com/UAL-RE/redata-commons/',
+    project_urls={
+        'Source': 'https://github.com/UAL-RE/redata-commons/',
+        'Tracker': 'https://github.com/UAL-RE/redata-commons/issues',
+    },
+    license='MIT License',
+    author='Chun Ly',
+    author_email='astro.chun@gmail.com',
+    description='Commons code used by ReDATA software',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    python_requires='>=3.7',
+    install_requires=requirements,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9'
+    ]
+)
