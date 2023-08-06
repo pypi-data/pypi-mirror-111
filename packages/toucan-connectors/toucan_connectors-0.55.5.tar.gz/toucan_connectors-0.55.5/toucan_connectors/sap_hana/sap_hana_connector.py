ba3bcf5ba74b0db824b95da330aec4cb23c75794
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:17 +0000

    Add package toucan-connectors/0.55.5/toucan_connectors-0.55.5.tar.gz

---
+++ packages/toucan-connectors/toucan_connectors-0.55.5.tar.gz/toucan_connectors-0.55.5/toucan_connectors/sap_hana/sap_hana_connector.py
@@ -0,0 +1,43 @@
+from hdbcli import dbapi
+from pydantic import Field, SecretStr, constr
+
+from toucan_connectors.common import pandas_read_sql
+from toucan_connectors.toucan_connector import ToucanConnector, ToucanDataSource
+
+
+class SapHanaDataSource(ToucanDataSource):
+    query: constr(min_length=1) = Field(
+        ..., description='You can write your SQL query here', widget='sql'
+    )
+
+
+class SapHanaConnector(ToucanConnector):
+    """
+    Import data from Sap Hana.
+    """
+
+    data_source_model: SapHanaDataSource
+
+    host: str = Field(
+        ...,
+        description='The domain name (preferred option as more dynamic) or '
+        'the hardcoded IP address of your database server',
+    )
+
+    port: int = Field(..., description='The listening port of your database server')
+    user: str = Field(..., description='Your login username')
+    password: SecretStr = Field('', description='Your login password')
+
+    def _retrieve_data(self, data_source):
+        connection = dbapi.connect(
+            address=self.host,
+            port=self.port,
+            user=self.user,
+            password=self.password.get_secret_value() if self.password else '',
+        )
+
+        df = pandas_read_sql(data_source.query, con=connection)
+
+        connection.close()
+
+        return df
