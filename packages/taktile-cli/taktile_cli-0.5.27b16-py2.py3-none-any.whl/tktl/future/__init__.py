a7064fe69f2447d46d283e22ff6223c4a38ce7f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:56 +0000

    Add package taktile-cli/0.5.27b16/taktile_cli-0.5.27b16-py2.py3-none-any.whl

---
+++ packages/taktile-cli/taktile_cli-0.5.27b16-py2.py3-none-any.whl/tktl/future/__init__.py
@@ -0,0 +1,8 @@
+from .registration.decorator import Tktl  # noqa: F401
+from .registration.endpoints import (  # noqa: F401
+    ArrowEndpoint,
+    BasicEndpoint,
+    Endpoint,
+    ProfiledEndpoint,
+    TypedEndpoint,
+)
