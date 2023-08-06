7020bf948f72e3a16edde713441dc28fe6bfe8fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:03 +0000

    Add package GaussBin-dist/0.2/GaussBin_dist-0.2.tar.gz

---
+++ packages/GaussBin-dist/GaussBin_dist-0.2.tar.gz/GaussBin_dist-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='GaussBin_dist',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['GaussBin_dist'],
+      zip_safe=False)
