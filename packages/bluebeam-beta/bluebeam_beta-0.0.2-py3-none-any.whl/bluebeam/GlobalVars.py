29c7d614cf8eba993f4872ebc7fc5b32e1736fea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:36:51 +0000

    Add package bluebeam-beta/0.0.2/bluebeam_beta-0.0.2-py3-none-any.whl

---
+++ packages/bluebeam-beta/bluebeam_beta-0.0.2-py3-none-any.whl/bluebeam/GlobalVars.py
@@ -0,0 +1,42 @@
+class GlobalVars:
+
+    def __init__(self):
+        self._tick_rate = -1
+        self._delta_time = -1
+        self._instid = 100000
+
+    @property
+    def screen_size(self):
+        return self._screen_size
+
+    @screen_size.setter
+    def screen_size(self, sz):
+        self._screen_size = sz
+
+    @property
+    def tick_rate(self):
+        return self._tick_rate
+
+    @tick_rate.setter
+    def tick_rate(self, tr):
+        self._tick_rate = tr
+
+    @property
+    def delta_time(self):
+        return self._delta_time
+
+    @delta_time.setter
+    def delta_time(self, dt):
+        self._delta_time = dt
+
+    @property
+    def instid(self):
+        return self._instid
+
+    @instid.setter
+    def instid(self, id):
+        self._instid = id
+
+    def getid(self):
+        self._instid += 1
+        return self._instid - 1
