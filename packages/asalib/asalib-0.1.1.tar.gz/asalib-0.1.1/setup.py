ad27db534f4d5620a2f17a78001a4f8cdecfdce3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:36 +0000

    Add package asalib/0.1.1/asalib-0.1.1.tar.gz

---
+++ packages/asalib/asalib-0.1.1.tar.gz/asalib-0.1.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='asalib',
+    version='0.1.1',    
+    description='t.me/asalinks lib',
+    url='https://t.me/asalinks',
+    author='https://t.me/cagavo',
+    author_email='no@no.com',
+    license='nothing',
+    packages=['asalib'],
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Religion',
+        'License :: Freeware',
+        'Natural Language :: Italian',  
+        'Operating System :: OS Independent',        
+        'Programming Language :: Python'
+    ],
+)
