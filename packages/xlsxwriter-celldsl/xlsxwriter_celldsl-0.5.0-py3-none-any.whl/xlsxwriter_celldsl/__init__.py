65bef0e0f423e85f42720788170dfaf3755c5997
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:17:50 +0000

    Add package xlsxwriter-celldsl/0.5.0/xlsxwriter_celldsl-0.5.0-py3-none-any.whl

---
+++ packages/xlsxwriter-celldsl/xlsxwriter_celldsl-0.5.0-py3-none-any.whl/xlsxwriter_celldsl/__init__.py
@@ -0,0 +1,14 @@
+"""Cell DSL implementation for XlsxWriter, providing ways to index cells using relative movement and giving names to
+various key coordinates, overall allowing one to write Excel table generating code by imagining how they would do it
+manually and translating the actions into commands in `ops` module, after `commit`ing them inside `cell_dsl_context`,
+after which stats can be transmitted to a `StatReceiver` and used for further writing, all the while constructing
+text formats by conjunction `FormatDict`s and using some default formats from `FormatsNamespace`."""
+
+import cell_dsl
+import formats
+import ops
+import utils
+from cell_dsl import StatReceiver, cell_dsl_context
+from utils import WorkbookPair
+
+__all__ = ['cell_dsl', 'ops', 'formats', 'utils', 'cell_dsl_context', 'StatReceiver', 'WorkbookPair']
