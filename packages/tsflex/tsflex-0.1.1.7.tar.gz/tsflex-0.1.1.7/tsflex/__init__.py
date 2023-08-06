678b1189e55b2c5968038be5931567395ea7aa92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:32 +0000

    Add package tsflex/0.1.1.7/tsflex-0.1.1.7.tar.gz

---
+++ packages/tsflex/tsflex-0.1.1.7.tar.gz/tsflex-0.1.1.7/tsflex/__init__.py
@@ -0,0 +1,17 @@
+"""<i><b>flex</b>ible <b>t</b>ime-<b>s</b>eries operations</i>
+
+.. include:: ../docs/pdoc_include/root_documentation.md
+"""
+
+__docformat__ = 'numpy'
+__author__ = "Jonas Van Der Donckt, Jeroen Van Der Donckt, Emiel Deprost"
+__version__ = '0.1.1.7'
+__pdoc__ = {
+    # do not show tue utils module
+    'tsflex.utils': False,
+    # show the seriesprocessor & numpyfuncwrapper their call method
+    'SeriesProcessor.__call__': True,
+    'NumpyFuncWrapper.__call__': True,
+}
+
+__all__ = ["__version__", "__pdoc__"]
