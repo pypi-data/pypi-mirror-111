1fc7c84b7af9c50af5f17ea3a5fc03b645750a71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:55 +0000

    Add package yami/0.1.3/Yami-0.1.3.tar.gz

---
+++ packages/yami/Yami-0.1.3.tar.gz/Yami-0.1.3/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['yami']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['hikari==2.0.0.dev100']
+
+setup_kwargs = {
+    'name': 'yami',
+    'version': '0.1.3',
+    'description': 'A command handler that complements Hikari, a Discord API wrapper written in Python.',
+    'long_description': None,
+    'author': 'Jonxslays',
+    'author_email': 'jon@jonxslays.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/Jonxslays/Yami',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<3.11',
+}
+
+
+setup(**setup_kwargs)
