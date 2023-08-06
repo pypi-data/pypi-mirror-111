81c592fe2780532e80c9cc427178752c4197fb94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:51 +0000

    Add package simplepinyin/0.1.1/simplepinyin-0.1.1.tar.gz

---
+++ packages/simplepinyin/simplepinyin-0.1.1.tar.gz/simplepinyin-0.1.1/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+setuptools.setup(
+    name="simplepinyin", # 名称
+    version="0.1.1", # 版本
+    author="Chen, Mingxiang", # 作者
+    author_email="chenmingxiang110@gmail.com", # 邮箱
+    package_data={
+        '': [
+            '*.pickle',
+            'LICENSE',
+        ]},
+    description="This package integrates the basic Pinyin to Chinese functions, making Python-based Pinyin-Chinese conversion simple and convenient.", # 简介
+    long_description="", # 详细介绍
+    long_description_content_type="text/markdown", # 详细介绍的文件类型
+    url="https://github.com/chenmingxiang110/SimpleChinese2", # 包的链接
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+    install_requires=[
+        'numpy',
+        'pypinyin',
+        'tqdm',
+    ]
+)
