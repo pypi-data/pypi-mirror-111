7c7282bf3dd072ea18323085450301b2b925002d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:23 +0000

    Add package clickhouse-http-client/1.0.2/clickhouse-http-client-1.0.2.tar.gz

---
+++ packages/clickhouse-http-client/clickhouse-http-client-1.0.2.tar.gz/clickhouse-http-client-1.0.2/clickhouse_http_client/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2021/1/30 15:32
+# @Author  : tomoncle
+# @Site    : https://github.com/tomoncle/clickhouse-http-client
+# @File    : __init__.py.py
+# @Software: PyCharm
+
+from .client import ClickHouse
+
+__all__ = [
+    'ClickHouse'
+]
