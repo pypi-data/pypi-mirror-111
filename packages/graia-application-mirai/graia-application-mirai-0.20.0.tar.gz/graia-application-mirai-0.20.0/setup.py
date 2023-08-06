be89c81c6662c6e2f60941a676d52bbb237d6755
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:55 +0000

    Add package graia-application-mirai/0.20.0/graia-application-mirai-0.20.0.tar.gz

---
+++ packages/graia-application-mirai/graia-application-mirai-0.20.0.tar.gz/graia-application-mirai-0.20.0/setup.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['graia',
+ 'graia.application',
+ 'graia.application.event',
+ 'graia.application.message',
+ 'graia.application.message.elements',
+ 'graia.application.message.parser',
+ 'graia.application.test']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['aiohttp>=3.6.2,<4.0.0',
+ 'graia-broadcast>=0.11.0,<0.12.0',
+ 'pydantic>=1.8.2,<2.0.0',
+ 'regex>=2020.7.14,<2021.0.0',
+ 'yarl>=1.4.2,<2.0.0']
+
+extras_require = \
+{':python_version < "3.7"': ['contextvars>=2.4,<3.0']}
+
+setup_kwargs = {
+    'name': 'graia-application-mirai',
+    'version': '0.20.0',
+    'description': '',
+    'long_description': None,
+    'author': 'GreyElaina',
+    'author_email': 'GreyElaina@outlook.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'extras_require': extras_require,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
