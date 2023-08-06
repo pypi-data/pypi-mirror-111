adc9e695381771b304f7bdf3b75be1b9dc71b537
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:54 +0000

    Add package zy-test-pip/1.0.0/zy-test-pip-1.0.0.tar.gz

---
+++ packages/zy-test-pip/zy-test-pip-1.0.0.tar.gz/zy-test-pip-1.0.0/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import re
+
+import setuptools
+
+version = "1.0.0"
+
+
+setuptools.setup(
+    name="zy-test-pip",
+    version=version,
+    author="bwz0328",
+    author_email="bwz0328@126.com",
+    description="This is the SDK for example.",
+    long_description="",
+    long_description_content_type="text/markdown",
+    url="http://example.com",
+    install_requires=[
+        'requests!=2.9.0',
+        'lxml>=4.2.3',
+        'monotonic>=1.5',
+    ],
+    packages=setuptools.find_packages(exclude=("test"))
+)
