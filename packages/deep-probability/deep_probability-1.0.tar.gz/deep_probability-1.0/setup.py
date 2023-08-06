229a91f989b688716b00103a6946a94794038427
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:43 +0000

    Add package deep-probability/1.0/deep_probability-1.0.tar.gz

---
+++ packages/deep-probability/deep_probability-1.0.tar.gz/deep_probability-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='deep_probability',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['deep_probability'],
+      author = 'Deep Rodge',
+      author_email = 'deeprodge14@gmail.com',
+      zip_safe=False)
