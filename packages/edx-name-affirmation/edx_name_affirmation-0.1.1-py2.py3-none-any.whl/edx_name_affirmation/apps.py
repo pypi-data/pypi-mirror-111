51784c3707997b3e0ea28065ef8e9819b3d138c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:30 +0000

    Add package edx-name-affirmation/0.1.1/edx_name_affirmation-0.1.1-py2.py3-none-any.whl

---
+++ packages/edx-name-affirmation/edx_name_affirmation-0.1.1-py2.py3-none-any.whl/edx_name_affirmation/apps.py
@@ -0,0 +1,13 @@
+"""
+edx_name_affirmation Django application initialization.
+"""
+
+from django.apps import AppConfig
+
+
+class EdxNameAffirmationConfig(AppConfig):
+    """
+    Configuration for the edx_name_affirmation Django application.
+    """
+
+    name = 'edx_name_affirmation'
