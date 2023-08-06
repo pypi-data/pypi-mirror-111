6da0f86d0b6625f03544d8edce774ffb903b5cdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:06 +0000

    Add package ctpros/0.0.5/ctpros-0.0.5.tar.gz

---
+++ packages/ctpros/ctpros-0.0.5.tar.gz/ctpros-0.0.5/setup.py
@@ -0,0 +1,36 @@
+import setuptools, glob, os, sys
+
+with open("README.md", "r") as fh:
+    readme = fh.read()
+
+version = "0.0.5"
+author = "Carlos Osuna"
+author_email = "charlie@caosuna.com"
+ldct = "text/markdown"
+url = "https://gitlab.com/caosuna/ctpros"
+dependencies = [
+    "itk==4.13.2.post1",
+    "lazy-import==0.2.2",
+    "mayavi==4.6.2",
+    "numpy==1.16.5",
+    "pandas==0.25.0",
+    "scipy==1.6.0",
+    "scikit-image==0.17.2",
+    "tqdm==4.48.2",
+    "vtk==8.1.2",
+]
+
+setuptools.setup(
+    name="ctpros",
+    version=version,
+    author=author,
+    author_email=author_email,
+    description="Computed Tomography Processing Registration Open Sourced",
+    long_description=readme,
+    long_description_content_type=ldct,
+    url=url,
+    packages=setuptools.find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=dependencies,
+    python_requires=">=3.7",
+)
