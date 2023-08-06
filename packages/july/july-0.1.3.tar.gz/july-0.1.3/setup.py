4f55a67befd42499aa0d9fb696dccffdf521042a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:50 +0000

    Add package july/0.1.3/july-0.1.3.tar.gz

---
+++ packages/july/july-0.1.3.tar.gz/july-0.1.3/setup.py
@@ -0,0 +1,42 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="july",
+    version="0.1.3",
+    author="Edvard Hultén",
+    author_email="edvard.hulten@gmail.com",
+    description="A small library for creating pretty heatmaps of daily data.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    license="MIT",
+    url="https://github.com/e-hulten/july/",
+    keywords=[
+        "heatmap",
+        "visualisation",
+        "calendar",
+        "daily",
+        "matplotlib",
+        "github plot",
+        "month plot",
+        "date plot",
+        "plot",
+        "plotting",
+    ],
+    classifiers=[
+        "Topic :: Scientific/Engineering :: Visualization",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    install_requires=[
+        "matplotlib",
+        "numpy",
+    ],
+    python_requires=">=3.6",
+)
