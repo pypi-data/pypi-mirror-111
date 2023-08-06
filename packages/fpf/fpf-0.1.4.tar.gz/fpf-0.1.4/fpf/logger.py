c2742967b2624abcb569e2d868b59f5419f22157
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:55 +0000

    Add package fpf/0.1.4/fpf-0.1.4.tar.gz

---
+++ packages/fpf/fpf-0.1.4.tar.gz/fpf-0.1.4/fpf/logger.py
@@ -0,0 +1,16 @@
+"""Provides logging for the File Path Filtering module.
+
+A logger is available, created with the name 'FPF`'.
+At the time of writing, not much actual logging will happen.
+
+
+.. code-block:: python
+
+    from logging import getLogger
+    logger = getLogger('FPF')
+
+"""
+
+from logging import getLogger
+
+logger = getLogger('FPF')
