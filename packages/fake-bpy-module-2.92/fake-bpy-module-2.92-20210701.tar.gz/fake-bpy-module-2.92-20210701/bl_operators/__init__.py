6a0c3d056ab499fb20bc3cf3c7951e017201e1b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:57 +0000

    Add package fake-bpy-module-2.92/20210701/fake-bpy-module-2.92-20210701.tar.gz

---
+++ packages/fake-bpy-module-2.92/fake-bpy-module-2.92-20210701.tar.gz/fake-bpy-module-2.92-20210701/bl_operators/__init__.py
@@ -0,0 +1,45 @@
+import sys
+import typing
+from . import add_mesh_torus
+from . import rigidbody
+from . import uvcalc_follow_active
+from . import presets
+from . import object_align
+from . import screen_play_rendered_anim
+from . import anim
+from . import view3d
+from . import object
+from . import wm
+from . import object_quick_effects
+from . import sequencer
+from . import console
+from . import uvcalc_lightmap
+from . import userpref
+from . import image
+from . import freestyle
+from . import assets
+from . import vertexpaint_dirt
+from . import object_randomize_transform
+from . import bmesh
+from . import clip
+from . import node
+from . import geometry_nodes
+from . import file
+from . import constraint
+from . import mesh
+
+
+def register():
+    ''' 
+
+    '''
+
+    pass
+
+
+def unregister():
+    ''' 
+
+    '''
+
+    pass
---
+++ packages/fake-bpy-module-2.92/fake-bpy-module-2.92-20210701.tar.gz/fake-bpy-module-2.92-20210701/gpu/__init__.py
@@ -0,0 +1,6 @@
+import sys
+import typing
+from . import matrix
+from . import shader
+from . import select
+from . import types
