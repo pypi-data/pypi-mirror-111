4bd50da95e6e986d8898963b331452b01603e194
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:17 +0000

    Add package vital/0.2.26/vital-0.2.26.tar.gz

---
+++ packages/vital/vital-0.2.26.tar.gz/vital-0.2.26/vital/api/workouts.py
@@ -0,0 +1,26 @@
+from typing import List, Mapping, Optional
+
+from vital.api.api import API
+
+
+class Workouts(API):
+    """Endpoints for getting sleep data."""
+
+    def get(
+        self,
+        user_key: str,
+        start_date: str,
+        end_date: str,
+        provider: Optional[str] = "",
+    ) -> Mapping[str, List[Mapping]]:
+        """
+        GET Workout data.
+        """
+        return self.client.get(
+            f"/workouts/{user_key}",
+            params={
+                "start_date": start_date,
+                "end_date": end_date,
+                "provider": provider,
+            },
+        )
