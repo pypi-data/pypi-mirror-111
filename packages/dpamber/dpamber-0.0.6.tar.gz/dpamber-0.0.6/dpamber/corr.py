9ee8a7f16b65c2803f8d2fcecd3470a1818a4021
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:39 +0000

    Add package dpamber/0.0.6/dpamber-0.0.6.tar.gz

---
+++ packages/dpamber/dpamber-0.0.6.tar.gz/dpamber-0.0.6/dpamber/corr.py
@@ -0,0 +1,38 @@
+import dpdata
+import numpy as np
+
+
+def get_amber_fp(cutoff: float,
+                 parmfile: str,
+                 ncfile: str,
+                 ll: str,
+                 hl: str,
+                 target: str = ":1",
+                 out: str = "dataset"):
+    ms = dpdata.MultiSystems()
+    ep = r'@%EP'
+    interactwith = "(%s)<@%f&!%s" % (target, cutoff, ep)
+
+    s_ll = dpdata.LabeledSystem(
+        ll, nc_file=ncfile, parm7_file=parmfile, fmt='amber/md', use_element_symbols=target)
+    s_hl = dpdata.LabeledSystem(
+        hl, nc_file=ncfile, parm7_file=parmfile, fmt='amber/md', use_element_symbols=target)
+    s_corr = s_ll.correction(s_hl)
+    s_corr = s_corr.pick_by_amber_mask(
+        parmfile, interactwith, pass_coords=True, nopbc=True)
+    for ss in s_corr:
+        ss = ss.remove_atom_names('EP')
+        ms.append(ss)
+
+    ms.to_deepmd_npy(out)
+
+
+def run(args):
+    get_amber_fp(cutoff=args.cutoff,
+                 parmfile=args.parm7_file,
+                 ncfile=args.nc,
+                 ll=args.ll,
+                 hl=args.hl,
+                 target=args.qm_region,
+                 out=args.out,
+                 )
