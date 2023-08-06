cdb043185e6972ab6194e91813e0fb5e57f18b1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:19:04 +0000

    Add package AIRPrediction/0.0.3/AIRPrediction-0.0.3.tar.gz

---
+++ packages/AIRPrediction/AIRPrediction-0.0.3.tar.gz/AIRPrediction-0.0.3/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name = 'AIRPrediction',
+    version = '0.0.3',
+    url = 'https://github.com/Data-for-Good-by-UF/AIRPrediction',
+    license = 'MIT'
+)
