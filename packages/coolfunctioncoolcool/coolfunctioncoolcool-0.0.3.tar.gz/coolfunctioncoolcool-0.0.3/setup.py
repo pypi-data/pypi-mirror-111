52dfe9dc4cbc9e47daa86d881b6589a309383e68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:53 +0000

    Add package coolfunctioncoolcool/0.0.3/coolfunctioncoolcool-0.0.3.tar.gz

---
+++ packages/coolfunctioncoolcool/coolfunctioncoolcool-0.0.3.tar.gz/coolfunctioncoolcool-0.0.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+setup(
+    name='coolfunctioncoolcool',
+    version = '0.0.3',
+    author = 'RaphaelKalfon',
+    author_email = 'raphaelkalfon9@gmail.com',
+    description = 'Say hello!',
+    packages = find_packages(),
+    install_requires = [],
+    keywords = ['python'],
+)
+
+# Run the pip install -e . everytime you change the setup
