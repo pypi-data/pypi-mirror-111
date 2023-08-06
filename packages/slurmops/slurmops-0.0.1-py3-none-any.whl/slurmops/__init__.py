43b83c092ea9f2bf41934f05ce6b7c2196c994a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:56:47 +0000

    Add package slurmops/0.0.1/slurmops-0.0.1-py3-none-any.whl

---
+++ packages/slurmops/slurmops-0.0.1-py3-none-any.whl/slurmops/__init__.py
@@ -0,0 +1,14 @@
+"""
+## NVIDIA/DeepOps on AWS EC2.
+
+:copyright: (c) 2021 by Leo Gallucci.
+:license: Apache 2.0, see LICENSE for more details.
+"""
+# Do not change the version here but rather `tbump "0.0.0.1" --only-patch`
+__version__ = "0.0.1"
+
+# `__all__` is left here for documentation purposes and as a
+# reference to which interfaces are meant to be imported.
+__all__ = [
+    "__version__",
+]
