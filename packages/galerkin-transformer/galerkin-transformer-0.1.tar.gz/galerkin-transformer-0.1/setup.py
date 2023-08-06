1e35532b2103d1c7b3b4ba866d9c881d0d033543
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:59 +0000

    Add package galerkin-transformer/0.1/galerkin-transformer-0.1.tar.gz

---
+++ packages/galerkin-transformer/galerkin-transformer-0.1.tar.gz/galerkin-transformer-0.1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="galerkin-transformer",
+    version="0.1",
+    author="Shuhao Cao",
+    author_email="scao.math@gmail.com",
+    description="Fourier and Galerkin Transformer",
+    long_description="Fourier and Galerkin Transformer: attention without softmax",
+    long_description_content_type="text/markdown",
+    url="https://github.com/scaomath/fourier-transformer",
+    project_urls={
+        "Bug Tracker": "https://github.com/scaomath/fourier-transformer/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.7",
+)
