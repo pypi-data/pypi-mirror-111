81ca41cd89ac9b1126e585e344455f87dc366efb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:25 +0000

    Add package efinance/0.2/efinance-0.2.tar.gz

---
+++ packages/efinance/efinance-0.2.tar.gz/efinance-0.2/setup.py
@@ -0,0 +1,36 @@
+
+import pathlib
+from setuptools import setup,find_packages
+
+here = pathlib.Path(__file__).parent
+# require = (here / "requirements.txt").read_text(encoding='utf-8').split()
+require = ['requests', 'pandas', 'tqdm', 'retry', 'multitasking']
+readme = (here / "README.md").read_text(encoding='utf-8')
+setup(
+    name="efinance",
+    version="0.2",
+    description="A finance tool to get stock,fund and futures data base on eastmoney",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Micro-sheep/efinance",
+    author="micro sheep",
+    author_email="micro-sheep@outlook.com",
+    license="MIT",
+    platforms=['any'],
+    keywords=['finance', 'quant', 'stock', 'fund', 'futures'],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    packages=find_packages(),
+    install_requires=require,
+    package_data={'': ['LICENSE', 'requirements.txt']},
+    include_package_data=True,
+    project_urls={
+        'Documentation': 'https://micro-sheep.github.io/efinance',
+        'Source': 'https://github.com/Micro-sheep/efinance',
+    },
+)
