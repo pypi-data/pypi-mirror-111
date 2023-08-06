c047686be8f77e2853fdeb694bcaba9366c30181
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:49 +0000

    Add package csi-preprocess/0.0.1/csi_preprocess-0.0.1-py3-none-any.whl

---
+++ packages/csi-preprocess/csi_preprocess-0.0.1-py3-none-any.whl/csi_preprocess/phaseratio.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+
+def csi_ratio(x):
+    """
+
+    :param x: csi data with shape:(Time,Antenna,Subcarrier)
+    :return: processed csi data with shape:(Time,Antenna,Subcarrier)
+    """
+    x = x[:, 0, :] / x[:, 1, :]
+    return x
+
+
+if __name__ == "__main__":
+
+    N_subcarrier = 30
+    N_antenna = 2
+    x = np.random.randn(1000, N_antenna, N_subcarrier)
+    csi_ratio(x)
