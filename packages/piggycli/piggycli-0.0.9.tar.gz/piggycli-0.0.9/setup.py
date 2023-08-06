79a6fa4cdab52939ab4dde7c85773fe360ef7967
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:04 +0000

    Add package piggycli/0.0.9/piggycli-0.0.9.tar.gz

---
+++ packages/piggycli/piggycli-0.0.9.tar.gz/piggycli-0.0.9/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+requirements = ['click']
+
+
+setup(
+    author="Altmirai LLC",
+    author_email='kyle.stewart@altmirai.com',
+    python_requires='>=3.5',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Education',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    description="A command line tool that provides the functionality to use AWS CloudHSM services as a bitcoin wallet.",
+    name='piggycli',
+    version='0.0.9',
+    packages=['app'],
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    entry_points='''
+        [console_scripts]
+        piggy=app.routes.click:piggy''',
+    url='',
+    keywords='altpiggybank'
+)
