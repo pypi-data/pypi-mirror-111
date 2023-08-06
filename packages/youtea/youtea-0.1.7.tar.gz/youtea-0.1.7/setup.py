6d4609ad473a84683050c704217a1bef4f76e05b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:17:12 +0000

    Add package youtea/0.1.7/youtea-0.1.7.tar.gz

---
+++ packages/youtea/youtea-0.1.7.tar.gz/youtea-0.1.7/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='youtea',
+      version='0.1.7',
+      include_package_data = True,
+      description='Display statistics from youtube video',
+      packages=['youtea'],
+      author_email='leykoderto@gmail.com',
+      long_description=open('README.md').read(),
+      zip_safe=False)
