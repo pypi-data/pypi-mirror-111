d150e0646e51367dcc3df3e0a9b7c959a766a573
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:48 +0000

    Add package versioningit/0.1.0a1/versioningit-0.1.0a1-py3-none-any.whl

---
+++ packages/versioningit/versioningit-0.1.0a1-py3-none-any.whl/versioningit/basics.py
@@ -0,0 +1,111 @@
+from pathlib import Path
+import re
+from typing import Any, Optional, Union
+from .core import VCSDescription
+from .errors import ConfigError, InvalidTagError
+from .logging import log, warn_extra_fields
+from .util import str_guard, strip_prefix, strip_suffix
+
+DEFAULT_FORMATS = {
+    "distance": "{next_version}.dev{distance}+{vcs}{rev}",
+    "dirty": "{version}+dirty",
+    "distance-dirty": "{next_version}.dev{distance}+{vcs}{rev}.dirty",
+}
+
+
+def basic_tag2version(tag: str, **kwargs: Any) -> str:
+    try:
+        rmprefix = str_guard(
+            kwargs.pop("rmprefix"), "tool.versioningit.tag2version.rmprefix"
+        )
+    except KeyError:
+        pass
+    else:
+        tag = strip_prefix(tag, rmprefix)
+    try:
+        rmsuffix = str_guard(
+            kwargs.pop("rmsuffix"), "tool.versioningit.tag2version.rmsuffix"
+        )
+    except KeyError:
+        pass
+    else:
+        tag = strip_suffix(tag, rmsuffix)
+    try:
+        regex = str_guard(kwargs.pop("regex"), "tool.versioningit.tag2version.regex")
+    except KeyError:
+        pass
+    else:
+        m = re.search(regex, tag)
+        if m is None:
+            log.info("tag2version.regex did not match tag; leaving unmodified")
+        elif not m.groups():
+            raise ConfigError(
+                "No capturing groups in tool.versioningit.tag2version.regex"
+            )
+        else:
+            if "version" in m.groupdict():
+                tag = m["version"]
+            else:
+                tag = m[1]
+            if tag is None:
+                raise InvalidTagError(
+                    "Version group in tool.versioningit.tag2version.regex did"
+                    " not participate in match"
+                )
+    warn_extra_fields(kwargs, "tool.versioningit.tag2version")
+    return tag.lstrip("v")
+
+
+def basic_format(
+    description: VCSDescription, version: str, next_version: str, **kwargs: Any
+) -> str:
+    branch: Optional[str]
+    if description.branch is not None:
+        branch = re.sub(r"[^A-Za-z0-9.]", ".", description.branch)
+    else:
+        branch = None
+    fields = {
+        **description.fields,
+        "branch": branch,
+        "version": version,
+        "next_version": next_version,
+    }
+    formats = {**DEFAULT_FORMATS, **kwargs}
+    try:
+        fmt = formats[description.state]
+    except KeyError:
+        raise ConfigError(
+            f"No format string for {description.state!r} state found in"
+            " tool.versioningit.format"
+        )
+    return fmt.format_map(fields)
+
+
+def basic_write(project_dir: Union[str, Path], version: str, **kwargs: Any) -> None:
+    try:
+        filename = str_guard(kwargs.pop("file"), "tool.versioningit.write.file")
+    except KeyError:
+        log.debug("No 'file' field in tool.versioningit.write; not writing anything")
+        return
+    path = Path(project_dir, filename)
+    enc = kwargs.pop("encoding", None)
+    encoding: Optional[str]
+    if enc is not None:
+        encoding = str_guard(enc, "tool.versioningit.write.encoding")
+    else:
+        encoding = None
+    try:
+        template = str_guard(kwargs.pop("template"), "tool.versioningit.write.template")
+    except KeyError:
+        if path.suffix == ".py":
+            template = '__version__ = "{version}"'
+        elif path.suffix == ".txt" or path.suffix == "":
+            template = "{version}"
+        else:
+            raise ConfigError(
+                "tool.versioningit.write.template not specified and file has"
+                f" unknown suffix {path.suffix!r}"
+            )
+    warn_extra_fields(kwargs, "tool.versioningit.write")
+    path.parent.mkdir(parents=True, exist_ok=True)
+    path.write_text(template.format(version=version) + "\n", encoding=encoding)
---
+++ packages/versioningit/versioningit-0.1.0a1-py3-none-any.whl/versioningit/core.py
@@ -0,0 +1,145 @@
+from dataclasses import dataclass
+import os
+from pathlib import Path
+from typing import Any, Dict, Optional, Union
+from .config import Config
+from .errors import MethodError, NotSdistError, NotVCSError, NotVersioningitError
+from .logging import warn_bad_version
+from .methods import VersioningitMethod
+from .util import parse_version_from_metadata
+
+
+@dataclass
+class VCSDescription:
+    tag: str
+    state: str
+    branch: Optional[str]
+    fields: Dict[str, Any]
+
+
+@dataclass
+class Versioningit:
+    project_dir: Path
+    vcs: VersioningitMethod
+    tag2version: VersioningitMethod
+    next_version: VersioningitMethod
+    format: VersioningitMethod
+    write: VersioningitMethod
+
+    @classmethod
+    def from_project_dir(
+        cls, project_dir: Union[str, Path] = os.curdir
+    ) -> "Versioningit":
+        try:
+            config = Config.parse_toml_file(Path(project_dir, "pyproject.toml"))
+        except FileNotFoundError:
+            raise NotVersioningitError(f"No pyproject.toml file in {project_dir}")
+        return cls.from_config_obj(project_dir, config)
+
+    @classmethod
+    def from_config(cls, project_dir: Union[str, Path], config: Any) -> "Versioningit":
+        return cls.from_config_obj(project_dir, Config.parse_obj(config))
+
+    @classmethod
+    def from_config_obj(
+        cls, project_dir: Union[str, Path], config: Config
+    ) -> "Versioningit":
+        pdir = Path(project_dir)
+        return cls(
+            project_dir=pdir,
+            vcs=config.vcs.load(pdir),
+            tag2version=config.tag2version.load(pdir),
+            next_version=config.next_version.load(pdir),
+            format=config.format.load(pdir),
+            write=config.write.load(pdir),
+        )
+
+    def get_version(self) -> str:
+        description = self.get_vcs_description()
+        tag_version = self.get_tag2version(description.tag)
+        warn_bad_version(tag_version, "Version extracted from tag")
+        if description.state == "exact":
+            version = tag_version
+        else:
+            next_version = self.get_next_version(tag_version, description.branch)
+            warn_bad_version(next_version, "Calculated next version")
+            version = self.format_version(
+                description=description,
+                version=tag_version,
+                next_version=next_version,
+            )
+        warn_bad_version(version, "Final version")
+        return version
+
+    def get_vcs_description(self) -> VCSDescription:
+        description = self.vcs(project_dir=self.project_dir)
+        if not isinstance(description, VCSDescription):
+            raise MethodError(
+                f"vcs method returned {description!r} instead of a VCSDescription"
+            )
+        return description
+
+    def get_tag2version(self, tag: str) -> str:
+        version = self.tag2version(tag=tag)
+        if not isinstance(version, str):
+            raise MethodError(
+                f"tag2version method returned {version!r} instead of a string"
+            )
+        return version
+
+    def get_next_version(self, version: str, branch: Optional[str]) -> str:
+        next_version = self.next_version(version=version, branch=branch)
+        if not isinstance(next_version, str):
+            raise MethodError(
+                f"next-version method returned {next_version!r} instead of a string"
+            )
+        return next_version
+
+    def format_version(
+        self, description: VCSDescription, version: str, next_version: str
+    ) -> str:
+        new_version = self.format(
+            description=description, version=version, next_version=next_version
+        )
+        if not isinstance(new_version, str):
+            raise MethodError(
+                f"format method returned {new_version!r} instead of a string"
+            )
+        return new_version
+
+    def write_version(self, version: str) -> None:
+        self.write(project_dir=self.project_dir, version=version)
+
+
+def get_version(
+    project_dir: Union[str, Path] = os.curdir,
+    config: Optional[dict] = None,
+    write: bool = False,
+    fallback: bool = True,
+) -> str:
+    if config is None:
+        vgit = Versioningit.from_project_dir(project_dir)
+    else:
+        vgit = Versioningit.from_config(project_dir, config)
+    try:
+        version = vgit.get_version()
+    except NotVCSError:
+        if fallback:
+            version = get_version_from_pkg_info(project_dir)
+            fellback = True
+        else:
+            raise
+    else:
+        fellback = False
+    if write and not fellback:
+        vgit.write_version(version)
+    return version
+
+
+def get_version_from_pkg_info(project_dir: Union[str, Path]) -> str:
+    try:
+        return parse_version_from_metadata(
+            Path(project_dir, "PKG-INFO").read_text(encoding="utf-8")
+        )
+    except FileNotFoundError:
+        raise NotSdistError(f"{project_dir} does not contain a PKG-INFO file")
