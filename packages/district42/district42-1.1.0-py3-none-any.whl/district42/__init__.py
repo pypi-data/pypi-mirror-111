831fe6d476e8026890d7419f40acb2d4f58cf547
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:36 +0000

    Add package district42/1.1.0/district42-1.1.0-py3-none-any.whl

---
+++ packages/district42/district42-1.1.0-py3-none-any.whl/district42/__init__.py
@@ -0,0 +1,28 @@
+from typing import Any
+
+from ._from_native import from_native
+from ._props import Props
+from ._schema_facade import SchemaFacade
+from ._schema_visitor import SchemaVisitor
+from ._version import version
+from .representor import Representor
+from .types import AnySchema, GenericSchema, Schema, optional
+
+__version__ = version
+__all__ = ("schema", "GenericSchema", "Props", "SchemaVisitor", "from_native", "optional",)
+
+
+schema = SchemaFacade()
+_representor = Representor()
+
+
+def represent(self: GenericSchema, **kwargs: Any) -> str:
+    return self.__accept__(_representor, **kwargs)
+
+
+def union(self: GenericSchema, other: Any) -> AnySchema:
+    return schema.any(self, other)
+
+
+Schema.__override__("__repr__", represent)
+Schema.__override__("__or__", union)
