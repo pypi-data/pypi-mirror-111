8b9f407c1ade96f6e56321f50fe53fc8657301d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:05 +0000

    Add package funsor/0.4.1/funsor-0.4.1-py3-none-any.whl

---
+++ packages/funsor/funsor-0.4.1-py3-none-any.whl/funsor/torch/__init__.py
@@ -0,0 +1,30 @@
+# Copyright Contributors to the Pyro project.
+# SPDX-License-Identifier: Apache-2.0
+
+import torch
+from multipledispatch import dispatch
+
+from funsor.tensor import tensor_to_funsor
+from funsor.terms import to_funsor
+from funsor.util import quote
+
+from . import distributions as _
+from . import ops as _
+
+del _  # flake8
+
+
+@quote.register(torch.Tensor)
+def _quote(x, indent, out):
+    """
+    Work around PyTorch not supporting reproducible repr.
+    """
+    out.append((indent, "torch.tensor({}, dtype={})".format(repr(x.tolist()), x.dtype)))
+
+
+to_funsor.register(torch.Tensor)(tensor_to_funsor)
+
+
+@dispatch(torch.Tensor, torch.Tensor, [float])
+def allclose(a, b, rtol=1e-05, atol=1e-08):
+    return torch.allclose(a, b, rtol=rtol, atol=atol)
