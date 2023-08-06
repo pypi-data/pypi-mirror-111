60bd058fb312b78068acd423f57fe1177245b0ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:47 +0000

    Add package logging-command/0.1/logging-command-0.1.tar.gz

---
+++ packages/logging-command/logging-command-0.1.tar.gz/logging-command-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+import setuptools
+
+setup(name='logging-command',
+      version='0.1',
+      description='clinical trial information retriver',
+      url='https://github.com/PQR96137/logging2',
+      author='PQR96137',
+      author_email='jay3864660@gmail.com',
+      license='MIT',
+      packages=setuptools.find_packages(),
+      zip_safe=False)
