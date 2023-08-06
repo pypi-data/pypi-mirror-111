7fca72fb0265b4d4853927bbe5578a829a0369fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:27 +0000

    Add package paqproof2/0.0.2/paqproof2-0.0.2.tar.gz

---
+++ packages/paqproof2/paqproof2-0.0.2.tar.gz/paqproof2-0.0.2/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="paqproof2",
+    version="0.0.2",
+    author="Leandro A. Zambrano",
+    author_email="leandrozambrano@hotmail.com",
+    description="A small example package",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
