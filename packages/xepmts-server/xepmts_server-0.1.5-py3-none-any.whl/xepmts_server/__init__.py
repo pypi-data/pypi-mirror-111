7d44e1a2147f13abb6bda2f38b8264a4f15605c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:35 +0000

    Add package xepmts-server/0.1.5/xepmts_server-0.1.5-py3-none-any.whl

---
+++ packages/xepmts-server/xepmts_server-0.1.5-py3-none-any.whl/xepmts_server/__init__.py
@@ -0,0 +1,22 @@
+from . import v1
+from . import v2
+from .server import run_simple, make_app, create_app
+
+__author__ = """Yossi Mosbacher"""
+__email__ = 'joe.mosbacher@gmail.com'
+__version__ = '0.1.5'
+
+VERSIONS = {
+    "v1": v1,
+    "v2": v2,
+}
+
+DEFAULT_VERSION = "v2"
+
+def get_server(version, **kwargs):
+    app = VERSIONS[version].app.make_app(**kwargs)
+    return app
+
+def default_server():
+    return get_server(DEFAULT_VERSION)
+
