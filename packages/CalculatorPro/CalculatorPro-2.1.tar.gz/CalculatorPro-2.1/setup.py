ec606b456d20c65d66d348144a520d25a733e0a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:58 +0000

    Add package CalculatorPro/2.1/CalculatorPro-2.1.tar.gz

---
+++ packages/CalculatorPro/CalculatorPro-2.1.tar.gz/CalculatorPro-2.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+# long description
+with open("README.md", "r", encoding="utf-8") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+VERSION = '2.1'
+DESCRIPTION = 'A basic hello package'
+
+
+# Setting up
+setup(
+    name="CalculatorPro",
+    version=VERSION,
+    author="codechamp2006 (Sagnik Ray)",
+    author_email="sagnikraycoder27@gmail.com",
+    description=DESCRIPTION,
+    long_description= LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    scripts=[],
+    keywords=['python', 'calculator', 'mentalMaths'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
