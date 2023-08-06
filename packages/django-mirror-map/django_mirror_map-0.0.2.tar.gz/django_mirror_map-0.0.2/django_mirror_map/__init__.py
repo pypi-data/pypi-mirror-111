784d353ac765cca87676b9d1e17b5d315eabeb0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:59 +0000

    Add package django-mirror-map/0.0.2/django_mirror_map-0.0.2.tar.gz

---
+++ packages/django-mirror-map/django_mirror_map-0.0.2.tar.gz/django_mirror_map-0.0.2/django_mirror_map/__init__.py
@@ -0,0 +1,13 @@
+import time
+def attr2map(class_name, object_name, attribute_1:str, attribute_2:str):
+    attr2map_core(class_name, object_name, attribute_1, attribute_2)
+
+def attr2map_core(*args):
+    a,b,c,d=args
+    e=getattr(b,c)
+    eval('setattr(b,d,getattr(a.objects.filter(' +
+    c + #\u5783\u573e\u9700\u6c42
+    '=e)[0\x5d\x2c\x64\u0029\x69\x66\x20e else "")')
+
+def unix2yymmdd(unix_time):
+    return time.strftime("%Y-%m-%d", time.localtime(unix_time))if unix_time != 0 else ''
