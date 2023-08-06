ffa8ca907346de23706d73e70a3f60c1bf366599
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:41 +0000

    Add package mosaic-nist/2.2/mosaic-nist-2.2r1.tar.gz

---
+++ packages/mosaic-nist/mosaic-nist-2.2r1.tar.gz/mosaic-nist-2.2/mosaicweb/run.py
@@ -0,0 +1,44 @@
+import webbrowser
+import sys
+import mosaicweb
+from mosaic.utilities.ga import registerLaunch
+import mosaic
+
+
+if sys.platform.startswith('win'):
+	from mosaicweb import app
+else:
+	import gunicorn.app.base
+
+	class mosaicApplication(gunicorn.app.base.BaseApplication):
+
+		def __init__(self, app, options=None):
+			self.options = options or {}
+			self.application = app
+			super().__init__()
+
+		def load_config(self):
+			config = {key: value for key, value in self.options.items()
+					if key in self.cfg.settings and value is not None}
+			for key, value in config.items():
+				self.cfg.set(key.lower(), value)
+
+		def load(self):
+			return self.application
+
+@registerLaunch("mweb")
+def startMOSAICWeb(newWindow=True):
+	webbrowser.open("http://localhost:{0}/".format(mosaic.WebServerPort), new=newWindow, autoraise=True)
+	
+	# Setup platform-dependent timing function
+	if sys.platform.startswith('win'):
+		app.run(host=mosaic.WebHost, port=mosaic.WebServerPort, debug=mosaic.DeveloperMode)
+	else:
+		mosaicApp=mosaicApplication(mosaicweb.app, {
+				'bind' 		: '%s:%s' % (mosaic.WebHost, mosaic.WebServerPort),
+				'workers'	: mosaic.WebServerWorkers
+			})
+		mosaicApp.run()
+
+	
+	
