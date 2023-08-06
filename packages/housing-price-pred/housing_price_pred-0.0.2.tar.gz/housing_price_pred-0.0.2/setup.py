c5a88d88c64eceb210b8b63da5358acd657e70b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:51 +0000

    Add package housing-price-pred/0.0.2/housing_price_pred-0.0.2.tar.gz

---
+++ packages/housing-price-pred/housing_price_pred-0.0.2.tar.gz/housing_price_pred-0.0.2/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+from setuptools import setup
+
+with open("README.rst", "r", encoding="utf-8") as fh:
+    ld = fh.read()
+
+
+setup(
+    name = "housing_price_pred",
+    version = "0.0.2",
+    author = "Sibashis Chakraborty",
+    author_email = "sibashis.chakrab@tigeranalytics.com",
+    url= "https://github.com/sibashisc/mle-training/tree/fix/9/ml-workflow",
+    description = "Housing Price Prediction",
+    long_description = ld,
+    long_description_content_type = "text/markdown",
+    py_modules = ["logger","ingest_data","train","score"],
+    package_dir = {"": "src"},
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
