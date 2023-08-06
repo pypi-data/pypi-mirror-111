76ed86169c05839c0aecc04d72814c1945e27286
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:36:51 +0000

    Add package bluebeam-beta/0.0.2/bluebeam_beta-0.0.2.tar.gz

---
+++ packages/bluebeam-beta/bluebeam_beta-0.0.2.tar.gz/bluebeam_beta-0.0.2/src/bluebeam/objects/__init__.py
@@ -0,0 +1,41 @@
+# Generics
+from .GameObject import GameObject
+from .PhysObject import PhysObject
+from .Bullet import Bullet
+from .Block import Block
+from .Background import Background
+# Player
+from .Player import Player
+from .PlayerBullet import PlayerBullet
+# Enemies
+from .Enemy import Enemy
+from .Target import Target
+from .BadTarget import BadTarget
+from .SliderEnemy import SliderEnemy
+from .ShootyEnemy import ShootyEnemy
+# Enemy Projectiles
+from .EnemyBullet import EnemyBullet
+from .Rocket import Rocket
+from .Laser import Laser
+from .SplitBomb import SplitBomb
+from .Shockwave import Shockwave
+from .Explosion import Explosion
+# Boss 1 Parts
+from .WhaleTurret import WhaleTurret
+from .WhaleCore import WhaleCore
+from .WhaleShield import WhaleShield
+
+from .WhaleCommander import WhaleCommander
+from .WhaleTurretFlex import WhaleTurretFlex
+from .WhaleTurretSide import WhaleTurretSide
+from .WhaleTurretRocket import WhaleTurretRocket
+from .WhaleTurretLaser import WhaleTurretLaser
+
+from .AlphaWhale import AlphaWhale
+
+from .BossSpawnTrigger import BossSpawnTrigger
+
+#PowerUps
+from .YellowPower import YellowPower
+from .BluePower import BluePower
+from .RedPower import RedPower
