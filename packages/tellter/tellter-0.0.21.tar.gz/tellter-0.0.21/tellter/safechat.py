4c8ab2d5712fc946b40687179cf74e4d95a4cb3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:23 +0000

    Add package tellter/0.0.21/tellter-0.0.21.tar.gz

---
+++ packages/tellter/tellter-0.0.21.tar.gz/tellter-0.0.21/tellter/safechat.py
@@ -0,0 +1,17 @@
+import requests
+
+class APIKeyError(Exception):
+    pass
+
+class Safechat:
+    def __init__(self, key):
+        self.api_key = key # Defines the API key
+
+    def vulgar(self, text, sensitivity):
+        query = text.replace("?", "-")
+        r = requests.get("http://dedicated.dcounter.space:3838/check/"+self.api_key+"/"+str(sensitivity)+"/"+query)
+        response = r.content.decode()
+        if response == "WRONG_API_KEY":
+            raise APIKeyError("API Key is invalid.")
+        else:
+            return int(response)
