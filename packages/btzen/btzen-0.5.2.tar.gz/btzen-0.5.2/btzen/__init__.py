29cec0847f77a08a17cfd592b7706223acde6d82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:39 +0000

    Add package btzen/0.5.2/btzen-0.5.2.tar.gz

---
+++ packages/btzen/btzen-0.5.2.tar.gz/btzen-0.5.2/btzen/__init__.py
@@ -0,0 +1,30 @@
+#
+# BTZen - library to asynchronously access Bluetooth devices.
+#
+# Copyright (C) 2015-2020 by Artur Wroblewski <wrobell@riseup.net>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import pkg_resources
+
+from .btweight import WeightFlags, WeightData, WeightMeasurement
+from .device import BatteryLevel, Device, Info, Trigger, TriggerCondition
+from .cm import ConnectionManager
+from .serial import Serial
+from .error import *
+
+__version__ = pkg_resources.get_distribution('btzen').version
+
+# vim: sw=4:et:ai
