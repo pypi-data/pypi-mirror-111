07c1738bfd144be301fc792bf0c48b2a9f9c0695
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:37 +0000

    Add package pymitblod/0.0.9/pymitblod-0.0.9-py3-none-any.whl

---
+++ packages/pymitblod/pymitblod-0.0.9-py3-none-any.whl/pymitblod/__init__.py
@@ -0,0 +1,12 @@
+'''
+Init file for pymitblod
+'''
+
+
+from .mitblod import MitBlod
+from .institution import Institution
+from .gender import Gender
+from .consts import Institutions, Genders
+
+from .person import Person
+from .donor import Donor
