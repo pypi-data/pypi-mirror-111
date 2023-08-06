02b5e6001d55f93c2141444df3fe7cd01c7e95bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:40 +0000

    Add package data-check/0.4.0/data_check-0.4.0.tar.gz

---
+++ packages/data-check/data_check-0.4.0.tar.gz/data_check-0.4.0/data_check/data_check.py
@@ -0,0 +1,132 @@
+from pathlib import Path
+import pandas as pd
+from typing import List, Tuple, Dict, Any
+
+from .config import DataCheckConfig
+from .result import DataCheckResult, ResultType
+from .output import DataCheckOutput
+from .io import expand_files, read_sql_file, get_expect_file, read_csv, read_yaml
+from .sql import DataCheckSql
+from .generator import DataCheckGenerator
+from .runner import DataCheckRunner
+
+
+class DataCheck:
+    """
+    Main class for data_check.
+    """
+
+    def __init__(self, config: DataCheckConfig = DataCheckConfig()):
+        self.config = config
+        self.runner = DataCheckRunner(config.parallel_workers)
+        self.sql = DataCheckSql(connection=config.connection, runner=self.runner)
+        self.generator = DataCheckGenerator(self.sql)
+        self.output = DataCheckOutput()
+        self.template_data: Dict[str, Any] = {}
+
+    def load_template(self):
+        template_yaml = self.config.checks_path / self.config.tempate_path
+        if template_yaml.exists():
+            self.template_data = read_yaml(template_yaml)
+
+    @staticmethod
+    def merge_results(
+        sql_result: pd.DataFrame, expect_result: pd.DataFrame
+    ) -> pd.DataFrame:
+        """
+        Merges the results of a SQL query and the expected results.
+        Returns the merged DataFrame.
+        """
+        try:
+            df_merged = sql_result.merge(expect_result, indicator=True, how="outer")
+        except ValueError:
+            # treat both columns as str if their data types differ
+            for sc in sql_result.columns:
+                if sc in expect_result.columns:
+                    if sql_result[sc].dtype != expect_result[sc].dtype:
+                        expect_result[sc] = expect_result[sc].astype("str")
+                        sql_result[sc] = sql_result[sc].astype("str")
+            df_merged = sql_result.merge(expect_result, indicator=True, how="outer")
+        return df_merged
+
+    @staticmethod
+    def get_result(
+        sql_result: pd.DataFrame, expect_result: pd.DataFrame, return_all: bool
+    ) -> Tuple[ResultType, pd.DataFrame]:
+        # replace missing values and None with pd.NA
+        sql_result.fillna(value=pd.NA, inplace=True)
+        # using "" instead of r'^$' doesn't work somehow, so we need to use regex
+        sql_result.replace(r"^$", pd.NA, regex=True, inplace=True)
+
+        expect_result.fillna(value=pd.NA, inplace=True)
+
+        df_merged = DataCheck.merge_results(sql_result, expect_result)
+        df_diff = df_merged[df_merged._merge != "both"]
+        assert isinstance(df_diff, pd.DataFrame)  # assert a DataFrame, not a Series
+        df_result = df_merged if return_all else df_diff
+
+        # empty diff means there are no differences and the test has passed
+        passed = len(df_diff) == 0
+        return (DataCheckResult.passed_to_result_type(passed), df_result)
+
+    def run_test(
+        self,
+        sql_file: Path,
+        return_all: bool = False,
+    ) -> DataCheckResult:
+        """
+        Run a data_check test on a single input file.
+        Returns a DataCheckResult with the result.
+
+        If return_all is set, the DataCheckResult will contail all results,
+        not only the failed ones.
+        """
+        expect_file = get_expect_file(sql_file)
+        if not expect_file.exists():
+            # no need to run queries, if no expected results found
+            return self.output.prepare_result(
+                ResultType.NO_EXPECTED_RESULTS_FILE, source=sql_file
+            )
+
+        try:
+            sql_result = self.sql.run_query(
+                read_sql_file(sql_file=sql_file, template_data=self.template_data)
+            )
+        except Exception as exc:
+            return self.output.prepare_result(
+                ResultType.FAILED_WITH_EXCEPTION, source=sql_file, exception=exc
+            )
+
+        try:
+            expect_result = read_csv(expect_file)
+        except Exception as exc_csv:
+            return self.output.prepare_result(
+                ResultType.FAILED_WITH_EXCEPTION, source=expect_file, exception=exc_csv
+            )
+
+        result_type, df_result = self.get_result(sql_result, expect_result, return_all)
+        return self.output.prepare_result(
+            result_type, source=sql_file, result=df_result
+        )
+
+    def run(self, files: List[Path]) -> bool:
+        """
+        Runs a data_check test for all element in the list.
+        Returns True, if all calls returned True, otherweise False.
+        """
+        all_files = expand_files(files)
+        results = self.runner.run(self.run_test, all_files)
+
+        overall_result = all(results)
+        self.output.pprint_overall_result(overall_result)
+        return overall_result
+
+    def run_sql_file(self, sql_file: Path):
+        sql_text = read_sql_file(sql_file=sql_file, template_data=self.template_data)
+        print("executing:")
+        print(sql_text)
+        return self.sql.run_sql(sql_text=sql_text)
+
+    def run_sql_files(self, sql_files: List[Path]):
+        parameters = [{"sql_file": f} for f in expand_files(sql_files)]
+        return self.runner.run_any(run_method=self.run_sql_file, parameters=parameters)
---
+++ packages/data-check/data_check-0.4.0.tar.gz/data_check-0.4.0/setup.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['data_check']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['Jinja2>=2.11.3,<3.0.0',
+ 'SQLAlchemy>=1.4,<2.0',
+ 'click>=7.1.2,<8.0.0',
+ 'colorama>=0.4.4,<0.5.0',
+ 'importlib-metadata>=3.4.0,<4.0.0',
+ 'numpy>=1.19.5,<2.0.0',
+ 'pandas>=1.1.5,<2.0.0',
+ 'pyyaml>=5.3.1,<6.0.0']
+
+extras_require = \
+{'mssql': ['pyodbc>=4.0.30,<5.0.0'],
+ 'mysql': ['pymysql[rsa]'],
+ 'oracle': ['cx_Oracle>=8.1.0,<9.0.0'],
+ 'postgres': ['psycopg2-binary>=2.8.6,<3.0.0']}
+
+entry_points = \
+{'console_scripts': ['data_check = data_check.__main__:main']}
+
+setup_kwargs = {
+    'name': 'data-check',
+    'version': '0.4.0',
+    'description': 'simple data validation',
+    'long_description': '# data_check\n\ndata_check is a simple data validation tool. Write SQL queries and CSV files with the expected result sets and data_check will test the result sets against the queries. You can also use CSV files to populate tables with data for pipeline tests with data_check.\n\ndata_check should work with any database that works with [SQLAlchemy](https://docs.sqlalchemy.org/en/14/dialects/). Currently data_check is tested against PostgreSQL, MySQL, SQLite, Oracle and Microsoft SQL Server.\n\n## Quickstart\n\nYou need Python 3.6.2 or above to run data_check. The easiest way to install data_check is via [pipx](https://github.com/pipxproject/pipx):\n\n`pipx install data-check`\n\nThe data_check Git repository is also a sample data_check project. Clone the repository, switch to the folder and run data_check:\n\n```\ngit clone git@github.com:andrjas/data_check.git\ncd data_check\ndata_check\n```\n\nThis will run the tests in the _checks_ folder using the default connection as set in data_check.yml.\n\nSee the [documentation](https://andrjas.github.io/data_check) how to install data_check in different environments with additional database drivers and other usages of data_check.\n\n## Project layout\n\ndata_check has a simple layout for projects: a single configuration file and a folder with the test files. You can also organize the test files in subfolders.\n\n    data_check.yml    # The configuration file\n    checks/           # Default folder for data tests\n        some_test.sql # SQL file with the query to run against the database\n        some_test.csv # CSV file with the expected result\n        subfolder/    # Tests can be nested in subfolders\n\n## Documentation\n\nSee the [documentation](https://andrjas.github.io/data_check) how to setup data_check, how to create a new project and more options.\n',
+    'author': 'Andreas Rjasanow',
+    'author_email': 'andrjas@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://andrjas.github.io/data_check/',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'extras_require': extras_require,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6.2,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
