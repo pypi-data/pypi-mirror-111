24b1a042dd96a29c6b201ce0444784cc44327bb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:05 +0000

    Add package olles-little-data-and-model-pack/0.0.1/Olles_Little_Data_and_Model_Pack-0.0.1.tar.gz

---
+++ packages/olles-little-data-and-model-pack/Olles_Little_Data_and_Model_Pack-0.0.1.tar.gz/Olles_Little_Data_and_Model_Pack-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='Olles_Little_Data_and_Model_Pack',
+    version='0.0.1',
+    description='R Passenger dataset but in list and Pandas format. Will later include time series models (will be added later).',
+    py_modules='olles_little_data_and_model_pack',
+    package_dir={'':'src'}
+    )
