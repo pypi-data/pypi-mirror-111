a4fa57ba8e516ce70a1390d91ce616f149b20a96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:39 +0000

    Add package panda-gym/1.1.0/panda_gym-1.1.0.tar.gz

---
+++ packages/panda-gym/panda_gym-1.1.0.tar.gz/panda_gym-1.1.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="panda_gym",
+    description="OpenAI Gym Franka Emika Panda robot environment based on PyBullet.",
+    author="Quentin GALLOUÉDEC",
+    author_email="gallouedec.quentin@gmail.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/qgallouedec/panda-gym",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={},
+    version="1.1.0",
+    install_requires=["gym", "pybullet", "numpy"],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
