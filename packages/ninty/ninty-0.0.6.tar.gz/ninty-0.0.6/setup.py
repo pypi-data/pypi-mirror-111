3c99854d8d2458710409b43266afc712bdf6bd62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:33 +0000

    Add package ninty/0.0.6/ninty-0.0.6.tar.gz

---
+++ packages/ninty/ninty-0.0.6.tar.gz/ninty-0.0.6/setup.py
@@ -0,0 +1,29 @@
+
+import setuptools
+
+MODULES = ["lzss", "gx2", "endian", "yaz0", "audio"]
+
+description = \
+	"C++ extension with functions for which python is too slow."
+
+long_description = description
+
+extensions = []
+for module in MODULES:
+	extension = setuptools.Extension(
+		name = "ninty.%s" %module,
+		sources = ["src/module_%s.cpp" %module]
+	)
+	extensions.append(extension)
+
+setuptools.setup(
+	name = "ninty",
+	version = "0.0.6",
+	description = description,
+	long_description = long_description,
+	author = "Yannik Marchand",
+	author_email = "ymarchand@me.com",
+	url = "https://github.com/kinnay/ninty",
+	license = "GPLv3",
+	ext_modules = extensions
+)
