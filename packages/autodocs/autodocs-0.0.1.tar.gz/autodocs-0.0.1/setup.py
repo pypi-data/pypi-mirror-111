573d49337b2780c0df6893665f42aa92d4772441
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:19 +0000

    Add package autodocs/0.0.1/autodocs-0.0.1.tar.gz

---
+++ packages/autodocs/autodocs-0.0.1.tar.gz/autodocs-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+  name='autodocs',
+  version='0.0.1',
+  description='Automatically generate documentation for your code using artficial intelligence!',
+  py_modules=['autodocs'],
+  package_dir={'': 'src'}
+)
