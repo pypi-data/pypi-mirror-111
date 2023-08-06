2b0e54a822532d9c7638a8b891940e1ef201ea92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:03 +0000

    Add package asalib/0.1.0/asalib-0.1.0-py2.py3-none-any.whl

---
+++ packages/asalib/asalib-0.1.0-py2.py3-none-any.whl/asalib/asamod.py
@@ -0,0 +1,21 @@
+
+def starte(aggettivo):
+    print(f"starte è {aggettivo}")
+    
+def stormer(aggettivo):
+    print(f"stormer è {aggettivo}")
+    
+def fede(aggettivo):
+    print(f'"fede" è {aggettivo}')
+    
+def nemos(aggettivo):
+    print(f"nemos è {aggettivo}")
+    
+def nonno(aggettivo):
+    print(f"nonno è {aggettivo}")
+
+def doggy(aggettivo):
+    print(f"doggy è {aggettivo}")
+    
+def cozta(aggettivo):
+    print(f"cozta è {aggettivo}")
