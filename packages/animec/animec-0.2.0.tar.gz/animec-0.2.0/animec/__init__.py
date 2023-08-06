c1b41179c59dc268bc213174f133bc1e84b12e60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:46 +0000

    Add package animec/0.2.0/animec-0.2.0.tar.gz

---
+++ packages/animec/animec-0.2.0.tar.gz/animec-0.2.0/animec/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+'''
+Animec
+~~~~~~~~~~~~~~~~~~~
+A module to get data about anime characters, news, info, lyrics and more.
+'''
+
+from .sagasu import *
+from .aninews import *
+from .anicore import *
+from .errors import *
+
+__author__ = "DriftAsimov"
+__title__ = "animec"
+__copyright__ = "Copyright Drift Asimov 2021"
+__version__ = "0.2.0"
+__license__ = "MIT"
