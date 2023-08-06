03470294fd1a876c9a0c5a6ed0978cc8020ef453
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:08 +0000

    Add package clarinpl-embeddings/0.0.1rc16/clarinpl_embeddings-0.0.1rc16-py3-none-any.whl

---
+++ packages/clarinpl-embeddings/clarinpl_embeddings-0.0.1rc16-py3-none-any.whl/embeddings/evaluator/sequence_labeling_evaluator.py
@@ -0,0 +1,29 @@
+from typing import Any, Dict, List, Sequence, Union
+
+import numpy as np
+import torch
+
+from embeddings.evaluator.metrics_evaluator import MetricsEvaluator
+from embeddings.metric.hugging_face_metric import HuggingFaceMetric
+from embeddings.metric.metric import Metric
+from embeddings.metric.pos_metric import POSTaggingSeqevalMetric
+
+
+class SequenceLabelingEvaluator(MetricsEvaluator):
+    @property
+    def metrics(
+        self,
+    ) -> Sequence[Metric[Union[List[Any], np.ndarray, torch.Tensor], Dict[Any, Any]]]:
+        return [
+            HuggingFaceMetric("seqeval"),
+        ]
+
+
+class POSTaggingEvaluator(MetricsEvaluator):
+    @property
+    def metrics(
+        self,
+    ) -> Sequence[POSTaggingSeqevalMetric]:
+        return [
+            POSTaggingSeqevalMetric(),
+        ]
