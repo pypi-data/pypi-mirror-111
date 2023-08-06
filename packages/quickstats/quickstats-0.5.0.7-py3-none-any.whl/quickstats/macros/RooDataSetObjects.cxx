63cda5b941dd0123b47615fc75e7260f6d032eb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:16 +0000

    Add package quickstats/0.5.0.7/quickstats-0.5.0.7-py3-none-any.whl

---
+++ packages/quickstats/quickstats-0.5.0.7-py3-none-any.whl/quickstats/macros/RooDataSetObjects.cxx
@@ -0,0 +1,13 @@
+#include "map"
+#include "string"
+#include "RooDataSet.h"
+#ifdef __CINT__ 
+#pragma link C++ nestedclasses;
+#pragma link C++ nestedtypedefs;
+#pragma link C++ class std::map<std::string, RooDataSet*>+;
+#pragma link C++ class std::map<std::string, RooDataSet*>::*;
+#pragma link C++ class std::pair<std::map<string,RooDataSet*>::iterator, bool>+;
+#pragma link C++ class std::pair<std::map<string,RooDataSet*>::iterator, bool>::*+;
+#pragma link C++ class std::pair<std::string, RooDataSet*>+;
+#pragma link C++ class std::pair<std::string, RooDataSet*>::*+;
+#endif
