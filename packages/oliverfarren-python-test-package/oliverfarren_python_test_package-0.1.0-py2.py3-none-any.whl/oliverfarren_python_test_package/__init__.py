7f2210ddade5e36377f77a283b992d5e431f57c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:16 +0000

    Add package oliverfarren-python-test-package/0.1.0/oliverfarren_python_test_package-0.1.0-py2.py3-none-any.whl

---
+++ packages/oliverfarren-python-test-package/oliverfarren_python_test_package-0.1.0-py2.py3-none-any.whl/oliverfarren_python_test_package/__init__.py
@@ -0,0 +1,9 @@
+"""
+oliverfarren-python_test_package
+
+A test package for exploring devops
+"""
+
+__version__ = "0.1.0"
+
+from .main import *
---
+++ packages/oliverfarren-python-test-package/oliverfarren_python_test_package-0.1.0-py2.py3-none-any.whl/oliverfarren_python_test_package/main.py
@@ -0,0 +1,3 @@
+def add_2(x):
+    """Adds 2 to input value"""
+    return x+2
