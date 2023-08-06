6729a09dc0c929d4dc096d9e6d693785b6ce531d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:48 +0000

    Add package arcfutil/0.5.1/arcfutil-0.5.1-py3-none-any.whl

---
+++ packages/arcfutil/arcfutil-0.5.1-py3-none-any.whl/arcfutil/aff/note/validstrings.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+# (c)2021 .direwolf <kururinmiracle@outlook.com>
+# Licensed under the MIT License.
+
+slideeasinglist = [
+    'b',
+    's',
+    'si',
+    'so',
+    'sisi',
+    'soso',
+    'sosi',
+    'siso'
+]
+
+fxlist = [
+    'full',
+    'incremental'
+]
+
+cameraeasinglist = [
+    'qi',
+    'qo',
+    'l',
+    'reset',
+    's'
+]
+
+scenetypelist = [
+    'trackshow',
+    'trackhide',
+    'redline',
+    'arcahvdistort',
+    'arcahvdebris',
+    'hidegroup'
+]
