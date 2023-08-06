7d89cc90c2566e241b557266e6cc5ba9a9b22cd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:10 +0000

    Add package well-profile/0.6.1b1/well_profile-0.6.1b1.tar.gz

---
+++ packages/well-profile/well_profile-0.6.1b1.tar.gz/well_profile-0.6.1b1/well_profile/load_trajectory.py
@@ -0,0 +1,283 @@
+from .equations import *
+from numpy import interp, linspace
+import pandas as pd
+from math import degrees
+from .well import Well, define_sections
+
+
+def load(data, **kwargs):
+    """
+    Load an existing wellpath.
+
+    Parameters
+    ----------
+    data:
+        Excel file, dataframe or list of dictionaries.
+        Must contain at least md, inclination and azimuth. Can also contain tvd, northing and easting.
+
+    Keyword Args
+    ------------
+        set_start: dict, None
+            set initial point in m {'north': 0, 'east': 0}.
+        equidistant: bool
+            True to get same md difference between points.
+        points: int, None
+            set number of points if equidistant is True.
+        change_azimuth: float, int, None
+            add specific degrees to azimuth values along the entire well.
+        set_info: dict, None
+            dict, {'dlsResolution', 'wellType': 'onshore'|'offshore', 'units': 'metric'|'english'}.
+        calc_loc: bool
+            calculate north, east and tvd, even if this data is available.
+        ndigits: int
+            number of decimals for TVD, North and East
+
+
+    Returns
+    -------
+    well: well object
+        A wellpath object with 3D position
+    """
+
+    # Settings
+    params = {'set_start': None, 'equidistant': True, 'points': None, 'change_azimuth': None, 'set_info': None,
+              'calc_loc': False, 'ndigits': 2}
+    for key, value in kwargs.items():
+        params[key] = value
+    set_start = params['set_start']
+    equidistant = params['equidistant']
+    points = params['points']
+    change_azimuth = params['change_azimuth']
+    set_info = params['set_info']
+    calc_loc = params['calc_loc']
+    ndigits = params['ndigits']
+
+    info = {'dlsResolution': 30, 'wellType': 'offshore', 'units': 'metric'}
+
+    initial_point = {'north': 0, 'east': 0}
+
+    base_data = False
+    data_initial = None
+
+    processed = False
+
+    if isinstance(set_info, dict):
+        for param in set_info:  # changing default values
+            if param in info:
+                info[param] = set_info[param]
+
+    if isinstance(set_start, dict):
+        for x in set_start:  # changing default values
+            if x in initial_point:
+                initial_point[x] = set_start[x]
+
+    if isinstance(data, pd.DataFrame):
+        base_data = True
+        data_initial = data.copy()
+        data.dropna(axis=1, how='all', inplace=True)
+        data.dropna(inplace=True)
+        data = solve_key_similarities(data)
+        data = data.to_dict('records')
+        processed = True
+
+    if ".xlsx" in data:
+        base_data = True
+        data = pd.read_excel(data)  # open excel file with pandas
+        data_initial = data.copy()
+        data.dropna(axis=1, how='all', inplace=True)
+        data.dropna(inplace=True)
+        data = solve_key_similarities(data)
+        data = data.to_dict('records')
+        processed = True
+
+    if ".csv" in data:
+        base_data = True
+        data = pd.read_csv(data)  # open csv file with pandas
+        data_initial = data.copy()
+        data.dropna(axis=1, how='all', inplace=True)
+        data.dropna(inplace=True)
+        data = solve_key_similarities(data)
+        data = data.to_dict('records')
+        processed = True
+
+    if type(data[0]) is dict:
+        if not processed:
+            data = solve_key_similarities(data)
+        md = [x['md'] for x in data]
+        inc = [x['inc'] for x in data]
+        az = [x['azi'] for x in data]
+    else:       # if data is not a list of dicts, but a list of lists
+        md, inc, az = data[:3]
+
+    if change_azimuth is not None:
+        for a in range(len(az)):
+            az[a] += change_azimuth
+
+    for x, y in enumerate(md):      # change values to numbers if are strings
+        if type(y) == str:
+            md[x] = float(y.split(",", 1)[0])
+            inc[x] = float(inc[x].split(",", 1)[0])
+            az[x] = float(az[x].split(",", 1)[0])
+
+    if equidistant:
+        if points is None:
+            points = len(data)
+        md_new = list(linspace(min(md), max(md), num=points))
+        inc_new = []
+        az_new = []
+        _initial_azi = 0
+        _kickoff = 0
+        _eob = 0
+        for idx, point in enumerate(md):
+            if inc[idx] != 0:
+                _initial_azi = az[idx]
+                _kickoff = md[idx - 1]
+                _eob = point
+                break
+        for i in md_new:
+            inc_new.append(interp(i, md, inc))
+            if _kickoff <= i <= _eob:
+                az_new.append(_initial_azi)
+            else:
+                az_new.append(interp(i, md, az))
+        depth_step = md_new[1] - md_new[0]
+    else:
+        md_new = md
+        inc_new = inc
+        az_new = az
+        points = len(md_new)
+        depth_step = None
+
+    dogleg = [0]
+    for x in range(1, len(md_new)):
+        dogleg.append(calc_dogleg(inc_new[x-1], inc_new[x], az_new[x-1], az_new[x]))
+
+    if 'north' and 'east' in data[0] and not calc_loc:
+        north = [x['north'] for x in data]
+        east = [x['east'] for x in data]
+
+        for x, y in enumerate(north):       # change values to numbers if are strings
+            if type(y) == str:
+                north[x] = float(y.split(",", 1)[0])
+                east[x] = float(east[x].split(",", 1)[0])
+
+        north_new = [data[0]['north']]
+        east_new = [data[0]['east']]
+        for i in md_new[1:]:
+            north_new.append(interp(i, md, north))
+            east_new.append(interp(i, md, east))
+        north = north_new
+        east = east_new
+
+    else:
+        north = [0]
+        east = [0]
+        for x in range(1, len(md_new)):
+            north.append(calc_north(north[-1],
+                                    md_new[x-1], md_new[x],
+                                    inc_new[x-1], inc_new[x],
+                                    az_new[x-1], az_new[x],
+                                    dogleg[x]))
+            east.append(calc_east(east[-1],
+                                  md_new[x - 1], md_new[x],
+                                  inc_new[x - 1], inc_new[x],
+                                  az_new[x - 1], az_new[x],
+                                  dogleg[x]))
+
+    if type(data[0]) is dict and 'tvd' in data[0] and not calc_loc:
+        tvd = [x['tvd'] for x in data]
+        for x, y in enumerate(tvd):     # change values to numbers if are strings
+            if type(y) == str:
+                tvd[x] = float(y.split(",", 1)[0])
+        tvd_new = []
+        for i in md_new:
+            tvd_new.append(interp(i, md, tvd))
+        tvd = tvd_new
+
+    else:
+        tvd = [md_new[0]]
+        for x in range(1, len(md_new)):
+            tvd.append(calc_tvd(tvd[-1],
+                                md_new[x-1],
+                                md_new[x],
+                                inc_new[x-1],
+                                inc_new[x],
+                                dogleg[x]))
+
+    dogleg = [degrees(x) for x in dogleg]
+
+    # Defining type of section
+    sections = define_sections(tvd, inc_new)
+
+    data = {'md': md_new, 'tvd': tvd, 'inclination': inc_new, 'azimuth': az_new, 'dogleg': dogleg,
+            'north': [n + initial_point['north'] for n in north],
+            'east': [e + initial_point['east'] for e in east],
+            'info': info, 'depthStep': depth_step, 'points': points, 'sections': sections}
+
+    well = Well(data, ndigits)
+
+    if base_data:
+        well._base_data = data_initial
+
+    return well
+
+
+def solve_key_similarities(data):
+    md_similarities = ['MD', 'md(ft)', 'md(m)', 'MD(m)', 'MD(ft)',
+                       'measureddepth', 'MeasuredDepth',
+                       'measureddepth(m)', 'MeasuredDepth(m)',
+                       'measureddepth(ft)', 'MeasuredDepth(ft)']
+
+    inc_similarities = ['Inclination', 'inclination', 'Inc', 'Incl', 'incl',
+                        'inclination(°)', 'Inclination(°)', 'Incl(°)',
+                        'incl(°)', 'Inc(°)', 'inc(°)', 'INC', 'INC(°)', 'INCL',
+                        'INCL(°)', 'Inc(deg)', 'inc(deg)']
+
+    azi_similarities = ['az', 'az(°)',
+                        'Az', 'Az(°)',
+                        'AZ', 'AZ(°)',
+                        'Azi', 'Azi(°)',
+                        'azi(°)',
+                        'AZI', 'AZI(°)',
+                        'Azimuth', 'Azimuth(°)',
+                        'azimuth', 'azimuth(°)',
+                        'Azi(deg)', 'azi(deg)']
+
+    tvd_similarities = ['TVD', 'TVD (m)', 'TVD (ft)', 'TVD(m)', 'TVD(ft)',
+                        'tvd (m)', 'tvd (ft)', 'tvd(m)', 'tvd(ft)']
+
+    north_similarities = ['NORTH', 'NORTH(m)', 'NORTH(ft)',
+                          'North', 'North(m)', 'North(ft)',
+                          'Northing(m)', 'Northing(ft)'
+                          'N/S(m)', 'N/S(ft)',
+                          'Ns(m)', 'Ns(ft)']
+
+    east_similarities = ['EAST', 'EAST(m)', 'EAST(ft)',
+                         'East', 'East(m)', 'East(ft)',
+                         'Easting(m)', 'Easting(ft)'
+                         'E/W(m)', 'E/W(ft)',
+                         'Ew(m)', 'Ew(ft)']
+
+    possible_keys = [md_similarities,
+                     tvd_similarities,
+                     inc_similarities,
+                     azi_similarities,
+                     north_similarities,
+                     east_similarities]
+
+    correct_keys = ['md', 'tvd', 'inc', 'azi', 'north', 'east']
+
+    true_key = 0
+    for i in possible_keys:
+        for x in i:
+            if isinstance(data, pd.DataFrame):
+                data.columns = data.columns.str.replace(' ', '')
+                if x in data.columns:
+                    data.rename(columns={x: correct_keys[true_key]}, inplace=True)
+            else:
+                if x.replace(' ', '') in data[0]:
+                    for point in data:
+                        point[correct_keys[true_key]] = point[x]
+        true_key += 1
+
+    return data
---
+++ packages/well-profile/well_profile-0.6.1b1.tar.gz/well_profile-0.6.1b1/well_profile/well.py
@@ -0,0 +1,102 @@
+from .equations import *
+from .plot import plot_wellpath, plot_top_view, plot_vs
+import pandas as pd
+
+
+class Well(object):
+    def __init__(self, data, ndigits=2):
+        self.depth_step = data['depthStep']
+        self.points = data['points']
+        self.info = data['info']
+        data['dls'] = calc_dls(data['dogleg'], data['md'], resolution=self.info['dlsResolution'])
+        data['delta'] = get_delta(data)
+        self.trajectory = []
+        for point in range(len(data['md'])):
+            self.trajectory.append({'md': data['md'][point],
+                                    'tvd': round(data['tvd'][point], ndigits),
+                                    'inc': data['inclination'][point],
+                                    'azi': data['azimuth'][point],
+                                    'dl': data['dogleg'][point],
+                                    'north': round(data['north'][point], ndigits),
+                                    'east': round(data['east'][point], ndigits),
+                                    'dls': data['dls'][point],
+                                    'sectionType': data['sections'][point],
+                                    'delta': data['delta'][point]})
+
+    def plot(self, **kwargs):
+        default = {'plot_type': '3d', 'add_well': None, 'names': None, 'style': None, 'y_axis': 'md', 'x_axis': 'inc'}
+        for key, value in kwargs.items():
+            default[key] = value
+
+        if default['plot_type'] == '3d':
+            fig = plot_wellpath(self, add_well=default['add_well'], names=default['names'], style=default['style'])
+            return fig
+        elif default['plot_type'] == 'top':
+            fig = plot_top_view(self, add_well=default['add_well'], names=default['names'], style=default['style'])
+            return fig
+        elif default['plot_type'] == 'vs':
+            fig = plot_vs(self, y_axis=default['y_axis'], x_axis=default['x_axis'], add_well=default['add_well'],
+                          names=default['names'], style=default['style'])
+            return fig
+        else:
+            raise ValueError('The plot type "{}" is not recognised'.format(default['plot_type']))
+
+    def df(self):
+        dataframe = pd.DataFrame(self.trajectory)
+        return dataframe
+
+    def add_location(self, lat, lon):
+        """
+        set specific location lat and lon in decimal degrees
+        """
+        self.info['location'] = {'lat': lat, 'lon': lon}
+
+    def add_reference(self, references):
+        self.info['rkb'] = references['rkb']
+        if 'waterDepth' in references:
+            self.info.update({'waterDepth': references['waterDepth'],
+                              'seabed': references['rkb'] + references['waterDepth']})
+
+
+def define_sections(tvd, inc):
+    sections = ['vertical', 'vertical']
+    for z in range(2, len(tvd)):
+        delta_tvd = round(tvd[z] - tvd[z - 1], 9)
+        if inc[z] == 0:  # Vertical Section
+            sections.append('vertical')
+        else:
+            if round(inc[z], 2) == round(inc[z - 1], 2):
+                if delta_tvd == 0:
+                    sections.append('horizontal')  # Horizontal Section
+                else:
+                    sections.append('hold')  # Straight Inclined Section
+            else:
+                if inc[z] > inc[z - 1]:  # Built-up Section
+                    sections.append('build-up')
+                if inc[z] < inc[z - 1]:  # Drop-off Section
+                    sections.append('drop-off')
+
+    return sections
+
+
+def get_delta(trajectory):
+    delta = [{'md': 0,
+              'tvd': 0,
+              'inc': 0,
+              'azi': 0,
+              'dl': 0,
+              'dls': 0,
+              'north': 0,
+              'east': 0}]
+
+    for idx in range(1, len(trajectory['md'])):
+        delta.append({'md': trajectory['md'][idx] - trajectory['md'][idx-1],
+                      'tvd': trajectory['tvd'][idx] - trajectory['tvd'][idx-1],
+                      'inc': trajectory['inclination'][idx] - trajectory['inclination'][idx-1],
+                      'azi': trajectory['azimuth'][idx] - trajectory['azimuth'][idx-1],
+                      'dl': trajectory['dogleg'][idx] - trajectory['dogleg'][idx-1],
+                      'dls': trajectory['dls'][idx] - trajectory['dls'][idx-1],
+                      'north': trajectory['north'][idx] - trajectory['north'][idx-1],
+                      'east': trajectory['east'][idx] - trajectory['east'][idx-1]})
+
+    return delta
