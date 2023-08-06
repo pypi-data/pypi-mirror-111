8f33cbd015a1ec2be201121c3e4f8e3e788478fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:48 +0000

    Add package pgn/0.1.0/pgn-0.1.0.tar.gz

---
+++ packages/pgn/pgn-0.1.0.tar.gz/pgn-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='pgn',
+    version='0.1.0',
+    description='',
+    url='http://github.com/qe/pgn',
+    author='Alex Ismodes',
+    author_email='helloemailmerighthere@gmail.com',
+    license='MIT',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        'Topic :: Games/Entertainment :: Board Games'
+    ],
+    keywords='chess, pgn',
+    packages=['pgn'],
+    install_requires=[''],
+    )
