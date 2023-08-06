d4a27a08d98cdf07c34a19a6078e649abf5989e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:39 +0000

    Add package optiontools/0.1.1/optiontools-0.1.1.tar.gz

---
+++ packages/optiontools/optiontools-0.1.1.tar.gz/optiontools-0.1.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='optiontools',
+    packages=find_packages(include=['optiontools']),
+    version='0.1.1',
+    description='Tools for valuating financial derivatives',
+    author='Luis Felipe Gomez Estrada',
+	author_email='luisfelipegomezestrada@gmail.com',
+    license='MIT',
+    url='https://github.com/lf-gomez/optiontools',
+    download_url='https://github.com/lf-gomez/optiontools/archive/refs/tags/v0.1.1-alpha.tar.gz',
+    keywords=['FINANCE', 'OPTIONS', 'DERIVATIVES'],
+    install_requires=['numpy', 'scipy'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest==4.4.1'],
+    test_suite='tests',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Financial and Insurance Industry',
+        'Topic :: Office/Business :: Financial :: Investment',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only'
+    ],
+)
