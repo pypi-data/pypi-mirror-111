a24ba9b45c8b906dca37a2ec2cca22608593eef9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:03 +0000

    Add package magicwrap/0.0.0/magicwrap-0.0.0.tar.gz

---
+++ packages/magicwrap/magicwrap-0.0.0.tar.gz/magicwrap-0.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='magicwrap',
+    packages=["magicwrap"],
+    version='0.0.0',
+    author='Perzan',
+    author_email='PerzanDevelopment@gmail.com'
+)
