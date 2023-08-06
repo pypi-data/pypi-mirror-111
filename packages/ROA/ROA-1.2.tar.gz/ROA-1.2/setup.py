7db111d427dfc534dc8decb1e92ef1d9f36e6cb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:36 +0000

    Add package ROA/1.2/ROA-1.2.tar.gz

---
+++ packages/ROA/ROA-1.2.tar.gz/ROA-1.2/setup.py
@@ -0,0 +1,29 @@
+#from distutils.core import setup
+from setuptools import find_packages, setup
+from Cython.Build import cythonize
+import numpy
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setup(
+    name="ROA",
+    version="1.2",
+    packages=find_packages(),
+    author="Fergus Donnan",
+    description="A simple function to compute the running optimal average and effective no. of parameters.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="https://github.com/FergusDonnan/Running-Optimal-Average",
+
+    ext_modules = cythonize("ROA/ROA.pyx", annotate=True,
+        compiler_directives={'wraparound': False,
+                            'nonecheck': False,
+                            'cdivision': True,
+                            'boundscheck':False
+                            }),
+    include_dirs=[numpy.get_include()],
+    install_requires=[
+        'numpy>=1.19.2']
+)
+
