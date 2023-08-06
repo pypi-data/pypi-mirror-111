b743676e8edf019b78a2eb718a4fe4a315242a22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:17:23 +0000

    Add package muffin-rest/2.8.0/muffin_rest-2.8.0-py3-none-any.whl

---
+++ packages/muffin-rest/muffin_rest-2.8.0-py3-none-any.whl/muffin_rest/__init__.py
@@ -0,0 +1,38 @@
+"""REST helpers for Muffin Framework."""
+
+__version__ = "2.8.0"
+__project__ = "muffin-rest"
+__author__ = "Kirill Klenov <horneds@gmail.com>"
+__license__ = "MIT"
+
+# Default query params
+FILTERS_PARAM = 'where'
+LIMIT_PARAM = 'limit'
+OFFSET_PARAM = 'offset'
+SORT_PARAM = 'sort'
+
+
+from .api import API                # noqa
+from .handler import RESTHandler    # noqa
+from .errors import APIError        # noqa
+
+# Just an alias to support legacy style
+Api = API
+
+
+try:
+    from .peewee import PWRESTHandler, PWFilter
+except ImportError:
+    pass
+
+
+try:
+    from .sqlalchemy import SARESTHandler, SAFilter
+except ImportError:
+    pass
+
+
+try:
+    from .mongo import MongoRESTHandler, MongoFilter
+except ImportError:
+    pass
